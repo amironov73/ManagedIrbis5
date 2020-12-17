8c6b3c7d935e7937c8de75a8429711589eda60f4
Alexey Mironov <amironov73@gmail.com>
2020-12-17 10:57:54 +0800

    Working on GBL

---
+++ Source/Libs/ManagedIrbis5/Source/Gbl/GblParameter.cs
@@ -0,0 +1,151 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable StringLiteralTypo
+// ReSharper disable UnusedParameter.Local
+
+/* GblParameter.cs -- параметр для глобальной корректировки
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System.Diagnostics;
+using System.IO;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+using AM;
+using AM.IO;
+using AM.Runtime;
+
+#endregion
+
+#nullable enable
+
+namespace ManagedIrbis.Gbl
+{
+    //
+    // Первая строка файла задания – это число, задающее
+    // количество параметров, используемых в операторах корректировки.
+    //
+    // Последующие пары строк, число пар должно быть равно
+    // количеству параметров, используются программой
+    // глобальной корректировки.
+    //
+    // Первая строка пары - значение параметра или пусто,
+    // если пользователю предлагается задать его значение
+    // перед выполнением корректировки. В этой строке можно
+    // задать имя файла меню (с расширением MNU)
+    // или имя рабочего листа подполей (с расширением Wss),
+    // которые будут поданы для выбора значения параметра.
+    // Вторая строка пары – наименование параметра,
+    // которое появится в названии столбца, задающего параметр.
+    //
+
+    /// <summary>
+    /// Параметр для глобальной корректировки.
+    /// </summary>
+    [DebuggerDisplay("{Name}: {Value}")]
+    public sealed class GblParameter
+        : IHandmadeSerializable,
+            IVerifiable
+    {
+        #region Properties
+
+        /// <summary>
+        /// Parameter name.
+        /// </summary>
+        [JsonPropertyName("name")]
+        [XmlAttribute("name")]
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Parameter value.
+        /// </summary>
+        [JsonPropertyName("value")]
+        [XmlAttribute("value")]
+        public string? Value { get; set; }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Декодирование текстового представления параметра.
+        /// </summary>
+        public static GblParameter Decode
+            (
+                TextReader reader
+            )
+        {
+            var result = new GblParameter
+            {
+                Value = reader.RequireLine(),
+                Name = reader.RequireLine()
+            };
+
+            return result;
+        } // method Decode
+
+        #endregion
+
+        #region IHandmadeSerializable members
+
+        /// <inheritdoc cref="IHandmadeSerializable.RestoreFromStream" />
+        public void RestoreFromStream
+            (
+                BinaryReader reader
+            )
+        {
+            Name = reader.ReadNullableString();
+            Value = reader.ReadNullableString();
+        }
+
+        /// <inheritdoc cref="IHandmadeSerializable.SaveToStream" />
+        public void SaveToStream
+            (
+                BinaryWriter writer
+            )
+        {
+            writer.WriteNullable(Name);
+            writer.WriteNullable(Value);
+        }
+
+        #endregion
+
+        #region IVerifiable members
+
+        /// <inheritdoc cref="IVerifiable.Verify" />
+        public bool Verify
+            (
+                bool throwOnError
+            )
+        {
+            var verifier = new Verifier<GblParameter>(this, throwOnError);
+
+            verifier
+                .NotNullNorEmpty(Name, nameof(Name))
+                .NotNullNorEmpty(Value, nameof(Value));
+
+            return verifier.Result;
+        }
+
+        #endregion
+
+        #region Object members
+
+        /// <inheritdoc cref="object.ToString" />
+        public override string ToString()
+        {
+            return $"Name: {Name}, Value: {Value}";
+        }
+
+        #endregion
+    }
+}
---
+++ Source/Libs/ManagedIrbis5/Source/Gbl/GblResult.cs
@@ -0,0 +1,161 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable StringLiteralTypo
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable UnusedParameter.Local
+
+/* GblResult.cs -- результат исполнения глобальной корректировки
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
+using System.Linq;
+
+using AM;
+
+using ManagedIrbis.Infrastructure;
+
+#endregion
+
+#nullable enable
+
+namespace ManagedIrbis.Gbl
+{
+    /// <summary>
+    /// Результат исполнения глобальной корректировки.
+    /// </summary>
+    public sealed class GblResult
+    {
+        #region Properties
+
+        /// <summary>
+        /// Момент начала обработки.
+        /// </summary>
+        public DateTime TimeStarted { get; set; }
+
+        /// <summary>
+        /// Всего времени затрачено (с момента начала обработки).
+        /// </summary>
+        public TimeSpan TimeElapsed { get; set; }
+
+        /// <summary>
+        /// Отменено пользователем.
+        /// </summary>
+        public bool Canceled { get; set; }
+
+        /// <summary>
+        /// Исключение (если возникло).
+        /// </summary>
+        public Exception? Exception { get; set; }
+
+        /// <summary>
+        /// Предполагалось обработать записей.
+        /// </summary>
+        public int RecordsSupposed { get; set; }
+
+        /// <summary>
+        /// Обработано записей.
+        /// </summary>
+        public int RecordsProcessed { get; set; }
+
+        /// <summary>
+        /// Успешно обработано записей.
+        /// </summary>
+        public int RecordsSucceeded { get; set; }
+
+        /// <summary>
+        /// Ошибок при обработке записей.
+        /// </summary>
+        public int RecordsFailed { get; set; }
+
+        /// <summary>
+        /// Результаты для каждой записи.
+        /// </summary>
+        public GblProtocolLine[]? Protocol { get; set; }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Get empty result.
+        /// </summary>
+        public static GblResult GetEmptyResult()
+        {
+            var result = new GblResult
+            {
+                TimeStarted = DateTime.Now,
+                TimeElapsed = new TimeSpan(0)
+            };
+
+            return result;
+        } // method GetEmptyResult
+
+        /// <summary>
+        /// Merge result.
+        /// </summary>
+        public void MergeResult
+            (
+                GblResult intermediateResult
+            )
+        {
+            if (intermediateResult.Canceled)
+            {
+                Canceled = intermediateResult.Canceled;
+            }
+
+            if (!ReferenceEquals(intermediateResult.Exception, null))
+            {
+                Exception = intermediateResult.Exception;
+            }
+
+            RecordsProcessed += intermediateResult.RecordsProcessed;
+            RecordsFailed += intermediateResult.RecordsFailed;
+            RecordsSucceeded += intermediateResult.RecordsSucceeded;
+            Protocol ??= Array.Empty<GblProtocolLine>();
+            var otherLines
+                = intermediateResult.Protocol ?? Array.Empty<GblProtocolLine>();
+            Protocol = ArrayUtility.Merge
+                (
+                    Protocol,
+                    otherLines
+                );
+        } // method MergeResult
+
+        /// <summary>
+        /// Parse server response.
+        /// </summary>
+        public void Parse
+            (
+                Response response
+            )
+        {
+            Protocol = GblProtocolLine.Decode(response);
+            RecordsProcessed = Protocol.Length;
+            RecordsSucceeded = Protocol.Count(line => line.Success);
+        }
+
+        #endregion
+
+        #region Object members
+
+        /// <inheritdoc cref="object.ToString" />
+        public override string ToString()
+        {
+            return $"Records processed: {RecordsProcessed}, Canceled: {Canceled}";
+        } // method ToString
+
+        #endregion
+
+    } // class GblResult
+
+} // namespace ManagedIrbis.Gbl
