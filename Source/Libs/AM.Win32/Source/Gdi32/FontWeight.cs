fe69b4d7648e77ddaac7822698eac50a80bb5c81
Alexey Mironov <amironov73@gmail.com>
2021-01-03 13:26:50 +0800

    Working on Win32 interoperability

---
+++ Source/Libs/AM.Win32/Source/Gdi32/FontWeight.cs
@@ -0,0 +1,111 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable CommentTypo
+// ReSharper disable FieldCanBeMadeReadOnly.Global
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable StringLiteralTypo
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedType.Global
+
+/* FontWeight.cs -- weight of the font
+   Ars Magna project, http://arsmagna.ru */
+
+#region Using directives
+
+using System;
+
+#endregion
+
+namespace AM.Win32
+{
+    /// <summary>
+    /// The weight of the font in the range 0 through 1000.
+    /// For example, 400 is normal and 700 is bold.
+    /// If this value is zero, a default weight is used.
+    /// </summary>
+    [Flags]
+    public enum FontWeight
+    {
+        /// <summary>
+        /// Default weight is used.
+        /// </summary>
+        FW_DONTCARE = 0,
+
+        /// <summary>
+        /// Thin.
+        /// </summary>
+        FW_THIN = 100,
+
+        /// <summary>
+        /// Extra-light.
+        /// </summary>
+        FW_EXTRALIGHT = 200,
+
+        /// <summary>
+        /// Light.
+        /// </summary>
+        FW_LIGHT = 300,
+
+        /// <summary>
+        /// Normal.
+        /// </summary>
+        FW_NORMAL = 400,
+
+        /// <summary>
+        /// Medium.
+        /// </summary>
+        FW_MEDIUM = 500,
+
+        /// <summary>
+        /// Semi-bold.
+        /// </summary>
+        FW_SEMIBOLD = 600,
+
+        /// <summary>
+        /// Bold.
+        /// </summary>
+        FW_BOLD = 700,
+
+        /// <summary>
+        /// Extra-bold.
+        /// </summary>
+        FW_EXTRABOLD = 800,
+
+        /// <summary>
+        /// Heavy.
+        /// </summary>
+        FW_HEAVY = 900,
+
+        /// <summary>
+        /// Ultra-light.
+        /// </summary>
+        FW_ULTRALIGHT = FW_EXTRALIGHT,
+
+        /// <summary>
+        /// Regular.
+        /// </summary>
+        FW_REGULAR = FW_NORMAL,
+
+        /// <summary>
+        /// Demi-bold.
+        /// </summary>
+        FW_DEMIBOLD = FW_SEMIBOLD,
+
+        /// <summary>
+        /// Ultra-bold.
+        /// </summary>
+        FW_ULTRABOLD = FW_EXTRABOLD,
+
+        /// <summary>
+        /// Black.
+        /// </summary>
+        FW_BLACK = FW_HEAVY
+
+    } // enum FontWeight
+
+} // namespace AM.Win32
---
+++ Source/Libs/AM.Win32/Source/Gdi32/HatchStyle.cs
@@ -0,0 +1,57 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable CommentTypo
+// ReSharper disable FieldCanBeMadeReadOnly.Global
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable StringLiteralTypo
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedType.Global
+
+/* HatchStyle.cs -- specifies the orientation of the lines used to create the hatch
+   Ars Magna project, http://arsmagna.ru */
+
+namespace AM.Win32
+{
+    /// <summary>
+    /// Specifies the orientation of the lines used to create the hatch.
+    /// </summary>
+    public enum HatchStyle
+    {
+        /// <summary>
+        /// Horizontal hatch. -----
+        /// </summary>
+        HS_HORIZONTAL = 0,
+
+        /// <summary>
+        /// Vertical hatch. |||||
+        /// </summary>
+        HS_VERTICAL = 1,
+
+        /// <summary>
+        /// A 45-degree downward, left-to-right hatch. \\\\\
+        /// </summary>
+        HS_FDIAGONAL = 2,
+
+        /// <summary>
+        /// A 45-degree upward, left-to-right hatch. /////
+        /// </summary>
+        HS_BDIAGONAL = 3,
+
+        /// <summary>
+        /// Horizontal and vertical cross-hatch. +++++
+        /// </summary>
+        HS_CROSS = 4,
+
+        /// <summary>
+        /// 45-degree crosshatch. xxxxx
+        /// </summary>
+        HS_DIAGCROSS = 5
+
+    } // enum HatchStyle
+
+} // namespace AM.Win32
