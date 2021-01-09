45a5964d1c826cfa2569e385b59982318bcf533b
Alexey Mironov <amironov73@gmail.com>
2021-01-09 14:09:39 +0800

    Add MemoryArena and UnmanagedMemoryManager classes

---
+++ Source/Libs/AM.Core5/Source/Memory/MemoryArena.cs
@@ -0,0 +1,135 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable StringLiteralTypo
+// ReSharper disable UnusedParameter.Local
+
+/* MemoryArena.cs -- простой распределитель памяти
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#endregion
+
+#nullable enable
+
+namespace AM.Memory
+{
+    /// <summary>
+    /// Простой распределитель памяти для типов, не требующих очистки.
+    /// </summary>
+    public sealed class MemoryArena
+    {
+        #region Construction
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="size">Общий размер арены в байтах.</param>
+        /// <param name="alignment">Выравнивание. По умолчанию 8 байт.</param>
+        public MemoryArena
+            (
+                int size = 128 * 1024,
+                int alignment = 8
+            )
+        {
+            _memory = new Memory<byte>(new byte[size]);
+            _alingnemnt = alignment - 1;
+            _offset = 0;
+        } // constructor
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="memory">Непрерывный блок памяти любого происхождения
+        /// (в т. ч. unmanaged).</param>
+        /// <param name="alingnemnt">Выравнивание. По умолчанию 8 байт.</param>
+        /// <remarks>
+        /// Конструктор сам устанавливает правильное выравнивание,
+        /// так что можно передавать в него невыравненную память.
+        /// </remarks>
+        public unsafe MemoryArena
+            (
+                Memory<byte> memory,
+                int alingnemnt = 8
+            )
+        {
+            _alingnemnt = alingnemnt - 1;
+            _memory = memory;
+
+            fixed (byte* ptr = memory.Span)
+            {
+                var border = new IntPtr(ptr).ToInt64();
+                var begin = unchecked((border + _alingnemnt) & ~_alingnemnt);
+                _offset = unchecked((int)(begin - border));
+            }
+        } // constructor
+
+        #endregion
+
+        #region Private members
+
+        private readonly Memory<byte> _memory;
+        private readonly int _alingnemnt;
+        private int _offset;
+
+        #endregion
+
+        #region Public methods
+
+        public void Reset()
+        {
+            _offset = 0;
+        } // method Reset
+
+        /// <summary>
+        /// Выделение куска памяти.
+        /// </summary>
+        /// <param name="size">Размер в байтах.</param>
+        /// <returns>Выделенная память.</returns>
+        /// <exception cref="ApplicationException">При нехватке места на арене.
+        /// </exception>
+        public Memory<byte> Allocate
+            (
+                int size
+            )
+        {
+            if (_offset + size > _memory.Length)
+            {
+                throw new ApplicationException();
+            }
+
+            var result = _memory.Slice(_offset, size);
+            _offset = (_offset + size + _alingnemnt) & ~_alingnemnt;
+
+            return result;
+        } // method Allocate
+
+        /// <summary>
+        /// Создание структуры.
+        /// </summary>
+        /// <typeparam name="T">Тип структуры.</typeparam>
+        /// <returns>Ссылка на созданную структуру.</returns>
+        public ref T Allocate<T>()
+            where T: struct
+        {
+            var span = Allocate(Marshal.SizeOf<T>()).Span;
+
+            return ref MemoryMarshal.AsRef<T>(span);
+        } // method Allocate<T>
+
+        #endregion
+
+    } // class MemoryArena
+
+} // namespace AM.Memory
---
+++ Source/Libs/AM.Core5/Source/Memory/UnmanagedMemoryManager.cs
@@ -0,0 +1,166 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable StringLiteralTypo
+// ReSharper disable UnusedParameter.Local
+
+/* UnmanagedMemoryManager.cs -- простой менеджер для неуправляемой памяти
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
+using System.Buffers;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#endregion
+
+#nullable enable
+
+namespace AM.Memory
+{
+    /// <summary>
+    /// Простой менеджер для неуправляемой памяти.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public sealed unsafe class UnmanagedMemoryManager<T>
+        : MemoryManager<T>
+        where T: unmanaged
+    {
+        #region Private members
+
+        private readonly T* _pointer;
+        private readonly int _length;
+        private readonly IntPtr _handle;
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Create a new UnmanagedMemoryManager instance at the given
+        /// pointer and size.
+        /// </summary>
+        ///
+        /// <remarks>It is assumed that the span provided is already
+        /// unmanaged or externally pinned.</remarks>
+        public UnmanagedMemoryManager
+            (
+                Span<T> span
+            )
+        {
+            fixed (T* ptr = &MemoryMarshal.GetReference(span))
+            {
+                _pointer = ptr;
+                _length = span.Length;
+            }
+
+            _handle = IntPtr.Zero;
+        }
+        /// <summary>
+        /// Create a new UnmanagedMemoryManager instance
+        /// at the given pointer and size.
+        /// </summary>
+        public UnmanagedMemoryManager
+            (
+                T* pointer,
+                int length
+            )
+        {
+            if (length < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(length));
+            }
+
+            _pointer = pointer;
+            _length = length;
+            _handle = IntPtr.Zero;
+        }
+
+        /// <summary>
+        /// Создает менеджер для указанного дескриптора неуправвляемой
+        /// памяти, полученного от <c>Marshal.AllocHGlobal</c>.
+        /// </summary>
+        /// <param name="handle"></param>
+        /// <param name="length"></param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public UnmanagedMemoryManager
+            (
+                IntPtr handle,
+                int length
+            )
+        {
+            if (length < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(length));
+            }
+
+            _handle = handle;
+            _pointer = (T*) _handle.ToPointer();
+            _length = length;
+            Unsafe.InitBlock(_pointer, 0, unchecked((uint)_length));
+        }
+
+        /// <summary>
+        /// Создает менеджер для неуправляемой памяти, полученной
+        /// от <c>Marshal.AllocHGlobal.</c>
+        /// </summary>
+        /// <param name="length"></param>
+        public UnmanagedMemoryManager
+            (
+                int length
+            )
+            : this(Marshal.AllocHGlobal(length), length)
+        {
+        }
+
+        #endregion
+
+        #region MemoryManager<T> members
+
+        /// <summary>
+        /// Obtains a span that represents the region
+        /// </summary>
+        public override Span<T> GetSpan() => new (_pointer, _length);
+
+        /// <summary>
+        /// Provides access to a pointer that represents the data (note: no actual pin occurs)
+        /// </summary>
+        public override MemoryHandle Pin
+            (
+                int elementIndex = 0
+            )
+        {
+            if (elementIndex < 0 || elementIndex >= _length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(elementIndex));
+            }
+
+            return new MemoryHandle(_pointer + elementIndex);
+        }
+        /// <summary>
+        /// Has no effect
+        /// </summary>
+        public override void Unpin() { }
+
+        /// <summary>
+        /// Releases all resources associated with this object
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (_handle != IntPtr.Zero)
+            {
+                Marshal.FreeHGlobal(_handle);
+            }
+        }
+
+        #endregion
+    }
+}
