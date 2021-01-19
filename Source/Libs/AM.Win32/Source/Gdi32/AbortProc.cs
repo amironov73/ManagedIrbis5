8b53fc3b04e6d924adf3eab0f3cd8349f31463c6
Alexey Mironov <amironov73@gmail.com>
2021-01-19 09:47:10 +0800

    Add BitmapUtility class

--- Source/Libs/AM.Win32/Source/Gdi32/AbortProc.cs
+++ Source/Libs/AM.Win32/Source/Gdi32/AbortProc.cs
@@ -21,17 +21,17 @@ using System;
 
 namespace AM.Win32
 {
-	/// <summary>
-	/// The AbortProc function is an application-defined callback function
-	/// used with the SetAbortProc function. It is called when a print job
-	/// is to be canceled during spooling. The ABORTPROC type defines a pointer
-	/// to this callback function. AbortProc is a placeholder for the
-	/// application-defined function name.
-	/// </summary>
-	public delegate bool AbortProc
-		(
-			IntPtr hdc,
-			int iError
-		);
+    /// <summary>
+    /// The AbortProc function is an application-defined callback function
+    /// used with the SetAbortProc function. It is called when a print job
+    /// is to be canceled during spooling. The ABORTPROC type defines a pointer
+    /// to this callback function. AbortProc is a placeholder for the
+    /// application-defined function name.
+    /// </summary>
+    public delegate bool AbortProc
+        (
+            IntPtr hdc,
+            int iError
+        );
 
 } // namespace AM.Win32
---
+++ Source/Libs/AM.Win32/Source/Gdi32/BitmapUtility.cs
@@ -0,0 +1,68 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedType.Global
+
+/* BitmapUtility.cs -- utility routines for GDI bitmap
+   Ars Magna project, http://arsmagna.ru */
+
+#region Using directives
+
+using System;
+using System.Runtime.InteropServices;
+
+#endregion
+
+namespace AM.Win32
+{
+    /// <summary>
+    /// Utility routines for GDI bitmap.
+    /// </summary>
+    public static class BitmapUtility
+    {
+        #region Public methods
+
+        /// <summary>
+        /// Gets the pixel data.
+        /// </summary>
+        public static IntPtr GetPixelData
+            (
+                IntPtr dibptr
+            )
+        {
+            var bmi = (BITMAPINFOHEADER) Marshal.PtrToStructure
+                (
+                    dibptr,
+                    typeof(BITMAPINFOHEADER)
+                );
+            unchecked
+            {
+                if ( bmi.biSizeImage == 0 )
+                {
+                    bmi.biSizeImage =
+                        (uint) ((((bmi.biWidth * bmi.biBitCount + 31 ) & ~31 ) >> 3)
+                                 * bmi.biHeight );
+                }
+
+                var result = (int) bmi.biClrUsed;
+                if (result == 0
+                     && bmi.biBitCount <= 8)
+                {
+                    result = 1 << bmi.biBitCount;
+                }
+
+                result = (int) (result * 4 + bmi.biSize + dibptr.ToInt32 ());
+                return new IntPtr ( result );
+            }
+        } // method GetPixelData
+
+        #endregion
+
+    } // class BitmapUtility
+
+} // namespace AM.Win32
