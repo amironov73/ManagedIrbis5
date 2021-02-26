fa6cc0bd6d094e247c74d1fe1273b1199f08d3ff
Alexey Mironov <amironov73@gmail.com>
2021-02-26 15:27:15 +0800

    Start documentation

---
+++ Docs/index.rst
@@ -0,0 +1,25 @@
+.. ManagedIrbis5 documentation master file, created by
+   sphinx-quickstart on Fri Feb 26 13:24:47 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Фреймворк ManagedIrbis5
+=======================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Содержание:
+
+   chapter1
+   chapter2
+   chapter3
+   chapter4
+   chapter5
+   chapter6
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ Source/Examples/Example1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using ManagedIrbis;
+
+using ManagedIrbis.Infrastructure;
+
+using static System.Console;
+
+#nullable enable
+
+class Program
+{
+    static async Task<int> Main(string[] args)
+    {
+        try
+        {
+            await using var connection = ConnectionFactory.Default
+                .CreateConnection();
+
+            connection.Host = args.Length == 0
+                ? "127.0.0.1"
+                : args[0];
+            connection.Username = "librarian";
+            connection.Password = "secret";
+
+            var success = await connection.ConnectAsync();
+            if (!success)
+            {
+                await Error.WriteLineAsync("Can't connect");
+                return 1;
+            }
+
+            WriteLine("Successfully connected");
+
+            // Ищем все книги, автором которых является А. С. Пушкин
+            // Обратите внимание на двойные кавычки в тексте запроса
+            var found = await connection.SearchAsync
+                (
+                    "\"A=ПУШКИН$\""
+                );
+
+            WriteLine($"Найдено записей: {found.Length}");
+
+            // Чтобы не распечатывать все найденные записи,
+            // отберем только 10 первых
+            foreach (var mfn in found[..10])
+            {
+                // Получаем запись из базы данных
+                var record = await connection.ReadRecordAsync(mfn);
+
+                // Извлекаем из записи интересующее нас поле и подполе
+                var title = record.FM(200, 'a');
+                WriteLine($"Title: {title}");
+
+                // Форматируем запись средствами сервера
+                var description = await connection.FormatRecordAsync
+                    (
+                        "@brief",
+                        mfn
+                    );
+                WriteLine($"Биб. описание: {description}");
+
+                WriteLine(); // Добавляем пустую строку
+            }
+
+            // Отключаемся от сервера
+            await connection.DisconnectAsync();
+            WriteLine("Successfully disconnected");
+        }
+        catch (Exception exception)
+        {
+            WriteLine(exception);
+            return 1;
+        }
+
+        return 0;
+    }
+}
