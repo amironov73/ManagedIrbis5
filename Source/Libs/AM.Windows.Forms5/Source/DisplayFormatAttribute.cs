d83cae084c72f3995bfb49d4e32b5f777c0b17a8
Alexey Mironov <amironov73@gmail.com>
2021-03-11 11:31:28 +0800

    Working on WinForms components

---
+++ Source/Libs/AM.Windows.Forms5/Source/DisplayFormatAttribute.cs
@@ -0,0 +1,58 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable UnusedType.Global
+
+/* DisplayFormatAttribute.cs -- задает формат отображения значений
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
+
+#endregion
+
+#nullable enable
+
+namespace AM.Windows.Forms
+{
+    /// <summary>
+    /// Задает формат отображения значений.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class DisplayFormatAttribute
+        : Attribute
+    {
+        #region Properties
+
+        /// <summary>
+        /// Формат отображения значений.
+        /// </summary>
+        public string DisplayFormat { get; }
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        public DisplayFormatAttribute
+            (
+                string displayFormat
+            )
+        {
+            DisplayFormat = displayFormat;
+        } // constructor
+
+        #endregion
+
+    } // class DisplayFormatAttribute
+
+} // namespace AM.Windows.Forms
