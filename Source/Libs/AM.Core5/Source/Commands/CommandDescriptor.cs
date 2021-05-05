192a696989e239b2e3595e345f9a09b048a99ccd
Alexey Mironov <amironov73@gmail.com>
2021-05-05 13:31:27 +0800

    Add CommandDescriptor class

---
+++ Source/Libs/AM.Core5/Source/Commands/CommandDescriptor.cs
@@ -0,0 +1,93 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable CommentTypo
+// ReSharper disable EventNeverSubscribedTo.Global
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable StringLiteralTypo
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedParameter.Local
+// ReSharper disable UnusedType.Global
+
+/* CommandDescriptor.cs -- описатель команды
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+#endregion
+
+#nullable enable
+
+namespace AM.Commands
+{
+    /// <summary>
+    /// Описатель команды (для сохранения/восстановления из файла).
+    /// </summary>
+    [XmlRoot("command")]
+    public sealed class CommandDescriptor
+    {
+        #region Properties
+
+        /// <summary>
+        /// Вызывается при выполнении команды.
+        /// </summary>
+        [JsonPropertyName("execute")]
+        [XmlElement("execute")]
+        public DelegateDescriptor? Execute;
+
+        /// <summary>
+        /// Вызывается, когда команда должна обновить свое состояние.
+        /// </summary>
+        [JsonPropertyName("update")]
+        [XmlElement("update")]
+        public DelegateDescriptor? Update;
+
+        /// <summary>
+        /// Вызывается, когда в команде что-то поменялось
+        /// (например, состояние <see cref="Enabled"/>).
+        /// </summary>
+        [JsonPropertyName("changed")]
+        [XmlElement("changed")]
+        public DelegateDescriptor? Changed;
+
+        /// <summary>
+        /// Вызывается при очистке команды.
+        /// </summary>
+        [JsonPropertyName("disposed")]
+        [XmlElement("disposed")]
+        public DelegateDescriptor? Disposed;
+
+        /// <summary>
+        /// Команда разрешена к выполнению?
+        /// </summary>
+        [JsonPropertyName("enabled")]
+        [XmlAttribute("enabled")]
+        public bool Enabled { get; set; }
+
+        /// <summary>
+        /// Заглавие команды (произвольное).
+        /// </summary>
+        [JsonPropertyName("title")]
+        [XmlAttribute("title")]
+        public string? Title { get; set; }
+
+        /// <summary>
+        /// Описание команды в произвольной форме.
+        /// </summary>
+        [JsonPropertyName("description")]
+        [XmlAttribute("description")]
+        public string? Description { get; set; }
+
+        #endregion
+
+    } // class CommandDescriptor
+
+} // namespace AM.Commands
---
+++ Source/Libs/AM.Core5/Source/Commands/DelegateDescriptor.cs
@@ -0,0 +1,58 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable CommentTypo
+// ReSharper disable EventNeverSubscribedTo.Global
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable StringLiteralTypo
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedMemberInSuper.Global
+// ReSharper disable UnusedParameter.Local
+// ReSharper disable UnusedType.Global
+
+/* DelegateDescriptor.cs -- описатель делегата
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+#endregion
+
+#nullable enable
+
+namespace AM.Commands
+{
+    /// <summary>
+    /// Описатель делегата.
+    /// </summary>
+    [XmlRoot("delegate")]
+    public sealed class DelegateDescriptor
+    {
+        #region Properties
+
+        /// <summary>
+        /// Объект, к которому привязан делегат.
+        /// </summary>
+        [JsonPropertyName("object")]
+        [XmlAttribute("object")]
+        public string? Object { get; set; }
+
+        /// <summary>
+        /// Метод, выполняющий действие
+        /// </summary>
+        [JsonPropertyName("method")]
+        [XmlAttribute("method")]
+        public string? Method { get; set; }
+
+        #endregion
+
+    } // class DelegateDesriptor
+
+} // namespace AM.Commands
