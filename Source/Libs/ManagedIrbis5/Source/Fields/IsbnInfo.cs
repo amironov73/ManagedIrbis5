3bfecd63acc976ec843ffef16c5ddb835335903d
Alexey Mironov <amironov73@gmail.com>
2021-05-31 09:22:38 +0800

    Add IsbnInfo class

---
+++ Source/Libs/ManagedIrbis5/Source/Fields/IsbnInfo.cs
@@ -0,0 +1,325 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable CommentTypo
+// ReSharper disable ConvertClosureToMethodGroup
+// ReSharper disable IdentifierTypo
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable PropertyCanBeMadeInitOnly.Local
+// ReSharper disable StringLiteralTypo
+
+/* IsbnInfo.cs -- ISBN и цена, поле 10
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+using AM;
+using AM.IO;
+using AM.Runtime;
+
+using ManagedIrbis.Mapping;
+
+#endregion
+
+#nullable enable
+
+namespace ManagedIrbis.Fields
+{
+    /// <summary>
+    /// ISBN и цена, поле 10.
+    /// </summary>
+    [XmlRoot("isbn")]
+    public sealed class IsbnInfo
+        : IHandmadeSerializable,
+        IVerifiable
+    {
+        #region Constants
+
+        /// <summary>
+        /// Known subfield codes.
+        /// </summary>
+        public const string KnownCodes = "abcdz";
+
+        /// <summary>
+        /// Tag.
+        /// </summary>
+        public const int Tag = 10;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// ISBN.
+        /// </summary>
+        [SubField('a')]
+        [XmlElement("isbn")]
+        [JsonPropertyName("isbn")]
+        [Description("ISBN. Подполе a.")]
+        [DisplayName("ISBN")]
+        public string? Isbn { get; set; }
+
+        /// <summary>
+        /// Уточнение. Подполе b.
+        /// </summary>
+        [SubField('b')]
+        [XmlElement("refinement")]
+        [JsonPropertyName("refinement")]
+        [Description("Уточнение")]
+        [DisplayName("Уточнение")]
+        public string? Refinement { get; set; }
+
+        /// <summary>
+        /// Ошибочный ISBN. Подполе z.
+        /// </summary>
+        [SubField('z')]
+        [XmlElement("erroneous")]
+        [JsonPropertyName("erroneous")]
+        [Description("Ошибочный ISBN")]
+        [DisplayName("Ошибочный ISBN")]
+        public string? Erroneous { get; set; }
+
+        /// <summary>
+        /// Цена общая для всех экземпляров. Подполе d.
+        /// </summary>
+        [SubField('d')]
+        [XmlElement("price")]
+        [JsonPropertyName("price")]
+        [Description("Цена общая для всех экземпляров")]
+        [DisplayName("Цена общая для всех экземпляров")]
+        public string? PriceString { get; set; }
+
+        /// <summary>
+        /// Цена общая для всех экземпляров. Подполе d.
+        /// </summary>
+        [XmlIgnore]
+        [JsonIgnore]
+        [Browsable(false)]
+        public decimal Price
+        {
+            get => PriceString.SafeToDecimal();
+            set => PriceString = value.ToString("#.00", CultureInfo.InvariantCulture);
+        }
+
+        /// <summary>
+        /// Обозначение валюты. Подполе c.
+        /// </summary>
+        [SubField('c')]
+        [XmlElement("currency")]
+        [JsonPropertyName("currency")]
+        [Description("Обозначение валюты")]
+        [DisplayName("Обозначение валюты")]
+        public string? Currency { get; set; }
+
+        /// <summary>
+        /// Unknown subfields.
+        /// </summary>
+        [XmlIgnore]
+        [JsonIgnore]
+        [Browsable(false)]
+        public SubField[]? UnknownSubFields { get; set; }
+
+        /// <summary>
+        /// Поле, из которого были загруженные данные.
+        /// </summary>
+        [XmlIgnore]
+        [JsonIgnore]
+        [Browsable(false)]
+        public Field? Field { get; set; }
+
+        /// <summary>
+        /// Произвольные пользовательские данные.
+        /// </summary>
+        [XmlIgnore]
+        [JsonIgnore]
+        [Browsable(false)]
+        public object? UserData { get; set; }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Apply the <see cref="IsbnInfo"/>
+        /// to the <see cref="Field"/>.
+        /// </summary>
+        public void ApplyToField
+            (
+                Field field
+            )
+        {
+            field
+                .ApplySubField('a', Isbn)
+                .ApplySubField('b', Refinement)
+                .ApplySubField('z', Erroneous)
+                .ApplySubField('d', PriceString)
+                .ApplySubField('c', Currency);
+
+        } // method ApplyToField
+
+        /// <summary>
+        /// Parse the <see cref="Record"/>.
+        /// </summary>
+        public static IsbnInfo[] ParseRecord
+            (
+                Record record
+            )
+        {
+            var result = new List<IsbnInfo>();
+            foreach (Field field in record.Fields)
+            {
+                if (field.Tag == Tag)
+                {
+                    IsbnInfo isbn = ParseField(field);
+                    result.Add(isbn);
+                }
+            }
+
+            return result.ToArray();
+
+        } // method ParseRecord
+
+        /// <summary>
+        /// Parse the specified field.
+        /// </summary>
+        public static IsbnInfo ParseField
+            (
+                Field field
+            )
+        {
+            var result = new IsbnInfo
+            {
+                Isbn = field.GetFirstSubFieldValue('a'),
+                Refinement = field.GetFirstSubFieldValue('b'),
+                Erroneous = field.GetFirstSubFieldValue('z'),
+                PriceString = field.GetFirstSubFieldValue('d'),
+                Currency = field.GetFirstSubFieldValue('c'),
+                UnknownSubFields = field.Subfields.GetUnknownSubFields(KnownCodes),
+                Field = field
+            };
+
+            return result;
+
+        } // method ParseField
+
+        /// <summary>
+        /// Should serialize the <see cref="UnknownSubFields"/> array?
+        /// </summary>
+        [ExcludeFromCodeCoverage]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public bool ShouldSerializeUnknownSubFields() =>
+            !ArrayUtility.IsNullOrEmpty(UnknownSubFields);
+
+        /// <summary>
+        /// Transform back to field.
+        /// </summary>
+        public Field ToField()
+        {
+            var result = new Field(Tag)
+                .AddNonEmptySubField('a', Isbn)
+                .AddNonEmptySubField('b', Refinement)
+                .AddNonEmptySubField('z', Erroneous)
+                .AddNonEmptySubField
+                    (
+                        'd',
+                        Price != 0.0m ? Price.ToInvariantString() : null
+                    )
+                .AddNonEmptySubField('c', Currency);
+
+            return result;
+
+        } // method ToField
+
+        #endregion
+
+        #region IHandmadeSerializable members
+
+        /// <inheritdoc cref="IHandmadeSerializable.RestoreFromStream" />
+        public void RestoreFromStream
+            (
+                BinaryReader reader
+            )
+        {
+            Isbn = reader.ReadNullableString();
+            Refinement = reader.ReadNullableString();
+            Erroneous = reader.ReadNullableString();
+            Currency = reader.ReadNullableString();
+            PriceString = reader.ReadNullableString();
+
+        } // method RestoreFromStream
+
+        /// <inheritdoc cref="IHandmadeSerializable.SaveToStream" />
+        public void SaveToStream
+            (
+                BinaryWriter writer
+            )
+        {
+            writer
+                .WriteNullable(Isbn)
+                .WriteNullable(Refinement)
+                .WriteNullable(Erroneous)
+                .WriteNullable(Currency)
+                .WriteNullable(PriceString);
+
+        } // method SaveToStream
+
+        #endregion
+
+        #region IVerifiable members
+
+        /// <inheritdoc cref="IVerifiable.Verify" />
+        public bool Verify
+            (
+                bool throwOnError
+            )
+        {
+            var verifier = new Verifier<IsbnInfo>(this, throwOnError);
+
+            verifier.Assert
+                (
+                    !string.IsNullOrEmpty(PriceString)
+                    || !string.IsNullOrEmpty(Isbn)
+                    || !string.IsNullOrEmpty(Erroneous)
+                );
+
+            return verifier.Result;
+
+        } // method Verify
+
+        #endregion
+
+        #region Object members
+
+        /// <inheritdoc cref="object.ToString" />
+        public override string ToString()
+        {
+            if (string.IsNullOrEmpty(Isbn))
+            {
+                return string.IsNullOrEmpty(PriceString) ? "(null)" : PriceString;
+            }
+
+            if (string.IsNullOrEmpty(PriceString))
+            {
+                return Isbn;
+            }
+
+            return Isbn + " : " + PriceString;
+
+        } // method ToString
+
+        #endregion
+
+    } // class IsbnInfo
+
+} // namespace ManagedIrbis.Fields
---
+++ Source/Tests/UnitTests/Source/ManagedIrbis/Fields/IsbnInfoTest.cs
@@ -0,0 +1,223 @@
+﻿// ReSharper disable CheckNamespace
+// ReSharper disable ForCanBeConvertedToForeach
+// ReSharper disable IdentifierTypo
+// ReSharper disable InvokeAsExtensionMethod
+// ReSharper disable StringLiteralTypo
+
+using AM.Json;
+using AM.Runtime;
+using AM.Text;
+using AM.Xml;
+
+using ManagedIrbis;
+using ManagedIrbis.Fields;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+#nullable enable
+
+namespace UnitTests.ManagedIrbis.Fields
+{
+    [TestClass]
+    public class IsbnInfoTest
+    {
+        private IsbnInfo _GetIsbn()
+        {
+            return new ()
+            {
+                Isbn = "5-200-00723-2",
+                PriceString = "3.40"
+            };
+        }
+
+        private Field _GetField()
+        {
+            return new Field(IsbnInfo.Tag)
+                .Add('a', "5-200-00723-2")
+                .Add('d', "3.40");
+        }
+
+        [TestMethod]
+        public void IsbnInfo_Construction_1()
+        {
+            var isbn = new IsbnInfo();
+            Assert.IsNull(isbn.Isbn);
+            Assert.IsNull(isbn.Refinement);
+            Assert.IsNull(isbn.Erroneous);
+            Assert.IsNull(isbn.PriceString);
+            Assert.IsNull(isbn.Currency);
+            Assert.IsNull(isbn.UnknownSubFields);
+            Assert.IsNull(isbn.Field);
+            Assert.IsNull(isbn.UserData);
+            Assert.AreEqual(0.0m, isbn.Price);
+        }
+
+        private void _TestSerialization
+            (
+                IsbnInfo first
+            )
+        {
+            var bytes = first.SaveToMemory();
+            var second = bytes.RestoreObjectFromMemory<IsbnInfo>();
+            Assert.IsNotNull(second);
+            Assert.AreEqual(first.Isbn, second!.Isbn);
+            Assert.AreEqual(first.Refinement, second.Refinement);
+            Assert.AreEqual(first.Erroneous, second.Erroneous);
+            Assert.AreEqual(first.PriceString, second.PriceString);
+            Assert.AreEqual(first.Price, second.Price);
+            Assert.AreEqual(first.Currency, second.Currency);
+            Assert.AreSame(first.UnknownSubFields, second.UnknownSubFields);
+            Assert.IsNull(second.Field);
+            Assert.IsNull(second.UserData);
+        }
+
+        [TestMethod]
+        public void IsbnInfo_Serialization_1()
+        {
+            var isbn = new IsbnInfo();
+            _TestSerialization(isbn);
+
+            isbn.Field = new Field();
+            isbn.UserData = "User data";
+            _TestSerialization(isbn);
+
+            isbn = _GetIsbn();
+            _TestSerialization(isbn);
+        }
+
+        [TestMethod]
+        public void IsbnInfo_ParseField_1()
+        {
+            var field = _GetField();
+            var isbn = IsbnInfo.ParseField(field);
+            Assert.AreSame(field, isbn.Field);
+            Assert.AreEqual(field.GetFirstSubFieldValue('a'), isbn.Isbn);
+            Assert.AreEqual(field.GetFirstSubFieldValue('b'), isbn.Refinement);
+            Assert.AreEqual(field.GetFirstSubFieldValue('z'), isbn.Erroneous);
+            Assert.AreEqual(field.GetFirstSubFieldValue('d'), isbn.PriceString);
+            Assert.AreEqual(field.GetFirstSubFieldValue('c'), isbn.Currency);
+            Assert.IsNotNull(isbn.UnknownSubFields);
+            Assert.AreEqual(0, isbn.UnknownSubFields.Length);
+        }
+
+        [TestMethod]
+        public void IsbnInfo_ParseRecord_1()
+        {
+            var record = new Record();
+            var field = _GetField();
+            record.Fields.Add(field);
+            var isbn = IsbnInfo.ParseRecord(record);
+            Assert.AreEqual(1, isbn.Length);
+            Assert.AreSame(field, isbn[0].Field);
+            Assert.AreEqual(field.GetFirstSubFieldValue('a'), isbn[0].Isbn);
+            Assert.AreEqual(field.GetFirstSubFieldValue('b'), isbn[0].Refinement);
+            Assert.AreEqual(field.GetFirstSubFieldValue('z'), isbn[0].Erroneous);
+            Assert.AreEqual(field.GetFirstSubFieldValue('d'), isbn[0].PriceString);
+            Assert.AreEqual(field.GetFirstSubFieldValue('c'), isbn[0].Currency);
+            Assert.IsNotNull(isbn[0].UnknownSubFields);
+            Assert.AreEqual(0, isbn[0].UnknownSubFields.Length);
+        }
+
+        [TestMethod]
+        public void IsbnInfo_ToField_1()
+        {
+            var isbn = _GetIsbn();
+            var field = isbn.ToField();
+            Assert.AreEqual(2, field.Subfields.Count);
+            Assert.AreEqual(isbn.Isbn, field.GetFirstSubFieldValue('a'));
+            Assert.AreEqual(isbn.Refinement, field.GetFirstSubFieldValue('b'));
+            Assert.AreEqual(isbn.Erroneous, field.GetFirstSubFieldValue('z'));
+            Assert.AreEqual(isbn.PriceString, field.GetFirstSubFieldValue('d'));
+            Assert.AreEqual(isbn.Currency, field.GetFirstSubFieldValue('c'));
+        }
+
+        [TestMethod]
+        public void IsbnInfo_ApplyToField_1()
+        {
+            var field = new Field(IsbnInfo.Tag)
+                .Add('a', "???");
+            var isbn = _GetIsbn();
+            isbn.ApplyToField(field);
+            Assert.AreEqual(2, field.Subfields.Count);
+            Assert.AreEqual(isbn.Isbn, field.GetFirstSubFieldValue('a'));
+            Assert.AreEqual(isbn.Refinement, field.GetFirstSubFieldValue('b'));
+            Assert.AreEqual(isbn.Erroneous, field.GetFirstSubFieldValue('z'));
+            Assert.AreEqual(isbn.PriceString, field.GetFirstSubFieldValue('d'));
+            Assert.AreEqual(isbn.Currency, field.GetFirstSubFieldValue('c'));
+        }
+
+        [TestMethod]
+        public void Isbn_Verify_1()
+        {
+            var isbn = new IsbnInfo();
+            Assert.IsFalse(isbn.Verify(false));
+
+            isbn = _GetIsbn();
+            Assert.IsTrue(isbn.Verify(false));
+        }
+
+        [TestMethod]
+        public void IsbnInfo_ToXml_1()
+        {
+            var isbn = new IsbnInfo();
+            Assert.AreEqual("<isbn />", XmlUtility.SerializeShort(isbn));
+
+            isbn = _GetIsbn();
+            Assert.AreEqual("<isbn><isbn>5-200-00723-2</isbn><price>3.40</price></isbn>", XmlUtility.SerializeShort(isbn));
+        }
+
+        [TestMethod]
+        public void IsbnInfo_ToJson_1()
+        {
+            var isbn = new IsbnInfo();
+            Assert.AreEqual("{}", JsonUtility.SerializeShort(isbn));
+
+            isbn = _GetIsbn();
+            Assert.AreEqual("{\"isbn\":\"5-200-00723-2\",\"price\":\"3.40\"}", JsonUtility.SerializeShort(isbn));
+        }
+
+        [TestMethod]
+        public void IsbnInfo_Price_1()
+        {
+            var isbn = new IsbnInfo();
+            Assert.AreEqual(0.0m, isbn.Price);
+
+            isbn.Price = 1.5m;
+            Assert.AreEqual("1.50", isbn.PriceString);
+        }
+
+        [TestMethod]
+        public void IsbnInfo_ToString_1()
+        {
+            var isbn = new IsbnInfo();
+            Assert.AreEqual
+                (
+                    "(null)",
+                    isbn.ToString().DosToUnix()
+                );
+
+            isbn = _GetIsbn();
+            Assert.AreEqual
+                (
+                    "5-200-00723-2 : 3.40",
+                    isbn.ToString().DosToUnix()
+                );
+
+            isbn = _GetIsbn();
+            isbn.Isbn = null;
+            Assert.AreEqual
+            (
+                "3.40",
+                isbn.ToString().DosToUnix()
+            );
+
+            isbn = _GetIsbn();
+            isbn.PriceString = null;
+            Assert.AreEqual
+            (
+                "5-200-00723-2",
+                isbn.ToString().DosToUnix()
+            );
+        }
+    }
+}
