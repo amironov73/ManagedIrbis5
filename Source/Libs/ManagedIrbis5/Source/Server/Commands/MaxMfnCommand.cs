37fed5c045f139fea465cebed4760938fbfb93a3
Alexey Mironov <amironov73@gmail.com>
2021-06-21 09:03:11 +0800

    Working on server infrastructure

---
+++ Source/Libs/ManagedIrbis5/Source/Server/Commands/MaxMfnCommand.cs
@@ -0,0 +1,98 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable StringLiteralTypo
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedParameter.Local
+// ReSharper disable UnusedType.Global
+
+/* GetMaxMfnCommand.cs -- получение максимального MFN для указанной базы данных
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
+
+using AM;
+
+#endregion
+
+#nullable enable
+
+namespace ManagedIrbis.Server.Commands
+{
+    /// <summary>
+    /// Получение максимального MFN для указанной базы данных.
+    /// </summary>
+    public class MaxMfnCommand
+        : ServerCommand
+    {
+        #region Construction
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        public MaxMfnCommand
+            (
+                WorkData data
+            )
+            : base(data)
+        {
+        } // constructor
+
+        #endregion
+
+        #region ServerCommand members
+
+        /// <inheritdoc cref="ServerCommand.Execute" />
+        public override void Execute()
+        {
+            var engine = Data.Engine.ThrowIfNull(nameof(Data.Engine));
+            engine.OnBeforeExecute(Data);
+
+            try
+            {
+                var context = engine.RequireContext(Data);
+                Data.Context = context;
+                UpdateContext();
+
+                var request = Data.Request.ThrowIfNull(nameof(Data.Request));
+                var database = request.RequireAnsiString();
+
+                int result;
+                using (var direct = engine.GetDatabase(database))
+                {
+                    result = direct.GetMaxMfn();
+                }
+
+                var response = Data.Response.ThrowIfNull(nameof(Data.Response));
+                response.WriteInt32(result).NewLine();
+                SendResponse();
+            }
+            catch (IrbisException exception)
+            {
+                SendError(exception.ErrorCode);
+            }
+            catch (Exception exception)
+            {
+                Magna.TraceException(nameof(MaxMfnCommand) + "::" + nameof(Execute), exception);
+                SendError(-8888);
+            }
+
+            engine.OnAfterExecute(Data);
+
+        } // method Execute
+
+        #endregion
+
+    } // class MaxMfnCommand
+
+} // namespace ManagedIrbis.Server.Commands
