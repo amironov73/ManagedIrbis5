c138dbe2f552ce023c314534875cc60b5a91afa5
Alexey Mironov <amironov73@gmail.com>
2021-09-03 11:36:54 +0800

    Add PartInfo class

---
+++ Source/Libs/ManagedIrbis5/Source/Fields/PartInfo.cs
@@ -0,0 +1,282 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable CommentTypo
+// ReSharper disable ConvertClosureToMethodGroup
+// ReSharper disable IdentifierTypo
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable PropertyCanBeMadeInitOnly.Local
+// ReSharper disable StringLiteralTypo
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable UnusedMember.Global
+
+/* PartInfo.cs -- выпуск, часть: поле 923
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+using AM;
+using AM.IO;
+using AM.Runtime;
+
+using ManagedIrbis.Mapping;
+
+#endregion
+
+#nullable enable
+
+namespace ManagedIrbis.Fields
+{
+    /// <summary>
+    /// Выпуск, часть. Поле 923.
+    /// </summary>
+    [XmlRoot("part")]
+    public sealed class PartInfo
+        : IHandmadeSerializable,
+        IVerifiable
+    {
+        #region Constants
+
+        /// <summary>
+        /// Известные коды полей.
+        /// </summary>
+        public const string KnownCodes = "hiklu";
+
+        /// <summary>
+        /// Метка поля.
+        /// </summary>
+        public const int Tag = 923;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Обозначение и № 2-й единицы деления (Выпуск). Подполе h.
+        /// </summary>
+        [SubField('h')]
+        [XmlElement("secondLevelNumber")]
+        [JsonPropertyName("secondLevelNumber")]
+        [Description("Обозначение и № 2-й единицы деления (Выпуск)")]
+        [DisplayName("Обозначение и № 2-й единицы деления (Выпуск)")]
+        public string? SecondLevelNumber { get; set; }
+
+        /// <summary>
+        /// Заглавие 2-й единицы деления (Выпуск). Подполе i.
+        /// </summary>
+        [SubField('i')]
+        [XmlElement("secondLevelTitle")]
+        [JsonPropertyName("secondLevelTitle")]
+        [Description("Заглавие 2-й единицы деления (Выпуск)")]
+        [DisplayName("Заглавие 2-й единицы деления (Выпуск)")]
+        public string? SecondLevelTitle { get; set; }
+
+        /// <summary>
+        /// Обозначение и № 3-й единицы деления (Часть). Подполе k.
+        /// </summary>
+        [SubField('k')]
+        [XmlElement("thirdLevelNumber")]
+        [JsonPropertyName("thirdLevelNumber")]
+        [Description("Обозначение и № 3-й единицы деления (Часть)")]
+        [DisplayName("Обозначение и № 3-й единицы деления (Часть)")]
+        public string? ThirdLevelNumber { get; set; }
+
+        /// <summary>
+        /// Заглавие 3-й единицы деления (Часть). Подполе l.
+        /// </summary>
+        [SubField('l')]
+        [XmlElement("thirdLevelTitle")]
+        [JsonPropertyName("thirdLevelTitle")]
+        [Description("Заглавие 3-й единицы деления (Часть)")]
+        [DisplayName("Заглавие 3-й единицы деления (Часть)")]
+        public string? ThirdLevelTitle { get; set; }
+
+        /// <summary>
+        /// Роль (как выводить в словарь?). Подполе u.
+        /// </summary>
+        [SubField('u')]
+        [XmlElement("role")]
+        [JsonPropertyName("role")]
+        [Description("Роль (как выводить в словарь?)")]
+        [DisplayName("Роль (как выводить в словарь?)")]
+        public string? Role { get; set; }
+
+        /// <summary>
+        /// Неизвестные подполя.
+        /// </summary>
+        [XmlElement("unknown")]
+        [JsonPropertyName("unknown")]
+        [Browsable(false)]
+        public SubField[]? UnknownSubFields { get; set; }
+
+        /// <summary>
+        /// Связанное поле записи.
+        /// </summary>
+        [XmlIgnore]
+        [JsonIgnore]
+        [DisplayName("Поле с подполями")]
+        public Field? Field { get; set; }
+
+        /// <summary>
+        /// Произвольные пользовательские данные.
+        /// </summary>
+        [XmlIgnore]
+        [JsonIgnore]
+        [Browsable(false)]
+        public object? UserData { get; set; }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Применение данных о выпуске/части <see cref="PartInfo"/>
+        /// к полю записи <see cref="Field"/>.
+        /// </summary>
+        public void ApplyToField (Field field) => field
+            .ApplySubField('h', SecondLevelNumber)
+            .ApplySubField('i', SecondLevelTitle)
+            .ApplySubField('k', ThirdLevelNumber)
+            .ApplySubField('l', ThirdLevelTitle)
+            .ApplySubField('u', Role);
+
+        /// <summary>
+        /// Разбор библиографической записи <see cref="Record"/>.
+        /// </summary>
+        public static PartInfo[] ParseRecord
+            (
+                Record record
+            )
+        {
+            Sure.NotNull(record, "record");
+
+            var result = new List<PartInfo>();
+            foreach (var field in record.Fields)
+            {
+                if (field.Tag == Tag)
+                {
+                    var part = ParseField(field);
+                    result.Add(part);
+                }
+            }
+
+            return result.ToArray();
+
+        } // method ParseRecord
+
+        /// <summary>
+        /// Разбор поля библиографической записи <see cref="Field"/>.
+        /// </summary>
+        public static PartInfo ParseField (Field field) => new ()
+            {
+                SecondLevelNumber = field.GetFirstSubFieldValue('h'),
+                SecondLevelTitle = field.GetFirstSubFieldValue('i'),
+                ThirdLevelNumber = field.GetFirstSubFieldValue('k'),
+                ThirdLevelTitle = field.GetFirstSubFieldValue('l'),
+                Role = field.GetFirstSubFieldValue('u'),
+                UnknownSubFields = field.Subfields.GetUnknownSubFields(KnownCodes),
+                Field = field
+            };
+
+        /// <summary>
+        /// Should serialize the <see cref="UnknownSubFields"/> array?
+        /// </summary>
+        [ExcludeFromCodeCoverage]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public bool ShouldSerializeUnknownSubFields() =>
+            !ArrayUtility.IsNullOrEmpty(UnknownSubFields);
+
+        /// <summary>
+        /// Transform back to field.
+        /// </summary>
+        public Field ToField() => new Field(Tag)
+            .AddNonEmptySubField('h', SecondLevelNumber)
+            .AddNonEmptySubField('i', SecondLevelTitle)
+            .AddNonEmptySubField('k', ThirdLevelNumber)
+            .AddNonEmptySubField('l', ThirdLevelTitle)
+            .AddNonEmptySubField('u', Role)
+            .AddSubFields(UnknownSubFields);
+
+        #endregion
+
+        #region IHandmadeSerializable members
+
+        /// <inheritdoc cref="IHandmadeSerializable.RestoreFromStream" />
+        public void RestoreFromStream
+            (
+                BinaryReader reader
+            )
+        {
+            Sure.NotNull(reader, nameof(reader));
+
+            SecondLevelNumber = reader.ReadNullableString();
+            SecondLevelTitle = reader.ReadNullableString();
+            ThirdLevelNumber = reader.ReadNullableString();
+            ThirdLevelTitle = reader.ReadNullableString();
+            Role = reader.ReadNullableString();
+            UnknownSubFields = reader.ReadNullableArray<SubField>();
+
+        } // method RestoreFromStream
+
+        /// <inheritdoc cref="IHandmadeSerializable.SaveToStream" />
+        public void SaveToStream
+            (
+                BinaryWriter writer
+            )
+        {
+            Sure.NotNull(writer, nameof(writer));
+
+            writer
+                .WriteNullable(SecondLevelNumber)
+                .WriteNullable(SecondLevelTitle)
+                .WriteNullable(ThirdLevelNumber)
+                .WriteNullable(ThirdLevelTitle)
+                .WriteNullable(Role)
+                .WriteNullableArray(UnknownSubFields);
+
+        } // method SaveToStream
+
+        #endregion
+
+        #region IVerifiable members
+
+        /// <inheritdoc cref="IVerifiable.Verify" />
+        public bool Verify (bool throwOnError) => new Verifier<PartInfo>(this, throwOnError)
+            .Assert
+            (
+                !string.IsNullOrEmpty(SecondLevelNumber)
+                || !string.IsNullOrEmpty(SecondLevelTitle)
+                || !string.IsNullOrEmpty(ThirdLevelNumber)
+                || !string.IsNullOrEmpty(ThirdLevelTitle)
+            )
+            .Result;
+
+        #endregion
+
+        #region Object members
+
+        /// <inheritdoc cref="object.ToString" />
+        public override string ToString() => string.Join
+            (
+                " -- ",
+                SecondLevelNumber,
+                SecondLevelTitle,
+                ThirdLevelNumber,
+                ThirdLevelTitle
+            ).ToVisibleString();
+
+        #endregion
+
+    } // class PartInfo
+
+} // namespace ManagedIrbis.Fields
---
+++ Source/Tests/UnitTests/Source/ManagedIrbis/Fields/PartInfoTest.cs
@@ -0,0 +1,186 @@
+﻿// ReSharper disable CheckNamespace
+// ReSharper disable ForCanBeConvertedToForeach
+// ReSharper disable IdentifierTypo
+// ReSharper disable InvokeAsExtensionMethod
+// ReSharper disable StringLiteralTypo
+
+using AM.Json;
+using AM.Runtime;
+using AM.Xml;
+
+using ManagedIrbis;
+using ManagedIrbis.Fields;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+#nullable enable
+
+namespace UnitTests.ManagedIrbis.Fields
+{
+    [TestClass]
+    public class PartInfoTest
+    {
+        private PartInfo _GetPart() => new ()
+            {
+                SecondLevelNumber = "Ч. 2",
+                SecondLevelTitle = "Отрочество"
+            };
+
+        private Field _GetField() => new Field(PartInfo.Tag)
+                .Add('h', "Ч. 2")
+                .Add('i', "Отрочество");
+
+        [TestMethod]
+        public void PartInfo_Construction_1()
+        {
+            var part = new PartInfo();
+            Assert.IsNull(part.SecondLevelNumber);
+            Assert.IsNull(part.SecondLevelTitle);
+            Assert.IsNull(part.ThirdLevelNumber);
+            Assert.IsNull(part.ThirdLevelTitle);
+            Assert.IsNull(part.Role);
+            Assert.IsNull(part.UnknownSubFields);
+            Assert.IsNull(part.Field);
+            Assert.IsNull(part.UserData);
+        }
+
+        private void _TestSerialization
+            (
+                PartInfo first
+            )
+        {
+            var bytes = first.SaveToMemory();
+            var second = bytes.RestoreObjectFromMemory<PartInfo>();
+            Assert.IsNotNull(second);
+            Assert.AreEqual(first.SecondLevelNumber, second.SecondLevelNumber);
+            Assert.AreEqual(first.SecondLevelTitle, second.SecondLevelTitle);
+            Assert.AreEqual(first.ThirdLevelNumber, second.ThirdLevelNumber);
+            Assert.AreEqual(first.ThirdLevelTitle, second.ThirdLevelTitle);
+            Assert.AreEqual(first.Role, second.Role);
+            Assert.AreSame(first.UnknownSubFields, second.UnknownSubFields);
+            Assert.IsNull(second.Field);
+            Assert.IsNull(second.UserData);
+        }
+
+        [TestMethod]
+        public void PartInfo_Serialization_1()
+        {
+            var part = new PartInfo();
+            _TestSerialization(part);
+
+            part.Field = new Field();
+            part.UserData = "User data";
+            _TestSerialization(part);
+
+            part = _GetPart();
+            _TestSerialization(part);
+        }
+
+        [TestMethod]
+        public void PartInfo_ParseField_1()
+        {
+            var field = _GetField();
+            var part = PartInfo.ParseField(field);
+            Assert.AreSame(field, part.Field);
+            Assert.AreEqual(field.GetFirstSubFieldValue('h'), part.SecondLevelNumber);
+            Assert.AreEqual(field.GetFirstSubFieldValue('i'), part.SecondLevelTitle);
+            Assert.AreEqual(field.GetFirstSubFieldValue('k'), part.ThirdLevelNumber);
+            Assert.AreEqual(field.GetFirstSubFieldValue('l'), part.ThirdLevelTitle);
+            Assert.AreEqual(field.GetFirstSubFieldValue('u'), part.Role);
+            Assert.IsNotNull(part.UnknownSubFields);
+            Assert.AreEqual(0, part.UnknownSubFields.Length);
+            Assert.IsNull(part.UserData);
+        }
+
+        [TestMethod]
+        public void PartInfo_ParseRecord_1()
+        {
+            var record = new Record();
+            var field = _GetField();
+            record.Fields.Add(field);
+            var part = PartInfo.ParseRecord(record);
+            Assert.AreEqual(1, part.Length);
+            Assert.AreSame(field, part[0].Field);
+            Assert.AreEqual(field.GetFirstSubFieldValue('h'), part[0].SecondLevelNumber);
+            Assert.AreEqual(field.GetFirstSubFieldValue('i'), part[0].SecondLevelTitle);
+            Assert.AreEqual(field.GetFirstSubFieldValue('k'), part[0].ThirdLevelNumber);
+            Assert.AreEqual(field.GetFirstSubFieldValue('l'), part[0].ThirdLevelTitle);
+            Assert.AreEqual(field.GetFirstSubFieldValue('u'), part[0].Role);
+            Assert.IsNotNull(part[0].UnknownSubFields);
+            Assert.AreEqual(0, part[0].UnknownSubFields!.Length);
+            Assert.IsNull(part[0].UserData);
+        }
+
+        [TestMethod]
+        public void PartInfo_ToField_1()
+        {
+            var part = _GetPart();
+            var field = part.ToField();
+            Assert.AreEqual(PartInfo.Tag, field.Tag);
+            Assert.AreEqual(2, field.Subfields.Count);
+            Assert.AreEqual(part.SecondLevelNumber, field.GetFirstSubFieldValue('h'));
+            Assert.AreEqual(part.SecondLevelTitle, field.GetFirstSubFieldValue('i'));
+            Assert.AreEqual(part.ThirdLevelNumber, field.GetFirstSubFieldValue('k'));
+            Assert.AreEqual(part.ThirdLevelTitle, field.GetFirstSubFieldValue('l'));
+            Assert.AreEqual(part.Role, field.GetFirstSubFieldValue('u'));
+        }
+
+        [TestMethod]
+        public void PartInfo_ApplyToField_1()
+        {
+            var field = new Field(PartInfo.Tag)
+                .Add('h', "???")
+                .Add('i', "???");
+            var part = _GetPart();
+            part.ApplyToField(field);
+            Assert.AreEqual(PartInfo.Tag, field.Tag);
+            Assert.AreEqual(2, field.Subfields.Count);
+            Assert.AreEqual(part.SecondLevelNumber, field.GetFirstSubFieldValue('h'));
+            Assert.AreEqual(part.SecondLevelTitle, field.GetFirstSubFieldValue('i'));
+            Assert.AreEqual(part.ThirdLevelNumber, field.GetFirstSubFieldValue('k'));
+            Assert.AreEqual(part.ThirdLevelTitle, field.GetFirstSubFieldValue('l'));
+            Assert.AreEqual(part.Role, field.GetFirstSubFieldValue('u'));
+        }
+
+        [TestMethod]
+        public void PartInfo_Verify_1()
+        {
+            var part = new PartInfo();
+            Assert.IsFalse(part.Verify(false));
+
+            part = _GetPart();
+            Assert.IsTrue(part.Verify(false));
+        }
+
+        [TestMethod]
+        public void PartInfo_ToXml_1()
+        {
+            var part = new PartInfo();
+            Assert.AreEqual("<part />", XmlUtility.SerializeShort(part));
+
+            part = _GetPart();
+            Assert.AreEqual("<part><secondLevelNumber>Ч. 2</secondLevelNumber><secondLevelTitle>Отрочество</secondLevelTitle></part>", XmlUtility.SerializeShort(part));
+        }
+
+        [TestMethod]
+        public void PartInfo_ToJson_1()
+        {
+            var part = new PartInfo();
+            Assert.AreEqual("{}", JsonUtility.SerializeShort(part));
+
+            part = _GetPart();
+            Assert.AreEqual("{\"secondLevelNumber\":\"\\u0427. 2\",\"secondLevelTitle\":\"\\u041E\\u0442\\u0440\\u043E\\u0447\\u0435\\u0441\\u0442\\u0432\\u043E\"}", JsonUtility.SerializeShort(part));
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void PartInfo_ToString_1()
+        {
+            var part = new PartInfo();
+            Assert.AreEqual("(empty)", part.ToString());
+
+            part = _GetPart();
+            Assert.AreEqual("Ч. 2 -- Отрочество", part.ToString());
+        }
+    }
+}
