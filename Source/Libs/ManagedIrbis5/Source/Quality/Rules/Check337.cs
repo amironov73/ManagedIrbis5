c517af9bf9d1ada07e4e04d5282f26851e919638
Alexey Mironov <amironov73@gmail.com>
2021-10-04 07:54:26 +0800

    Add some quality rules

---
+++ Source/Libs/ManagedIrbis5/Source/Quality/Rules/Check337.cs
@@ -0,0 +1,70 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable StringLiteralTypo
+
+/* Check337.cs -- электронный ресурс (примечания)
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using AM;
+
+#endregion
+
+#nullable enable
+
+namespace ManagedIrbis.Quality.Rules
+{
+    /// <summary>
+    /// Электронный ресурс (примечания).
+    /// </summary>
+    public sealed class Check337
+        : QualityRule
+    {
+        #region Private members
+
+        private void CheckField
+            (
+                Field field
+            )
+        {
+        } // method CheckField
+
+        #endregion
+
+        #region QualityRule members
+
+        /// <inheritdoc cref="QualityRule.FieldSpec"/>
+        public override string FieldSpec => "337";
+
+        /// <inheritdoc cref="QualityRule.CheckRecord"/>
+        public override RuleReport CheckRecord
+            (
+                RuleContext context
+            )
+        {
+            BeginCheck(context);
+
+            var fields = GetFields();
+            foreach (var field in fields)
+            {
+                CheckField(field);
+            }
+
+            return EndCheck();
+
+        } // method CheckRecord
+
+        #endregion
+
+    } // class Check337
+
+} // namespace ManagedIrbis.Quality.Rules
---
+++ Source/Libs/ManagedIrbis5/Source/Quality/Rules/Check452.cs
@@ -0,0 +1,70 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable StringLiteralTypo
+
+/* Check452.cs -- параллельное издание на другом носителе
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using AM;
+
+#endregion
+
+#nullable enable
+
+namespace ManagedIrbis.Quality.Rules
+{
+    /// <summary>
+    /// Параллельное издание на другом носителе.
+    /// </summary>
+    public sealed class Check452
+        : QualityRule
+    {
+        #region Private members
+
+        private void CheckField
+            (
+                Field field
+            )
+        {
+        } // method CheckField
+
+        #endregion
+
+        #region QualityRule members
+
+        /// <inheritdoc cref="QualityRule.FieldSpec"/>
+        public override string FieldSpec => "452";
+
+        /// <inheritdoc cref="QualityRule.CheckRecord"/>
+        public override RuleReport CheckRecord
+            (
+                RuleContext context
+            )
+        {
+            BeginCheck(context);
+
+            var fields = GetFields();
+            foreach (var field in fields)
+            {
+                CheckField(field);
+            }
+
+            return EndCheck();
+
+        } // method CheckRecord
+
+        #endregion
+
+    } // class Check452
+
+} // namespace ManagedIrbis.Quality.Rules
