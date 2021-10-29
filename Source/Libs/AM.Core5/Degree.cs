f709e2e7e12b2332a15239e8cec12d3fd3d4e106
Alexey Mironov <amironov73@gmail.com>
2021-10-29 11:44:49 +0800

    Add Degree structure

---
+++ Source/Libs/AM.Core5/Degree.cs
@@ -0,0 +1,267 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable CommentTypo
+// ReSharper disable CompareOfFloatsByEqualityOperator
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable NonReadonlyMemberInGetHashCode
+// ReSharper disable StringLiteralTypo
+// ReSharper disable UnusedParameter.Local
+
+/* Degree.cs -- градусы, минуты, секунды
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+#endregion
+
+#nullable enable
+
+namespace AM
+{
+    /// <summary>
+    /// Градусы, минуты, секунды.
+    /// </summary>
+    public struct Degree
+        : IEquatable<Degree>
+    {
+        #region Properties
+
+        /// <summary>
+        /// Градусы (целая часть).
+        /// </summary>
+        public int Degrees { get; set; }
+
+        /// <summary>
+        /// Минуты (целая часть).
+        /// </summary>
+        public int Minutes { get; set; }
+
+        /// <summary>
+        /// Секунды (целая и дробная части).
+        /// </summary>
+        public float Seconds { get; set; }
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        public Degree
+            (
+                double degrees
+            )
+            : this()
+        {
+            var minus = degrees < 0;
+
+            degrees = Math.Abs (degrees);
+            var wholeDegrees = Math.Truncate (degrees);
+            Degrees = (int) wholeDegrees;
+            if (minus)
+            {
+                Degrees = -Degrees;
+            }
+
+            var minutes = (degrees - wholeDegrees) * 60.0;
+            Minutes = (int) minutes;
+
+            Seconds = (float) ((minutes - Minutes) * 60.0);
+
+        } // constructor
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        public Degree
+            (
+                int degrees,
+                int minutes,
+                float seconds
+            )
+        {
+            Degrees = degrees;
+            Minutes = minutes;
+            Seconds = seconds;
+
+        } // constructor
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Перевод из радиан в градусы.
+        /// </summary>
+        public static Degree FromRadians (double radians) => new (radians * 180.0 / Math.PI);
+
+        /// <summary>
+        /// Преобразование в градусы без минут.
+        /// </summary>
+        public double ToDouble()
+        {
+            var minus = Degrees < 0;
+
+            var result = Math.Abs (Degrees) + Minutes / 60.0 + Seconds / 3600.0;
+            if (minus)
+            {
+                result = -result;
+            }
+
+            return result;
+
+        } // method ToDouble
+
+        /// <summary>
+        /// Перевод в радианы.
+        /// </summary>
+        public double ToRadians() => ToDouble() / 180.0 * Math.PI;
+
+        /// <summary>
+        /// Сложение.
+        /// </summary>
+        public static Degree operator + (Degree left, Degree right) =>
+            new (left.ToDouble() + right.ToDouble());
+
+        /// <summary>
+        /// Сложение.
+        /// </summary>
+        public static Degree operator + (Degree left, double right) =>
+            new (left.ToDouble() + right);
+
+        /// <summary>
+        /// Вычитание.
+        /// </summary>
+        public static Degree operator - (Degree left, Degree right) =>
+            new (left.ToDouble() - right.ToDouble());
+
+        /// <summary>
+        /// Вычитание.
+        /// </summary>
+        public static Degree operator - (Degree left, double right) =>
+            new (left.ToDouble() - right);
+
+        /// <summary>
+        /// Умножение.
+        /// </summary>
+        public static Degree operator * (Degree left, double right) =>
+            new (left.ToDouble() * right);
+
+        /// <summary>
+        /// Деление.
+        /// </summary>
+        public static Degree operator / (Degree left, double right) =>
+            new (left.ToDouble() / right);
+
+        /// <summary>
+        /// Равенство.
+        /// </summary>
+        public static bool operator == (Degree left, Degree right) =>
+            left.ToDouble() == right.ToDouble();
+
+        /// <summary>
+        /// Равенство.
+        /// </summary>
+        public static bool operator == (Degree left, double right) =>
+            left.ToDouble() == right;
+
+        /// <summary>
+        /// Неравенство.
+        /// </summary>
+        public static bool operator != (Degree left, Degree right) =>
+            left.ToDouble() != right.ToDouble();
+
+        /// <summary>
+        /// Неравенство.
+        /// </summary>
+        public static bool operator != (Degree left, double right) =>
+            left.ToDouble() != right;
+
+        /// <summary>
+        /// Меньше.
+        /// </summary>
+        public static bool operator < (Degree left, Degree right) =>
+            left.ToDouble() < right.ToDouble();
+
+        /// <summary>
+        /// Меньше.
+        /// </summary>
+        public static bool operator < (Degree left, double right) =>
+            left.ToDouble() < right;
+
+        /// <summary>
+        /// Меньше или равно.
+        /// </summary>
+        public static bool operator <= (Degree left, Degree right) =>
+            left.ToDouble() <= right.ToDouble();
+
+        /// <summary>
+        /// Меньше или равно.
+        /// </summary>
+        public static bool operator <= (Degree left, double right) =>
+            left.ToDouble() <= right;
+
+        /// <summary>
+        /// Больше.
+        /// </summary>
+        public static bool operator > (Degree left, Degree right) =>
+            left.ToDouble() > right.ToDouble();
+
+        /// <summary>
+        /// Больше.
+        /// </summary>
+        public static bool operator > (Degree left, double right) =>
+            left.ToDouble() > right;
+
+        /// <summary>
+        /// Больше или равно.
+        /// </summary>
+        public static bool operator >= (Degree left, Degree right) =>
+            left.ToDouble() >= right.ToDouble();
+
+        /// <summary>
+        /// Больше или равно.
+        /// </summary>
+        public static bool operator >= (Degree left, double right) =>
+            left.ToDouble() >= right;
+
+        #endregion
+
+        #region Object members
+
+        /// <inheritdoc cref="IEquatable{T}.Equals(T)"/>
+        public bool Equals (Degree other) => ToDouble() == other.ToDouble();
+
+        /// <inheritdoc cref="ValueType.Equals(object)"/>
+        [ExcludeFromCodeCoverage]
+        public override bool Equals (object? obj) => obj is Degree other && Equals (other);
+
+        /// <inheritdoc cref="ValueType.GetHashCode"/>
+        [ExcludeFromCodeCoverage]
+        public override int GetHashCode() => HashCode.Combine (Degrees, Minutes, Seconds);
+
+        /// <inheritdoc cref="ValueType.ToString"/>
+        public override string ToString() => string.Format
+            (
+                CultureInfo.InvariantCulture,
+                "{0}\u00B0 {1}' {2:F2}\"",
+                Degrees,
+                Minutes,
+                Seconds
+            );
+
+        #endregion
+
+    } // struct Degree
+
+} // namespace AM
---
+++ Source/Tests/UnitTests/Source/AM/DegreeTest.cs
@@ -0,0 +1,323 @@
+﻿// ReSharper disable CheckNamespace
+// ReSharper disable ForCanBeConvertedToForeach
+// ReSharper disable InvokeAsExtensionMethod
+// ReSharper disable PropertyCanBeMadeInitOnly.Local
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using AM;
+
+#nullable enable
+
+namespace UnitTests.AM
+{
+    [TestClass]
+    public sealed class DegreeTest
+    {
+        [TestMethod]
+        [Description ("Конструктор по умолчанию")]
+        public void Degree_Constructor_1()
+        {
+            var zero = new Degree();
+            Assert.AreEqual (0, zero.Degrees);
+            Assert.AreEqual (0, zero.Minutes);
+            Assert.AreEqual (0.0, zero.Seconds);
+        }
+
+        [TestMethod]
+        [Description ("Конструктор с минутами и секундами")]
+        public void Degree_Constructor_2()
+        {
+            var irkutsk = new Degree (52, 17, 52);
+            Assert.AreEqual (52, irkutsk.Degrees);
+            Assert.AreEqual (17, irkutsk.Minutes);
+            Assert.AreEqual (52.0, irkutsk.Seconds);
+        }
+
+        [TestMethod]
+        [Description ("Конструктор с десятичной дробью")]
+        public void Degree_Constructor_3()
+        {
+            var irkutsk = new Degree (52.2977800);
+            Assert.AreEqual (52, irkutsk.Degrees);
+            Assert.AreEqual (17, irkutsk.Minutes);
+            Assert.AreEqual (52.0, irkutsk.Seconds, 0.01);
+        }
+
+        [TestMethod]
+        [Description ("Отрицательное значение градусов")]
+        public void Degree_Constructor_4()
+        {
+            var irkutsk = new Degree (-52, 17, 52);
+            Assert.AreEqual (-52, irkutsk.Degrees);
+            Assert.AreEqual (17, irkutsk.Minutes);
+            Assert.AreEqual (52.0, irkutsk.Seconds);
+        }
+
+        [TestMethod]
+        [Description ("Отрицательная десятичная дробь")]
+        public void Degree_Constructor_5()
+        {
+            var irkutsk = new Degree (-52.2977800);
+            Assert.AreEqual (-52, irkutsk.Degrees);
+            Assert.AreEqual (17, irkutsk.Minutes);
+            Assert.AreEqual (52.0, irkutsk.Seconds, 0.01);
+        }
+
+        [TestMethod]
+        [Description ("Перевод радиан в градусы")]
+        public void Degree_FromRadians_1()
+        {
+            var value = Degree.FromRadians (0.0);
+            Assert.AreEqual (0.0, value.ToDouble(), 0.01);
+        }
+
+        [TestMethod]
+        [Description ("Перевод радиан в градусы")]
+        public void Degree_FromRadians_2()
+        {
+            var value = Degree.FromRadians (1.0);
+            Assert.AreEqual (57.2958, value.ToDouble(), 0.01);
+        }
+
+        [TestMethod]
+        [Description ("Получение десятичной дроби")]
+        public void Degree_ToDouble_1()
+        {
+            var zero = new Degree();
+            var value = zero.ToDouble();
+            Assert.AreEqual (0.0, value, 0.01);
+        }
+
+        [TestMethod]
+        [Description ("Получение десятичной дроби")]
+        public void Degree_ToDouble_2()
+        {
+            var irkutsk = new Degree (52, 17, 52);
+            var value = irkutsk.ToDouble();
+            Assert.AreEqual (52.2977800, value, 0.01);
+        }
+
+        [TestMethod]
+        [Description ("Получение отрицательной десятичной дроби")]
+        public void Degree_ToDouble_3()
+        {
+            var irkutsk = new Degree (-52, 17, 52);
+            var value = irkutsk.ToDouble();
+            Assert.AreEqual (-52.2977800, value, 0.01);
+        }
+
+        [TestMethod]
+        [Description ("Оператор сложения")]
+        public void Degree_Operator_Plus_1()
+        {
+            var left = new Degree (1.0);
+            var right = new Degree (2.0);
+            var result = left + right;
+            Assert.AreEqual (3, result.Degrees);
+            Assert.AreEqual (0, result.Minutes);
+            Assert.AreEqual (0.0, result.Seconds, 0.01);
+        }
+
+        [TestMethod]
+        [Description ("Оператор сложения")]
+        public void Degree_Operator_Plus_2()
+        {
+            var left = new Degree (1.0);
+            var right = 2.0;
+            var result = left + right;
+            Assert.AreEqual (3, result.Degrees);
+            Assert.AreEqual (0, result.Minutes);
+            Assert.AreEqual (0.0, result.Seconds, 0.01);
+        }
+
+        [TestMethod]
+        [Description ("Оператор вычитания")]
+        public void Degree_Operator_Minus_1()
+        {
+            var left = new Degree (1.0);
+            var right = new Degree (2.0);
+            var result = left - right;
+            Assert.AreEqual (-1, result.Degrees);
+            Assert.AreEqual (0, result.Minutes);
+            Assert.AreEqual (0.0, result.Seconds, 0.01);
+        }
+
+        [TestMethod]
+        [Description ("Оператор вычитания")]
+        public void Degree_Operator_Minus_2()
+        {
+            var left = new Degree (1.0);
+            var right = 2.0;
+            var result = left - right;
+            Assert.AreEqual (-1, result.Degrees);
+            Assert.AreEqual (0, result.Minutes);
+            Assert.AreEqual (0.0, result.Seconds, 0.01);
+        }
+
+        [TestMethod]
+        [Description ("Оператор умножения")]
+        public void Degree_Operator_Star_1()
+        {
+            var left = new Degree (1.0);
+            var result = left * 2.0;
+            Assert.AreEqual (2, result.Degrees);
+            Assert.AreEqual (0, result.Minutes);
+            Assert.AreEqual (0.0, result.Seconds, 0.01);
+        }
+
+        [TestMethod]
+        [Description ("Оператор деления")]
+        public void Degree_Operator_Divide_1()
+        {
+            var left = new Degree (1.0);
+            var result = left / 2.0;
+            Assert.AreEqual (0, result.Degrees);
+            Assert.AreEqual (30, result.Minutes);
+            Assert.AreEqual (0.0, result.Seconds, 0.01);
+        }
+
+        [TestMethod]
+        [Description ("Оператор Равно")]
+        public void Degree_Operator_Equals_1()
+        {
+            var left = new Degree (1.0);
+            var right = new Degree (1.1);
+            Assert.IsFalse (left == right);
+        }
+
+        [TestMethod]
+        [Description ("Оператор Равно")]
+        public void Degree_Operator_Equals_2()
+        {
+            var left = new Degree (1.0);
+            var right = 1.1;
+            Assert.IsFalse (left == right);
+        }
+
+        [TestMethod]
+        [Description ("Оператор Не равно")]
+        public void Degree_Operator_Not_Equals_1()
+        {
+            var left = new Degree (1.0);
+            var right = new Degree (1.1);
+            Assert.IsTrue (left != right);
+        }
+
+        [TestMethod]
+        [Description ("Оператор Не равно")]
+        public void Degree_Operator_Not_Equals_2()
+        {
+            var left = new Degree (1.0);
+            var right = 1.1;
+            Assert.IsTrue (left != right);
+        }
+
+        [TestMethod]
+        [Description ("Оператор Меньше")]
+        public void Degree_Operator_Less_1()
+        {
+            var left = new Degree (1.0);
+            var right = new Degree (1.1);
+            Assert.IsTrue (left < right);
+        }
+
+        [TestMethod]
+        [Description ("Оператор Меньше")]
+        public void Degree_Operator_Less_2()
+        {
+            var left = new Degree (1.0);
+            var right = 1.1;
+            Assert.IsTrue (left < right);
+        }
+
+        [TestMethod]
+        [Description ("Оператор Меньше или равно")]
+        public void Degree_Operator_Less_Or_Equals_1()
+        {
+            var left = new Degree (1.0);
+            var right = new Degree (1.1);
+            Assert.IsTrue (left <= right);
+        }
+
+        [TestMethod]
+        [Description ("Оператор Меньше или равно")]
+        public void Degree_Operator_Less_Or_Equals_2()
+        {
+            var left = new Degree (1.0);
+            var right = 1.1;
+            Assert.IsTrue (left <= right);
+        }
+
+        [TestMethod]
+        [Description ("Оператор Больше")]
+        public void Degree_Operator_More_1()
+        {
+            var left = new Degree (1.0);
+            var right = new Degree (1.1);
+            Assert.IsFalse (left > right);
+        }
+
+        [TestMethod]
+        [Description ("Оператор Больше")]
+        public void Degree_Operator_More_2()
+        {
+            var left = new Degree (1.0);
+            var right = 1.1;
+            Assert.IsFalse (left > right);
+        }
+
+        [TestMethod]
+        [Description ("Оператор Больше или равно")]
+        public void Degree_Operator_More_Or_Equals_1()
+        {
+            var left = new Degree (1.0);
+            var right = new Degree (1.1);
+            Assert.IsFalse (left >= right);
+        }
+
+        [TestMethod]
+        [Description ("Оператор Больше или равно")]
+        public void Degree_Operator_More_Or_Equals_2()
+        {
+            var left = new Degree (1.0);
+            var right = 1.1;
+            Assert.IsFalse (left >= right);
+        }
+
+        [TestMethod]
+        [Description ("Равенство")]
+        public void Degree_Equals_1()
+        {
+            var left = new Degree (1.0);
+            var right = new Degree (1.1);
+            Assert.IsFalse (left.Equals (right));
+            Assert.IsTrue (left.Equals (left));
+            Assert.IsTrue (right.Equals (right));
+        }
+
+        [TestMethod]
+        [Description ("Строка с нулевым значением")]
+        public void Degree_ToString_1()
+        {
+            var zero = new Degree();
+            Assert.AreEqual ("0\u00B0 0' 0.00\"", zero.ToString());
+        }
+
+        [TestMethod]
+        [Description ("Строка с положительным значением")]
+        public void Degree_ToString_2()
+        {
+            var zero = new Degree(52, 17, 52);
+            Assert.AreEqual ("52\u00B0 17' 52.00\"", zero.ToString());
+        }
+
+        [TestMethod]
+        [Description ("Строка с отрицательным значением")]
+        public void Degree_ToString_3()
+        {
+            var zero = new Degree(-52, 17, 52);
+            Assert.AreEqual ("-52\u00B0 17' 52.00\"", zero.ToString());
+        }
+    }
+}
