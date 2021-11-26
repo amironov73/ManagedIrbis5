e67c63ee30892cb54e632d70708bb7ec50bfb90d
Alexey Mironov <amironov73@gmail.com>
2021-11-26 18:36:30 +0800

    Working on AM.Scripting

---
+++ Source/Libs/AM.Scripting/Source/Resolve.cs
@@ -0,0 +1,153 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable StringLiteralTypo
+// ReSharper disable UseNameofExpression
+
+/* Resolve.cs -- набор парсеров для нужд ИРБИС
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System.Collections.Generic;
+using System.Globalization;
+
+using Sprache;
+
+#endregion
+
+#nullable enable
+
+namespace AM.Scripting
+{
+    /// <summary>
+    /// Набор парсеров для нужд ИРБИС.
+    /// </summary>
+    public static class Resolve
+    {
+
+        #region Fields
+
+        /// <summary>
+        /// Знак минус.
+        /// </summary>
+        public static readonly Parser<char> Minus = Parse.Char ('-');
+
+        /// <summary>
+        /// Точка.
+        /// </summary>
+        public static readonly Parser<char> Dot = Parse.Char ('.');
+
+        /// <summary>
+        /// Символ подчеркивания.
+        /// </summary>
+        public static readonly Parser<char> Underscore = Parse.Char ('_');
+
+        /// <summary>
+        /// Арабские цифры.
+        /// </summary>
+        public static readonly Parser<char> Arabic
+            = Parse.Chars ("0123456789").Named ("arabic digit");
+
+        /// <summary>
+        /// Латинская буква.
+        /// </summary>
+        public static readonly Parser<char> Latin
+            = Parse.Chars ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
+                .Named ("latin character");
+
+        /// <summary>
+        /// Латинская буква или символ пдчеркивания.
+        /// </summary>
+        public static readonly Parser<char> LatinOrUnderscore
+            = Latin.Or (Underscore);
+
+        /// <summary>
+        /// Латинская буква, арабская цифра или символ пдчеркивания.
+        /// </summary>
+        public static readonly Parser<char> LatinOrArabicOrUnderscore
+            = Latin.Or (Arabic).Or (Underscore);
+
+        /// <summary>
+        /// Общепринятый идентификатор.
+        /// </summary>
+        public static readonly Parser<string> Identifier =
+            from firstLetter in LatinOrUnderscore
+            from tail in LatinOrArabicOrUnderscore.Many().Text()
+            select firstLetter + tail;
+
+        /// <summary>
+        /// Беззнаковое целое.
+        /// </summary>
+        public static readonly Parser<uint> UnsignedInteger =
+            Arabic.AtLeastOnce().Text().Select
+                (
+                    s => uint.Parse (s, CultureInfo.InvariantCulture)
+                );
+
+        /// <summary>
+        /// Целое со знаком.
+        /// </summary>
+        public static readonly Parser<int> SignedInteger =
+            Minus.Perhaps().Concat (Arabic.AtLeastOnce()).Text().Select
+                (
+                    s => int.Parse (s, CultureInfo.InvariantCulture)
+                );
+
+        /// <summary>
+        /// Беззнаковое длинное целое.
+        /// </summary>
+        public static readonly Parser<ulong> UnsignedLong =
+            Arabic.AtLeastOnce()
+                .Text()
+                .Select (s => ulong.Parse (s, CultureInfo.InvariantCulture));
+
+        /// <summary>
+        /// Длинное целое со знаком.
+        /// </summary>
+        public static readonly Parser<long> SignedLong =
+            Minus.Perhaps()
+                .Concat (Arabic.AtLeastOnce())
+                .Text()
+                .Select (s => long.Parse (s, CultureInfo.InvariantCulture));
+
+        private static readonly Parser<string> FloatBody =
+            from minus in Minus.Perhaps().Text()
+            from whole in Arabic.AtLeastOnce().Text()
+            from frac in Dot.Once().Concat (Arabic.AtLeastOnce()).Text().Optional()
+            from exp in Parse.Chars ('e', 'E').Once()
+                .Concat (Minus.Perhaps())
+                .Concat (Arabic.AtLeastOnce())
+                .Text().Optional()
+            select minus + whole + frac.GetOrDefault() + exp.GetOrDefault();
+
+        /// <summary>
+        /// Число с плавающей точкой одинарной точности.
+        /// </summary>
+        public static readonly Parser<float> Float =
+            FloatBody.Select (s => float.Parse (s, CultureInfo.InvariantCulture));
+
+        /// <summary>
+        /// Число с плавающей точкой двойной точности.
+        /// </summary>
+        public static readonly Parser<double> Double =
+            FloatBody.Select (s => double.Parse (s, CultureInfo.InvariantCulture));
+
+        #endregion
+
+        #region Extension methods
+
+        /// <summary>
+        /// Повторяется ноль или один раз.
+        /// </summary>
+        public static Parser<IEnumerable<T>> Perhaps<T> (this Parser<T> parser) => parser.Repeat (0, 1);
+
+        #endregion
+
+    }
+}
---
+++ Source/Tests/UnitTests/Source/Scripting/ResolveTest.cs
@@ -0,0 +1,132 @@
+﻿// ReSharper disable CheckNamespace
+// ReSharper disable ForCanBeConvertedToForeach
+// ReSharper disable InvokeAsExtensionMethod
+// ReSharper disable PropertyCanBeMadeInitOnly.Local
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using AM.Scripting;
+
+using Sprache;
+
+#nullable enable
+
+namespace UnitTests.Source.Scripting
+{
+    [TestClass]
+    public sealed class ResolveTest
+    {
+        [TestMethod]
+        [Description ("Знак минус")]
+        public void Resolve_Minus_1()
+        {
+            Assert.AreEqual ('-', Resolve.Minus.Parse ("-"));
+            Assert.ThrowsException<ParseException> (() => Resolve.Minus.Parse ("+"));
+        }
+
+        [TestMethod]
+        [Description ("Арабские цифры")]
+        public void Resolve_Arabic_1()
+        {
+            Assert.AreEqual ('0', Resolve.Arabic.Parse ("0"));
+            Assert.ThrowsException<ParseException> (() => Resolve.Arabic.Parse ("-"));
+        }
+
+        [TestMethod]
+        [Description ("Целое без знака")]
+        public void Resolve_UnsignedInteger_1()
+        {
+            Assert.AreEqual (0U, Resolve.UnsignedInteger.End().Parse ("0"));
+            Assert.AreEqual (123U, Resolve.UnsignedInteger.End().Parse ("123"));
+            Assert.ThrowsException<ParseException> (() => Resolve.UnsignedInteger.End().Parse ("-0"));
+        }
+
+        [TestMethod]
+        [Description ("Целое со знаком")]
+        public void Resolve_SignedInteger_1()
+        {
+            Assert.AreEqual (0, Resolve.SignedInteger.End().Parse ("0"));
+            Assert.AreEqual (123, Resolve.SignedInteger.End().Parse ("123"));
+            Assert.AreEqual (-123, Resolve.SignedInteger.End().Parse ("-123"));
+            Assert.ThrowsException<ParseException> (() => Resolve.SignedInteger.End().Parse (""));
+        }
+
+        [TestMethod]
+        [Description ("Длинное целое без знака")]
+        public void Resolve_UnsignedLong_1()
+        {
+            Assert.AreEqual (0UL, Resolve.UnsignedLong.End().Parse ("0"));
+            Assert.AreEqual (123UL, Resolve.UnsignedLong.End().Parse ("123"));
+            Assert.ThrowsException<ParseException> (() => Resolve.UnsignedLong.End().Parse ("-0"));
+        }
+
+        [TestMethod]
+        [Description ("Длинное целое со знаком")]
+        public void Resolve_SignedLong_1()
+        {
+            Assert.AreEqual (0L, Resolve.SignedLong.End().Parse ("0"));
+            Assert.AreEqual (123L, Resolve.SignedLong.End().Parse ("123"));
+            Assert.AreEqual (-123L, Resolve.SignedLong.End().Parse ("-123"));
+            Assert.ThrowsException<ParseException> (() => Resolve.SignedLong.End().Parse (""));
+        }
+
+        [TestMethod]
+        [Description ("Число с плавающей точкой одинарной точности")]
+        public void Resolve_Float_1()
+        {
+            Assert.AreEqual (0.0f, Resolve.Float.End().Parse ("0"));
+            Assert.AreEqual (1.0f, Resolve.Float.End().Parse ("1"));
+            Assert.AreEqual (-1.0f, Resolve.Float.End().Parse ("-1"));
+
+            Assert.AreEqual (0.0f, Resolve.Float.End().Parse ("0.0"));
+            Assert.AreEqual (1.0f, Resolve.Float.End().Parse ("1.0"));
+            Assert.AreEqual (-1.0f, Resolve.Float.End().Parse ("-1.0"));
+
+            Assert.AreEqual (0.1f, Resolve.Float.End().Parse ("0.1"));
+            Assert.AreEqual (1.1f, Resolve.Float.End().Parse ("1.1"));
+            Assert.AreEqual (-1.1f, Resolve.Float.End().Parse ("-1.1"));
+
+            Assert.AreEqual (1.1e3f, Resolve.Float.End().Parse ("1.1e3"));
+            Assert.AreEqual (1.1e-3f, Resolve.Float.End().Parse ("1.1e-3"));
+            Assert.AreEqual (-1.1f, Resolve.Float.End().Parse ("-1.1"));
+            Assert.AreEqual (-1.1e3f, Resolve.Float.End().Parse ("-1.1e3"));
+            Assert.AreEqual (-1.1e-3f, Resolve.Float.End().Parse ("-1.1e-3"));
+        }
+
+        [TestMethod]
+        [Description ("Число с плавающей точкой двойной точности")]
+        public void Resolve_Double_1()
+        {
+            Assert.AreEqual (0.0, Resolve.Double.End().Parse ("0"));
+            Assert.AreEqual (1.0, Resolve.Double.End().Parse ("1"));
+            Assert.AreEqual (-1.0, Resolve.Double.End().Parse ("-1"));
+
+            Assert.AreEqual (0.0, Resolve.Double.End().Parse ("0.0"));
+            Assert.AreEqual (1.0, Resolve.Double.End().Parse ("1.0"));
+            Assert.AreEqual (-1.0, Resolve.Double.End().Parse ("-1.0"));
+
+            Assert.AreEqual (0.1, Resolve.Double.End().Parse ("0.1"));
+            Assert.AreEqual (1.1, Resolve.Double.End().Parse ("1.1"));
+            Assert.AreEqual (-1.1, Resolve.Double.End().Parse ("-1.1"));
+
+            Assert.AreEqual (1.1e3, Resolve.Double.End().Parse ("1.1e3"));
+            Assert.AreEqual (1.1e-3, Resolve.Double.End().Parse ("1.1e-3"));
+            Assert.AreEqual (-1.1, Resolve.Double.End().Parse ("-1.1"));
+            Assert.AreEqual (-1.1e3, Resolve.Double.End().Parse ("-1.1e3"));
+            Assert.AreEqual (-1.1e-3, Resolve.Double.End().Parse ("-1.1e-3"));
+        }
+
+        [TestMethod]
+        [Description ("Идентификатор")]
+        public void Resolve_Identifier_1()
+        {
+            Assert.AreEqual ("a", Resolve.Identifier.End ().Parse ("a"));
+            Assert.AreEqual ("a1", Resolve.Identifier.End ().Parse ("a1"));
+            Assert.AreEqual ("_a1", Resolve.Identifier.End ().Parse ("_a1"));
+            Assert.AreEqual ("_", Resolve.Identifier.End ().Parse ("_"));
+            Assert.AreEqual ("a_1", Resolve.Identifier.End ().Parse ("a_1"));
+            Assert.AreEqual ("_1", Resolve.Identifier.End ().Parse ("_1"));
+        }
+
+    }
+}
