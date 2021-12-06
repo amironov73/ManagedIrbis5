27b3e0df0d396fee35a037ad561bc4ef9e51945a
Alexey Mironov <amironov73@gmail.com>
2021-12-06 16:25:13 +0800

    Add ViafSuggester application

---
+++ Source/Apps/ViafSuggester/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable CoVariantArrayConversion
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable LocalizableElement
+// ReSharper disable StringLiteralTypo
+
+/* App.cs -- точка входа в приложение
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System.Windows;
+
+#endregion
+
+#nullable enable
+
+namespace ViafSuggester
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Source/Apps/ViafSuggester/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable CoVariantArrayConversion
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable LocalizableElement
+// ReSharper disable StringLiteralTypo
+
+/* MainWindows.cs -- главное окно приложения
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
+using System.Linq;
+using System.Windows;
+
+using RestfulIrbis.Viaf;
+
+#endregion
+
+#nullable enable
+
+namespace ViafSuggester
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GoButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            resultBox.Clear();
+            var name = termBox.Text.Trim();
+            if (string.IsNullOrEmpty(name))
+            {
+                return;
+            }
+
+            try
+            {
+                var client = new ViafClient();
+                var suggestions = client.GetSuggestions(name);
+                var text = string.Join
+                    (
+                        Environment.NewLine,
+                        suggestions.Select(s => s.DisplayForm)
+                            .OrderBy(s => s)
+                    );
+                resultBox.Text = text;
+            }
+            catch (Exception exception)
+            {
+                resultBox.Text = exception.ToString();
+            }
+        }
+    }
+}
