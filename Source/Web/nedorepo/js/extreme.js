d1f1339ba50785c11ac98d5411ab21b703e9af3a
Alexey Mironov <amironov73@gmail.com>
2021-12-07 14:53:27 +0800

    Working on nedorepo

--- Source/Web/nedorepo/js/extreme.js
+++ Source/Web/nedorepo/js/extreme.js
@@ -1,36 +1,107 @@
+const prefixes = [
+    {prefix: 'A=', text: 'Автор'},
+    {prefix: 'T=', text: 'Заглавие'},
+    {prefix: 'MR=', text: 'Кафедра'},
+]
+
 let theData = {
+    prefix1: '',
     input1: 'Начальное значение',
+    check1: true,
     input2: 'Тоже начальное',
-    input3: 'И здесь'
+    value2: '',
+    check2: true,
+    input3: 'И здесь',
+    value3: '',
+    check3: false
 }
 
 $(function () {
 
-    const form = $('#form').dxForm ({
+    const form = $('#form').dxForm({
         formData: theData,
-        items: [{
-            dataField: 'input1'
-        }, {
-            dataField: 'input2'
-        }, {
-            dataField: 'input3'
-        }
+        items: [
+            {
+                itemType: 'group',
+                colCount: 3,
+                items: [{
+                    label: {visible: false},
+                    dataField: 'prefix1',
+                    editorType: 'dxSelectBox',
+                    editorOptions: {
+                        valueExpr: 'prefix',
+                        displayExpr: 'text',
+                        value: prefixes[0].prefix,
+                        dataSource: prefixes
+                    }
+                }, {
+                    label: {visible: false},
+                    dataField: 'input1'
+                }, {
+                    label: {visible: false},
+                    dataField: 'check1',
+                }]
+            },
+
+            {
+                itemType: 'group',
+                colCount: 3,
+                items: [{
+                    label: {visible: false},
+                    dataField: 'prefix2',
+                    editorType: 'dxSelectBox',
+                    editorOptions: {
+                        valueExpr: 'prefix',
+                        displayExpr: 'text',
+                        value: prefixes[1].prefix,
+                        dataSource: prefixes
+                    }
+                }, {
+                    label: {visible: false},
+                    dataField: 'input2'
+                }, {
+                    label: {visible: false},
+                    dataField: 'check2',
+                }]
+            },
+
+            {
+                itemType: 'group',
+                colCount: 3,
+                items: [{
+                    label: {visible: false},
+                    dataField: 'prefix3',
+                    editorType: 'dxSelectBox',
+                    editorOptions: {
+                        valueExpr: 'prefix',
+                        displayExpr: 'text',
+                        value: prefixes[2].prefix,
+                        dataSource: prefixes
+                    }
+                }, {
+                    label: {visible: false},
+                    dataField: 'input3'
+                }, {
+                    label: {visible: false},
+                    dataField: 'check3',
+                }]
+            }
 
         ]
     }).dxForm('instance')
 
-    const button = $('#show-button').dxButton ({
+    const button = $('#show-button').dxButton({
         type: 'success',
         text: 'Нажми меня',
         onClick: function (e) {
             let r = form.option('formData')
-            alert (r)
+            //alert (r)
             result.option('value', JSON.stringify(r))
         }
     })
 
-    const result = $('#result').dxTextBox ({
+    const result = $('#result').dxTextBox({
         value: 'Пока ничего не вычислено'
-    }).dxTextBox ('instance')
+    }).dxTextBox('instance')
 
 });
