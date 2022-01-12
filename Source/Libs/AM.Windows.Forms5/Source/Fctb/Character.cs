adaaa037d46d872f8315083d3f3f225da823bd19
Alexey Mironov <amironov73@gmail.com>
2022-01-12 17:17:03 +0800

    Add Fctb subproject

---
+++ Source/Libs/AM.Windows.Forms5/Source/Fctb/Character.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Fctb
+{
+    /// <summary>
+    /// Char and style
+    /// </summary>
+    public struct Character
+    {
+        /// <summary>
+        /// Unicode character
+        /// </summary>
+        public char c;
+        /// <summary>
+        /// Style bit mask
+        /// </summary>
+        /// <remarks>Bit 1 in position n means that this char will rendering by FastColoredTextBox.Styles[n]</remarks>
+        public StyleIndex style;
+
+        public Character(char c)
+        {
+            this.c = c;
+            style = StyleIndex.None;
+        }
+    }
+}
