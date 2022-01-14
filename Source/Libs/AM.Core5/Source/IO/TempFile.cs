c36e6053e342544ce5b403b844176f979ecb7f73
Alexey Mironov <amironov73@gmail.com>
2022-01-14 10:25:14 +0800

    Add TempFile and TimeMeasure classes

---
+++ Source/Libs/AM.Core5/Source/IO/TempFile.cs
@@ -0,0 +1,77 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable UnusedMember.Global
+
+/* TempFile.cs -- временный файл
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
+using System.IO;
+
+#endregion
+
+#nullable enable
+
+namespace AM.IO;
+
+/// <summary>
+/// Временный файл, удаляемый сразу после того, как стал ненужным.
+/// </summary>
+public sealed class TempFile
+    : IDisposable
+{
+    #region Properties
+
+    /// <summary>
+    /// Поток.
+    /// </summary>
+    public Stream Stream { get; }
+
+    /// <summary>
+    /// Полный путь ко временному файлу.
+    /// </summary>
+    public string FullPath { get; }
+
+    #endregion
+
+    #region Construction
+
+    /// <summary>
+    /// Конструктор.
+    /// </summary>
+    public TempFile()
+    {
+        FullPath = Path.GetTempFileName();
+        Stream = File.Create (FullPath);
+    }
+
+    #endregion
+
+    #region IDisposable members
+
+    /// <inheritdoc cref="IDisposable.Dispose"/>
+    public void Dispose()
+    {
+        Stream.Dispose();
+        File.Delete (FullPath);
+    }
+
+    #endregion
+
+    #region Objecte members
+
+    /// <inheritdoc cref="object.ToString"/>
+    public override string ToString()
+    {
+        return FullPath;
+    }
+
+    #endregion
+}
---
+++ Source/Libs/AM.Core5/Source/TimeMeasure.cs
@@ -0,0 +1,76 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedMember.Global
+
+/* TimeMeasure.cs -- измерение продолжительности операции
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
+using System.Diagnostics;
+
+#endregion
+
+#nullable enable
+
+namespace AM;
+
+/// <summary>
+/// Измерение продолжительности операции.
+/// </summary>
+public sealed class TimeMeasure
+    : IDisposable
+{
+    #region Properties
+
+    /// <inheritdoc cref="Stopwatch.ElapsedTicks"/>
+    public long ElapsedTicks => _stopwatch.ElapsedTicks;
+
+    /// <inheritdoc cref="Stopwatch.ElapsedMilliseconds"/>
+    public long ElapsedMilliseconds => _stopwatch.ElapsedMilliseconds;
+
+
+    /// <inheritdoc cref="Stopwatch.Elapsed"/>
+    public TimeSpan Elapsed => _stopwatch.Elapsed;
+
+
+    #endregion
+
+    #region Construction
+
+    /// <summary>
+    /// Конструктор.
+    /// </summary>
+    public TimeMeasure()
+    {
+        _stopwatch.Start();
+    }
+
+    #endregion
+
+    #region Private members
+
+    private readonly Stopwatch _stopwatch = new();
+
+    #endregion
+
+    #region IDisposable members
+
+    /// <inheritdoc cref="IDisposable.Dispose"/>
+    public void Dispose()
+    {
+        if (_stopwatch.IsRunning)
+        {
+            _stopwatch.Stop();
+        }
+    }
+
+    #endregion
+}
