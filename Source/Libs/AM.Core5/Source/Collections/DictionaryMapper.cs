0da5c26d725cfa44032543fbba796abd7ed92a40
Alexey Mironov <amironov73@gmail.com>
2022-01-19 12:10:15 +0800

    Add DictionaryMapper class

---
+++ Source/Libs/AM.Core5/Source/Collections/DictionaryMapper.cs
@@ -0,0 +1,96 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+
+/* DictionaryMapper.cs -- отображает элементы словаря на свойства объекта и обратно
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System.Collections;
+using System.Reflection;
+
+#endregion
+
+#nullable enable
+
+namespace AM.Collections;
+
+/// <summary>
+/// Отображает элементы словаря на свойства объекта и обратно.
+/// </summary>
+public static class DictionaryMapper
+{
+    #region Constants
+
+    private const BindingFlags Flags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.IgnoreCase;
+
+    #endregion
+
+    #region Public methods
+
+    /// <summary>
+    /// Отображение элементов словаря на свойства объекта.
+    /// </summary>
+    public static void FromObject
+        (
+            object source,
+            IDictionary target
+        )
+    {
+        Sure.NotNull (target);
+        Sure.NotNull (source);
+
+        var properties = source.GetType().GetProperties (Flags);
+        foreach (var property in properties)
+        {
+            var indexParameters = property.GetIndexParameters();
+            if (indexParameters.Length == 0)
+            {
+                var value = property.GetValue (source);
+                target[property.Name] = value;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Отображение элементов словаря на свойства объекта.
+    /// </summary>
+    public static void ToObject
+        (
+            IDictionary source,
+            object target
+        )
+    {
+        Sure.NotNull (source);
+        Sure.NotNull (target);
+
+        var type = target.GetType();
+        foreach (DictionaryEntry entry in source)
+        {
+            var name = entry.Key as string;
+            if (string.IsNullOrEmpty (name))
+            {
+                continue;
+            }
+
+            var property = type.GetProperty (name, Flags);
+            if (property is null)
+            {
+                continue;
+            }
+
+            var indexParameters = property.GetIndexParameters();
+            if (indexParameters.Length == 0)
+            {
+                property.SetValue (target, entry.Value);
+            }
+        }
+    }
+
+    #endregion
+}
---
+++ Source/Tests/UnitTests/Source/AM/Collections/DictionaryMapperTest.cs
@@ -0,0 +1,127 @@
+﻿// ReSharper disable CheckNamespace
+// ReSharper disable StringLiteralTypo
+
+using System.Collections.Generic;
+
+using AM;
+using AM.Collections;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+#nullable enable
+
+namespace UnitTests.AM.Collections;
+
+[TestClass]
+public sealed class DictionaryMapperTest
+{
+    /// <summary>
+    /// Подопытный класс.
+    /// </summary>
+    internal sealed class Person
+    {
+        /// <summary>
+        /// Индексеры должны пропускаться.
+        /// </summary>
+        public object? this [int index]
+        {
+            get => null;
+            set => value.NotUsed();
+        }
+
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public string? Address { get; set; }
+    }
+
+    [TestMethod]
+    [Description ("Простой случай")]
+    public void DictionaryMapper_FromObject_1()
+    {
+        var person = new Person()
+        {
+            Name = "Alexey Mironov",
+            Age = 48,
+            Address = "Irkuts"
+        };
+        var dictionary = new Dictionary<string, object?>();
+
+        DictionaryMapper.FromObject (person, dictionary);
+        Assert.AreEqual (person.Name, dictionary["Name"]);
+        Assert.AreEqual (person.Age, dictionary["Age"]);
+        Assert.AreEqual (person.Address, dictionary["Address"]);
+    }
+
+    [TestMethod]
+    [Description ("Простой случай")]
+    public void DictionaryMapper_ToObject_1()
+    {
+        var dictionary = new Dictionary<string, object?>
+        {
+            { "Name", "Alexey Mironov" },
+            { "Age", 48 },
+            { "Address", "Irkutsk" }
+        };
+        var person = new Person();
+
+        DictionaryMapper.ToObject (dictionary, person);
+        Assert.AreEqual (dictionary["Name"], person.Name);
+        Assert.AreEqual (dictionary["Age"], person.Age);
+        Assert.AreEqual (dictionary["Address"], person.Address);
+    }
+
+    [TestMethod]
+    [Description ("С точностью до регистра символов")]
+    public void DictionaryMapper_ToObject_2()
+    {
+        var dictionary = new Dictionary<string, object?>
+        {
+            { "name", "Alexey Mironov" },
+            { "age", 48 },
+            { "address", "Irkutsk" }
+        };
+        var person = new Person();
+
+        DictionaryMapper.ToObject (dictionary, person);
+        Assert.AreEqual (dictionary["name"], person.Name);
+        Assert.AreEqual (dictionary["age"], person.Age);
+        Assert.AreEqual (dictionary["address"], person.Address);
+    }
+
+    [TestMethod]
+    [Description ("Отсутствующие совйства")]
+    public void DictionaryMapper_ToObject_3()
+    {
+        var dictionary = new Dictionary<string, object?>
+        {
+            { "Name", "Alexey Mironov" },
+            { "Age", 48 },
+            { "Address", "Irkutsk" },
+            { "NoSuchProperty", "He-he" }
+        };
+        var person = new Person();
+
+        DictionaryMapper.ToObject (dictionary, person);
+        Assert.AreEqual (dictionary["Name"], person.Name);
+        Assert.AreEqual (dictionary["Age"], person.Age);
+        Assert.AreEqual (dictionary["Address"], person.Address);
+    }
+
+    [TestMethod]
+    [Description ("Ключ - не строка")]
+    public void DictionaryMapper_ToObject_4()
+    {
+        var dictionary = new Dictionary<int, object?>
+        {
+            { 1, "Alexey Mironov" },
+            { 2, 48 },
+            { 3, "Irkutsk" }
+        };
+        var person = new Person();
+
+        DictionaryMapper.ToObject (dictionary, person);
+        Assert.IsNull (person.Name);
+        Assert.AreEqual (0, person.Age);
+        Assert.IsNull (person.Address);
+    }
+}
