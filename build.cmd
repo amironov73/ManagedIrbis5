4bd2ea93ddbbb1653ec1bb30f65ad4266a506347
Alexey Mironov <amironov73@gmail.com>
2022-01-26 16:34:55 +0800

    Fighting with dotnet pack

--- build.cmd
+++ build.cmd
@@ -1,17 +1,7 @@
 @echo off
 
-set BUILD_VERSION=5.0.0.657
-
 if exist NuGet   rmdir /s /q NuGet
 if exist Publish rmdir /s /q Publish
-
-dotnet clean                                        Source/Utils/Utils.sln
-dotnet restore                                      Source/Utils/Utils.sln
-dotnet build   --no-restore --configuration Release Source/Utils/Utils.sln
-
-echo VERSION %BUILD_VERSION%
-dotnet run --no-restore --no-build --configuration Release --project Source\Utils\PatchVersion\PatchVersion.csproj Source\Common\ArsMagna.targets %BUILD_VERSION%
-type Source\Common\ArsMagna.targets
 
 dotnet clean   Source/ManagedIrbis5-windows.sln
 dotnet clean   Source/TinyClient.sln
@@ -19,11 +9,11 @@ dotnet clean   Source/TinyClient.sln
 dotnet restore Source/ManagedIrbis5-windows.sln
 dotnet restore Source/TinyClient.sln
 
-dotnet build   --no-restore            --configuration Release -p:Version=%BUILD_VERSION%       Source/ManagedIrbis5-windows.sln
-dotnet build   --no-restore            --configuration Release -p:Version=%BUILD_VERSION%       Source/TinyClient.sln
+dotnet build   --no-restore            --configuration Release Source/ManagedIrbis5-windows.sln
+dotnet build   --no-restore            --configuration Release Source/TinyClient.sln
 
-dotnet pack    --no-restore --no-build --configuration Release -p:PackageVersion=%BUILD_VERSION% --include-symbols --include-source --output NuGet Source/ManagedIrbis5-publish.sln
-dotnet pack    --no-restore --no-build --configuration Release -p:PackageVersion=%BUILD_VERSION% --include-symbols --include-source --output NuGet Source/Libs/TinyClient/TinyClient.csproj
+dotnet pack    --no-restore --no-build --configuration Release --include-symbols --include-source --output NuGet Source/ManagedIrbis5-publish.sln
+dotnet pack    --no-restore --no-build --configuration Release --include-symbols --include-source --output NuGet Source/Libs/TinyClient/TinyClient.csproj
 
 dotnet publish --no-restore --no-build --configuration Release --output Publish Source/ManagedIrbis5-publish.sln
 dotnet publish --no-restore --no-build --configuration Release --output Publish Source/Libs/TinyClient/TinyClient.csproj
