d8acce908f6f009d8cdc9b75d06f57e6dd87f896
Alexey Mironov <amironov73@gmail.com>
2022-03-04 15:38:49 +0800

    Add MssqlScripter class with tests

---
+++ Source/Libs/AM.Core5/Source/Data/MssqlScripter.cs
@@ -0,0 +1,375 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable StringLiteralTypo
+
+/* MssqlScripter.cs -- генерирует базу данных по ее описанию
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
+using System.IO;
+
+#endregion
+
+#nullable enable
+
+/*
+    Типы данных в SQL Server объединены в следующие категории:
+
+    * Точные числа;
+    * Приблизительные числа;
+    * Символьные строки;
+    * Символьные строки в Юникоде;
+    * Дата и время;
+    * Двоичные данные;
+    * Прочие типы данных.
+
+    Точные числа:
+
+    * int        - 4 байта (применяем по умолчанию);
+    * tinyint    - 1 байт;
+    * smallint   - 2 байта;
+    * bigint     - 8 байт;
+    * decimal    - в зависимости от запрошенной точности от 5 до 17 байт, по умолчанию 9 байт;
+    * numeric    - алиас для decimal;
+    * bit        - 1 бит;
+    * money      - 8 байт;
+    * smallmoney - 4 байта.
+
+    Приблизительные числа:
+
+    * float - в зависимости от запрошенной точности от 4 до 8 байт, по умолчанию 8 байт;
+    * real  - всегда 4 байта.
+
+    Дата и время:
+
+    * date           - 3 байта;
+    * time           - 5 байт;
+    * datetime       - 8 байт;
+    * datetime2      - от 6 до 8 байт в зависимости от заданной точности;
+    * datetimeoffset - 10 байт;
+    * smalldatetime  - 4 байта.
+
+    Символьные строки:
+
+    * char    - от 1 до 8000 символов;
+    * varchar - от 1 до 8000 символов, переменная длина;
+    * text    - переменная длина.
+
+    Символьные строки в Юникоде:
+
+    * nchar    - от 1 до 4000 символов;
+    * nvarchar - от 1 до 4000 символов, переменная длина;
+    * ntext    - переменная длина.
+
+    Двоичные данные:
+
+    * binary    - от 1 до 8000 байт;
+    * varbinary - от 1 до 8000 байт, переменная длина;
+    * image     - переменная длина.
+
+    Прочие типы данных:
+
+    * rowversion       - 8 байт;
+    * uniqueidentifier - 16 байт (GUID);
+    * sql_variant      - до 8000 байт;
+    * xml              - до 2 Гб.
+
+ */
+
+namespace AM.Data;
+
+/// <summary>
+/// Генерирует базу данных по ее описанию.
+/// </summary>
+public sealed class MssqlScripter
+{
+    #region Properties
+
+    /// <summary>
+    /// Владелец.
+    /// </summary>
+    public string Owner { get; set; }= "[dbo]";
+
+    /// <summary>
+    /// Создавать, только если не существует.
+    /// </summary>
+    public bool IfNotExist { get; set; }
+
+    /// <summary>
+    /// Удалять, если существует.
+    /// </summary>
+    public bool DropIfExist { get; set; }
+
+    #endregion
+
+    #region Private members
+
+    private void _Go
+        (
+            TextWriter output
+        )
+    {
+        output.WriteLine();
+        output.WriteLine ("GO");
+    }
+
+    private void _Indent
+        (
+            TextWriter output,
+            int width = 4
+        )
+    {
+        for (var i = 0; i < width; i++)
+        {
+            output.Write (' ');
+        }
+    }
+
+    #endregion
+
+    #region Public methods
+
+    /// <summary>
+    /// Генерация базы данных по ее описанию.
+    /// </summary>
+    /// <param name="output">Куда помещать результат.</param>
+    /// <param name="database">Описание базы данных.</param>
+    public void GenerateDatabase
+        (
+            TextWriter output,
+            DatabaseDescriptor database
+        )
+    {
+        Sure.NotNull (output);
+        Sure.VerifyNotNull (database);
+
+        if (DropIfExist)
+        {
+            output.WriteLine ($"drop database if exists [{database.Name}]");
+            _Go (output);
+        }
+        else if (IfNotExist)
+        {
+            output.WriteLine ($"if not exists (select * from sys.databases where name='{database.Name}')");
+        }
+
+        output.WriteLine ($"create database [{database.Name}]");
+        _Go (output);
+        output.WriteLine();
+        output.WriteLine ($"use [{database.Name}]");
+        _Go (output);
+        output.WriteLine();
+
+        var first = true;
+        foreach (var table in database.Tables.ThrowIfNull())
+        {
+            if (!first)
+            {
+                output.WriteLine();
+            }
+
+            GenerateTable (output, table);
+            first = false;
+        }
+    }
+
+    /// <summary>
+    /// Генерация поля таблицы.
+    /// </summary>
+    /// <param name="output">Куда помещать результат.</param>
+    /// <param name="field">Описание поля таблицы.</param>
+    public void GenerateField
+        (
+            TextWriter output,
+            FieldDescriptor field
+        )
+    {
+        Sure.NotNull (output);
+        Sure.VerifyNotNull (field);
+
+        _Indent (output);
+        var fieldName = "[" + field.Name + "]";
+        output.Write ($"{fieldName, -16} ");
+
+        string typespec;
+        switch (field.Type)
+        {
+            case DataType.Boolean:
+                typespec = "[bit]";
+                break;
+
+            case DataType.Integer:
+                switch (field.Length)
+                {
+                    case 0:
+                    case 4:
+                        typespec = "[int]";
+                        break;
+
+                    case 1:
+                        typespec = "[tinyint]";
+                        break;
+
+                    case 2:
+                        typespec = "[smallint]";
+                        break;
+
+                    case 8:
+                        typespec = "[bigint]";
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException (nameof (field.Length));
+                }
+
+                break;
+
+            case DataType.Money:
+                typespec = "[money]";
+                break;
+
+            case DataType.Text:
+                if (field.Length < 0)
+                {
+                    throw new ArgumentOutOfRangeException (nameof (field.Length));
+                }
+                else if (field.Length == 0)
+                {
+                    typespec = "[ntext]";
+                }
+                else
+                {
+                    typespec = $"[nvarchar]({field.Length})";
+                }
+                break;
+
+            case DataType.Date:
+                switch (field.Length)
+                {
+                    case 0:
+                    case 8:
+                        typespec = "[datetime]";
+                        break;
+
+                    case 4:
+                        typespec = "[smalldatetime]";
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException (nameof (field.Length));
+                }
+                break;
+
+            default:
+                throw new ArgumentOutOfRangeException (nameof (field.Type));
+        }
+
+        output.Write ($"{typespec, -16} ");
+
+        if (field.Identity)
+        {
+            output.Write ("identity ");
+        }
+
+        if (field.PrimaryKey)
+        {
+            output.Write ("primary key ");
+        }
+        else
+        {
+            if (field.Unique)
+            {
+                output.Write ("unique ");
+            }
+            else
+            {
+                if (field.Indexed)
+                {
+                    output.Write ($"index [IX_{field.Name}] ");
+                }
+            }
+        }
+
+        output.Write (field.Nullable ? "NULL" : "NOT NULL");
+
+    }
+
+    /// <summary>
+    /// Генерация индекса.
+    /// </summary>
+    /// <param name="output">Куда помещать результат.</param>
+    /// <param name="index">Описание индекса.</param>
+    public void GenerateIndex
+        (
+            TextWriter output,
+            IndexDescriptor index
+        )
+    {
+        Sure.NotNull (output);
+        Sure.VerifyNotNull (index);
+
+        // TODO implement
+    }
+
+    /// <summary>
+    /// Генерация таблицы по ее описанию.
+    /// </summary>
+    /// <param name="output">Куда помещать результат.</param>
+    /// <param name="table">Описание таблицы.</param>
+    public void GenerateTable
+        (
+            TextWriter output,
+            TableDescriptor table
+        )
+    {
+        Sure.NotNull (output);
+        Sure.VerifyNotNull (table);
+
+        if (DropIfExist)
+        {
+            output.WriteLine ($"drop table if exists [{table.Name}]");
+        }
+        else if (IfNotExist)
+        {
+            output.WriteLine ($"if not exists (select * from sysobjects where name='{table.Name}' and xtype='U')");
+        }
+
+        output.WriteLine ($"create table {Owner}.[{table.Name}]");
+        output.WriteLine("(");
+        var first = true;
+        foreach (var field in table.Fields.ThrowIfNull())
+        {
+            if (!first)
+            {
+                output.WriteLine (",");
+            }
+
+            GenerateField (output, field);
+            first = false;
+        }
+
+        if (table.Indexes is not null)
+        {
+            output.WriteLine (",");
+            output.WriteLine();
+            foreach (var index in table.Indexes)
+            {
+                GenerateIndex (output, index);
+            }
+        }
+
+        output.WriteLine();
+        output.WriteLine(")");
+
+        _Go (output);
+    }
+
+    #endregion
+}
---
+++ Source/Tests/UnitTests/Source/AM/Data/MssqlScripterTest.cs
@@ -0,0 +1,365 @@
+﻿// ReSharper disable CheckNamespace
+// ReSharper disable IdentifierTypo
+// ReSharper disable StringLiteralTypo
+
+using System.IO;
+
+using AM;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using AM.Data;
+using AM.Text;
+
+#nullable enable
+
+namespace UnitTests.AM.Data;
+
+[TestClass]
+public sealed class MssqlScripterTest
+{
+    private void _Field
+        (
+            FieldDescriptor field,
+            string expected
+        )
+    {
+        var scripter = new MssqlScripter();
+        var output = new StringWriter();
+        scripter.GenerateField (output, field);
+        var actual = output.ToString();
+        Assert.AreEqual (expected, actual);
+    }
+
+    private void _Table
+        (
+            TableDescriptor table,
+            string expected,
+            bool ifNotExist = false,
+            bool dropIfExist = false
+        )
+    {
+        var scripter = new MssqlScripter
+        {
+            IfNotExist = ifNotExist,
+            DropIfExist = dropIfExist
+        };
+        var output = new StringWriter();
+        scripter.GenerateTable (output, table);
+        var actual = output.ToString().DosToUnix();
+        Assert.AreEqual (expected, actual);
+    }
+
+    private void _Database
+        (
+            DatabaseDescriptor database,
+            string expected,
+            bool ifNotExist = false,
+            bool dropIfExist = false
+        )
+    {
+        var scripter = new MssqlScripter
+        {
+            IfNotExist = ifNotExist,
+            DropIfExist = dropIfExist
+        };
+        var output = new StringWriter();
+        scripter.GenerateDatabase (output, database);
+        var actual = output.ToString().DosToUnix();
+        Assert.AreEqual (expected, actual);
+    }
+
+    private TableDescriptor _ReaderTable()
+    {
+        return new TableDescriptor()
+        {
+            Name = "readers",
+            Fields = new FieldDescriptor[]
+            {
+                new()
+                {
+                    Name = "ticket",
+                    Type = DataType.Text,
+                    Length = 50,
+                    PrimaryKey = true
+                },
+
+                new()
+                {
+                    Name = "name",
+                    Type = DataType.Text,
+                    Length = 50,
+                    Indexed = true
+                },
+
+                new()
+                {
+                    Name = "category",
+                    Type = DataType.Text,
+                    Length = 10,
+                    Indexed = true
+                }
+            }
+        };
+    }
+
+    private DatabaseDescriptor _Kladovka()
+    {
+        return new DatabaseDescriptor
+        {
+            Name = "kladovka",
+            Tables = new []
+            {
+                _BookTable(),
+                _ReaderTable()
+            }
+        };
+    }
+
+    private TableDescriptor _BookTable()
+    {
+        return new TableDescriptor
+        {
+            Name = "books",
+            Fields = new FieldDescriptor[]
+            {
+                new()
+                {
+                    Name = "id",
+                    Type = DataType.Integer,
+                    Identity = true,
+                    PrimaryKey = true
+                },
+
+                new()
+                {
+                    Name = "catalog",
+                    Type = DataType.Text,
+                    Length = 50,
+                    Indexed = true
+                },
+
+                new()
+                {
+                    Name = "number",
+                    Type = DataType.Text,
+                    Length = 50,
+                    Unique = true
+                },
+
+                new()
+                {
+                    Name = "card",
+                    Type = DataType.Text,
+                    Length = 50
+                },
+
+                new()
+                {
+                    Name = "moment",
+                    Type = DataType.Date,
+                    Indexed = true
+                },
+
+                new()
+                {
+                    Name = "deadline",
+                    Type = DataType.Date,
+                    Length = 4
+                },
+
+                new()
+                {
+                    Name = "prolong",
+                    Type = DataType.Integer
+                },
+
+                new()
+                {
+                    Name = "pilot",
+                    Type = DataType.Boolean
+                },
+
+                new()
+                {
+                    Name = "price",
+                    Type = DataType.Money
+                }
+            }
+        };
+    }
+
+    [TestMethod]
+    public void MssqlScripter_GenerateField_1()
+    {
+        var field = new FieldDescriptor
+        {
+            Name = "id",
+            Type = DataType.Integer,
+            Identity = true,
+            Nullable = false,
+            PrimaryKey = true
+        };
+        _Field (field, "    [id]             [int]            identity primary key NOT NULL");
+    }
+
+    [TestMethod]
+    public void MssqlScripter_GenerateField_2()
+    {
+        var field = new FieldDescriptor
+        {
+            Name = "catalog",
+            Type = DataType.Text,
+            Length = 50,
+            Indexed = true
+        };
+        _Field (field, "    [catalog]        [nvarchar](50)   index [IX_catalog] NOT NULL");
+    }
+
+    [TestMethod]
+    public void MssqlScripter_GenerateField_3()
+    {
+        var field = new FieldDescriptor
+        {
+            Name = "number",
+            Type = DataType.Text,
+            Length = 50,
+            Indexed = true,
+            Nullable = true
+        };
+        _Field (field, "    [number]         [nvarchar](50)   index [IX_number] NULL");
+    }
+
+    [TestMethod]
+    public void MssqlScripter_GenerateField_4()
+    {
+        var field = new FieldDescriptor
+        {
+            Name = "moment",
+            Type = DataType.Date,
+            Length = 4,
+            Nullable = true
+        };
+        _Field (field, "    [moment]         [smalldatetime]  NULL");
+    }
+
+    [TestMethod]
+    public void MssqlScripter_GenerateField_5()
+    {
+        var field = new FieldDescriptor
+        {
+            Name = "moment",
+            Type = DataType.Date,
+            Nullable = true
+        };
+        _Field (field, "    [moment]         [datetime]       NULL");
+    }
+
+    [TestMethod]
+    public void MssqlScripter_GenerateField_6()
+    {
+        var field = new FieldDescriptor
+        {
+            Name = "pilot",
+            Type = DataType.Boolean,
+            Nullable = true
+        };
+        _Field (field, "    [pilot]          [bit]            NULL");
+    }
+
+    [TestMethod]
+    public void MssqlScripter_GenerateField_7()
+    {
+        var field = new FieldDescriptor
+        {
+            Name = "price",
+            Type = DataType.Money,
+            Nullable = true
+        };
+        _Field (field, "    [price]          [money]          NULL");
+    }
+
+    [TestMethod]
+    [ExpectedException (typeof (VerificationException))]
+    public void MssqlScripter_GenerateField_8()
+    {
+        var field = new FieldDescriptor
+        {
+            Type = DataType.Money,
+            Nullable = true
+        };
+        _Field (field, "");
+    }
+
+    [TestMethod]
+    public void MssqlScripter_GenerateTable_1()
+    {
+        var table = _BookTable();
+        var expected = "create table [dbo].[books]\n(\n    [id]             [int]            identity primary key NOT NULL,\n    [catalog]        [nvarchar](50)   index [IX_catalog] NOT NULL,\n    [number]         [nvarchar](50)   unique NOT NULL,\n    [card]           [nvarchar](50)   NOT NULL,\n    [moment]         [datetime]       index [IX_moment] NOT NULL,\n    [deadline]       [smalldatetime]  NOT NULL,\n    [prolong]        [int]            NOT NULL,\n    [pilot]          [bit]            NOT NULL,\n    [price]          [money]          NOT NULL\n)\n\nGO\n";
+        _Table (table, expected);
+    }
+
+    [TestMethod]
+    public void MssqlScripter_GenerateTable_2()
+    {
+        var table = _ReaderTable();
+        var expected = "create table [dbo].[readers]\n(\n    [ticket]         [nvarchar](50)   primary key NOT NULL,\n    [name]           [nvarchar](50)   index [IX_name] NOT NULL,\n    [category]       [nvarchar](10)   index [IX_category] NOT NULL\n)\n\nGO\n";
+        _Table (table, expected);
+    }
+
+    [TestMethod]
+    public void MssqlScripter_GenerateTable_3()
+    {
+        var table = _ReaderTable();
+        var expected = "create table [dbo].[readers]\n(\n    [ticket]         [nvarchar](50)   primary key NOT NULL,\n    [name]           [nvarchar](50)   index [IX_name] NOT NULL,\n    [category]       [nvarchar](10)   index [IX_category] NOT NULL\n)\n\nGO\n";
+        _Table (table, expected);
+    }
+
+    [TestMethod]
+    public void MssqlScripter_GenerateTable_4()
+    {
+        var table = _ReaderTable();
+        var expected = "drop table if exists [readers]\ncreate table [dbo].[readers]\n(\n    [ticket]         [nvarchar](50)   primary key NOT NULL,\n    [name]           [nvarchar](50)   index [IX_name] NOT NULL,\n    [category]       [nvarchar](10)   index [IX_category] NOT NULL\n)\n\nGO\n";
+        _Table (table, expected, dropIfExist: true);
+    }
+
+    [TestMethod]
+    public void MssqlScripter_GenerateTable_5()
+    {
+        var table = _ReaderTable();
+        var expected = "if not exists (select * from sysobjects where name='readers' and xtype='U')\ncreate table [dbo].[readers]\n(\n    [ticket]         [nvarchar](50)   primary key NOT NULL,\n    [name]           [nvarchar](50)   index [IX_name] NOT NULL,\n    [category]       [nvarchar](10)   index [IX_category] NOT NULL\n)\n\nGO\n";
+        _Table (table, expected, true);
+    }
+
+    [TestMethod]
+    [ExpectedException (typeof (VerificationException))]
+    public void MssqlScripter_GenerateTable_6()
+    {
+        var table = new TableDescriptor();
+        _Table (table, "");
+    }
+
+    [TestMethod]
+    public void MssqlScripter_GenerateDatabase_1()
+    {
+        var db = _Kladovka();
+        var expected = "create database [kladovka]\n\nGO\n\nuse [kladovka]\n\nGO\n\ncreate table [dbo].[books]\n(\n    [id]             [int]            identity primary key NOT NULL,\n    [catalog]        [nvarchar](50)   index [IX_catalog] NOT NULL,\n    [number]         [nvarchar](50)   unique NOT NULL,\n    [card]           [nvarchar](50)   NOT NULL,\n    [moment]         [datetime]       index [IX_moment] NOT NULL,\n    [deadline]       [smalldatetime]  NOT NULL,\n    [prolong]        [int]            NOT NULL,\n    [pilot]          [bit]            NOT NULL,\n    [price]          [money]          NOT NULL\n)\n\nGO\n\ncreate table [dbo].[readers]\n(\n    [ticket]         [nvarchar](50)   primary key NOT NULL,\n    [name]           [nvarchar](50)   index [IX_name] NOT NULL,\n    [category]       [nvarchar](10)   index [IX_category] NOT NULL\n)\n\nGO\n";
+        _Database (db, expected);
+    }
+
+    [TestMethod]
+    public void MssqlScripter_GenerateDatabase_2()
+    {
+        var db = _Kladovka();
+        var expected = "drop database if exists [kladovka]\n\nGO\ncreate database [kladovka]\n\nGO\n\nuse [kladovka]\n\nGO\n\ndrop table if exists [books]\ncreate table [dbo].[books]\n(\n    [id]             [int]            identity primary key NOT NULL,\n    [catalog]        [nvarchar](50)   index [IX_catalog] NOT NULL,\n    [number]         [nvarchar](50)   unique NOT NULL,\n    [card]           [nvarchar](50)   NOT NULL,\n    [moment]         [datetime]       index [IX_moment] NOT NULL,\n    [deadline]       [smalldatetime]  NOT NULL,\n    [prolong]        [int]            NOT NULL,\n    [pilot]          [bit]            NOT NULL,\n    [price]          [money]          NOT NULL\n)\n\nGO\n\ndrop table if exists [readers]\ncreate table [dbo].[readers]\n(\n    [ticket]         [nvarchar](50)   primary key NOT NULL,\n    [name]           [nvarchar](50)   index [IX_name] NOT NULL,\n    [category]       [nvarchar](10)   index [IX_category] NOT NULL\n)\n\nGO\n";
+        _Database (db, expected, dropIfExist: true);
+    }
+
+    [TestMethod]
+    public void MssqlScripter_GenerateDatabase_3()
+    {
+        var db = _Kladovka();
+        var expected = "if not exists (select * from sys.databases where name='kladovka')\ncreate database [kladovka]\n\nGO\n\nuse [kladovka]\n\nGO\n\nif not exists (select * from sysobjects where name='books' and xtype='U')\ncreate table [dbo].[books]\n(\n    [id]             [int]            identity primary key NOT NULL,\n    [catalog]        [nvarchar](50)   index [IX_catalog] NOT NULL,\n    [number]         [nvarchar](50)   unique NOT NULL,\n    [card]           [nvarchar](50)   NOT NULL,\n    [moment]         [datetime]       index [IX_moment] NOT NULL,\n    [deadline]       [smalldatetime]  NOT NULL,\n    [prolong]        [int]            NOT NULL,\n    [pilot]          [bit]            NOT NULL,\n    [price]          [money]          NOT NULL\n)\n\nGO\n\nif not exists (select * from sysobjects where name='readers' and xtype='U')\ncreate table [dbo].[readers]\n(\n    [ticket]         [nvarchar](50)   primary key NOT NULL,\n    [name]           [nvarchar](50)   index [IX_name] NOT NULL,\n    [category]       [nvarchar](10)   index [IX_category] NOT NULL\n)\n\nGO\n";
+        _Database (db, expected, true);
+    }
+}
