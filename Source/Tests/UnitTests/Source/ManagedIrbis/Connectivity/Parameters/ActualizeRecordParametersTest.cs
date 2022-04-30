bd549ecda472d78e5d6e34263434aff873507d59
Alexey Mironov <amironov73@gmail.com>
2022-04-30 17:02:26 +0800

    Code cleanup

--- Source/Tests/UnitTests/Source/ManagedIrbis/Connectivity/Parameters/ActualizeRecordParametersTest.cs
+++ Source/Tests/UnitTests/Source/ManagedIrbis/Connectivity/Parameters/ActualizeRecordParametersTest.cs
@@ -8,31 +8,30 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 #nullable enable
 
-namespace UnitTests.ManagedIrbis.Connectivity.Parameters
-{
-    [TestClass]
-    public sealed class ActualizeRecordParametersTest
-    {
-        [TestMethod]
-        [Description ("Конструктор по умолчанию")]
-        public void ActualizeRecordParameters_Construction_1()
-        {
-            var parameters = new ActualizeRecordParameters();
-            Assert.IsNull (parameters.Database);
-            Assert.AreEqual (0, parameters.Mfn);
-        }
+namespace UnitTests.ManagedIrbis.Connectivity.Parameters;
 
-        [TestMethod]
-        [Description ("Присвоение")]
-        public void ActualizeRecordParameters_Construction_2()
+[TestClass]
+public sealed class ActualizeRecordParametersTest
+{
+    [TestMethod]
+    [Description ("Конструктор по умолчанию")]
+    public void ActualizeRecordParameters_Construction_1()
+    {
+        var parameters = new ActualizeRecordParameters();
+        Assert.IsNull (parameters.Database);
+        Assert.AreEqual (0, parameters.Mfn);
+    }
+
+    [TestMethod]
+    [Description ("Присвоение")]
+    public void ActualizeRecordParameters_Construction_2()
+    {
+        var parameters = new ActualizeRecordParameters
         {
-            var parameters = new ActualizeRecordParameters
-            {
-                Database = "IBIS",
-                Mfn = 123
-            };
-            Assert.AreEqual ("IBIS", parameters.Database);
-            Assert.AreEqual (123, parameters.Mfn);
-        }
+            Database = "IBIS",
+            Mfn = 123
+        };
+        Assert.AreEqual ("IBIS", parameters.Database);
+        Assert.AreEqual (123, parameters.Mfn);
     }
 }
