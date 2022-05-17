a6284dee7389be3113eca1c3c08fcce313d6f412
Alexey Mironov <amironov73@gmail.com>
2022-05-17 12:32:55 +0800

    Code cleanup

--- Source/Tests/UnitTests/Source/ManagedIrbis/Direct/MstControlRecord64Test.cs
+++ Source/Tests/UnitTests/Source/ManagedIrbis/Direct/MstControlRecord64Test.cs
@@ -12,141 +12,151 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 #nullable enable
 
-namespace UnitTests.ManagedIrbis.Direct
+namespace UnitTests.ManagedIrbis.Direct;
+
+[TestClass]
+public class MstControlRecord64Test
+    : Common.CommonUnitTest
 {
-    [TestClass]
-    public class MstControlRecord64Test
-        : Common.CommonUnitTest
+    [TestMethod]
+    [Description ("Конструктор по умолчани")]
+    public void MstControlRecord64_Construction_1()
     {
-        [TestMethod]
-        public void MstControlRecord64_Construction_1()
+        var control = new MstControlRecord64();
+        Assert.AreEqual (0, control.Reserv1);
+        Assert.AreEqual (0, control.NextMfn);
+        Assert.AreEqual (0L, control.NextPosition);
+        Assert.AreEqual (0, control.Version);
+        Assert.AreEqual (0, control.Reserv3);
+        Assert.AreEqual (0, control.Reserv4);
+        Assert.AreEqual (0, control.Blocked);
+    }
+
+    [TestMethod]
+    [Description ("Чтение")]
+    public void MstControlRecord64_Read_1()
+    {
+        byte[] bytes =
         {
-            var control = new MstControlRecord64();
-            Assert.AreEqual(0, control.Reserv1);
-            Assert.AreEqual(0, control.NextMfn);
-            Assert.AreEqual(0L, control.NextPosition);
-            Assert.AreEqual(0, control.Version);
-            Assert.AreEqual(0, control.Reserv3);
-            Assert.AreEqual(0, control.Reserv4);
-            Assert.AreEqual(0, control.Blocked);
+            0, 0, 0, 0, 0, 0, 0, 111, 0, 188, 97,
+            78, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 1
+        };
+        var stream = new MemoryStream (bytes);
+        var control = MstControlRecord64.Read (stream);
+        Assert.AreEqual (0, control.Reserv1);
+        Assert.AreEqual (111, control.NextMfn);
+        Assert.AreEqual (12345678L, control.NextPosition);
+        Assert.AreEqual (0, control.Version);
+        Assert.AreEqual (0, control.Reserv3);
+        Assert.AreEqual (0, control.Reserv4);
+        Assert.AreEqual (1, control.Blocked);
+    }
+
+    [TestMethod]
+    [Description ("Чтение")]
+    public void MstControlRecord64_Read_2()
+    {
+        var fileName = Path.Combine (TestDataPath, "kzd.mst");
+        using (var stream = File.OpenRead (fileName))
+        {
+            var control = MstControlRecord64.Read (stream);
+            Assert.AreEqual (0, control.Reserv1);
+            Assert.AreEqual (32, control.NextMfn);
+            Assert.AreEqual (37308L, control.NextPosition);
+            Assert.AreEqual (0, control.Version);
+            Assert.AreEqual (0, control.Reserv3);
+            Assert.AreEqual (0, control.Reserv1);
+            Assert.AreEqual (0, control.Blocked);
+        }
+    }
+
+    [TestMethod]
+    [Description ("Чтение")]
+    public void MstControlRecord64_Read_3()
+    {
+        var fileName = Path.Combine (Irbis64RootPath, "Datai/IBIS/ibis.mst");
+        using var stream = File.OpenRead (fileName);
+        var control = MstControlRecord64.Read (stream);
+        Assert.AreEqual (0, control.Version);
+        Assert.AreEqual (333, control.NextMfn);
+        Assert.AreEqual (45843589L, control.NextPosition);
+        Assert.AreEqual (0, control.Version);
+        Assert.AreEqual (0, control.Reserv3);
+        Assert.AreEqual (0, control.Reserv1);
+        Assert.AreEqual (0, control.Blocked);
+    }
+
+    [TestMethod]
+    [Description ("Сохранение")]
+    public void MstControlRecord64_Write_1()
+    {
+        var control = new MstControlRecord64
+        {
+            NextMfn = 111,
+            NextPosition = 12345678L,
+            Blocked = 1
+        };
+        var stream = new MemoryStream();
+        control.Write (stream);
+        byte[] expected =
+        {
+            0, 0, 0, 0, 0, 0, 0, 111, 0, 188, 97,
+            78, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 1
+        };
+        var actual = stream.ToArray();
+        CollectionAssert.AreEqual (expected, actual);
+    }
+
+    [TestMethod]
+    [Description ("Дамп")]
+    public void MstControlRecord64_Dump_1()
+    {
+        var fileName = Path.Combine (TestDataPath, "empty.mst");
+        var writer = new StringWriter();
+        using (var stream = File.OpenRead (fileName))
+        {
+            var control = MstControlRecord64.Read (stream);
+            control.Dump (writer);
         }
 
-        [TestMethod]
-        public void MstControlRecord64_Read_1()
+        var expected = "CTLMFN: 0\nNXTMFN: 1\nNXTPOS: 36\nMFTYPE: 0\nRECCNT: 0\nLOCKED: 0\n";
+        var actual = writer.ToString().DosToUnix();
+        Assert.AreEqual (expected, actual);
+    }
+
+    [TestMethod]
+    [Description ("Дамп")]
+    public void MstControlRecord64_Dump_2()
+    {
+        var fileName = Path.Combine (TestDataPath, "kzd.mst");
+        var writer = new StringWriter();
+        using (var stream = File.OpenRead (fileName))
         {
-            byte[] bytes = { 0, 0, 0, 0, 0, 0, 0, 111, 0, 188, 97,
-                78, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                0, 0, 0, 1 };
-            var stream = new MemoryStream(bytes);
-            var control = MstControlRecord64.Read(stream);
-            Assert.AreEqual(0, control.Reserv1);
-            Assert.AreEqual(111, control.NextMfn);
-            Assert.AreEqual(12345678L, control.NextPosition);
-            Assert.AreEqual(0, control.Version);
-            Assert.AreEqual(0, control.Reserv3);
-            Assert.AreEqual(0, control.Reserv4);
-            Assert.AreEqual(1, control.Blocked);
+            var control = MstControlRecord64.Read (stream);
+            control.Dump (writer);
         }
 
-        [TestMethod]
-        public void MstControlRecord64_Read_2()
+        var expected = "CTLMFN: 0\nNXTMFN: 32\nNXTPOS: 37308\nMFTYPE: 0\nRECCNT: 0\nLOCKED: 0\n";
+        var actual = writer.ToString().DosToUnix();
+        Assert.AreEqual (expected, actual);
+    }
+
+    [TestMethod]
+    [Description ("Дамп")]
+    public void MstControlRecord64_Dump_3()
+    {
+        var fileName = Path.Combine (Irbis64RootPath, "Datai/IBIS/ibis.mst");
+        var writer = new StringWriter();
+        using (var stream = File.OpenRead (fileName))
         {
-            var fileName = Path.Combine(TestDataPath, "kzd.mst");
-            using (var stream = File.OpenRead(fileName))
-            {
-                var control = MstControlRecord64.Read(stream);
-                Assert.AreEqual(0, control.Reserv1);
-                Assert.AreEqual(32, control.NextMfn);
-                Assert.AreEqual(37308L, control.NextPosition);
-                Assert.AreEqual(0, control.Version);
-                Assert.AreEqual(0, control.Reserv3);
-                Assert.AreEqual(0, control.Reserv1);
-                Assert.AreEqual(0, control.Blocked);
-            }
+            var control = MstControlRecord64.Read (stream);
+            control.Dump (writer);
         }
 
-        [TestMethod]
-        public void MstControlRecord64_Read_3()
-        {
-            var fileName = Path.Combine(Irbis64RootPath, "Datai/IBIS/ibis.mst");
-            using (var stream = File.OpenRead(fileName))
-            {
-                var control = MstControlRecord64.Read(stream);
-                Assert.AreEqual(0, control.Version);
-                Assert.AreEqual(333, control.NextMfn);
-                Assert.AreEqual(45843589L, control.NextPosition);
-                Assert.AreEqual(0, control.Version);
-                Assert.AreEqual(0, control.Reserv3);
-                Assert.AreEqual(0, control.Reserv1);
-                Assert.AreEqual(0, control.Blocked);
-            }
-        }
-
-        [TestMethod]
-        public void MstControlRecord64_Write_1()
-        {
-            var control = new MstControlRecord64
-            {
-                NextMfn = 111,
-                NextPosition = 12345678L,
-                Blocked = 1
-            };
-            var stream = new MemoryStream();
-            control.Write(stream);
-            byte[] expected = { 0, 0, 0, 0, 0, 0, 0, 111, 0, 188, 97,
-                78, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                0, 0, 0, 1 };
-            var actual = stream.ToArray();
-            CollectionAssert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void MstControlRecord64_Dump_1()
-        {
-            var fileName = Path.Combine(TestDataPath, "empty.mst");
-            var writer = new StringWriter();
-            using (var stream = File.OpenRead(fileName))
-            {
-                var control = MstControlRecord64.Read(stream);
-                control.Dump(writer);
-            }
-
-            var expected = "CTLMFN: 0\nNXTMFN: 1\nNXTPOS: 36\nMFTYPE: 0\nRECCNT: 0\nLOCKED: 0\n";
-            var actual = writer.ToString().DosToUnix();
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void MstControlRecord64_Dump_2()
-        {
-            var fileName = Path.Combine(TestDataPath, "kzd.mst");
-            var writer = new StringWriter();
-            using (var stream = File.OpenRead(fileName))
-            {
-                var control = MstControlRecord64.Read(stream);
-                control.Dump(writer);
-            }
-
-            var expected = "CTLMFN: 0\nNXTMFN: 32\nNXTPOS: 37308\nMFTYPE: 0\nRECCNT: 0\nLOCKED: 0\n";
-            var actual = writer.ToString().DosToUnix();
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void MstControlRecord64_Dump_3()
-        {
-            var fileName = Path.Combine(Irbis64RootPath, "Datai/IBIS/ibis.mst");
-            var writer = new StringWriter();
-            using (var stream = File.OpenRead(fileName))
-            {
-                var control = MstControlRecord64.Read(stream);
-                control.Dump(writer);
-            }
-
-            var expected = "CTLMFN: 0\nNXTMFN: 333\nNXTPOS: 45843589\nMFTYPE: 0\nRECCNT: 0\nLOCKED: 0\n";
-            var actual = writer.ToString().DosToUnix();
-            Assert.AreEqual(expected, actual);
-        }
-
+        var expected = "CTLMFN: 0\nNXTMFN: 333\nNXTPOS: 45843589\nMFTYPE: 0\nRECCNT: 0\nLOCKED: 0\n";
+        var actual = writer.ToString().DosToUnix();
+        Assert.AreEqual (expected, actual);
     }
 }
--- Source/Tests/UnitTests/Source/ManagedIrbis/Direct/MstDictionaryEntry64Test.cs
+++ Source/Tests/UnitTests/Source/ManagedIrbis/Direct/MstDictionaryEntry64Test.cs
@@ -10,34 +10,39 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 #nullable enable
 
-namespace UnitTests.ManagedIrbis.Direct
-{
-    [TestClass]
-    public class MstDictionaryEntry64Test
-    {
-        [TestMethod]
-        public void MstDictionaryEntry64_Construction_1()
-        {
-            var entry = new MstDictionaryEntry64();
-            Assert.AreEqual(0, entry.Tag);
-            Assert.AreEqual(0, entry.Position);
-            Assert.AreEqual(0, entry.Length);
-            Assert.IsNull(entry.Bytes);
-            Assert.IsNull(entry.Text);
-        }
+namespace UnitTests.ManagedIrbis.Direct;
 
-        [TestMethod]
-        public void MstDictionaryEntry64_ToString_1()
+[TestClass]
+public class MstDictionaryEntry64Test
+{
+    [TestMethod]
+    [Description ("Конструктор по умолчанию")]
+    public void MstDictionaryEntry64_Construction_1()
+    {
+        var entry = new MstDictionaryEntry64();
+        Assert.AreEqual (0, entry.Tag);
+        Assert.AreEqual (0, entry.Position);
+        Assert.AreEqual (0, entry.Length);
+        Assert.IsNull (entry.Bytes);
+        Assert.IsNull (entry.Text);
+    }
+
+    [TestMethod]
+    [Description ("Текстовое представление")]
+    public void MstDictionaryEntry64_ToString_1()
+    {
+        var entry = new MstDictionaryEntry64
         {
-            var entry = new MstDictionaryEntry64
-            {
-                Tag = 123,
-                Position = 345,
-                Length = 5,
-                Bytes = new byte[] { 72, 101, 108, 108, 111 },
-                Text = "Hello"
-            };
-            Assert.AreEqual("Tag: 123, Position: 345, Length: 5, Text: Hello", entry.ToString().DosToUnix());
-        }
+            Tag = 123,
+            Position = 345,
+            Length = 5,
+            Bytes = new byte[] { 72, 101, 108, 108, 111 },
+            Text = "Hello"
+        };
+        Assert.AreEqual
+            (
+                "Tag: 123, Position: 345, Length: 5, Text: Hello",
+                entry.ToString().DosToUnix()
+            );
     }
 }
