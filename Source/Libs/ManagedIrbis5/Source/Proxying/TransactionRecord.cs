cb138b9b342c43cb43eb6fe32c27dad86fad837c
Alexey Mironov <amironov73@gmail.com>
2022-06-30 08:01:59 +0800

    Working on IRBIS proxying

---
+++ Source/Libs/ManagedIrbis5/Source/Proxying/TransactionRecord.cs
@@ -0,0 +1,78 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable UnusedMember.Global
+
+/* TransactionRecord.cs -- вся собранная прокси информация о транзакции
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
+using System.Net;
+
+#endregion
+
+#nullable enable
+
+namespace ManagedIrbis.Proxying;
+
+/// <summary>
+/// Вся собранная прокси информация о транзакции "клиент-сервер".
+/// </summary>
+public sealed class TransactionRecord
+{
+    #region Properties
+
+    /// <summary>
+    /// Порядковый номер (отсчет с 1).
+    /// </summary>
+    public long Index { get; set; }
+
+    /// <summary>
+    /// Стадия, до которой дошло выполнение команды.
+    /// Расшифровку см. <see cref="TransactionStage"/>.
+    /// </summary>
+    public int Stage { get; set; }
+
+    /// <summary>
+    /// Момент поступления команды (по часам прокси).
+    /// </summary>
+    public DateTime Moment { get; set; }
+
+    /// <summary>
+    /// Адрес, с которого пришла команда.
+    /// </summary>
+    public EndPoint? EndPoint { get; set; }
+
+    /// <summary>
+    /// Запрос клиента в необработанном виде
+    /// (включая строки заголовка)
+    /// </summary>
+    public byte[]? Request { get; set; }
+
+    /// <summary>
+    /// Ответ сервера в необработанном виде
+    /// (включая строки заголовка)
+    /// </summary>
+    public byte[]? Response { get; set; }
+
+    /// <summary>
+    /// Общая продолжительность обработки запроса
+    /// со всеми пересылками туда-сюда
+    /// (в миллисекундах).
+    /// </summary>
+    public int RoundtripDuration { get; set; }
+
+    /// <summary>
+    /// Продолжительность обработки запроса сервером
+    /// ИРБИС (в миллисекундах).
+    /// </summary>
+    public int ServerDuration { get; set; }
+
+    #endregion
+}
---
+++ Source/Libs/ManagedIrbis5/Source/Proxying/TransactionStage.cs
@@ -0,0 +1,87 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable UnusedMember.Global
+
+/* TransactionStage.cs -- стадии транзакции при проксировании
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+namespace ManagedIrbis.Proxying;
+
+/// <summary>
+/// Стадии транзакции при проксировании ИРБИС64.
+/// </summary>
+public enum TransactionStage
+{
+    /// <summary>
+    /// Начальная стадия: произошло подключение клиента
+    /// к прослушивающему сокету.
+    /// </summary>
+    Initial = 1,
+
+    /// <summary>
+    /// Успешно прочитаны первые байты запроса, содержащие длину
+    /// клиентского пакета.
+    /// </summary>
+    HeaderFetched = 2,
+
+    /// <summary>
+    /// В прочитанных байтах обнаружен ограничитель строки.
+    /// </summary>
+    DelimiterFound = 3,
+
+    /// <summary>
+    /// Из клиентского сокета прочитан весь пакет с запросом
+    /// (согласно длине, указанной в первой строке).
+    /// </summary>
+    RequestFetched = 4,
+
+    /// <summary>
+    /// Полученный пакет прошел проверку.
+    /// </summary>
+    RequestValidated = 5,
+
+    /// <summary>
+    /// Установлено соединение с сервером.
+    /// </summary>
+    ConnectedToServer = 6,
+
+    /// <summary>
+    /// Клиенский запрос полностью переслан на сервер.
+    /// </summary>
+    RequestForwarded = 7,
+
+    /// <summary>
+    /// Получен ответ сервера.
+    /// </summary>
+    GotReply = 8,
+
+    /// <summary>
+    /// Пакет с ответом прошел проверку.
+    /// </summary>
+    ReplyValidated = 9,
+
+    /// <summary>
+    /// Пакет с ответом отослан клиенту.
+    /// </summary>
+    ReplySent = 10,
+
+    /// <summary>
+    /// "Нижний" сокет заглушен.
+    /// </summary>
+    BottomShutdown = 11,
+
+    /// <summary>
+    /// "Верхний" сокет закрыт.
+    /// </summary>
+    UpperClosed = 12,
+
+    /// <summary>
+    /// "Нижний" сокет закрыт. Обработка успешно завершена.
+    /// </summary>
+    AllDone = 13
+}
