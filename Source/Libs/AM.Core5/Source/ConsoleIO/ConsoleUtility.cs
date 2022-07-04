26f720c4cfed3d8d69ee450f93ca15f363279c17
Alexey Mironov <amironov73@gmail.com>
2022-07-04 08:00:32 +0800

    Add ObjectStringFlector class

---
+++ Source/Libs/AM.Core5/Source/ConsoleIO/ConsoleUtility.cs
@@ -0,0 +1,83 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable PropertyCanBeMadeInitOnly.Global
+// ReSharper disable UnusedMember.Global
+
+/* ConsoleUtility.cs -- полезные методы для System.Console1
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
+
+#endregion
+
+#nullable enable
+
+namespace AM.ConsoleIO;
+
+/// <summary>
+/// Полезные методы для <see cref="Console"/>.
+/// </summary>
+public static class ConsoleUtility
+{
+    #region Public methods
+
+    /// <summary>
+    /// Вывод заданного количества пустых строк.
+    /// </summary>
+    /// <param name="howMany"></param>
+    public static void WriteEmptyLines
+        (
+            int howMany
+        )
+    {
+        while (howMany > 0)
+        {
+            Console.WriteLine();
+            --howMany;
+        }
+    }
+
+    /// <summary>
+    /// Вывод параграфа текста.
+    /// </summary>
+    public static void WriteParagraph
+        (
+            string? text,
+            int linesAfter = 1,
+            int linesBefore = 0
+        )
+    {
+        if (!string.IsNullOrEmpty (text))
+        {
+            WriteEmptyLines (linesBefore);
+            Console.Write (text);
+            WriteEmptyLines (linesAfter);
+        }
+    }
+
+    /// <summary>
+    /// Вывод (псевдо) подчеркнутой строки.
+    /// </summary>
+    public static void WriteUnderlined
+        (
+            string? text
+        )
+    {
+        if (!string.IsNullOrEmpty (text))
+        {
+            Console.WriteLine (text);
+            Console.WriteLine (new string ('-', text.Length));
+        }
+    }
+
+    #endregion
+}
---
+++ Source/Libs/AM.Core5/Source/Reflection/ObjectStringFlector.cs
@@ -0,0 +1,137 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedType.Global
+
+/* ObjectStringFlector.cs -- извлекает значения свойств
+ *  Ars Magna project, http://arsmagna.ru
+ */
+
+#nullable enable
+
+using System;
+
+namespace AM.Reflection;
+
+/// <summary>
+/// Извлекает значения свойств из объекта.
+/// </summary>
+public static class ObjectStringFlector
+{
+    #region Private members
+
+    private static object? GetValueByIndex
+        (
+            object source,
+            string path,
+            int startIndex
+        )
+    {
+        var sourceType = source.GetType();
+        var boundaryEndIndex = path.IndexOf (']', startIndex);
+        var indexString = path.Substring
+            (
+                startIndex + 1,
+                boundaryEndIndex - startIndex - 1
+            );
+
+        if (sourceType.IsArray)
+        {
+            return ((Array) source).GetValue (indexString.SafeToInt32());
+        }
+
+        if (sourceType == typeof (string))
+        {
+            return ((string) source)[indexString.SafeToInt32()];
+        }
+
+        var index = indexString.IndexOfAny (_stringBoundaryChars) == 0
+            ? new object[] { indexString.Trim (_stringBoundaryChars) }
+            : new object[] { indexString.SafeToInt32() };
+
+        var propertyInfo = sourceType.GetProperty ("Item");
+        if (propertyInfo == null)
+        {
+            throw new Exception();
+        }
+
+        return propertyInfo.GetValue (source, index);
+    }
+
+    private static readonly char[] _boundaryStartChars = { '.', '[' };
+
+    private static readonly char[] _stringBoundaryChars = { '\'', '"' };
+
+    #endregion
+
+    #region Public methods
+
+    /// <summary>
+    /// Получение значения свойства объекта.
+    /// </summary>
+    public static object? GetValue
+        (
+            object source,
+            string path,
+            int startIndex = 0
+        )
+    {
+        Sure.NotNull (source);
+        Sure.NotNullNorEmpty (path);
+
+        if (path[startIndex] == '[')
+        {
+            var indexedProperty = GetValueByIndex (source, path, startIndex);
+            if (indexedProperty is null)
+            {
+                return null;
+            }
+
+            var closingBracketIndex = path.IndexOf (']', startIndex);
+
+            if (closingBracketIndex < path.Length - 1)
+            {
+                return GetValue (indexedProperty, path, closingBracketIndex + 1);
+            }
+
+            return indexedProperty;
+        }
+
+        if (path[startIndex] == '.')
+        {
+            startIndex++;
+        }
+
+        var sourceType = source.GetType();
+        var boundaryIndex = path.IndexOfAny (_boundaryStartChars, startIndex);
+        var currentPropertyName = boundaryIndex > -1
+            ? path.Substring (startIndex, boundaryIndex - startIndex)
+            : path.Substring (startIndex);
+
+        var propertyInfo = sourceType.GetProperty (currentPropertyName);
+        if (propertyInfo == null)
+        {
+            throw new Exception();
+        }
+
+        var currentProperty = propertyInfo.GetValue (source, null);
+        if (currentProperty is null)
+        {
+            return null;
+        }
+
+        if (currentPropertyName.Length + startIndex == path.Length)
+        {
+            return currentProperty;
+        }
+
+        return GetValue (currentProperty, path, boundaryIndex);
+    }
+
+    #endregion
+}
