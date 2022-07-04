57f9b5c9a0c94a483934ba6cf30dd6aaef31ec7a
Alexey Mironov <amironov73@gmail.com>
2022-07-04 08:47:49 +0800

    Code cleanup

--- Source/Libs/AM.Drawing5/Source/Pdfing/Drawing/ImageSource.cs
+++ Source/Libs/AM.Drawing5/Source/Pdfing/Drawing/ImageSource.cs
@@ -1,9 +1,26 @@
-﻿
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedMember.Global
+
+/* ImageSoruce.cs --
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
+#endregion
+
+#nullable enable
 
 namespace MigraDocCore.DocumentObjectModel.MigraDoc.DocumentObjectModel.Shapes
 {
