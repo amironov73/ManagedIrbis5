1d8ea3b002a844130c03a59d639e1f040eaf1920
Alexey Mironov <amironov73@gmail.com>
2022-07-07 10:06:57 +0800

    Code cleanup

--- Source/Libs/AM.Drawing5/Source/Pdfing/Utils/ImageSharpImageSource.cs
+++ Source/Libs/AM.Drawing5/Source/Pdfing/Utils/ImageSharpImageSource.cs
@@ -1,77 +1,113 @@
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.PixelFormats;
-using MigraDocCore.DocumentObjectModel.MigraDoc.DocumentObjectModel.Shapes;
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedMember.Global
+
+/* ImageSharpImageSource.cs --
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
 using System;
 using System.IO;
+
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Formats;
 using SixLabors.ImageSharp.Formats.Bmp;
 using SixLabors.ImageSharp.Formats.Jpeg;
 using SixLabors.ImageSharp.Formats.Png;
 
-namespace PdfSharpCore.Utils
+using MigraDocCore.DocumentObjectModel.MigraDoc.DocumentObjectModel.Shapes;
+
+#endregion
+
+#nullable enable
+
+namespace PdfSharpCore.Utils;
+
+/// <summary>
+/// Источник картинок -- ImageSharp.
+/// </summary>
+public class ImageSharpImageSource<TPixel>
+    : ImageSource
+    where TPixel : unmanaged, IPixel<TPixel>
 {
-    public class ImageSharpImageSource<TPixel> : ImageSource where TPixel : unmanaged, IPixel<TPixel>
+    public static IImageSource FromImageSharpImage
+        (
+            Image<TPixel> image,
+            IImageFormat imgFormat,
+            int? quality = 75
+        )
     {
+        var _path = "*" + Guid.NewGuid().ToString ("B");
 
-        public static IImageSource FromImageSharpImage(Image<TPixel> image, IImageFormat imgFormat, int? quality = 75)
+        return new ImageSharpImageSourceImpl<TPixel> (_path, image, (int)quality, imgFormat is PngFormat);
+    }
+
+    protected override IImageSource FromBinaryImpl
+        (
+            string name,
+            Func<byte[]> imageSource,
+            int? quality = 75
+        )
+    {
+        var image = Image.Load<TPixel> (imageSource.Invoke(), out IImageFormat imgFormat);
+        return new ImageSharpImageSourceImpl<TPixel> (name, image, (int)quality, imgFormat is PngFormat);
+    }
+
+    protected override IImageSource FromFileImpl (string path, int? quality = 75)
+    {
+        var image = Image.Load<TPixel> (path, out IImageFormat imgFormat);
+        return new ImageSharpImageSourceImpl<TPixel> (path, image, (int)quality, imgFormat is PngFormat);
+    }
+
+    protected override IImageSource FromStreamImpl (string name, Func<Stream> imageStream, int? quality = 75)
+    {
+        using (var stream = imageStream.Invoke())
         {
-            var _path = "*" + Guid.NewGuid().ToString("B");
-            return new ImageSharpImageSourceImpl<TPixel>(_path, image, (int)quality, imgFormat is PngFormat);
+            var image = Image.Load<TPixel> (stream, out IImageFormat imgFormat);
+            return new ImageSharpImageSourceImpl<TPixel> (name, image, (int)quality, imgFormat is PngFormat);
+        }
+    }
+
+    private class ImageSharpImageSourceImpl<TPixel2> : IImageSource where TPixel2 : unmanaged, IPixel<TPixel2>
+    {
+        private Image<TPixel2> Image { get; }
+        private readonly int _quality;
+
+        public int Width => Image.Width;
+        public int Height => Image.Height;
+        public string Name { get; }
+        public bool Transparent { get; internal set; }
+
+        public ImageSharpImageSourceImpl (string name, Image<TPixel2> image, int quality, bool isTransparent)
+        {
+            Name = name;
+            Image = image;
+            _quality = quality;
+            Transparent = isTransparent;
         }
 
-        protected override IImageSource FromBinaryImpl(string name, Func<byte[]> imageSource, int? quality = 75)
+        public void SaveAsJpeg (MemoryStream ms)
         {
-            var image = Image.Load<TPixel>(imageSource.Invoke(), out IImageFormat imgFormat);
-            return new ImageSharpImageSourceImpl<TPixel>(name, image, (int)quality, imgFormat is PngFormat);
+            Image.SaveAsJpeg (ms, new JpegEncoder() { Quality = this._quality });
         }
 
-        protected override IImageSource FromFileImpl(string path, int? quality = 75)
+        public void Dispose()
         {
-            var image = Image.Load<TPixel>(path, out IImageFormat imgFormat);
-            return new ImageSharpImageSourceImpl<TPixel>(path, image, (int) quality, imgFormat is PngFormat);
+            Image.Dispose();
         }
 
-        protected override IImageSource FromStreamImpl(string name, Func<Stream> imageStream, int? quality = 75)
+        public void SaveAsPdfBitmap (MemoryStream ms)
         {
-            using (var stream = imageStream.Invoke())
-            {
-                var image = Image.Load<TPixel>(stream, out IImageFormat imgFormat);
-                return new ImageSharpImageSourceImpl<TPixel>(name, image, (int)quality, imgFormat is PngFormat);
-            }
-        }
-
-        private class ImageSharpImageSourceImpl<TPixel2> : IImageSource where TPixel2 : unmanaged, IPixel<TPixel2>
-        {
-            private Image<TPixel2> Image { get; }
-            private readonly int _quality;
-
-            public int Width => Image.Width;
-            public int Height => Image.Height;
-            public string Name { get; }
-            public bool Transparent { get; internal set; }
-
-            public ImageSharpImageSourceImpl(string name, Image<TPixel2> image, int quality, bool isTransparent)
-            {
-                Name = name;
-                Image = image;
-                _quality = quality;
-                Transparent = isTransparent;
-            }
-
-            public void SaveAsJpeg(MemoryStream ms)
-            {
-                Image.SaveAsJpeg(ms, new JpegEncoder() { Quality = this._quality });
-            }
-
-            public void Dispose()
-            {
-                Image.Dispose();
-            }
-            public void SaveAsPdfBitmap(MemoryStream ms)
-            {
-                BmpEncoder bmp = new BmpEncoder { BitsPerPixel = BmpBitsPerPixel.Pixel32 };
-                Image.Save(ms, bmp);
-            }
+            BmpEncoder bmp = new BmpEncoder { BitsPerPixel = BmpBitsPerPixel.Pixel32 };
+            Image.Save (ms, bmp);
         }
     }
 }
--- Source/Libs/AM.Drawing5/Source/Pdfing/Utils/LinuxSystemFontResolver.cs
+++ Source/Libs/AM.Drawing5/Source/Pdfing/Utils/LinuxSystemFontResolver.cs
@@ -1,207 +1,244 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedMember.Global
+
+/* LinuxSystemFontResolver.cs --
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
-
 using System.Text.RegularExpressions;
 
+#endregion
 
-namespace PdfSharpCore.Utils
+#nullable enable
+
+namespace PdfSharpCore.Utils;
+
+public static class LinuxSystemFontResolver
 {
-    public static class LinuxSystemFontResolver
+    const string libfontconfig = "libfontconfig.so.1";
+
+
+    [DllImport (libfontconfig)]
+    private static extern IntPtr FcInitLoadConfigAndFonts();
+
+    static readonly Lazy<IntPtr> fcConfig = new Lazy<IntPtr> (FcInitLoadConfigAndFonts);
+
+
+    [DllImport (libfontconfig)]
+    public static extern FcPatternHandle FcPatternCreate();
+
+    [DllImport (libfontconfig)]
+    public static extern int FcPatternGetString (IntPtr p, [MarshalAs (UnmanagedType.LPStr)] string obj, int n,
+        ref IntPtr s);
+
+    [DllImport (libfontconfig)]
+    public static extern void FcPatternDestroy (IntPtr pattern);
+
+    public class FcPatternHandle : SafeHandle
     {
-        const string libfontconfig = "libfontconfig.so.1";
-
-
-        [DllImport(libfontconfig)] private static extern IntPtr FcInitLoadConfigAndFonts();
-
-        static readonly Lazy<IntPtr> fcConfig = new Lazy<IntPtr>(FcInitLoadConfigAndFonts);
-
-
-        [DllImport(libfontconfig)] public static extern FcPatternHandle FcPatternCreate();
-        [DllImport(libfontconfig)] public static extern int FcPatternGetString(IntPtr p, [MarshalAs(UnmanagedType.LPStr)] string obj, int n, ref IntPtr s);
-        [DllImport(libfontconfig)] public static extern void FcPatternDestroy(IntPtr pattern);
-
-        public class FcPatternHandle : SafeHandle
+        FcPatternHandle() : base (IntPtr.Zero, true)
         {
-            FcPatternHandle() : base(IntPtr.Zero, true) { }
-
-            public override bool IsInvalid => this.handle == IntPtr.Zero;
-
-            protected override bool ReleaseHandle()
-            {
-                FcPatternDestroy(this.handle);
-                return true;
-            }
         }
 
+        public override bool IsInvalid => this.handle == IntPtr.Zero;
 
-        [DllImport(libfontconfig)] public static extern FcObjectSetHandle FcObjectSetCreate();
-        [DllImport(libfontconfig)] public static extern int FcObjectSetAdd(FcObjectSetHandle os, [MarshalAs(UnmanagedType.LPStr)] string obj);
-        [DllImport(libfontconfig)] public static extern void FcObjectSetDestroy(IntPtr os);
-
-        public class FcObjectSetHandle : SafeHandle
+        protected override bool ReleaseHandle()
         {
-            FcObjectSetHandle() : base(IntPtr.Zero, true) { }
+            FcPatternDestroy (this.handle);
+            return true;
+        }
+    }
 
-            public override bool IsInvalid => this.handle == IntPtr.Zero;
 
-            protected override bool ReleaseHandle()
-            {
-                FcObjectSetDestroy(this.handle);
-                return true;
-            }
+    [DllImport (libfontconfig)]
+    public static extern FcObjectSetHandle FcObjectSetCreate();
 
-            public static FcObjectSetHandle Create(params string[] objs)
-            {
-                var os = FcObjectSetCreate();
-                foreach (var obj in objs)
-                    FcObjectSetAdd(os, obj);
-                FcObjectSetAdd(os, "");
-                return os;
-            }
+    [DllImport (libfontconfig)]
+    public static extern int FcObjectSetAdd (FcObjectSetHandle os, [MarshalAs (UnmanagedType.LPStr)] string obj);
+
+    [DllImport (libfontconfig)]
+    public static extern void FcObjectSetDestroy (IntPtr os);
+
+    public class FcObjectSetHandle : SafeHandle
+    {
+        FcObjectSetHandle() : base (IntPtr.Zero, true)
+        {
         }
 
+        public override bool IsInvalid => this.handle == IntPtr.Zero;
 
-        [DllImport(libfontconfig)] public static extern FcFontSetHandle FcFontList(IntPtr config, FcPatternHandle pattern, FcObjectSetHandle os);
-        [DllImport(libfontconfig)] public static extern void FcFontSetDestroy(IntPtr fs);
-
-        public struct FcFontSet
+        protected override bool ReleaseHandle()
         {
-            public int nfont;
-            public int sfont;
-            public IntPtr fonts;
+            FcObjectSetDestroy (this.handle);
+            return true;
         }
 
-        public class FcFontSetHandle : SafeHandle
+        public static FcObjectSetHandle Create (params string[] objs)
         {
-            FcFontSetHandle() : base(IntPtr.Zero, true) { }
+            var os = FcObjectSetCreate();
+            foreach (var obj in objs)
+                FcObjectSetAdd (os, obj);
+            FcObjectSetAdd (os, "");
+            return os;
+        }
+    }
 
-            public override bool IsInvalid => this.handle == IntPtr.Zero;
 
-            protected override bool ReleaseHandle()
+    [DllImport (libfontconfig)]
+    public static extern FcFontSetHandle FcFontList (IntPtr config, FcPatternHandle pattern, FcObjectSetHandle os);
+
+    [DllImport (libfontconfig)]
+    public static extern void FcFontSetDestroy (IntPtr fs);
+
+    public struct FcFontSet
+    {
+        public int nfont;
+        public int sfont;
+        public IntPtr fonts;
+    }
+
+    public class FcFontSetHandle : SafeHandle
+    {
+        FcFontSetHandle() : base (IntPtr.Zero, true)
+        {
+        }
+
+        public override bool IsInvalid => this.handle == IntPtr.Zero;
+
+        protected override bool ReleaseHandle()
+        {
+            FcFontSetDestroy (this.handle);
+            return true;
+        }
+
+        public FcFontSet Read()
+        {
+            return Marshal.PtrToStructure<FcFontSet> (this.handle);
+        }
+    }
+
+
+    static string GetString (IntPtr handle, string obj)
+    {
+        var ptr = IntPtr.Zero;
+        var result = FcPatternGetString (handle, obj, 0, ref ptr);
+        if (result == 0)
+            return Marshal.PtrToStringAnsi (ptr);
+        else
+            return null;
+    }
+
+
+    static IEnumerable<string> ResolveFontConfig()
+    {
+        var config = fcConfig.Value;
+        using (var pattern = FcPatternCreate())
+        using (var os = FcObjectSetHandle.Create ("family", "style", "file"))
+        using (var fs = FcFontList (config, pattern, os))
+        {
+            var fset = fs.Read();
+            for (int index = 0; index < fset.nfont; index++)
             {
-                FcFontSetDestroy(this.handle);
-                return true;
+                var font = Marshal.ReadIntPtr (fset.fonts, index * Marshal.SizeOf<IntPtr>());
+                var family = GetString (font, "family");
+                var style = GetString (font, "style");
+                var file = GetString (font, "file");
+
+                if (family is null || style is null || file is null)
+                    continue;
+
+                yield return file;
             }
+        }
+    }
 
-            public FcFontSet Read()
-            {
-                return Marshal.PtrToStructure<FcFontSet>(this.handle);
-            }
+
+    public static string[] Resolve()
+    {
+        try
+        {
+            return ResolveFontConfig().Where (x => x.EndsWith (".ttf", StringComparison.OrdinalIgnoreCase)).ToArray();
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine (ex.ToString());
+            return ResolveFallback().Where (x => x.EndsWith (".ttf", StringComparison.OrdinalIgnoreCase)).ToArray();
+        }
+    }
+
+
+    static IEnumerable<string> ResolveFallback()
+    {
+        var fontList = new List<string>();
+
+        void AddFontsToFontList (string path)
+        {
+            if (!Directory.Exists (path))
+                return;
+
+            foreach (string subDir in Directory.EnumerateDirectories (path, "*", SearchOption.AllDirectories))
+                fontList.AddRange (Directory.EnumerateFiles (subDir, "*", SearchOption.AllDirectories));
         }
 
-
-        static string GetString(IntPtr handle, string obj)
+        var hs = new HashSet<string> (StringComparer.OrdinalIgnoreCase);
+        foreach (var path in SearchPaths())
         {
-            var ptr = IntPtr.Zero;
-            var result = FcPatternGetString(handle, obj, 0, ref ptr);
-            if (result == 0)
-                return Marshal.PtrToStringAnsi(ptr);
-            else
-                return null;
+            if (hs.Contains (path))
+                continue;
+            hs.Add (path);
+            AddFontsToFontList (path);
         }
 
+        return fontList.ToArray();
+    }
 
-        static IEnumerable<string> ResolveFontConfig()
+    static IEnumerable<string> SearchPaths()
+    {
+        var dirs = new List<string>();
+        try
         {
-            var config = fcConfig.Value;
-            using (var pattern = FcPatternCreate())
-            using (var os = FcObjectSetHandle.Create("family", "style", "file"))
-            using (var fs = FcFontList(config, pattern, os))
+            Regex confRegex = new Regex ("<dir>(?<dir>.*)</dir>", RegexOptions.Compiled);
+            using (var reader = new StreamReader (File.OpenRead ("/etc/fonts/fonts.conf")))
             {
-                var fset = fs.Read();
-                for (int index = 0; index < fset.nfont; index++)
+                string line;
+                while ((line = reader.ReadLine()) != null)
                 {
-                    var font = Marshal.ReadIntPtr(fset.fonts, index * Marshal.SizeOf<IntPtr>());
-                    var family = GetString(font, "family");
-                    var style = GetString(font, "style");
-                    var file = GetString(font, "file");
-
-                    if (family is null || style is null || file is null)
+                    Match match = confRegex.Match (line);
+                    if (!match.Success)
                         continue;
 
-                    yield return file;
-                }
-            }
-        }
-
-
-        public static string[] Resolve()
-        {
-            try
-            {
-                return ResolveFontConfig().Where(x => x.EndsWith(".ttf", StringComparison.OrdinalIgnoreCase)).ToArray();
-            }
-            catch(Exception ex)
-            {
-                Console.Error.WriteLine(ex.ToString());
-                return ResolveFallback().Where(x => x.EndsWith(".ttf", StringComparison.OrdinalIgnoreCase)).ToArray();
-            }
-        }
-
-
-        static IEnumerable<string> ResolveFallback()
-        {
-            var fontList = new List<string>();
-
-            void AddFontsToFontList(string path)
-            {
-                if (!Directory.Exists(path))
-                    return;
-
-                foreach (string subDir in Directory.EnumerateDirectories(path, "*", SearchOption.AllDirectories))
-                    fontList.AddRange(Directory.EnumerateFiles(subDir, "*", SearchOption.AllDirectories));
-            }
-
-            var hs = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
-            foreach (var path in SearchPaths())
-            {
-                if (hs.Contains(path))
-                    continue;
-                hs.Add(path);
-                AddFontsToFontList(path);
-            }
-
-            return fontList.ToArray();
-        }
-
-        static IEnumerable<string> SearchPaths()
-        {
-            var dirs = new List<string>();
-            try
-            {
-                Regex confRegex = new Regex("<dir>(?<dir>.*)</dir>", RegexOptions.Compiled);
-                using (var reader = new StreamReader(File.OpenRead("/etc/fonts/fonts.conf")))
-                {
-                    string line;
-                    while ((line = reader.ReadLine()) != null)
+                    string path = match.Groups["dir"].Value.Trim();
+                    if (path.StartsWith ("~"))
                     {
-                        Match match = confRegex.Match(line);
-                        if (!match.Success)
-                            continue;
+                        path = Environment.GetEnvironmentVariable ("HOME") + path.Substring (1);
+                    }
 
-                        string path = match.Groups["dir"].Value.Trim();
-                        if (path.StartsWith("~"))
-                        {
-                            path = Environment.GetEnvironmentVariable("HOME") + path.Substring(1);
-                        }
-
-                        dirs.Add(path);
-                    } // Whend 
-                } // End Using reader 
-            }
-            catch (Exception ex)
-            {
-                Console.Error.WriteLine(ex.Message);
-                Console.Error.WriteLine(ex.StackTrace);
-            }
-
-            dirs.Add("/usr/share/fonts");
-            dirs.Add("/usr/local/share/fonts");
-            dirs.Add(Environment.GetEnvironmentVariable("HOME") + "/.fonts");
-            return dirs;
+                    dirs.Add (path);
+                } // Whend
+            } // End Using reader
         }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine (ex.Message);
+            Console.Error.WriteLine (ex.StackTrace);
+        }
+
+        dirs.Add ("/usr/share/fonts");
+        dirs.Add ("/usr/local/share/fonts");
+        dirs.Add (Environment.GetEnvironmentVariable ("HOME") + "/.fonts");
+        return dirs;
     }
 }
