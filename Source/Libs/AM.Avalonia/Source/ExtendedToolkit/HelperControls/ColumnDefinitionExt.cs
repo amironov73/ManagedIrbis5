090a956decc8866c15e2fbe17eeb807c596ef6da
Alexey Mironov <amironov73@gmail.com>
2022-07-09 12:45:53 +0800

    Add some Avalonia controls

---
+++ Source/Libs/AM.Avalonia/Source/ExtendedToolkit/HelperControls/ColumnDefinitionExt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Avalonia.Controls;
+
+namespace Avalonia.ExtendedToolkit
+{
+    /// <summary>
+    /// should be remove if avalonia supports naming of ColumnDefinition
+    /// </summary>
+    public class ColumnDefinitionExt : ColumnDefinition
+    {
+        public Type StyleKey { get { return typeof(ColumnDefinition); } }
+
+
+        /// <summary>
+        /// get / get Name
+        /// </summary>
+        public string Name
+        {
+            get { return (string)GetValue(NameProperty); }
+            set { SetValue(NameProperty, value); }
+        }
+
+        /// <summary>
+        /// <see cref="Name"/>
+        /// </summary>
+        public static readonly StyledProperty<string> NameProperty =
+            AvaloniaProperty.Register<ColumnDefinitionExt, string>(nameof(Name));
+    }
+}
