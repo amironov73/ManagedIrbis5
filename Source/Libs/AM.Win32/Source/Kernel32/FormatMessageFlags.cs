68bce3e0b291f091a5896f6d6a379cd439f6feca
Alexey Mironov <amironov73@gmail.com>
2022-07-16 10:30:53 +0800

    Code cleanup

--- Source/Libs/AM.Win32/Source/Kernel32/FormatMessageFlags.cs
+++ Source/Libs/AM.Win32/Source/Kernel32/FormatMessageFlags.cs
@@ -2,17 +2,12 @@
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
 // ReSharper disable CheckNamespace
-// ReSharper disable ClassNeverInstantiated.Global
 // ReSharper disable CommentTypo
-// ReSharper disable FieldCanBeMadeReadOnly.Global
 // ReSharper disable IdentifierTypo
 // ReSharper disable InconsistentNaming
-// ReSharper disable MemberCanBePrivate.Global
-// ReSharper disable StringLiteralTypo
 // ReSharper disable UnusedMember.Global
-// ReSharper disable UnusedType.Global
 
-/* FormatMessageFlags.cs -- flags for FormatMessage function
+/* FormatMessageFlags.cs -- флаги для функции FormatMessage
    Ars Magna project, http://arsmagna.ru */
 
 #region Using directives
@@ -21,79 +16,76 @@ using System;
 
 #endregion
 
-namespace AM.Win32
+namespace AM.Win32;
+
+/// <summary>
+/// Флаги для функции FormatMessage.
+/// </summary>
+[Flags]
+public enum FormatMessageFlags
 {
     /// <summary>
-    /// Flags for FormatMessage function.
+    /// The lpBuffer parameter is a pointer to a PVOID pointer,
+    /// and that the nSize parameter specifies the minimum number
+    /// of TCHARs to allocate for an output message buffer. The
+    /// function allocates a buffer large enough to hold the
+    /// formatted message, and places a pointer to the allocated
+    /// buffer at the address specified by lpBuffer. The caller
+    /// should use the LocalFree function to free the buffer when
+    /// it is no longer needed.
     /// </summary>
-    [Flags]
-    public enum FormatMessageFlags
-    {
-        /// <summary>
-        /// The lpBuffer parameter is a pointer to a PVOID pointer,
-        /// and that the nSize parameter specifies the minimum number
-        /// of TCHARs to allocate for an output message buffer. The
-        /// function allocates a buffer large enough to hold the
-        /// formatted message, and places a pointer to the allocated
-        /// buffer at the address specified by lpBuffer. The caller
-        /// should use the LocalFree function to free the buffer when
-        /// it is no longer needed.
-        /// </summary>
-        FORMAT_MESSAGE_ALLOCATE_BUFFER = 0x00000100,
+    FORMAT_MESSAGE_ALLOCATE_BUFFER = 0x00000100,
 
-        /// <summary>
-        /// Insert sequences in the message definition are to be ignored
-        /// and passed through to the output buffer unchanged. This flag
-        /// is useful for fetching a message for later formatting. If
-        /// this flag is set, the Arguments parameter is ignored.
-        /// </summary>
-        FORMAT_MESSAGE_IGNORE_INSERTS = 0x00000200,
+    /// <summary>
+    /// Insert sequences in the message definition are to be ignored
+    /// and passed through to the output buffer unchanged. This flag
+    /// is useful for fetching a message for later formatting. If
+    /// this flag is set, the Arguments parameter is ignored.
+    /// </summary>
+    FORMAT_MESSAGE_IGNORE_INSERTS = 0x00000200,
 
-        /// <summary>
-        /// The lpSource parameter is a pointer to a null-terminated
-        /// message definition. The message definition may contain insert
-        /// sequences, just as the message text in a message table
-        /// resource may. Cannot be used with FORMAT_MESSAGE_FROM_HMODULE
-        /// or FORMAT_MESSAGE_FROM_SYSTEM.
-        /// </summary>
-        FORMAT_MESSAGE_FROM_STRING = 0x00000400,
+    /// <summary>
+    /// The lpSource parameter is a pointer to a null-terminated
+    /// message definition. The message definition may contain insert
+    /// sequences, just as the message text in a message table
+    /// resource may. Cannot be used with FORMAT_MESSAGE_FROM_HMODULE
+    /// or FORMAT_MESSAGE_FROM_SYSTEM.
+    /// </summary>
+    FORMAT_MESSAGE_FROM_STRING = 0x00000400,
 
-        /// <summary>
-        /// The lpSource parameter is a module handle containing the
-        /// message-table resource(s) to search. If this lpSource handle
-        /// is NULL, the current process's application image file will
-        /// be searched. Cannot be used with FORMAT_MESSAGE_FROM_STRING.
-        /// </summary>
-        FORMAT_MESSAGE_FROM_HMODULE = 0x00000800,
+    /// <summary>
+    /// The lpSource parameter is a module handle containing the
+    /// message-table resource(s) to search. If this lpSource handle
+    /// is NULL, the current process's application image file will
+    /// be searched. Cannot be used with FORMAT_MESSAGE_FROM_STRING.
+    /// </summary>
+    FORMAT_MESSAGE_FROM_HMODULE = 0x00000800,
 
-        /// <summary>
-        /// <para>The function should search the system message-table
-        /// resource(s) for the requested message. If this flag is
-        /// specified with FORMAT_MESSAGE_FROM_HMODULE, the function
-        /// searches the system message table if the message is not
-        /// found in the module specified by lpSource. Cannot be used
-        /// with FORMAT_MESSAGE_FROM_STRING.</para>
-        /// <para>If this flag is specified, an application can pass
-        /// the result of the GetLastError function to retrieve the
-        /// message text for a system-defined error.</para>
-        /// </summary>
-        FORMAT_MESSAGE_FROM_SYSTEM = 0x00001000,
+    /// <summary>
+    /// <para>The function should search the system message-table
+    /// resource(s) for the requested message. If this flag is
+    /// specified with FORMAT_MESSAGE_FROM_HMODULE, the function
+    /// searches the system message table if the message is not
+    /// found in the module specified by lpSource. Cannot be used
+    /// with FORMAT_MESSAGE_FROM_STRING.</para>
+    /// <para>If this flag is specified, an application can pass
+    /// the result of the GetLastError function to retrieve the
+    /// message text for a system-defined error.</para>
+    /// </summary>
+    FORMAT_MESSAGE_FROM_SYSTEM = 0x00001000,
 
-        /// <summary>
-        /// <para>The Arguments parameter is not a va_list structure,
-        /// but is a pointer to an array of values that represent the
-        /// arguments.</para>
-        /// <para>This flag cannot be used with 64-bit argument values.
-        /// If you are using 64-bit values, you must use the va_list
-        /// structure.</para>
-        /// </summary>
-        FORMAT_MESSAGE_ARGUMENT_ARRAY = 0x00002000,
+    /// <summary>
+    /// <para>The Arguments parameter is not a va_list structure,
+    /// but is a pointer to an array of values that represent the
+    /// arguments.</para>
+    /// <para>This flag cannot be used with 64-bit argument values.
+    /// If you are using 64-bit values, you must use the va_list
+    /// structure.</para>
+    /// </summary>
+    FORMAT_MESSAGE_ARGUMENT_ARRAY = 0x00002000,
 
-        /// <summary>
-        /// ???
-        /// </summary>
-        FORMAT_MESSAGE_MAX_WIDTH_MASK = 0x000000FF
-
-    } // enum FormatMessageFlags
-
-} // namespace AM.Win32
+    /// <summary>
+    /// Маска для получения максимальной длины.
+    /// </summary>
+    FORMAT_MESSAGE_MAX_WIDTH_MASK = 0x000000FF
+}
--- Source/Libs/AM.Win32/Source/Kernel32/GlobalAllocFlags.cs
+++ Source/Libs/AM.Win32/Source/Kernel32/GlobalAllocFlags.cs
@@ -2,17 +2,12 @@
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
 // ReSharper disable CheckNamespace
-// ReSharper disable ClassNeverInstantiated.Global
 // ReSharper disable CommentTypo
-// ReSharper disable FieldCanBeMadeReadOnly.Global
 // ReSharper disable IdentifierTypo
 // ReSharper disable InconsistentNaming
-// ReSharper disable MemberCanBePrivate.Global
-// ReSharper disable StringLiteralTypo
 // ReSharper disable UnusedMember.Global
-// ReSharper disable UnusedType.Global
 
-/* GlobalAllocFlags.cs -- memory allocation flags
+/* GlobalAllocFlags.cs -- флаги, управляющие аллокацией памяти
    Ars Magna project, http://arsmagna.ru */
 
 #region Using directives
@@ -21,109 +16,106 @@ using System;
 
 #endregion
 
-namespace AM.Win32
+namespace AM.Win32;
+
+/// <summary>
+/// Флаги, управляющие аллокацией памяти.
+/// </summary>
+[Flags]
+public enum GlobalAllocFlags
 {
     /// <summary>
-    /// Memory allocation flags.
+    /// Allocates fixed memory. The return value is a pointer.
     /// </summary>
-    [Flags]
-    public enum GlobalAllocFlags
-    {
-        /// <summary>
-        /// Allocates fixed memory. The return value is a pointer.
-        /// </summary>
-        GMEM_FIXED = 0x0000,
+    GMEM_FIXED = 0x0000,
 
-        /// <summary>
-        /// <para>Allocates movable memory. Memory blocks are never
-        /// moved in physical memory, but they can be moved within the
-        /// default heap.</para>
-        /// <para>The return value is a handle to the memory object.
-        /// To translate the handle into a pointer, use the GlobalLock
-        /// function.</para>
-        /// <para>This value cannot be combined with GMEM_FIXED.</para>
-        /// </summary>
-        GMEM_MOVEABLE = 0x0002,
+    /// <summary>
+    /// <para>Allocates movable memory. Memory blocks are never
+    /// moved in physical memory, but they can be moved within the
+    /// default heap.</para>
+    /// <para>The return value is a handle to the memory object.
+    /// To translate the handle into a pointer, use the GlobalLock
+    /// function.</para>
+    /// <para>This value cannot be combined with GMEM_FIXED.</para>
+    /// </summary>
+    GMEM_MOVEABLE = 0x0002,
 
-        /// <summary>
-        /// Obsolete, but are provided for compatibility with 16-bit
-        /// Windows. Ignored.
-        /// </summary>
-        GMEM_NOCOMPACT = 0x0010,
+    /// <summary>
+    /// Obsolete, but are provided for compatibility with 16-bit
+    /// Windows. Ignored.
+    /// </summary>
+    GMEM_NOCOMPACT = 0x0010,
 
-        /// <summary>
-        /// Obsolete, but are provided for compatibility with 16-bit
-        /// Windows. Ignored.
-        /// </summary>
-        GMEM_NODISCARD = 0x0020,
+    /// <summary>
+    /// Obsolete, but are provided for compatibility with 16-bit
+    /// Windows. Ignored.
+    /// </summary>
+    GMEM_NODISCARD = 0x0020,
 
-        /// <summary>
-        /// Initializes memory contents to zero.
-        /// </summary>
-        GMEM_ZEROINIT = 0x0040,
+    /// <summary>
+    /// Initializes memory contents to zero.
+    /// </summary>
+    GMEM_ZEROINIT = 0x0040,
 
-        /// <summary>
-        /// Obsolete, but are provided for compatibility with 16-bit
-        /// Windows. Ignored.
-        /// </summary>
-        GMEM_MODIFY = 0x0080,
+    /// <summary>
+    /// Obsolete, but are provided for compatibility with 16-bit
+    /// Windows. Ignored.
+    /// </summary>
+    GMEM_MODIFY = 0x0080,
 
-        /// <summary>
-        /// Obsolete, but are provided for compatibility with 16-bit
-        /// Windows. Ignored.
-        /// </summary>
-        GMEM_DISCARDABLE = 0x0100,
+    /// <summary>
+    /// Obsolete, but are provided for compatibility with 16-bit
+    /// Windows. Ignored.
+    /// </summary>
+    GMEM_DISCARDABLE = 0x0100,
 
-        /// <summary>
-        /// Obsolete, but are provided for compatibility with 16-bit
-        /// Windows. Ignored.
-        /// </summary>
-        GMEM_NOT_BANKED = 0x1000,
+    /// <summary>
+    /// Obsolete, but are provided for compatibility with 16-bit
+    /// Windows. Ignored.
+    /// </summary>
+    GMEM_NOT_BANKED = 0x1000,
 
-        /// <summary>
-        /// Obsolete, but are provided for compatibility with 16-bit
-        /// Windows. Ignored.
-        /// </summary>
-        GMEM_SHARE = 0x2000,
+    /// <summary>
+    /// Obsolete, but are provided for compatibility with 16-bit
+    /// Windows. Ignored.
+    /// </summary>
+    GMEM_SHARE = 0x2000,
 
-        /// <summary>
-        /// Obsolete, but are provided for compatibility with 16-bit
-        /// Windows. Ignored.
-        /// </summary>
-        GMEM_DDESHARE = 0x2000,
+    /// <summary>
+    /// Obsolete, but are provided for compatibility with 16-bit
+    /// Windows. Ignored.
+    /// </summary>
+    GMEM_DDESHARE = 0x2000,
 
-        /// <summary>
-        /// Obsolete, but are provided for compatibility with 16-bit
-        /// Windows. Ignored.
-        /// </summary>
-        GMEM_NOTIFY = 0x4000,
+    /// <summary>
+    /// Obsolete, but are provided for compatibility with 16-bit
+    /// Windows. Ignored.
+    /// </summary>
+    GMEM_NOTIFY = 0x4000,
 
-        /// <summary>
-        /// Obsolete, but are provided for compatibility with 16-bit
-        /// Windows. Ignored.
-        /// </summary>
-        GMEM_LOWER = GMEM_NOT_BANKED,
+    /// <summary>
+    /// Obsolete, but are provided for compatibility with 16-bit
+    /// Windows. Ignored.
+    /// </summary>
+    GMEM_LOWER = GMEM_NOT_BANKED,
 
-        /// <summary>
-        /// ???
-        /// </summary>
-        GMEM_VALID_FLAGS = 0x7F72,
+    /// <summary>
+    /// ???
+    /// </summary>
+    GMEM_VALID_FLAGS = 0x7F72,
 
-        /// <summary>
-        /// ???
-        /// </summary>
-        GMEM_INVALID_HANDLE = 0x8000,
+    /// <summary>
+    /// ???
+    /// </summary>
+    GMEM_INVALID_HANDLE = 0x8000,
 
-        /// <summary>
-        /// Combines GMEM_MOVEABLE and GMEM_ZEROINIT.
-        /// </summary>
-        GHND = GMEM_MOVEABLE | GMEM_ZEROINIT,
+    /// <summary>
+    /// Combines GMEM_MOVEABLE and GMEM_ZEROINIT.
+    /// </summary>
+    GHND = GMEM_MOVEABLE | GMEM_ZEROINIT,
 
-        /// <summary>
-        /// Combines GMEM_FIXED and GMEM_ZEROINIT.
-        /// </summary>
-        GPTR = GMEM_FIXED | GMEM_ZEROINIT,
-
-    } // enum GlobalAllocFlags
-
-} // namespace AM.Win32
+    /// <summary>
+    /// Combines GMEM_FIXED and GMEM_ZEROINIT.
+    /// </summary>
+    GPTR = GMEM_FIXED | GMEM_ZEROINIT,
+}
