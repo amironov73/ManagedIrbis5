8b0f13ea13445bfa97f0609a4db2b0a5f5f8cdab
Alexey Mironov <amironov73@gmail.com>
2022-07-16 16:42:31 +0800

    Code cleanup

--- Source/Libs/AM.Win32/Source/Kernel32/KernelUtility.cs
+++ Source/Libs/AM.Win32/Source/Kernel32/KernelUtility.cs
@@ -2,15 +2,9 @@
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
 // ReSharper disable CheckNamespace
-// ReSharper disable ClassNeverInstantiated.Global
 // ReSharper disable CommentTypo
-// ReSharper disable FieldCanBeMadeReadOnly.Global
-// ReSharper disable IdentifierTypo
 // ReSharper disable InconsistentNaming
-// ReSharper disable MemberCanBePrivate.Global
-// ReSharper disable StringLiteralTypo
 // ReSharper disable UnusedMember.Global
-// ReSharper disable UnusedType.Global
 
 /* KernelUtil.cs -- kernel32.dll helper
    Ars Magna project, http://arsmagna.ru */
@@ -26,145 +20,142 @@ using System.Text;
 
 #nullable enable
 
-namespace AM.Win32
+namespace AM.Win32;
+
+/// <summary>
+/// Kernel32.dll helper.
+/// </summary>
+public static class KernelUtility
 {
-    /// <summary>
-    /// Kernel32.dll helper.
-    /// </summary>
-    public static class KernelUtility
+    #region Nested classes
+
+    private class CodePageEnumerator
     {
-        #region Nested classes
+        private List<string>? _result;
 
-        private class CodePageEnumerator
+        private bool _Callback (string codePage)
         {
-            private List<string>? _result;
-
-            private bool _Callback(string codePage)
-            {
-                _result!.Add(codePage);
-                return true;
-            }
-
-            public string[] Enumerate(bool onlyInstalled)
-            {
-                _result = new List<string>();
-                Kernel32.EnumSystemCodePages
-                    (
-                        _Callback,
-                        onlyInstalled
-                         ? CodePageEnumFlags.CP_INSTALLED
-                         : CodePageEnumFlags.CP_SUPPORTED
-                    );
-
-                return _result.ToArray();
-            }
+            _result!.Add (codePage);
+            return true;
         }
 
-        #endregion
-
-        #region Public methods
-
-        /// <summary>
-        /// Convert Win32 error code to string.
-        /// </summary>
-        public static string ErrorCodeToString
-            (
-                uint errorCode
-            )
+        public string[] Enumerate (bool onlyInstalled)
         {
-            const int bufferSize = 4 * 1024;
-            StringBuilder builder = new StringBuilder(bufferSize);
-            Kernel32.FormatMessage
+            _result = new List<string>();
+            Kernel32.EnumSystemCodePages
                 (
-                    FormatMessageFlags.FORMAT_MESSAGE_FROM_SYSTEM,
-                    0,
-                    errorCode,
-                    0,
-                    builder,
-                    bufferSize,
-                    0
+                    _Callback,
+                    onlyInstalled
+                        ? CodePageEnumFlags.CP_INSTALLED
+                        : CodePageEnumFlags.CP_SUPPORTED
                 );
 
-            return builder.ToString();
+            return _result.ToArray();
         }
+    }
 
-        /// <summary>
-        /// Fatal exit.
-        /// </summary>
-        public static void FatalExit
+    #endregion
+
+    #region Public methods
+
+    /// <summary>
+    /// Convert Win32 error code to string.
+    /// </summary>
+    public static string ErrorCodeToString
+        (
+            uint errorCode
+        )
+    {
+        const int bufferSize = 4 * 1024;
+        StringBuilder builder = new StringBuilder (bufferSize);
+        Kernel32.FormatMessage
             (
-                string format,
-                params object[] args
-            )
+                FormatMessageFlags.FORMAT_MESSAGE_FROM_SYSTEM,
+                0,
+                errorCode,
+                0,
+                builder,
+                bufferSize,
+                0
+            );
+
+        return builder.ToString();
+    }
+
+    /// <summary>
+    /// Fatal exit.
+    /// </summary>
+    public static void FatalExit
+        (
+            string format,
+            params object[] args
+        )
+    {
+        Kernel32.FatalAppExit (0, string.Format (format, args));
+    }
+
+    /// <summary>
+    /// Enumerates system code pages.
+    /// </summary>
+    /// <remarks>
+    /// Типичный вывод:
+    /// 10000, 10006, 10007, 10010, 10017, 10029, 10079, 10081,
+    /// 10082, 1026, 1250, 1251, 1252, 1253, 1254, 1255, 1256,
+    /// 1257, 1258, 1361, 20127, 20261, 20866, 21866, 28591, 28592,
+    /// 28594, 28595, 28597, 28599, 28605, 37, 437, 500, 737, 775,
+    /// 850, 852, 855, 857, 860, 861, 863, 865, 866, 869, 874, 875,
+    /// 932, 936, 949, 950, 28603, 65000, 65001
+    /// </remarks>
+    public static string[] GetSystemCodePages
+        (
+            bool onlyInstalled
+        )
+    {
+        return new CodePageEnumerator().Enumerate (onlyInstalled);
+    }
+
+    /// <summary>
+    /// Retrieves the size of a specified file.
+    /// </summary>
+    /// <param name="handle">Handle to the file whose size is to
+    /// be returned. This handle must have been created with either
+    /// the GENERIC_READ or GENERIC_WRITE access right.</param>
+    /// <returns>File size.</returns>
+    public static long GetFileSize
+        (
+            IntPtr handle
+        )
+    {
+        int highPart = 0;
+        uint lowPart = Kernel32.GetFileSize (handle, ref highPart);
+
+        return ((long)highPart << 32) | lowPart;
+    }
+
+    /// <summary>
+    /// Gets the name of the short path.
+    /// </summary>
+    public static string GetShortPathName
+        (
+            string longName
+        )
+    {
+        var buffer = new StringBuilder { Capacity = 300 };
+        if (Kernel32.GetShortPathName (longName, buffer, buffer.Capacity) <= 0)
         {
-            Kernel32.FatalAppExit(0, string.Format(format, args));
+            throw new Win32Exception();
         }
 
-        /// <summary>
-        /// Enumerates system code pages.
-        /// </summary>
-        /// <remarks>
-        /// Типичный вывод:
-        /// 10000, 10006, 10007, 10010, 10017, 10029, 10079, 10081,
-        /// 10082, 1026, 1250, 1251, 1252, 1253, 1254, 1255, 1256,
-        /// 1257, 1258, 1361, 20127, 20261, 20866, 21866, 28591, 28592,
-        /// 28594, 28595, 28597, 28599, 28605, 37, 437, 500, 737, 775,
-        /// 850, 852, 855, 857, 860, 861, 863, 865, 866, 869, 874, 875,
-        /// 932, 936, 949, 950, 28603, 65000, 65001
-        /// </remarks>
-        public static string[] GetSystemCodePages
-            (
-                bool onlyInstalled
-            )
-        {
-            return new CodePageEnumerator().Enumerate(onlyInstalled);
-        }
+        return buffer.ToString();
+    }
 
-        /// <summary>
-        /// Retrieves the size of a specified file.
-        /// </summary>
-        /// <param name="handle">Handle to the file whose size is to
-        /// be returned. This handle must have been created with either
-        /// the GENERIC_READ or GENERIC_WRITE access right.</param>
-        /// <returns>File size.</returns>
-        public static long GetFileSize
-            (
-                IntPtr handle
-            )
-        {
-            int highPart = 0;
-            uint lowPart = Kernel32.GetFileSize(handle, ref highPart);
+    /// <summary>
+    /// Retrieves handle for main module.
+    /// </summary>
+    public static IntPtr MainModuleHandle()
+    {
+        return Kernel32.GetModuleHandle (null);
+    }
 
-            return ((long)highPart << 32) | lowPart;
-        }
-
-        /// <summary>
-        /// Gets the name of the short path.
-        /// </summary>
-        public static string GetShortPathName
-            (
-                string longName
-            )
-        {
-            var buffer = new StringBuilder { Capacity = 300 };
-            if (Kernel32.GetShortPathName(longName, buffer, buffer.Capacity) <= 0)
-            {
-                throw new Win32Exception();
-            }
-
-            return buffer.ToString();
-        }
-
-        /// <summary>
-        /// Retrieves handle for main module.
-        /// </summary>
-        public static IntPtr MainModuleHandle()
-        {
-            return Kernel32.GetModuleHandle(null);
-        }
-
-        #endregion
-
-    } // class KernelUtility
-
-} // namespace AM.Win32
+    #endregion
+}
--- Source/Libs/AM.Win32/Source/Kernel32/LoadLibraryFlags.cs
+++ Source/Libs/AM.Win32/Source/Kernel32/LoadLibraryFlags.cs
@@ -2,15 +2,9 @@
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
 // ReSharper disable CheckNamespace
-// ReSharper disable ClassNeverInstantiated.Global
 // ReSharper disable CommentTypo
-// ReSharper disable FieldCanBeMadeReadOnly.Global
-// ReSharper disable IdentifierTypo
 // ReSharper disable InconsistentNaming
-// ReSharper disable MemberCanBePrivate.Global
-// ReSharper disable StringLiteralTypo
 // ReSharper disable UnusedMember.Global
-// ReSharper disable UnusedType.Global
 
 /* LoadLibraryFlags.cs -- flags for LoadLibraryEx function
    Ars Magna project, http://arsmagna.ru */
@@ -21,62 +15,59 @@ using System;
 
 #endregion
 
-namespace AM.Win32
+namespace AM.Win32;
+
+/// <summary>
+/// Flags for LoadLibraryEx function.
+/// </summary>
+[Flags]
+public enum LoadLibraryFlags
 {
     /// <summary>
-    /// Flags for LoadLibraryEx function.
+    /// <para>If this value is used, and the executable module is
+    /// a DLL, the system does not call DllMain for process and
+    /// thread initialization and termination. Also, the system
+    /// does not load additional executable modules that are
+    /// referenced by the specified module.</para>
+    /// <para>If this value is not used, and the executable module
+    /// is a DLL, the system calls DllMain for process and thread
+    /// initialization and termination. The system loads additional
+    /// executable modules that are referenced by the specified
+    /// module.</para>
+    /// <para>Windows Me/98/95: This value is not supported.
+    /// </para></summary>
+    DONT_RESOLVE_DLL_REFERENCES = 0x00000001,
+
+    /// <summary>
+    /// <para>If this value is used, the system maps the file into
+    /// the calling process's virtual address space as if it were
+    /// a data file. Nothing is done to execute or prepare to
+    /// execute the mapped file. Use this flag when you want to
+    /// load a DLL only to extract messages or resources from it.
+    /// </para>
+    /// <para>Windows Me/98/95: You can use the resulting module
+    /// handle only with resource management functions such as
+    /// EnumResourceLanguages, EnumResourceNames, EnumResourceTypes,
+    /// FindResource, FindResourceEx, LoadResource, and
+    /// SizeofResource. You cannot use this handle with specialized
+    /// resource management functions such as LoadBitmap,
+    /// LoadCursor, LoadIcon, LoadImage, and LoadMenu.</para>
     /// </summary>
-    [Flags]
-    public enum LoadLibraryFlags
-    {
-        /// <summary>
-        /// <para>If this value is used, and the executable module is
-        /// a DLL, the system does not call DllMain for process and
-        /// thread initialization and termination. Also, the system
-        /// does not load additional executable modules that are
-        /// referenced by the specified module.</para>
-        /// <para>If this value is not used, and the executable module
-        /// is a DLL, the system calls DllMain for process and thread
-        /// initialization and termination. The system loads additional
-        /// executable modules that are referenced by the specified
-        /// module.</para>
-        /// <para>Windows Me/98/95: This value is not supported.
-        /// </para></summary>
-        DONT_RESOLVE_DLL_REFERENCES = 0x00000001,
+    LOAD_LIBRARY_AS_DATAFILE = 0x00000002,
 
-        /// <summary>
-        /// <para>If this value is used, the system maps the file into
-        /// the calling process's virtual address space as if it were
-        /// a data file. Nothing is done to execute or prepare to
-        /// execute the mapped file. Use this flag when you want to
-        /// load a DLL only to extract messages or resources from it.
-        /// </para>
-        /// <para>Windows Me/98/95: You can use the resulting module
-        /// handle only with resource management functions such as
-        /// EnumResourceLanguages, EnumResourceNames, EnumResourceTypes,
-        /// FindResource, FindResourceEx, LoadResource, and
-        /// SizeofResource. You cannot use this handle with specialized
-        /// resource management functions such as LoadBitmap,
-        /// LoadCursor, LoadIcon, LoadImage, and LoadMenu.</para>
-        /// </summary>
-        LOAD_LIBRARY_AS_DATAFILE = 0x00000002,
+    /// <summary>
+    /// <para>If this value is used, and lpFileName specifies a path,
+    /// the system uses the alternate file search strategy.</para>
+    /// <para>If this value is not used, or if lpFileName does not
+    /// specify a path, the system uses the standard search strategy.
+    /// </para></summary>
+    LOAD_WITH_ALTERED_SEARCH_PATH = 0x00000008,
 
-        /// <summary>
-        /// <para>If this value is used, and lpFileName specifies a path,
-        /// the system uses the alternate file search strategy.</para>
-        /// <para>If this value is not used, or if lpFileName does not
-        /// specify a path, the system uses the standard search strategy.
-        /// </para></summary>
-        LOAD_WITH_ALTERED_SEARCH_PATH = 0x00000008,
-
-        /// <summary>
-        /// <para>If this value is used, the system does not perform
-        /// automatic trust comparisons on the DLL or its dependents
-        /// when they are loaded.</para>
-        /// <para>Windows 2000/NT, Windows Me/98/95:  This value is
-        /// not supported.</para></summary>
-        LOAD_IGNORE_CODE_AUTHZ_LEVEL = 0x00000010
-
-    } // enum LoadLibraryFlags
-
-} // namespace AM.Win32
+    /// <summary>
+    /// <para>If this value is used, the system does not perform
+    /// automatic trust comparisons on the DLL or its dependents
+    /// when they are loaded.</para>
+    /// <para>Windows 2000/NT, Windows Me/98/95:  This value is
+    /// not supported.</para></summary>
+    LOAD_IGNORE_CODE_AUTHZ_LEVEL = 0x00000010
+}
