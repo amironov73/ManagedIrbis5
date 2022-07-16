25b3ae1c79a057f4d6d6e6bffe22620d080120bf
Alexey Mironov <amironov73@gmail.com>
2022-07-16 18:58:59 +0800

    Code cleanup

--- Source/Libs/AM.Win32/Source/Kernel32/MEMORYSTATUS.cs
+++ Source/Libs/AM.Win32/Source/Kernel32/MEMORYSTATUS.cs
@@ -2,15 +2,10 @@
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
 // ReSharper disable CheckNamespace
-// ReSharper disable ClassNeverInstantiated.Global
 // ReSharper disable CommentTypo
-// ReSharper disable FieldCanBeMadeReadOnly.Global
 // ReSharper disable IdentifierTypo
 // ReSharper disable InconsistentNaming
-// ReSharper disable MemberCanBePrivate.Global
-// ReSharper disable StringLiteralTypo
 // ReSharper disable UnusedMember.Global
-// ReSharper disable UnusedType.Global
 
 /* MEMORYSTATUS.cs -- information about the current state of both physical and virtual memory
    Ars Magna project, http://arsmagna.ru */
@@ -22,71 +17,68 @@ using System.Runtime.InteropServices;
 
 #endregion
 
-namespace AM.Win32
+namespace AM.Win32;
+
+/// <summary>
+/// The MEMORYSTATUS structure contains information about
+/// the current state of both physical and virtual memory.
+/// The GlobalMemoryStatus function stores information in
+/// a MEMORYSTATUS structure.
+/// </summary>
+[Serializable]
+[StructLayout (LayoutKind.Sequential, Pack = 4, Size = StructureSize)]
+public struct MEMORYSTATUS
 {
     /// <summary>
-    /// The MEMORYSTATUS structure contains information about
-    /// the current state of both physical and virtual memory.
-    /// The GlobalMemoryStatus function stores information in
-    /// a MEMORYSTATUS structure.
+    /// Structure size.
     /// </summary>
-    [Serializable]
-    [StructLayout(LayoutKind.Sequential, Pack = 4, Size = StructureSize)]
-    public struct MEMORYSTATUS
-    {
-        /// <summary>
-        /// Structure size.
-        /// </summary>
-        public const int StructureSize = 32;
+    public const int StructureSize = 32;
 
-        /// <summary>
-        /// Size of the MEMORYSTATUS data structure, in bytes.
-        /// You do not need to set this member before calling the
-        /// GlobalMemoryStatus function; the function sets it.
-        /// </summary>
-        public uint dwLength;
+    /// <summary>
+    /// Size of the MEMORYSTATUS data structure, in bytes.
+    /// You do not need to set this member before calling the
+    /// GlobalMemoryStatus function; the function sets it.
+    /// </summary>
+    public uint dwLength;
 
-        /// <summary>
-        /// <para>Approximate percentage of total physical memory
-        /// that is in use.</para>
-        /// <para>Windows NT:  Percentage of approximately the last
-        /// 1000 pages of physical memory that is in use.</para>
-        /// </summary>
-        public uint dwMemoryLoad;
+    /// <summary>
+    /// <para>Approximate percentage of total physical memory
+    /// that is in use.</para>
+    /// <para>Windows NT:  Percentage of approximately the last
+    /// 1000 pages of physical memory that is in use.</para>
+    /// </summary>
+    public uint dwMemoryLoad;
 
-        /// <summary>
-        /// Total size of physical memory, in bytes.
-        /// </summary>
-        public uint dwTotalPhys;
+    /// <summary>
+    /// Total size of physical memory, in bytes.
+    /// </summary>
+    public uint dwTotalPhys;
 
-        /// <summary>
-        /// Size of physical memory available, in bytes.
-        /// </summary>
-        public uint dwAvailPhys;
+    /// <summary>
+    /// Size of physical memory available, in bytes.
+    /// </summary>
+    public uint dwAvailPhys;
 
-        /// <summary>
-        /// Size of the committed memory limit, in bytes.
-        /// </summary>
-        public uint dwTotalPageFile;
+    /// <summary>
+    /// Size of the committed memory limit, in bytes.
+    /// </summary>
+    public uint dwTotalPageFile;
 
-        /// <summary>
-        /// Size of available memory to commit, in bytes.
-        /// </summary>
-        public uint dwAvailPageFile;
+    /// <summary>
+    /// Size of available memory to commit, in bytes.
+    /// </summary>
+    public uint dwAvailPageFile;
 
-        /// <summary>
-        /// Total size of the user mode portion of the virtual address
-        /// space of the calling process, in bytes.
-        /// </summary>
-        public uint dwTotalVirtual;
+    /// <summary>
+    /// Total size of the user mode portion of the virtual address
+    /// space of the calling process, in bytes.
+    /// </summary>
+    public uint dwTotalVirtual;
 
-        /// <summary>
-        /// Size of unreserved and uncommitted memory in the user mode
-        /// portion of the virtual address space of the calling process,
-        /// in bytes.
-        /// </summary>
-        public uint dwAvailVirtual;
-
-    } // struct MEMORYSTATUS
-
-} // namespace AM.Win32
+    /// <summary>
+    /// Size of unreserved and uncommitted memory in the user mode
+    /// portion of the virtual address space of the calling process,
+    /// in bytes.
+    /// </summary>
+    public uint dwAvailVirtual;
+}
--- Source/Libs/AM.Win32/Source/Kernel32/MEMORYSTATUSEX.cs
+++ Source/Libs/AM.Win32/Source/Kernel32/MEMORYSTATUSEX.cs
@@ -2,15 +2,10 @@
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
 // ReSharper disable CheckNamespace
-// ReSharper disable ClassNeverInstantiated.Global
 // ReSharper disable CommentTypo
-// ReSharper disable FieldCanBeMadeReadOnly.Global
 // ReSharper disable IdentifierTypo
 // ReSharper disable InconsistentNaming
-// ReSharper disable MemberCanBePrivate.Global
-// ReSharper disable StringLiteralTypo
 // ReSharper disable UnusedMember.Global
-// ReSharper disable UnusedType.Global
 
 /* MEMORYSTATUSEX.cs -- information about the current state of both physical and virtual memory
    Ars Magna project, http://arsmagna.ru */
@@ -22,84 +17,81 @@ using System.Runtime.InteropServices;
 
 #endregion
 
-namespace AM.Win32
+namespace AM.Win32;
+
+/// <summary>
+/// The MEMORYSTATUSEX structure contains information about
+/// the current state of both physical and virtual memory,
+/// including extended memory. The GlobalMemoryStatusEx function
+/// stores information in this structure.
+/// </summary>
+///
+/// <remarks>MEMORYSTATUSEX reflects the state of memory at the
+/// time of the call. It reflects the size of the paging file at
+/// that time. The operating system can enlarge the paging file
+/// up to the maximum size set by the administrator.</remarks>
+[Serializable]
+[StructLayout (LayoutKind.Sequential, Pack = 4, Size = StructureSize)]
+public struct MEMORYSTATUSEX
 {
     /// <summary>
-    /// The MEMORYSTATUSEX structure contains information about
-    /// the current state of both physical and virtual memory,
-    /// including extended memory. The GlobalMemoryStatusEx function
-    /// stores information in this structure.
+    /// Structure size.
     /// </summary>
-    ///
-    /// <remarks>MEMORYSTATUSEX reflects the state of memory at the
-    /// time of the call. It reflects the size of the paging file at
-    /// that time. The operating system can enlarge the paging file
-    /// up to the maximum size set by the administrator.</remarks>
-    [Serializable]
-    [StructLayout(LayoutKind.Sequential, Pack = 4, Size = StructureSize)]
-    public struct MEMORYSTATUSEX
-    {
-        /// <summary>
-        /// Structure size.
-        /// </summary>
-        public const int StructureSize = 64;
+    public const int StructureSize = 64;
 
-        /// <summary>
-        /// Size of the structure, in bytes. You must set this member
-        /// before calling GlobalMemoryStatusEx.
-        /// </summary>
-        public uint dwLength;
+    /// <summary>
+    /// Size of the structure, in bytes. You must set this member
+    /// before calling GlobalMemoryStatusEx.
+    /// </summary>
+    public uint dwLength;
 
-        /// <summary>
-        /// Number between 0 and 100 that gives a general idea of
-        /// current memory utilization, in which 0 indicates no memory
-        /// use and 100 indicates full memory use.
-        /// </summary>
-        public uint dwMemoryLoad;
+    /// <summary>
+    /// Number between 0 and 100 that gives a general idea of
+    /// current memory utilization, in which 0 indicates no memory
+    /// use and 100 indicates full memory use.
+    /// </summary>
+    public uint dwMemoryLoad;
 
-        /// <summary>
-        /// Total size of physical memory, in bytes.
-        /// </summary>
-        public ulong ullTotalPhys;
+    /// <summary>
+    /// Total size of physical memory, in bytes.
+    /// </summary>
+    public ulong ullTotalPhys;
 
-        /// <summary>
-        /// Size of physical memory available, in bytes.
-        /// </summary>
-        public ulong ullAvailPhys;
+    /// <summary>
+    /// Size of physical memory available, in bytes.
+    /// </summary>
+    public ulong ullAvailPhys;
 
-        /// <summary>
-        /// Size of the committed memory limit, in bytes. This is
-        /// physical memory plus the size of the page file, minus
-        /// a small overhead.
-        /// </summary>
-        public ulong ullTotalPageFile;
+    /// <summary>
+    /// Size of the committed memory limit, in bytes. This is
+    /// physical memory plus the size of the page file, minus
+    /// a small overhead.
+    /// </summary>
+    public ulong ullTotalPageFile;
 
-        /// <summary>
-        /// Size of available memory to commit, in bytes. The limit
-        /// is ullTotalPageFile.
-        /// </summary>
-        public ulong ullAvailPageFile;
+    /// <summary>
+    /// Size of available memory to commit, in bytes. The limit
+    /// is ullTotalPageFile.
+    /// </summary>
+    public ulong ullAvailPageFile;
 
-        /// <summary>
-        /// Total size of the user mode portion of the virtual address
-        /// space of the calling process, in bytes.
-        /// </summary>
-        public ulong ullTotalVirtual;
+    /// <summary>
+    /// Total size of the user mode portion of the virtual address
+    /// space of the calling process, in bytes.
+    /// </summary>
+    public ulong ullTotalVirtual;
 
-        /// <summary>
-        /// Size of unreserved and uncommitted memory in the user mode
-        /// portion of the virtual address space of the calling process,
-        /// in bytes.
-        /// </summary>
-        public ulong ullAvailVirtual;
+    /// <summary>
+    /// Size of unreserved and uncommitted memory in the user mode
+    /// portion of the virtual address space of the calling process,
+    /// in bytes.
+    /// </summary>
+    public ulong ullAvailVirtual;
 
-        /// <summary>
-        /// Size of unreserved and uncommitted memory in the extended
-        /// portion of the virtual address space of the calling process,
-        /// in bytes.
-        /// </summary>
-        public ulong ullAvailExtendedVirtual;
-
-    } // struct MEMORYSTATUSEX
-
-} // namespace AM.Win32
+    /// <summary>
+    /// Size of unreserved and uncommitted memory in the extended
+    /// portion of the virtual address space of the calling process,
+    /// in bytes.
+    /// </summary>
+    public ulong ullAvailExtendedVirtual;
+}
