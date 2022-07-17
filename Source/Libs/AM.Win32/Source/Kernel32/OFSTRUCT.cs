a246c878d708ed95029b68fb54b0508e98adfe86
Alexey Mironov <amironov73@gmail.com>
2022-07-17 11:18:05 +0800

    Code cleanup

--- Source/Libs/AM.Win32/Source/Kernel32/OFSTRUCT.cs
+++ Source/Libs/AM.Win32/Source/Kernel32/OFSTRUCT.cs
@@ -2,15 +2,10 @@
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
 // ReSharper disable CheckNamespace
-// ReSharper disable ClassNeverInstantiated.Global
 // ReSharper disable CommentTypo
-// ReSharper disable FieldCanBeMadeReadOnly.Global
 // ReSharper disable IdentifierTypo
 // ReSharper disable InconsistentNaming
-// ReSharper disable MemberCanBePrivate.Global
-// ReSharper disable StringLiteralTypo
 // ReSharper disable UnusedMember.Global
-// ReSharper disable UnusedType.Global
 
 /* OFSTRUCT.cs -- information about a file that the OpenFile function opened or attempted to open
    Ars Magna project, http://arsmagna.ru */
@@ -22,58 +17,55 @@ using System.Runtime.InteropServices;
 
 #endregion
 
-namespace AM.Win32
+namespace AM.Win32;
+
+/// <summary>
+/// Contains information about a file that the OpenFile function
+/// opened or attempted to open.
+/// </summary>
+[Serializable]
+[StructLayout (LayoutKind.Sequential, Size = SIZE)]
+public struct OFSTRUCT
 {
     /// <summary>
-    /// Contains information about a file that the OpenFile function
-    /// opened or attempted to open.
+    /// Size of the structure, in bytes.
     /// </summary>
-    [Serializable]
-    [StructLayout(LayoutKind.Sequential, Size = SIZE)]
-    public struct OFSTRUCT
-    {
-        /// <summary>
-        /// Size of the structure, in bytes.
-        /// </summary>
-        public const int SIZE = 136;
+    public const int SIZE = 136;
 
-        /// <summary>
-        ///
-        /// </summary>
-        public const int OFS_MAXPATHNAME = 128;
+    /// <summary>
+    ///
+    /// </summary>
+    public const int OFS_MAXPATHNAME = 128;
 
-        /// <summary>
-        /// Length of the structure, in bytes.
-        /// </summary>
-        public byte cBytes;
+    /// <summary>
+    /// Length of the structure, in bytes.
+    /// </summary>
+    public byte cBytes;
 
-        /// <summary>
-        /// If this member is nonzero, the file is on a hard
-        /// (fixed) disk. Otherwise, it is not.
-        /// </summary>
-        public byte fFixedDisk;
+    /// <summary>
+    /// If this member is nonzero, the file is on a hard
+    /// (fixed) disk. Otherwise, it is not.
+    /// </summary>
+    public byte fFixedDisk;
 
-        /// <summary>
-        /// MS-DOS error code if the OpenFile function failed.
-        /// </summary>
-        public short nErrCode;
+    /// <summary>
+    /// MS-DOS error code if the OpenFile function failed.
+    /// </summary>
+    public short nErrCode;
 
-        /// <summary>
-        /// Reserved; do not use.
-        /// </summary>
-        public short Reserved1;
+    /// <summary>
+    /// Reserved; do not use.
+    /// </summary>
+    public short Reserved1;
 
-        /// <summary>
-        /// Reserved; do not use.
-        /// </summary>
-        public short Reserved2;
+    /// <summary>
+    /// Reserved; do not use.
+    /// </summary>
+    public short Reserved2;
 
-        /// <summary>
-        /// Path and file name of the file.
-        /// </summary>
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = OFS_MAXPATHNAME)]
-        public string szPathName;
-
-    } // struct OFSTRUCT
-
-} // namespace AM.Win32
+    /// <summary>
+    /// Path and file name of the file.
+    /// </summary>
+    [MarshalAs (UnmanagedType.ByValTStr, SizeConst = OFS_MAXPATHNAME)]
+    public string szPathName;
+}
--- Source/Libs/AM.Win32/Source/Kernel32/OVERLAPPED.cs
+++ Source/Libs/AM.Win32/Source/Kernel32/OVERLAPPED.cs
@@ -2,15 +2,10 @@
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
 // ReSharper disable CheckNamespace
-// ReSharper disable ClassNeverInstantiated.Global
 // ReSharper disable CommentTypo
-// ReSharper disable FieldCanBeMadeReadOnly.Global
 // ReSharper disable IdentifierTypo
 // ReSharper disable InconsistentNaming
-// ReSharper disable MemberCanBePrivate.Global
-// ReSharper disable StringLiteralTypo
 // ReSharper disable UnusedMember.Global
-// ReSharper disable UnusedType.Global
 
 /* OVERLAPPED.cs -- information used in asynchronous input and output
    Ars Magna project, http://arsmagna.ru */
@@ -22,57 +17,54 @@ using System.Runtime.InteropServices;
 
 #endregion
 
-namespace AM.Win32
+namespace AM.Win32;
+
+/// <summary>
+/// Contains information used in asynchronous input and output (I/O).
+/// </summary>
+[Serializable]
+[StructLayout (LayoutKind.Sequential, Size = 20)]
+public struct OVERLAPPED
 {
     /// <summary>
-    /// Contains information used in asynchronous input and output (I/O).
+    /// Reserved for operating system use. This member, which
+    /// specifies a system-dependent status, is valid when the
+    /// GetOverlappedResult function returns without setting the
+    /// extended error information to ERROR_IO_PENDING.
     /// </summary>
-    [Serializable]
-    [StructLayout(LayoutKind.Sequential, Size = 20)]
-    public struct OVERLAPPED
-    {
-        /// <summary>
-        /// Reserved for operating system use. This member, which
-        /// specifies a system-dependent status, is valid when the
-        /// GetOverlappedResult function returns without setting the
-        /// extended error information to ERROR_IO_PENDING.
-        /// </summary>
-        public int Internal;
+    public int Internal;
 
-        /// <summary>
-        /// Reserved for operating system use. This member, which
-        /// specifies the length of the data transferred, is valid
-        /// when the GetOverlappedResult function returns TRUE.
-        /// </summary>
-        public int InternalHigh;
+    /// <summary>
+    /// Reserved for operating system use. This member, which
+    /// specifies the length of the data transferred, is valid
+    /// when the GetOverlappedResult function returns TRUE.
+    /// </summary>
+    public int InternalHigh;
 
-        /// <summary>
-        /// File position at which to start the transfer. The file
-        /// position is a byte offset from the start of the file.
-        /// The calling process sets this member before calling the
-        /// ReadFile or WriteFile function. This member is ignored
-        /// when reading from or writing to named pipes and
-        /// communications devices and should be zero.
-        /// </summary>
-        public uint Offset;
+    /// <summary>
+    /// File position at which to start the transfer. The file
+    /// position is a byte offset from the start of the file.
+    /// The calling process sets this member before calling the
+    /// ReadFile or WriteFile function. This member is ignored
+    /// when reading from or writing to named pipes and
+    /// communications devices and should be zero.
+    /// </summary>
+    public uint Offset;
 
-        /// <summary>
-        /// High-order word of the byte offset at which to start the
-        /// transfer. This member is ignored when reading from or
-        /// writing to named pipes and communications devices and
-        /// should be zero.
-        /// </summary>
-        public uint OffsetHigh;
+    /// <summary>
+    /// High-order word of the byte offset at which to start the
+    /// transfer. This member is ignored when reading from or
+    /// writing to named pipes and communications devices and
+    /// should be zero.
+    /// </summary>
+    public uint OffsetHigh;
 
-        /// <summary>
-        /// Handle to an event set to the signaled state when the
-        /// operation has been completed. The calling process must set
-        /// this member either to zero or a valid event handle before
-        /// calling any overlapped functions. To create an event object,
-        /// use the CreateEvent function.
-        /// </summary>
-        public IntPtr hEvent;
-
-    } // struct OVERLAPPED
-
-} // namespace AM.Win32
+    /// <summary>
+    /// Handle to an event set to the signaled state when the
+    /// operation has been completed. The calling process must set
+    /// this member either to zero or a valid event handle before
+    /// calling any overlapped functions. To create an event object,
+    /// use the CreateEvent function.
+    /// </summary>
+    public IntPtr hEvent;
+}
