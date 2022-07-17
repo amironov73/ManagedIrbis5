27d159d024d31c5a9d1c3e8b407ae19f8811f1e0
Alexey Mironov <amironov73@gmail.com>
2022-07-17 11:42:12 +0800

    Code cleanup

--- Source/Libs/AM.Win32/Source/Kernel32/PipeWaitFlags.cs
+++ Source/Libs/AM.Win32/Source/Kernel32/PipeWaitFlags.cs
@@ -2,15 +2,10 @@
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
 // ReSharper disable CheckNamespace
-// ReSharper disable ClassNeverInstantiated.Global
 // ReSharper disable CommentTypo
-// ReSharper disable FieldCanBeMadeReadOnly.Global
 // ReSharper disable IdentifierTypo
 // ReSharper disable InconsistentNaming
-// ReSharper disable MemberCanBePrivate.Global
-// ReSharper disable StringLiteralTypo
 // ReSharper disable UnusedMember.Global
-// ReSharper disable UnusedType.Global
 
 /* PipeWaitFlags.cs -- wait intervals for named pipes
    Ars Magna project, http://arsmagna.ru */
@@ -21,31 +16,28 @@ using System;
 
 #endregion
 
-namespace AM.Win32
+namespace AM.Win32;
+
+/// <summary>
+/// Wait intervals for named pipes.
+/// </summary>
+[Flags]
+public enum PipeWaitFlags
 {
     /// <summary>
-    /// Wait intervals for named pipes.
+    /// Waits indefinitely.
     /// </summary>
-    [Flags]
-    public enum PipeWaitFlags
-    {
-        /// <summary>
-        /// Waits indefinitely.
-        /// </summary>
-        NMPWAIT_WAIT_FOREVER = unchecked((int)0xffffffff),
+    NMPWAIT_WAIT_FOREVER = unchecked ((int)0xffffffff),
 
-        /// <summary>
-        /// Does not wait for the named pipe. If the named pipe
-        /// is not available, the function returns an error.
-        /// </summary>
-        NMPWAIT_NOWAIT = 0x00000001,
+    /// <summary>
+    /// Does not wait for the named pipe. If the named pipe
+    /// is not available, the function returns an error.
+    /// </summary>
+    NMPWAIT_NOWAIT = 0x00000001,
 
-        /// <summary>
-        /// Uses the default time-out specified in a call to the
-        /// CreateNamedPipe function.
-        /// </summary>
-        NMPWAIT_USE_DEFAULT_WAIT = 0x00000000
-
-    } // enum PipeWaitFlags
-
-} // namespace AM.Win32
+    /// <summary>
+    /// Uses the default time-out specified in a call to the
+    /// CreateNamedPipe function.
+    /// </summary>
+    NMPWAIT_USE_DEFAULT_WAIT = 0x00000000
+}
