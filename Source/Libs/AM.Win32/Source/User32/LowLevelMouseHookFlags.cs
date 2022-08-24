77432b7862e51a227c7e64682392e4584307bc6a
Alexey Mironov <amironov73@gmail.com>
2022-08-24 13:35:03 +0800

    Code cleanup

--- Source/Libs/AM.Win32/Source/User32/LowLevelMouseHookFlags.cs
+++ Source/Libs/AM.Win32/Source/User32/LowLevelMouseHookFlags.cs
@@ -2,17 +2,12 @@
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
 // ReSharper disable CheckNamespace
-// ReSharper disable ClassNeverInstantiated.Global
 // ReSharper disable CommentTypo
-// ReSharper disable FieldCanBeMadeReadOnly.Global
 // ReSharper disable IdentifierTypo
 // ReSharper disable InconsistentNaming
-// ReSharper disable MemberCanBePrivate.Global
-// ReSharper disable StringLiteralTypo
 // ReSharper disable UnusedMember.Global
-// ReSharper disable UnusedType.Global
 
-/* LowLevelMouseHookFlags.cs -- event-injected flags
+/* LowLevelMouseHookFlags.cs -- флаг для добавленных событий
    Ars Magna project, http://arsmagna.ru */
 
 #region Using directives
@@ -21,19 +16,16 @@ using System;
 
 #endregion
 
-namespace AM.Win32
+namespace AM.Win32;
+
+/// <summary>
+/// Флаг для добавленных событий.
+/// </summary>
+[Flags]
+public enum LowLevelMouseHookFlags
 {
     /// <summary>
-    /// Specifies the event-injected flag.
+    /// Test the event-injected flag.
     /// </summary>
-    [Flags]
-    public enum LowLevelMouseHookFlags
-    {
-        /// <summary>
-        /// Test the event-injected flag.
-        /// </summary>
-        LLMHF_INJECTED = 0x00000001
-
-    } // enum LowLevelMouseHookFlags
-
-} // namespace AM.Win32
+    LLMHF_INJECTED = 0x00000001
+}
--- Source/Libs/AM.Win32/Source/User32/MessageBeepFlags.cs
+++ Source/Libs/AM.Win32/Source/User32/MessageBeepFlags.cs
@@ -2,57 +2,49 @@
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
 // ReSharper disable CheckNamespace
-// ReSharper disable ClassNeverInstantiated.Global
 // ReSharper disable CommentTypo
-// ReSharper disable FieldCanBeMadeReadOnly.Global
 // ReSharper disable IdentifierTypo
 // ReSharper disable InconsistentNaming
-// ReSharper disable MemberCanBePrivate.Global
-// ReSharper disable StringLiteralTypo
 // ReSharper disable UnusedMember.Global
-// ReSharper disable UnusedType.Global
 
-/* MessageBeepFlags.cs -- sound type
+/* MessageBeepFlags.cs -- тип звука для функции MessageBeep
    Ars Magna project, http://arsmagna.ru */
 
-namespace AM.Win32
+namespace AM.Win32;
+
+/// <summary>
+/// Тип звука, определяемый записью в реестре.
+/// </summary>
+public enum MessageBeepFlags
 {
     /// <summary>
-    /// Sound type, as identified by an entry in the registry.
+    /// Простой звуковой сигнал. Если звуковая карта недоступна,
+    /// звук генерируется с помощью динамика.
     /// </summary>
-    public enum MessageBeepFlags
-    {
-        /// <summary>
-        /// Simple beep. If the sound card is not available,
-        /// the sound is generated using the speaker.
-        /// </summary>
-        SimpleBeep = unchecked((int)0xFFFFFFFF),
+    SimpleBeep = unchecked ((int) 0xFFFFFFFF),
 
-        /// <summary>
-        /// SystemAsterisk.
-        /// </summary>
-        MB_ICONASTERISK = 0x40,
+    /// <summary>
+    /// Звук "звездочка".
+    /// </summary>
+    MB_ICONASTERISK = 0x40,
 
-        /// <summary>
-        /// SystemExclamation.
-        /// </summary>
-        MB_ICONEXCLAMATION = 0x30,
+    /// <summary>
+    /// Звук "восклицательный знак".
+    /// </summary>
+    MB_ICONEXCLAMATION = 0x30,
 
-        /// <summary>
-        /// SystemHand.
-        /// </summary>
-        MB_ICONHAND = 0x10,
+    /// <summary>
+    /// Звук "рука".
+    /// </summary>
+    MB_ICONHAND = 0x10,
 
-        /// <summary>
-        /// SystemQuestion.
-        /// </summary>
-        MB_ICONQUESTION = 0x20,
+    /// <summary>
+    /// Звук "вопросительный знак".
+    /// </summary>
+    MB_ICONQUESTION = 0x20,
 
-        /// <summary>
-        /// SystemDefault.
-        /// </summary>
-        MB_OK = 0
-
-    } // enum MessageBeepFlags
-
-} // namespace AM.Win32
+    /// <summary>
+    /// Звук по умолчанию.
+    /// </summary>
+    MB_OK = 0
+}
