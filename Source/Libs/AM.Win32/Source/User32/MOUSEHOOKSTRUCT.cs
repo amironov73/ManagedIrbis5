d253df9fad590a71399c384c63257b3c7675529a
Alexey Mironov <amironov73@gmail.com>
2022-08-25 11:11:00 +0800

    Code cleanup

--- Source/Libs/AM.Win32/Source/User32/MOUSEHOOKSTRUCT.cs
+++ Source/Libs/AM.Win32/Source/User32/MOUSEHOOKSTRUCT.cs
@@ -2,15 +2,10 @@
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
 // ReSharper disable CheckNamespace
-// ReSharper disable ClassNeverInstantiated.Global
 // ReSharper disable CommentTypo
-// ReSharper disable FieldCanBeMadeReadOnly.Global
 // ReSharper disable IdentifierTypo
 // ReSharper disable InconsistentNaming
-// ReSharper disable MemberCanBePrivate.Global
-// ReSharper disable StringLiteralTypo
 // ReSharper disable UnusedMember.Global
-// ReSharper disable UnusedType.Global
 
 /* MOUSEHOOKSTRUCT.cs -- mouse event passed to a WH_MOUSE hook procedure
    Ars Magna project, http://arsmagna.ru */
@@ -23,39 +18,36 @@ using System.Runtime.InteropServices;
 
 #endregion
 
-namespace AM.Win32
+namespace AM.Win32;
+
+/// <summary>
+/// Contains information about a mouse event passed
+/// to a WH_MOUSE hook procedure, MouseProc.
+/// </summary>
+[StructLayout (LayoutKind.Sequential)]
+public struct MOUSEHOOKSTRUCT
 {
     /// <summary>
-    /// Contains information about a mouse event passed
-    /// to a WH_MOUSE hook procedure, MouseProc.
+    /// Specifies a POINT structure that contains
+    /// the x- and y-coordinates of the cursor,
+    /// in screen coordinates.
     /// </summary>
-    [StructLayout(LayoutKind.Sequential)]
-    public struct MOUSEHOOKSTRUCT
-    {
-        /// <summary>
-        /// Specifies a POINT structure that contains
-        /// the x- and y-coordinates of the cursor,
-        /// in screen coordinates.
-        /// </summary>
-        public Point pt;
+    public Point pt;
 
-        /// <summary>
-        /// Handle to the window that will receive the
-        /// mouse message corresponding to the mouse event.
-        /// </summary>
-        public IntPtr hwnd;
+    /// <summary>
+    /// Handle to the window that will receive the
+    /// mouse message corresponding to the mouse event.
+    /// </summary>
+    public IntPtr hwnd;
 
-        /// <summary>
-        /// Specifies the hit-test value. For a list of hit-test
-        /// values, see the description of the WM_NCHITTEST message.
-        /// </summary>
-        public HitTestCode wHitTestCode;
+    /// <summary>
+    /// Specifies the hit-test value. For a list of hit-test
+    /// values, see the description of the WM_NCHITTEST message.
+    /// </summary>
+    public HitTestCode wHitTestCode;
 
-        /// <summary>
-        /// Specifies extra information associated with the message.
-        /// </summary>
-        public IntPtr dwExtraInfo;
-
-    } // struct MOUSEHOOKSTRUCT
-
-} // namespace AM.Win32
+    /// <summary>
+    /// Specifies extra information associated with the message.
+    /// </summary>
+    public IntPtr dwExtraInfo;
+}
--- Source/Libs/AM.Win32/Source/User32/MessageBoxResult.cs
+++ Source/Libs/AM.Win32/Source/User32/MessageBoxResult.cs
@@ -2,15 +2,10 @@
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
 // ReSharper disable CheckNamespace
-// ReSharper disable ClassNeverInstantiated.Global
 // ReSharper disable CommentTypo
-// ReSharper disable FieldCanBeMadeReadOnly.Global
 // ReSharper disable IdentifierTypo
 // ReSharper disable InconsistentNaming
-// ReSharper disable MemberCanBePrivate.Global
-// ReSharper disable StringLiteralTypo
 // ReSharper disable UnusedMember.Global
-// ReSharper disable UnusedType.Global
 
 /* MessageBoxResult.cs -- MessageBox function return codes
    Ars Magna project, http://arsmagna.ru */
