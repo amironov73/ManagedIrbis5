eaa800f1e36a64335d3fd8c392daff154a84b365
Alexey Mironov <amironov73@gmail.com>
2022-09-08 10:48:56 +0800

    Code cleanup

--- Source/Libs/AM.Drawing5/Source/Pdfing/Internal/FontFamilyModel.cs
+++ Source/Libs/AM.Drawing5/Source/Pdfing/Internal/FontFamilyModel.cs
@@ -1,17 +1,62 @@
-﻿using System.Collections.Generic;
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedMember.Global
+
+/* FontFamilyModel.cs --
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System.Collections.Generic;
+
+using AM;
+
 using PdfSharpCore.Drawing;
 
-namespace PdfSharpCore.Internal
+#endregion
+
+#nullable enable
+
+namespace PdfSharpCore.Internal;
+
+/// <summary>
+///
+/// </summary>
+public class FontFamilyModel
 {
-    public class FontFamilyModel
+    #region Properties
+
+    /// <summary>
+    /// Имя шрифта.
+    /// </summary>
+    public string Name { get; set; } = null!;
+
+    /// <summary>
+    /// Соответствие стилей шрифтов дисковым файлам.
+    /// </summary>
+    public Dictionary<XFontStyle, string> FontFiles = new ();
+
+    #endregion
+
+    #region Public methods
+
+    /// <summary>
+    /// Проверка, доступен ли указанный стиль шрифта/
+    /// </summary>
+    public bool IsStyleAvailable
+        (
+            XFontStyle fontStyle
+        )
     {
-        public string Name { get; set; }
+        Sure.Defined (fontStyle);
 
-        public Dictionary<XFontStyle, string> FontFiles = new Dictionary<XFontStyle, string>();
-
-        public bool IsStyleAvailable(XFontStyle fontStyle)
-        {
-            return FontFiles.ContainsKey(fontStyle);
-        }
+        return FontFiles.ContainsKey (fontStyle);
     }
+
+    #endregion
 }
