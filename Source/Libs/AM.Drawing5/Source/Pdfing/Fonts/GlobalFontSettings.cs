2975ab3dcf59a634d4ccb30a52e49c570beb5254
Alexey Mironov <amironov73@gmail.com>
2022-09-08 09:36:06 +0800

    Code cleanup

--- Source/Libs/AM.Drawing5/Source/Pdfing/Fonts/GlobalFontSettings.cs
+++ Source/Libs/AM.Drawing5/Source/Pdfing/Fonts/GlobalFontSettings.cs
@@ -1,123 +1,139 @@
-﻿#region PDFsharp - A .NET library for processing PDF
-//
-// Authors:
-//   Stefan Lange
-//
-// Copyright (c) 2005-2016 empira Software GmbH, Cologne Area (Germany)
-//
-// http://www.PdfSharpCore.com
-// http://sourceforge.net/projects/pdfsharp
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-// DEALINGS IN THE SOFTWARE.
-#endregion
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedMember.Global
+
+/*
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
 
 using System;
+
 using PdfSharpCore.Internal;
 using PdfSharpCore.Pdf;
 using PdfSharpCore.Utils;
 
-namespace PdfSharpCore.Fonts
+#endregion
+
+#nullable enable
+
+namespace PdfSharpCore.Fonts;
+
+/// <summary>
+/// Provides functionality to specify information about the handling
+/// of fonts in the current application domain.
+/// </summary>
+public static class GlobalFontSettings
 {
     /// <summary>
-    /// Provides functionality to specify information about the handling of fonts in the current application domain.
+    /// The name of the default font.
     /// </summary>
-    public static class GlobalFontSettings
+    public const string DefaultFontName = "PlatformDefault";
+
+    /// <summary>
+    /// Gets or sets the global font resolver for the current application domain.
+    /// This static property should be set only once and before any font operation was executed by PdfSharpCore.
+    /// If this is not easily to obtain, e.g. because your code is running on a web server, you must provide the
+    /// same instance of your font resolver in every subsequent setting of this property.
+    /// In a web application set the font resolver in Global.asax.
+    /// For .NetCore Apps, if a resolver is not set before the first get operation to this property,
+    /// the default Font resolver implementation: <see cref="T:PdfSharpCore.Utils.PdfSharpCore.Utils"/> is set and returned
+    /// </summary>
+    public static IFontResolver FontResolver
     {
-        /// <summary>
-        /// The name of the default font.
-        /// </summary>
-        public const string DefaultFontName = "PlatformDefault";
-
-        /// <summary>
-        /// Gets or sets the global font resolver for the current application domain.
-        /// This static property should be set only once and before any font operation was executed by PdfSharpCore.
-        /// If this is not easily to obtain, e.g. because your code is running on a web server, you must provide the
-        /// same instance of your font resolver in every subsequent setting of this property.
-        /// In a web application set the font resolver in Global.asax.
-        /// For .NetCore Apps, if a resolver is not set before the first get operation to this property,
-        /// the default Font resolver implementation: <see cref="T:PdfSharpCore.Utils.PdfSharpCore.Utils"/> is set and returned
-        /// </summary>
-        public static IFontResolver FontResolver
+        get
         {
-            get 
+            if (_fontResolver == null)
             {
-                if (_fontResolver == null) FontResolver = new FontResolver();
-                    return _fontResolver; 
+                FontResolver = new FontResolver();
             }
-            set
+
+            return _fontResolver;
+        }
+        set
+        {
+            // Cannot remove font resolver.
+            if (value == null)
             {
-                // Cannot remove font resolver.
-                if (value == null)
-                    throw new ArgumentNullException();
+                throw new ArgumentNullException();
+            }
 
-                try
+            try
+            {
+                Lock.EnterFontFactory();
+
+                // Ignore multiple setting e.g. in a web application.
+                if (ReferenceEquals (_fontResolver, value))
                 {
-                    Lock.EnterFontFactory();
-                    // Ignore multiple setting e.g. in a web application.
-                    if (ReferenceEquals(_fontResolver, value))
-                        return;
-
-                    if (FontFactory.HasFontSources)
-                        throw new InvalidOperationException("Must not change font resolver after is was once used.");
-
-                    _fontResolver = value;
+                    return;
                 }
-                finally { Lock.ExitFontFactory(); }
+
+                if (FontFactory.HasFontSources)
+                {
+                    throw new InvalidOperationException ("Must not change font resolver after is was once used.");
+                }
+
+                _fontResolver = value;
+            }
+            finally
+            {
+                Lock.ExitFontFactory();
             }
         }
-        static IFontResolver _fontResolver;
+    }
 
-        /// <summary>
-        /// Gets or sets the default font encoding used for XFont objects where encoding is not explicitly specified.
-        /// If it is not set, the default value is PdfFontEncoding.Unicode.
-        /// If you are sure your document contains only Windows-1252 characters (see https://en.wikipedia.org/wiki/Windows-1252) 
-        /// set default encoding to PdfFontEncodingj.Windows1252.
-        /// Must be set only once per app domain.
-        /// </summary>
-        public static PdfFontEncoding DefaultFontEncoding
+    static IFontResolver _fontResolver;
+
+    /// <summary>
+    /// Gets or sets the default font encoding used for XFont objects where encoding is not explicitly specified.
+    /// If it is not set, the default value is PdfFontEncoding.Unicode.
+    /// If you are sure your document contains only Windows-1252 characters (see https://en.wikipedia.org/wiki/Windows-1252)
+    /// set default encoding to PdfFontEncodingj.Windows1252.
+    /// Must be set only once per app domain.
+    /// </summary>
+    public static PdfFontEncoding DefaultFontEncoding
+    {
+        get
         {
-            get
+            if (!_fontEncodingInitialized)
             {
-                if (!_fontEncodingInitialized)
-                    DefaultFontEncoding = PdfFontEncoding.Unicode;
-                return _fontEncoding;
+                DefaultFontEncoding = PdfFontEncoding.Unicode;
             }
-            set
+
+            return _fontEncoding;
+        }
+        set
+        {
+            try
             {
-                try
+                Lock.EnterFontFactory();
+                if (_fontEncodingInitialized)
                 {
-                    Lock.EnterFontFactory();
-                    if (_fontEncodingInitialized)
+                    // Ignore multiple setting e.g. in a web application.
+                    if (_fontEncoding == value)
                     {
-                        // Ignore multiple setting e.g. in a web application.
-                        if (_fontEncoding == value)
-                            return;
-                        throw new InvalidOperationException("Must not change DefaultFontEncoding after is was set once.");
+                        return;
                     }
 
-                    _fontEncoding = value;
-                    _fontEncodingInitialized = true;
+                    throw new InvalidOperationException ("Must not change DefaultFontEncoding after is was set once.");
                 }
-                finally { Lock.ExitFontFactory(); }
+
+                _fontEncoding = value;
+                _fontEncodingInitialized = true;
+            }
+            finally
+            {
+                Lock.ExitFontFactory();
             }
         }
-        static PdfFontEncoding _fontEncoding;
-        static bool _fontEncodingInitialized;
     }
+
+    static PdfFontEncoding _fontEncoding;
+    static bool _fontEncodingInitialized;
 }
