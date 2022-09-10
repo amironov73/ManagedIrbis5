c6b8a7a1bbd58c40a218f1e9477e02148eca6482
Alexey Mironov <amironov73@gmail.com>
2022-09-10 12:45:05 +0800

    Code cleanup

--- Source/Libs/AM.Drawing5/Source/Pdfing/Pdf/Advanced/PdfObjectInternals.cs
+++ Source/Libs/AM.Drawing5/Source/Pdfing/Pdf/Advanced/PdfObjectInternals.cs
@@ -1,84 +1,71 @@
-#region PDFsharp - A .NET library for processing PDF
-//
-// Authors:
-//   Stefan Lange
-//
-// Copyright (c) 2005-2016 empira Software GmbH, Cologne Area (Germany)
-//
-// http://www.PdfSharpCore.com
-// http://sourceforge.net/projects/pdfsharp
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-// DEALINGS IN THE SOFTWARE.
-#endregion
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
-namespace PdfSharpCore.Pdf.Advanced
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedMember.Global
+
+/*
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#nullable enable
+
+namespace PdfSharpCore.Pdf.Advanced;
+
+/// <summary>
+/// Provides access to the internal PDF object data structures. This class prevents the public
+/// interfaces from pollution with to much internal functions.
+/// </summary>
+public class PdfObjectInternals
 {
-    /// <summary>
-    /// Provides access to the internal PDF object data structures. This class prevents the public
-    /// interfaces from pollution with to much internal functions.
-    /// </summary>
-    public class PdfObjectInternals
+    internal PdfObjectInternals (PdfObject obj)
     {
-        internal PdfObjectInternals(PdfObject obj)
-        {
-            _obj = obj;
-        }
-        readonly PdfObject _obj;
+        _obj = obj;
+    }
 
-        /// <summary>
-        /// Gets the object identifier. Returns PdfObjectID.Empty for direct objects.
-        /// </summary>
-        public PdfObjectID ObjectID
-        {
-            get { return _obj.ObjectID; }
-        }
+    readonly PdfObject _obj;
 
-        /// <summary>
-        /// Gets the object number.
-        /// </summary>
-        public int ObjectNumber
-        {
-            get { return _obj.ObjectID.ObjectNumber; }
-        }
+    /// <summary>
+    /// Gets the object identifier. Returns PdfObjectID.Empty for direct objects.
+    /// </summary>
+    public PdfObjectID ObjectID
+    {
+        get { return _obj.ObjectID; }
+    }
 
-        /// <summary>
-        /// Gets the generation number.
-        /// </summary>
-        public int GenerationNumber
-        {
-            get { return _obj.ObjectID.GenerationNumber; }
-        }
+    /// <summary>
+    /// Gets the object number.
+    /// </summary>
+    public int ObjectNumber
+    {
+        get { return _obj.ObjectID.ObjectNumber; }
+    }
 
-        /// <summary>
-        /// Gets the name of the current type.
-        /// Not a very useful property, but can be used for data binding.
-        /// </summary>
-        public string TypeID
+    /// <summary>
+    /// Gets the generation number.
+    /// </summary>
+    public int GenerationNumber
+    {
+        get { return _obj.ObjectID.GenerationNumber; }
+    }
+
+    /// <summary>
+    /// Gets the name of the current type.
+    /// Not a very useful property, but can be used for data binding.
+    /// </summary>
+    public string TypeID
+    {
+        get
         {
-            get
+            return _obj switch
             {
-                if (_obj is PdfArray)
-                    return "array";
-                if (_obj is PdfDictionary)
-                    return "dictionary";
-                return _obj.GetType().Name;
-            }
+                PdfArray => "array",
+                PdfDictionary => "dictionary",
+                _ => _obj.GetType().Name
+            };
         }
     }
 }
--- Source/Libs/AM.Drawing5/Source/Pdfing/Pdf/Advanced/PdfObjectStream.cs
+++ Source/Libs/AM.Drawing5/Source/Pdfing/Pdf/Advanced/PdfObjectStream.cs
@@ -1,152 +1,141 @@
-#region PDFsharp - A .NET library for processing PDF
-//
-// Authors:
-//   Stefan Lange
-//
-// Copyright (c) 2005-2016 empira Software GmbH, Cologne Area (Germany)
-//
-// http://www.PdfSharpCore.com
-// http://sourceforge.net/projects/pdfsharp
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-// DEALINGS IN THE SOFTWARE.
-#endregion
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
-using System;
-using System.Diagnostics;
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedMember.Global
+
+/*
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
 using System.IO;
+
 using PdfSharpCore.Pdf.IO;
 
-namespace PdfSharpCore.Pdf.Advanced
+#endregion
+
+#nullable enable
+
+namespace PdfSharpCore.Pdf.Advanced;
+
+/// <summary>
+/// Represents an object stream that contains compressed objects.
+/// PDF 1.5.
+/// </summary>
+public class PdfObjectStream
+    : PdfDictionary
 {
+    // Reference: 3.4.6  Object Streams / Page 100
+
     /// <summary>
-    /// Represents an object stream that contains compressed objects.
-    /// PDF 1.5.
+    /// Initializes a new instance of the <see cref="PdfObjectStream"/> class.
     /// </summary>
-    public class PdfObjectStream : PdfDictionary
+    public PdfObjectStream (PdfDocument document)
+        : base (document)
     {
-        // Reference: 3.4.6  Object Streams / Page 100
+    }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="PdfObjectStream"/> class.
-        /// </summary>
-        public PdfObjectStream(PdfDocument document)
-            : base(document)
+    /// <summary>
+    /// Initializes a new instance from an existing dictionary. Used for object type transformation.
+    /// </summary>
+    internal PdfObjectStream (PdfDictionary dict)
+        : base (dict)
+    {
+        var n = Elements.GetInteger (Keys.N);
+        var first = Elements.GetInteger (Keys.First);
+        Stream.TryUnfilter();
+
+        var parser = new Parser (null, new MemoryStream (Stream.Value));
+        _header = parser.ReadObjectStreamHeader (n, first);
+    }
+
+    /// <summary>
+    /// Reads the compressed object with the specified index.
+    /// </summary>
+    internal void ReadReferences (PdfCrossReferenceTable xrefTable)
+    {
+        ////// Create parser for stream.
+        ////Parser parser = new Parser(_document, new MemoryStream(Stream.Value));
+        for (var idx = 0; idx < _header.Length; idx++)
         {
-        }
+            var objectNumber = _header[idx][0];
+            var offset = _header[idx][1];
 
-        /// <summary>
-        /// Initializes a new instance from an existing dictionary. Used for object type transformation.
-        /// </summary>
-        internal PdfObjectStream(PdfDictionary dict)
-            : base(dict)
-        {
-            int n = Elements.GetInteger(Keys.N);
-            int first = Elements.GetInteger(Keys.First);
-            Stream.TryUnfilter();
+            var objectID = new PdfObjectID (objectNumber);
 
-            Parser parser = new Parser(null, new MemoryStream(Stream.Value));
-            _header = parser.ReadObjectStreamHeader(n, first);
-        }
-
-        /// <summary>
-        /// Reads the compressed object with the specified index.
-        /// </summary>
-        internal void ReadReferences(PdfCrossReferenceTable xrefTable)
-        {
-            ////// Create parser for stream.
-            ////Parser parser = new Parser(_document, new MemoryStream(Stream.Value));
-            for (int idx = 0; idx < _header.Length; idx++)
+            // HACK: -1 indicates compressed object.
+            var iref = new PdfReference (objectID, -1);
+            ////iref.ObjectID = objectID;
+            ////iref.Value = xrefStream;
+            if (!xrefTable.Contains (iref.ObjectID))
             {
-                int objectNumber = _header[idx][0];
-                int offset = _header[idx][1];
-
-                PdfObjectID objectID = new PdfObjectID(objectNumber);
-
-                // HACK: -1 indicates compressed object.
-                PdfReference iref = new PdfReference(objectID, -1);
-                ////iref.ObjectID = objectID;
-                ////iref.Value = xrefStream;
-                if (!xrefTable.Contains(iref.ObjectID))
-                {
-                    xrefTable.Add(iref);
-                }
-                else
-                {
-                    GetType();
-                }
+                xrefTable.Add (iref);
+            }
+            else
+            {
+                GetType();
             }
         }
+    }
+
+    /// <summary>
+    /// Reads the compressed object with the specified index.
+    /// </summary>
+    internal PdfReference ReadCompressedObject (int index)
+    {
+        var parser = new Parser (_document, new MemoryStream (Stream.Value));
+        var objectNumber = _header[index][0];
+        var offset = _header[index][1];
+        return parser.ReadCompressedObject (objectNumber, offset);
+    }
+
+    /// <summary>
+    /// N pairs of integers.
+    /// The first integer represents the object number of the compressed object.
+    /// The second integer represents the absolute offset of that object in the decoded stream,
+    /// i.e. the byte offset plus First entry.
+    /// </summary>
+    private readonly int[][] _header; // Reference: Page 102
+
+    /// <summary>
+    /// Predefined keys common to all font dictionaries.
+    /// </summary>
+    public class Keys : PdfStream.Keys
+    {
+        // Reference: TABLE 3.14  Additional entries specific to an object stream dictionary / Page 101
 
         /// <summary>
-        /// Reads the compressed object with the specified index.
+        /// (Required) The type of PDF object that this dictionary describes;
+        /// must be ObjStmfor an object stream.
         /// </summary>
-        internal PdfReference ReadCompressedObject(int index)
-        {
-            Parser parser = new Parser(_document, new MemoryStream(Stream.Value));
-            int objectNumber = _header[index][0];
-            int offset = _header[index][1];
-            return parser.ReadCompressedObject(objectNumber, offset);
-        }
+        [KeyInfo (KeyType.Name | KeyType.Required, FixedValue = "ObjStm")]
+        public const string Type = "/Type";
 
         /// <summary>
-        /// N pairs of integers.
-        /// The first integer represents the object number of the compressed object.
-        /// The second integer represents the absolute offset of that object in the decoded stream,
-        /// i.e. the byte offset plus First entry.
+        /// (Required) The number of compressed objects in the stream.
         /// </summary>
-        private readonly int[][] _header;  // Reference: Page 102
+        [KeyInfo (KeyType.Integer | KeyType.Required)]
+        public const string N = "/N";
 
         /// <summary>
-        /// Predefined keys common to all font dictionaries.
+        /// (Required) The byte offset (in the decoded stream) of the first
+        /// compressed object.
         /// </summary>
-        public class Keys : PdfStream.Keys
-        {
-            // Reference: TABLE 3.14  Additional entries specific to an object stream dictionary / Page 101
+        [KeyInfo (KeyType.Integer | KeyType.Required)]
+        public const string First = "/First";
 
-            /// <summary>
-            /// (Required) The type of PDF object that this dictionary describes;
-            /// must be ObjStmfor an object stream.
-            /// </summary>
-            [KeyInfo(KeyType.Name | KeyType.Required, FixedValue = "ObjStm")]
-            public const string Type = "/Type";
-
-            /// <summary>
-            /// (Required) The number of compressed objects in the stream.
-            /// </summary>
-            [KeyInfo(KeyType.Integer | KeyType.Required)]
-            public const string N = "/N";
-
-            /// <summary>
-            /// (Required) The byte offset (in the decoded stream) of the first
-            /// compressed object.
-            /// </summary>
-            [KeyInfo(KeyType.Integer | KeyType.Required)]
-            public const string First = "/First";
-
-            /// <summary>
-            /// (Optional) A reference to an object stream, of which the current object
-            /// stream is considered an extension. Both streams are considered part of
-            /// a collection of object streams (see below). A given collection consists
-            /// of a set of streams whose Extendslinks form a directed acyclic graph.
-            /// </summary>
-            [KeyInfo(KeyType.Stream | KeyType.Optional)]
-            public const string Extends = "/Extends";
-        }
+        /// <summary>
+        /// (Optional) A reference to an object stream, of which the current object
+        /// stream is considered an extension. Both streams are considered part of
+        /// a collection of object streams (see below). A given collection consists
+        /// of a set of streams whose Extendslinks form a directed acyclic graph.
+        /// </summary>
+        [KeyInfo (KeyType.Stream | KeyType.Optional)]
+        public const string Extends = "/Extends";
     }
 }
