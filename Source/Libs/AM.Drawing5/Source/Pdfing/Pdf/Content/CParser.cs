30059c95dd794eff9f6be62205a12febe0497744
Alexey Mironov <amironov73@gmail.com>
2022-09-11 13:58:02 +0800

    Code cleanup

--- Source/Libs/AM.Drawing5/Source/Pdfing/Pdf/Content/CParser.cs
+++ Source/Libs/AM.Drawing5/Source/Pdfing/Pdf/Content/CParser.cs
@@ -1,231 +1,252 @@
-#region PDFsharp - A .NET library for processing PDF
-//
-// Authors:
-//   Stefan Lange
-//
-// Copyright (c) 2005-2016 empira Software GmbH, Cologne Area (Germany)
-//
-// http://www.PdfSharpCore.com
-// http://sourceforge.net/projects/pdfsharp
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-// DEALINGS IN THE SOFTWARE.
-#endregion
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedMember.Local
+
+/*
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
 
 using System.Diagnostics;
 using System.IO;
+
+using AM;
+
 using PdfSharpCore.Internal;
-using PdfSharpCore.Pdf.Advanced;
 using PdfSharpCore.Pdf.Content.Objects;
+
+#endregion
+
+#nullable enable
 
 #pragma warning disable 1591
 
-namespace PdfSharpCore.Pdf.Content
+namespace PdfSharpCore.Pdf.Content;
+
+/// <summary>
+/// Provides the functionality to parse PDF content streams.
+/// </summary>
+public sealed class CParser
 {
+    #region Construction
+
     /// <summary>
-    /// Provides the functionality to parse PDF content streams.
+    ///
     /// </summary>
-    public sealed class CParser
+    /// <param name="page"></param>
+    public CParser (PdfPage page)
     {
-        public CParser(PdfPage page)
-        {
-            _page = page;
-            PdfContent content = page.Contents.CreateSingleContent();
-            byte[] bytes = content.Stream.Value;
-            _lexer = new CLexer(bytes);
-        }
-
-        public CParser(byte[] content)
-        {
-            _lexer = new CLexer(content);
-        }
-
-        public CParser(MemoryStream content)
-        {
-            _lexer = new CLexer(content.ToArray());
-        }
-
-
-        public CParser(CLexer lexer)
-        {
-            _lexer = lexer;
-        }
-
-        public CSymbol Symbol
-        {
-            get { return _lexer.Symbol; }
-        }
-
-        public CSequence ReadContent()
-        {
-            CSequence sequence = new CSequence();
-            ParseObject(sequence, CSymbol.Eof);
-
-            return sequence;
-        }
-
-        /// <summary>
-        /// Parses whatever comes until the specified stop symbol is reached.
-        /// </summary>
-        void ParseObject(CSequence sequence, CSymbol stop)
-        {
-            CSymbol symbol;
-            while ((symbol = ScanNextToken()) != CSymbol.Eof)
-            {
-                if (symbol == stop)
-                    return;
-
-                CString s;
-                COperator op;
-                switch (symbol)
-                {
-                    case CSymbol.Comment:
-                        // ignore comments
-                        break;
-
-                    case CSymbol.Integer:
-                        CInteger n = new CInteger();
-                        n.Value = _lexer.TokenToInteger;
-                        _operands.Add(n);
-                        break;
-
-                    case CSymbol.Real:
-                        CReal r = new CReal();
-                        r.Value = _lexer.TokenToReal;
-                        _operands.Add(r);
-                        break;
-
-                    case CSymbol.String:
-                    case CSymbol.HexString:
-                    case CSymbol.UnicodeString:
-                    case CSymbol.UnicodeHexString:
-                        s = new CString();
-                        s.Value = _lexer.Token;
-                        _operands.Add(s);
-                        break;
-
-                    case CSymbol.Dictionary:
-                        s = new CString();
-                        s.Value = _lexer.Token;
-                        s.CStringType = CStringType.Dictionary;
-                        _operands.Add(s);
-                        op = CreateOperator(OpCodeName.Dictionary);
-                        //_operands.Clear();
-                        sequence.Add(op);
-
-                        break;
-
-                    case CSymbol.Name:
-                        CName name = new CName();
-                        name.Name = _lexer.Token;
-                        _operands.Add(name);
-                        break;
-
-                    case CSymbol.Operator:
-                        op = CreateOperator();
-                        //_operands.Clear();
-                        sequence.Add(op);
-                        break;
-
-                    case CSymbol.BeginArray:
-                        CArray array = new CArray();
-                        if (_operands.Count != 0)
-                            ContentReaderDiagnostics.ThrowContentReaderException("Array within array...");
-
-                        ParseObject(array, CSymbol.EndArray);
-                        array.Add(_operands);
-                        _operands.Clear();
-                        _operands.Add((CObject)array);
-                        break;
-
-                    case CSymbol.EndArray:
-                        ContentReaderDiagnostics.HandleUnexpectedCharacter(']');
-                        break;
-
-#if DEBUG
-                    default:
-                        Debug.Assert(false);
-                        break;
-#endif
-                }
-            }
-        }
-
-        COperator CreateOperator()
-        {
-            string name = _lexer.Token;
-            COperator op = OpCodes.OperatorFromName(name);
-            return CreateOperator(op);
-        }
-
-        COperator CreateOperator(OpCodeName nameop)
-        {
-            string name = nameop.ToString();
-            COperator op = OpCodes.OperatorFromName(name);
-            return CreateOperator(op);
-        }
-
-        COperator CreateOperator(COperator op)
-        {
-            if (op.OpCode.OpCodeName == OpCodeName.BI)
-            {
-                _lexer.ScanInlineImage();
-            }
-#if DEBUG
-            if (op.OpCode.Operands != -1 && op.OpCode.Operands != _operands.Count)
-            {
-                if (op.OpCode.OpCodeName != OpCodeName.ID)
-                {
-                    GetType();
-                    Debug.Assert(false, "Invalid number of operands.");
-                }
-            }
-#endif
-            op.Operands.Add(_operands);
-            _operands.Clear();
-            return op;
-        }
-
-        CSymbol ScanNextToken()
-        {
-            return _lexer.ScanNextToken();
-        }
-
-        CSymbol ScanNextToken(out string token)
-        {
-            CSymbol symbol = _lexer.ScanNextToken();
-            token = _lexer.Token;
-            return symbol;
-        }
-
-        /// <summary>
-        /// Reads the next symbol that must be the specified one.
-        /// </summary>
-        CSymbol ReadSymbol(CSymbol symbol)
-        {
-            CSymbol current = _lexer.ScanNextToken();
-            if (symbol != current)
-                ContentReaderDiagnostics.ThrowContentReaderException(PSSR.UnexpectedToken(_lexer.Token));
-            return current;
-        }
-
-        readonly CSequence _operands = new CSequence();
-        PdfPage _page;
-        readonly CLexer _lexer;
+        _page = page;
+        var content = page.Contents.CreateSingleContent();
+        var bytes = content.Stream.ThrowIfNull().Value;
+        _lexer = new CLexer (bytes);
     }
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="content"></param>
+    public CParser (byte[] content)
+    {
+        _lexer = new CLexer (content);
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="content"></param>
+    public CParser (MemoryStream content)
+    {
+        _lexer = new CLexer (content.ToArray());
+    }
+
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="lexer"></param>
+    public CParser (CLexer lexer)
+    {
+        _lexer = lexer;
+    }
+
+    #endregion
+
+    public CSymbol Symbol => _lexer.Symbol;
+
+    public CSequence ReadContent()
+    {
+        var sequence = new CSequence();
+        ParseObject (sequence, CSymbol.Eof);
+
+        return sequence;
+    }
+
+    /// <summary>
+    /// Parses whatever comes until the specified stop symbol is reached.
+    /// </summary>
+    void ParseObject (CSequence sequence, CSymbol stop)
+    {
+        CSymbol symbol;
+        while ((symbol = ScanNextToken()) != CSymbol.Eof)
+        {
+            if (symbol == stop)
+                return;
+
+            CString s;
+            COperator op;
+            switch (symbol)
+            {
+                case CSymbol.Comment:
+                    // ignore comments
+                    break;
+
+                case CSymbol.Integer:
+                    var n = new CInteger
+                    {
+                        Value = _lexer.TokenToInteger
+                    };
+                    _operands.Add (n);
+                    break;
+
+                case CSymbol.Real:
+                    var r = new CReal
+                    {
+                        Value = _lexer.TokenToReal
+                    };
+                    _operands.Add (r);
+                    break;
+
+                case CSymbol.String:
+                case CSymbol.HexString:
+                case CSymbol.UnicodeString:
+                case CSymbol.UnicodeHexString:
+                    s = new CString
+                    {
+                        Value = _lexer.Token
+                    };
+                    _operands.Add (s);
+                    break;
+
+                case CSymbol.Dictionary:
+                    s = new CString
+                    {
+                        Value = _lexer.Token,
+                        CStringType = CStringType.Dictionary
+                    };
+                    _operands.Add (s);
+                    op = CreateOperator (OpCodeName.Dictionary);
+
+                    //_operands.Clear();
+                    sequence.Add (op);
+
+                    break;
+
+                case CSymbol.Name:
+                    var name = new CName
+                    {
+                        Name = _lexer.Token
+                    };
+                    _operands.Add (name);
+                    break;
+
+                case CSymbol.Operator:
+                    op = CreateOperator();
+
+                    //_operands.Clear();
+                    sequence.Add (op);
+                    break;
+
+                case CSymbol.BeginArray:
+                    var array = new CArray();
+                    if (_operands.Count != 0)
+                        ContentReaderDiagnostics.ThrowContentReaderException ("Array within array...");
+
+                    ParseObject (array, CSymbol.EndArray);
+                    array.Add (_operands);
+                    _operands.Clear();
+                    _operands.Add ((CObject)array);
+                    break;
+
+                case CSymbol.EndArray:
+                    ContentReaderDiagnostics.HandleUnexpectedCharacter (']');
+                    break;
+
+#if DEBUG
+                default:
+                    Debug.Assert (false);
+                    break;
+#endif
+            }
+        }
+    }
+
+    COperator CreateOperator()
+    {
+        var name = _lexer.Token;
+        var op = OpCodes.OperatorFromName (name);
+        return CreateOperator (op);
+    }
+
+    COperator CreateOperator (OpCodeName nameop)
+    {
+        var name = nameop.ToString();
+        var op = OpCodes.OperatorFromName (name);
+        return CreateOperator (op);
+    }
+
+    COperator CreateOperator (COperator op)
+    {
+        if (op.OpCode.OpCodeName == OpCodeName.BI)
+        {
+            _lexer.ScanInlineImage();
+        }
+#if DEBUG
+        if (op.OpCode.Operands != -1 && op.OpCode.Operands != _operands.Count)
+        {
+            if (op.OpCode.OpCodeName != OpCodeName.ID)
+            {
+                GetType();
+                Debug.Assert (false, "Invalid number of operands.");
+            }
+        }
+#endif
+        op.Operands.Add (_operands);
+        _operands.Clear();
+        return op;
+    }
+
+    CSymbol ScanNextToken()
+    {
+        return _lexer.ScanNextToken();
+    }
+
+    CSymbol ScanNextToken (out string token)
+    {
+        var symbol = _lexer.ScanNextToken();
+        token = _lexer.Token;
+        return symbol;
+    }
+
+    /// <summary>
+    /// Reads the next symbol that must be the specified one.
+    /// </summary>
+    CSymbol ReadSymbol (CSymbol symbol)
+    {
+        var current = _lexer.ScanNextToken();
+        if (symbol != current)
+            ContentReaderDiagnostics.ThrowContentReaderException (PSSR.UnexpectedToken (_lexer.Token));
+        return current;
+    }
+
+    readonly CSequence _operands = new CSequence();
+    private PdfPage? _page;
+    readonly CLexer _lexer;
 }
--- Source/Libs/AM.Drawing5/Source/Pdfing/Pdf/Content/ContentReader.cs
+++ Source/Libs/AM.Drawing5/Source/Pdfing/Pdf/Content/ContentReader.cs
@@ -1,74 +1,64 @@
-#region PDFsharp - A .NET library for processing PDF
-//
-// Authors:
-//   Stefan Lange
-//
-// Copyright (c) 2005-2016 empira Software GmbH, Cologne Area (Germany)
-//
-// http://www.PdfSharpCore.com
-// http://sourceforge.net/projects/pdfsharp
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-// DEALINGS IN THE SOFTWARE.
-#endregion
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedMember.Global
+
+/*
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
 
 using System.IO;
+
 using PdfSharpCore.Pdf.Content.Objects;
 
-namespace PdfSharpCore.Pdf.Content
+#endregion
+
+#nullable enable
+
+namespace PdfSharpCore.Pdf.Content;
+
+/// <summary>
+/// Represents the functionality for reading PDF content streams.
+/// </summary>
+public static class ContentReader
 {
     /// <summary>
-    /// Represents the functionality for reading PDF content streams.
+    /// Reads the content stream(s) of the specified page.
     /// </summary>
-    public static class ContentReader
+    /// <param name="page">The page.</param>
+    public static CSequence ReadContent (PdfPage page)
     {
-        /// <summary>
-        /// Reads the content stream(s) of the specified page.
-        /// </summary>
-        /// <param name="page">The page.</param>
-        static public CSequence ReadContent(PdfPage page)
-        {
-            CParser parser = new CParser(page);
-            CSequence sequence = parser.ReadContent();
+        var parser = new CParser (page);
+        var sequence = parser.ReadContent();
 
-            return sequence;
-        }
+        return sequence;
+    }
 
-        /// <summary>
-        /// Reads the specified content.
-        /// </summary>
-        /// <param name="content">The content.</param>
-        static public CSequence ReadContent(byte[] content)
-        {
-            CParser parser = new CParser(content);
-            CSequence sequence = parser.ReadContent();
-            return sequence;
-        }
+    /// <summary>
+    /// Reads the specified content.
+    /// </summary>
+    /// <param name="content">The content.</param>
+    public static CSequence ReadContent (byte[] content)
+    {
+        var parser = new CParser (content);
+        var sequence = parser.ReadContent();
+        return sequence;
+    }
 
-        /// <summary>
-        /// Reads the specified content.
-        /// </summary>
-        /// <param name="content">The content.</param>
-        static public CSequence ReadContent(MemoryStream content)
-        {
-            CParser parser = new CParser(content);
-            CSequence sequence = parser.ReadContent();
-            return sequence;
-        }
+    /// <summary>
+    /// Reads the specified content.
+    /// </summary>
+    /// <param name="content">The content.</param>
+    public static CSequence ReadContent (MemoryStream content)
+    {
+        var parser = new CParser (content);
+        var sequence = parser.ReadContent();
+        return sequence;
     }
 }
