902b8326e13b5a12a5b30638b07e48d5a9eda787
Alexey Mironov <amironov73@gmail.com>
2022-09-13 11:14:17 +0800

    Code cleanup

--- Source/Libs/AM.Drawing5/Source/Pdfing/Pdf/PdfBoolean.cs
+++ Source/Libs/AM.Drawing5/Source/Pdfing/Pdf/PdfBoolean.cs
@@ -1,91 +1,82 @@
-#region PDFsharp - A .NET library for processing PDF
-//
-// Authors:
-//   Stefan Lange
-//
-// Copyright (c) 2005-2016 empira Software GmbH, Cologne Area (Germany)
-//
-// http://www.PdfSharp.com
-// http://sourceforge.net/projects/pdfsharp
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-// DEALINGS IN THE SOFTWARE.
-#endregion
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
-using System.Diagnostics;
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedMember.Global
+
+/*
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
 using PdfSharpCore.Pdf.IO;
 
-namespace PdfSharpCore.Pdf
+#endregion
+
+#nullable enable
+
+namespace PdfSharpCore.Pdf;
+
+/// <summary>
+/// Represents a direct boolean value.
+/// </summary>
+public sealed class PdfBoolean
+    : PdfItem
 {
+    #region Construction
+
     /// <summary>
-    /// Represents a direct boolean value.
+    /// Initializes a new instance of the <see cref="PdfBoolean"/> class.
     /// </summary>
-    [DebuggerDisplay("({Value})")]
-    public sealed class PdfBoolean : PdfItem
+    public PdfBoolean()
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="PdfBoolean"/> class.
-        /// </summary>
-        public PdfBoolean()
-        { }
+        // пустое тело конструктора
+    }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="PdfBoolean"/> class.
-        /// </summary>
-        public PdfBoolean(bool value)
-        {
-            _value = value;
-        }
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PdfBoolean"/> class.
+    /// </summary>
+    public PdfBoolean (bool value)
+    {
+        Value = value;
+    }
 
-        /// <summary>
-        /// Gets the value of this instance as boolean value.
-        /// </summary>
-        public bool Value
-        {
-            // This class must behave like a value type. Therefore it cannot be changed (like System.String).
-            get { return _value; }
-        }
-        readonly bool _value;
+    #endregion
 
-        /// <summary>
-        /// A pre-defined value that represents <c>true</c>.
-        /// </summary>
-        public static readonly PdfBoolean True = new PdfBoolean(true);
+    /// <summary>
+    /// Gets the value of this instance as boolean value.
+    /// </summary>
+    /// <remarks>This class must behave like a value type.
+    /// Therefore it cannot be changed.</remarks>
+    public bool Value { get; }
 
-        /// <summary>
-        /// A pre-defined value that represents <c>false</c>.
-        /// </summary>
-        public static readonly PdfBoolean False = new PdfBoolean(false);
+    /// <summary>
+    /// A pre-defined value that represents <c>true</c>.
+    /// </summary>
+    public static readonly PdfBoolean True = new (true);
 
-        /// <summary>
-        /// Returns 'false' or 'true'.
-        /// </summary>
-        public override string ToString()
-        {
-            return _value ? bool.TrueString : bool.FalseString;
-        }
+    /// <summary>
+    /// A pre-defined value that represents <c>false</c>.
+    /// </summary>
+    public static readonly PdfBoolean False = new (false);
 
-        /// <summary>
-        /// Writes 'true' or 'false'.
-        /// </summary>
-        internal override void WriteObject(PdfWriter writer)
-        {
-            writer.Write(this);
-        }
+    /// <summary>
+    /// Returns 'false' or 'true'.
+    /// </summary>
+    public override string ToString()
+    {
+        return Value ? bool.TrueString : bool.FalseString;
+    }
+
+    /// <summary>
+    /// Writes 'true' or 'false'.
+    /// </summary>
+    internal override void WriteObject (PdfWriter writer)
+    {
+        writer.Write (this);
     }
 }
