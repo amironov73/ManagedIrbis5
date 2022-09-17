c57db202e31122c8a9c96207d447bfce64575e9e
Alexey Mironov <amironov73@gmail.com>
2022-09-17 14:54:04 +0800

    Code cleanup

--- Source/Libs/AM.Avalonia/Source/ExtendedToolkit/Controls/Buttons/MetroRadioButton.cs
+++ Source/Libs/AM.Avalonia/Source/ExtendedToolkit/Controls/Buttons/MetroRadioButton.cs
@@ -1,112 +1,115 @@
-﻿using System;
+﻿// ReSharper disable CheckNamespace
+// ReSharper disable IdentifierTypo
+
+using System;
+
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Controls.Shapes;
 using Avalonia.Media;
 
-namespace Avalonia.ExtendedToolkit.Controls
+namespace Avalonia.ExtendedToolkit.Controls;
+
+/// <summary>
+/// radiobutton with special properties
+/// </summary>
+public class MetroRadioButton
+    : RadioButton
 {
     /// <summary>
-    /// radiobutton with special properties
+    /// style key for this control
     /// </summary>
-    public class MetroRadioButton: RadioButton
+    public Type StyleKey => typeof (MetroRadioButton);
+
+    /// <summary>
+    /// get/sets FocusBorderBrush
+    /// </summary>
+    public IBrush FocusBorderBrush
     {
-        /// <summary>
-        /// style key for this control
-        /// </summary>
-        public Type StyleKey => typeof(MetroRadioButton);
+        get { return (IBrush)GetValue (FocusBorderBrushProperty); }
+        set { SetValue (FocusBorderBrushProperty, value); }
+    }
 
-        /// <summary>
-        /// get/sets FocusBorderBrush
-        /// </summary>
-        public IBrush FocusBorderBrush
+    /// <summary>
+    /// <see cref="FocusBorderBrush"/>
+    /// </summary>
+    public static readonly StyledProperty<IBrush> FocusBorderBrushProperty =
+        AvaloniaProperty.Register<MetroRadioButton, IBrush> (nameof (FocusBorderBrush));
+
+    /// <summary>
+    /// get/sets MouseOverBorderBrush
+    /// </summary>
+    public IBrush MouseOverBorderBrush
+    {
+        get { return (IBrush)GetValue (MouseOverBorderBrushProperty); }
+        set { SetValue (MouseOverBorderBrushProperty, value); }
+    }
+
+    /// <summary>
+    /// <see cref="MouseOverBorderBrush"/>
+    /// </summary>
+    public static readonly StyledProperty<IBrush> MouseOverBorderBrushProperty =
+        AvaloniaProperty.Register<MetroRadioButton, IBrush> (nameof (MouseOverBorderBrush));
+
+    /// <summary>
+    /// get/sets ContentDirection
+    /// </summary>
+    public FlowDirection ContentDirection
+    {
+        get { return (FlowDirection)GetValue (ContentDirectionProperty); }
+        set { SetValue (ContentDirectionProperty, value); }
+    }
+
+    /// <summary>
+    /// <see cref="ContentDirection"/>
+    /// </summary>
+    public static readonly StyledProperty<FlowDirection> ContentDirectionProperty =
+        AvaloniaProperty.Register<MetroRadioButton, FlowDirection> (nameof (ContentDirection));
+
+    private Ellipse _checkedEllipse;
+
+    /// <summary>
+    /// registered is checked property
+    /// </summary>
+    public MetroRadioButton()
+    {
+        IsCheckedProperty.Changed.AddClassHandler<MetroRadioButton> ((o, e) => OnIsCheckChanged (o, e));
+    }
+
+    private void OnIsCheckChanged (MetroRadioButton metroCheckBox, AvaloniaPropertyChangedEventArgs e)
+    {
+        if (metroCheckBox._checkedEllipse != null)
         {
-            get { return (IBrush)GetValue(FocusBorderBrushProperty); }
-            set { SetValue(FocusBorderBrushProperty, value); }
-        }
+            metroCheckBox._checkedEllipse.Opacity = e.NewValue != null && (bool)e.NewValue ? 1 : 0;
 
-        /// <summary>
-        /// <see cref="FocusBorderBrush"/>
-        /// </summary>
-        public static readonly StyledProperty<IBrush> FocusBorderBrushProperty =
-            AvaloniaProperty.Register<MetroRadioButton, IBrush>(nameof(FocusBorderBrush));
-
-        /// <summary>
-        /// get/sets MouseOverBorderBrush
-        /// </summary>
-        public IBrush MouseOverBorderBrush
-        {
-            get { return (IBrush)GetValue(MouseOverBorderBrushProperty); }
-            set { SetValue(MouseOverBorderBrushProperty, value); }
-        }
-
-        /// <summary>
-        /// <see cref="MouseOverBorderBrush"/>
-        /// </summary>
-        public static readonly StyledProperty<IBrush> MouseOverBorderBrushProperty =
-            AvaloniaProperty.Register<MetroRadioButton, IBrush>(nameof(MouseOverBorderBrush));
-
-        /// <summary>
-        /// get/sets ContentDirection
-        /// </summary>
-        public FlowDirection ContentDirection
-        {
-            get { return (FlowDirection)GetValue(ContentDirectionProperty); }
-            set { SetValue(ContentDirectionProperty, value); }
-        }
-
-        /// <summary>
-        /// <see cref="ContentDirection"/>
-        /// </summary>
-        public static readonly StyledProperty<FlowDirection> ContentDirectionProperty =
-            AvaloniaProperty.Register<MetroRadioButton, FlowDirection>(nameof(ContentDirection));
-
-        private Ellipse _checkedEllipse;
-
-        /// <summary>
-        /// registered is checked property
-        /// </summary>
-        public MetroRadioButton()
-        {
-            IsCheckedProperty.Changed.AddClassHandler<MetroRadioButton>((o, e) => OnIsCheckChanged(o, e));
-        }
-
-        private void OnIsCheckChanged(MetroRadioButton metroCheckBox, AvaloniaPropertyChangedEventArgs e)
-        {
-            if (metroCheckBox._checkedEllipse != null)
+            if (e.NewValue == null && metroCheckBox.IsThreeState)
             {
-                metroCheckBox._checkedEllipse.Opacity = e.NewValue != null && (bool)e.NewValue ? 1 : 0;
-
-                if (e.NewValue == null && metroCheckBox.IsThreeState)
-                {
-                    metroCheckBox._checkedEllipse.Opacity = 0;
-                }
+                metroCheckBox._checkedEllipse.Opacity = 0;
             }
         }
+    }
 
-        /// <summary>
-        /// gets _checkedEllipse
-        /// inits the default check state
-        /// </summary>
-        /// <param name="e"></param>
-        protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    /// <inheritdoc cref="TemplatedControl.OnApplyTemplate"/>
+    protected override void OnApplyTemplate
+        (
+            TemplateAppliedEventArgs eventArgs
+        )
+    {
+        _checkedEllipse = eventArgs.NameScope.Find<Ellipse> ("Checked1");
+
+        //set init value
+        bool? isChecked = IsChecked;
+        if (isChecked.HasValue)
         {
-            _checkedEllipse = e.NameScope.Find<Ellipse>("Checked1");
-
-            //set init value
-            bool? isChecked = IsChecked;
-            if (isChecked.HasValue)
-            {
-                IsChecked = null;
-            }
-            else
-            {
-                IsChecked = false;
-            }
-
-            IsChecked = isChecked;
-
-            base.OnApplyTemplate(e);
+            IsChecked = null;
         }
+        else
+        {
+            IsChecked = false;
+        }
+
+        IsChecked = isChecked;
+
+        base.OnApplyTemplate (eventArgs);
     }
 }
--- Source/Libs/AM.Avalonia/Source/ExtendedToolkit/Controls/CroppingControl/CroppingControl.Properties.cs
+++ Source/Libs/AM.Avalonia/Source/ExtendedToolkit/Controls/CroppingControl/CroppingControl.Properties.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.Windows.Input;
+
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Controls.Shapes;
@@ -47,83 +48,83 @@ namespace Avalonia.ExtendedToolkit.Controls
         /// </summary>
         public string ImagePath
         {
-            get { return (string)GetValue(ImagePathProperty); }
-            set { SetValue(ImagePathProperty, value); }
+            get { return (string)GetValue (ImagePathProperty); }
+            set { SetValue (ImagePathProperty, value); }
         }
 
         /// <summary>
         /// Defines the ImagePath property.
         /// </summary>
         public static readonly StyledProperty<string> ImagePathProperty =
-        AvaloniaProperty.Register<CroppingControl, string>(nameof(ImagePath));
+            AvaloniaProperty.Register<CroppingControl, string> (nameof (ImagePath));
 
         /// <summary>
         /// Gets or sets CroppedImage.
         /// </summary>
-        public IImage CroppedImage
+        public IImage? CroppedImage
         {
-            get { return (IImage)GetValue(CroppedImageProperty); }
-            private set { SetValue(CroppedImageProperty, value); }
+            get { return (IImage?)GetValue (CroppedImageProperty); }
+            private set { SetValue (CroppedImageProperty, value); }
         }
 
         /// <summary>
         /// Defines the CroppedImage property.
         /// </summary>
-        public static readonly StyledProperty<IImage> CroppedImageProperty =
-        AvaloniaProperty.Register<CroppingControl, IImage>(nameof(CroppedImage));
+        public static readonly StyledProperty<IImage?> CroppedImageProperty =
+            AvaloniaProperty.Register<CroppingControl, IImage?> (nameof (CroppedImage));
 
         /// <summary>
         /// Gets or sets ImageInputBytes.
         /// </summary>
         public byte[] ImageInputBytes
         {
-            get { return (byte[])GetValue(ImageInputBytesProperty); }
-            set { SetValue(ImageInputBytesProperty, value); }
+            get { return (byte[])GetValue (ImageInputBytesProperty); }
+            set { SetValue (ImageInputBytesProperty, value); }
         }
 
         /// <summary>
         /// Defines the ImageInputBytes property.
         /// </summary>
         public static readonly StyledProperty<byte[]> ImageInputBytesProperty =
-        AvaloniaProperty.Register<CroppingControl, byte[]>(nameof(ImageInputBytes), defaultValue: null);
+            AvaloniaProperty.Register<CroppingControl, byte[]> (nameof (ImageInputBytes), defaultValue: null);
 
         /// <summary>
         /// Gets or sets ImageOutputBytes.
         /// </summary>
         public byte[] ImageOutputBytes
         {
-            get { return (byte[])GetValue(ImageOutputBytesProperty); }
-            set { SetValue(ImageOutputBytesProperty, value); }
+            get { return (byte[])GetValue (ImageOutputBytesProperty); }
+            set { SetValue (ImageOutputBytesProperty, value); }
         }
 
         /// <summary>
         /// Defines the ImageOutputBytes property.
         /// </summary>
         public static readonly StyledProperty<byte[]> ImageOutputBytesProperty =
-        AvaloniaProperty.Register<CroppingControl, byte[]>(nameof(ImageOutputBytes), defaultValue: null);
+            AvaloniaProperty.Register<CroppingControl, byte[]> (nameof (ImageOutputBytes), defaultValue: null);
 
         /// <summary>
         /// Gets or sets SaveCommand.
         /// </summary>
         public ICommand SaveCommand
         {
-            get { return (ICommand)GetValue(SaveCommandProperty); }
-            private set { SetValue(SaveCommandProperty, value); }
+            get { return (ICommand)GetValue (SaveCommandProperty); }
+            private set { SetValue (SaveCommandProperty, value); }
         }
 
         /// <summary>
         /// Defines the SaveCommand property.
         /// </summary>
         public static readonly StyledProperty<ICommand> SaveCommandProperty =
-        AvaloniaProperty.Register<CroppingControl, ICommand>(nameof(SaveCommand));
+            AvaloniaProperty.Register<CroppingControl, ICommand> (nameof (SaveCommand));
 
         /// <summary>
         /// Gets or sets OpenImageCommand.
         /// </summary>
         public ICommand OpenImageCommand
         {
-            get { return (ICommand)GetValue(OpenImageCommandProperty); }
-            private set { SetValue(OpenImageCommandProperty, value); }
+            get { return (ICommand)GetValue (OpenImageCommandProperty); }
+            private set { SetValue (OpenImageCommandProperty, value); }
         }
 
         /// <summary>
@@ -131,81 +132,81 @@ namespace Avalonia.ExtendedToolkit.Controls
         /// </summary>
         public IBrush RotateResizeBorderBrush
         {
-            get { return (IBrush)GetValue(RotateResizeBorderBrushProperty); }
-            set { SetValue(RotateResizeBorderBrushProperty, value); }
+            get { return (IBrush)GetValue (RotateResizeBorderBrushProperty); }
+            set { SetValue (RotateResizeBorderBrushProperty, value); }
         }
 
         /// <summary>
         /// Defines the RotateResizeBorderBrush property.
         /// </summary>
         public static readonly StyledProperty<IBrush> RotateResizeBorderBrushProperty =
-        AvaloniaProperty.Register<CroppingControl, IBrush>(nameof(RotateResizeBorderBrush));
+            AvaloniaProperty.Register<CroppingControl, IBrush> (nameof (RotateResizeBorderBrush));
 
         /// <summary>
         /// Defines the OpenImageCommand property.
         /// </summary>
         public static readonly StyledProperty<ICommand> OpenImageCommandProperty =
-        AvaloniaProperty.Register<CroppingControl, ICommand>(nameof(OpenImageCommand));
+            AvaloniaProperty.Register<CroppingControl, ICommand> (nameof (OpenImageCommand));
 
         /// <summary>
         /// Gets or sets CanDoPictureModification.
         /// </summary>
         public bool CanDoPictureModification
         {
-            get { return (bool)GetValue(CanDoPictureModificationProperty); }
-            set { SetValue(CanDoPictureModificationProperty, value); }
+            get { return (bool)GetValue (CanDoPictureModificationProperty); }
+            set { SetValue (CanDoPictureModificationProperty, value); }
         }
 
         /// <summary>
         /// Defines the CanDoPictureModification property.
         /// </summary>
         public static readonly StyledProperty<bool> CanDoPictureModificationProperty =
-        AvaloniaProperty.Register<CroppingControl, bool>(nameof(CanDoPictureModification), defaultValue: false);
+            AvaloniaProperty.Register<CroppingControl, bool> (nameof (CanDoPictureModification), defaultValue: false);
 
         /// <summary>
         /// Gets or sets ZoomIncrementCommand.
         /// </summary>
         public ICommand ZoomIncrementCommand
         {
-            get { return (ICommand)GetValue(ZoomIncrementCommandProperty); }
-            set { SetValue(ZoomIncrementCommandProperty, value); }
+            get { return (ICommand)GetValue (ZoomIncrementCommandProperty); }
+            set { SetValue (ZoomIncrementCommandProperty, value); }
         }
 
         /// <summary>
         /// Defines the ZoomIncrementCommand property.
         /// </summary>
         public static readonly StyledProperty<ICommand> ZoomIncrementCommandProperty =
-        AvaloniaProperty.Register<CroppingControl, ICommand>(nameof(ZoomIncrementCommand));
+            AvaloniaProperty.Register<CroppingControl, ICommand> (nameof (ZoomIncrementCommand));
 
         /// <summary>
         /// Gets or sets ZoomDecrementCommand.
         /// </summary>
         public ICommand ZoomDecrementCommand
         {
-            get { return (ICommand)GetValue(ZoomDecrementCommandProperty); }
-            set { SetValue(ZoomDecrementCommandProperty, value); }
+            get { return (ICommand)GetValue (ZoomDecrementCommandProperty); }
+            set { SetValue (ZoomDecrementCommandProperty, value); }
         }
 
         /// <summary>
         /// Defines the ZoomDecrementCommand property.
         /// </summary>
         public static readonly StyledProperty<ICommand> ZoomDecrementCommandProperty =
-        AvaloniaProperty.Register<CroppingControl, ICommand>(nameof(ZoomDecrementCommand));
+            AvaloniaProperty.Register<CroppingControl, ICommand> (nameof (ZoomDecrementCommand));
 
         /// <summary>
         /// Gets or sets ZoomFactor.
         /// </summary>
         public double ZoomFactor
         {
-            get { return (double)GetValue(ZoomFactorProperty); }
-            set { SetValue(ZoomFactorProperty, value); }
+            get { return (double)GetValue (ZoomFactorProperty); }
+            set { SetValue (ZoomFactorProperty, value); }
         }
 
         /// <summary>
         /// Defines the ZoomFactor property.
         /// </summary>
         public static readonly StyledProperty<double> ZoomFactorProperty =
-        AvaloniaProperty.Register<CroppingControl, double>(nameof(ZoomFactor), defaultValue: 1.0);
+            AvaloniaProperty.Register<CroppingControl, double> (nameof (ZoomFactor), defaultValue: 1.0);
 
         //public ItemVM FlipVerticalVM{get;set;}=new ItemVM{Text="Flip Vertical",Image};
         /// <summary>
@@ -213,164 +214,165 @@ namespace Avalonia.ExtendedToolkit.Controls
         /// </summary>
         public ObservableCollection<ItemVM> FlipItems
         {
-            get { return (ObservableCollection<ItemVM>)GetValue(FlipItemsProperty); }
-            set { SetValue(FlipItemsProperty, value); }
+            get { return (ObservableCollection<ItemVM>)GetValue (FlipItemsProperty); }
+            set { SetValue (FlipItemsProperty, value); }
         }
 
         /// <summary>
         /// Defines the FlipItems property.
         /// </summary>
         public static readonly StyledProperty<ObservableCollection<ItemVM>> FlipItemsProperty =
-        AvaloniaProperty.Register<CroppingControl, ObservableCollection<ItemVM>>(nameof(FlipItems));
+            AvaloniaProperty.Register<CroppingControl, ObservableCollection<ItemVM>> (nameof (FlipItems));
 
         /// <summary>
         /// Gets or sets CroppingTypes.
         /// </summary>
         public ObservableCollection<ItemVM> CroppingTypes
         {
-            get { return (ObservableCollection<ItemVM>)GetValue(CroppingTypesProperty); }
-            set { SetValue(CroppingTypesProperty, value); }
+            get { return (ObservableCollection<ItemVM>)GetValue (CroppingTypesProperty); }
+            set { SetValue (CroppingTypesProperty, value); }
         }
 
         /// <summary>
         /// Defines the CroppingTypes property.
         /// </summary>
         public static readonly StyledProperty<ObservableCollection<ItemVM>> CroppingTypesProperty =
-        AvaloniaProperty.Register<CroppingControl, ObservableCollection<ItemVM>>(nameof(CroppingTypes));
+            AvaloniaProperty.Register<CroppingControl, ObservableCollection<ItemVM>> (nameof (CroppingTypes));
 
         /// <summary>
         /// Gets or sets CurrentCroppingType.
         /// </summary>
         public ItemVM CurrentCroppingType
         {
-            get { return (ItemVM)GetValue(CurrentCroppingTypeProperty); }
-            set { SetValue(CurrentCroppingTypeProperty, value); }
+            get { return (ItemVM)GetValue (CurrentCroppingTypeProperty); }
+            set { SetValue (CurrentCroppingTypeProperty, value); }
         }
 
         /// <summary>
         /// Defines the CurrentCroppingType property.
         /// </summary>
         public static readonly StyledProperty<ItemVM> CurrentCroppingTypeProperty =
-        AvaloniaProperty.Register<CroppingControl, ItemVM>(nameof(CurrentCroppingType));
+            AvaloniaProperty.Register<CroppingControl, ItemVM> (nameof (CurrentCroppingType));
 
         /// <summary>
         /// Gets or sets RotateItems.
         /// </summary>
         public ObservableCollection<ItemVM> RotateItems
         {
-            get { return (ObservableCollection<ItemVM>)GetValue(RotateItemsProperty); }
-            set { SetValue(RotateItemsProperty, value); }
+            get { return (ObservableCollection<ItemVM>)GetValue (RotateItemsProperty); }
+            set { SetValue (RotateItemsProperty, value); }
         }
 
         /// <summary>
         /// Defines the RotateItems property.
         /// </summary>
         public static readonly StyledProperty<ObservableCollection<ItemVM>> RotateItemsProperty =
-        AvaloniaProperty.Register<CroppingControl, ObservableCollection<ItemVM>>(nameof(RotateItems));
+            AvaloniaProperty.Register<CroppingControl, ObservableCollection<ItemVM>> (nameof (RotateItems));
 
         /// <summary>
         /// Gets or sets PictureManipulationCommand.
         /// </summary>
         public ICommand PictureManipulationCommand
         {
-            get { return (ICommand)GetValue(PictureManipulationCommandProperty); }
-            set { SetValue(PictureManipulationCommandProperty, value); }
+            get { return (ICommand)GetValue (PictureManipulationCommandProperty); }
+            set { SetValue (PictureManipulationCommandProperty, value); }
         }
 
         /// <summary>
         /// Defines the PictureManipulationCommand property.
         /// </summary>
         public static readonly StyledProperty<ICommand> PictureManipulationCommandProperty =
-        AvaloniaProperty.Register<CroppingControl, ICommand>(nameof(PictureManipulationCommand));
+            AvaloniaProperty.Register<CroppingControl, ICommand> (nameof (PictureManipulationCommand));
 
         /// <summary>
         /// Gets or sets ZoomMaximum.
         /// </summary>
         public double ZoomMaximum
         {
-            get { return (double)GetValue(ZoomMaximumProperty); }
-            set { SetValue(ZoomMaximumProperty, value); }
+            get { return (double)GetValue (ZoomMaximumProperty); }
+            set { SetValue (ZoomMaximumProperty, value); }
         }
 
         /// <summary>
         /// Defines the <see cref="ZoomMaximum"/> property.
         /// </summary>
         public static readonly StyledProperty<double> ZoomMaximumProperty =
-            AvaloniaProperty.Register<CroppingControl, double>(nameof(ZoomMaximum), defaultValue: 200);
+            AvaloniaProperty.Register<CroppingControl, double> (nameof (ZoomMaximum), defaultValue: 200);
 
         /// <summary>
         /// Gets or sets ZoomMinimum.
         /// </summary>
         public double ZoomMinimum
         {
-            get { return (double)GetValue(ZoomMinimumProperty); }
-            set { SetValue(ZoomMinimumProperty, value); }
+            get { return (double)GetValue (ZoomMinimumProperty); }
+            set { SetValue (ZoomMinimumProperty, value); }
         }
 
         /// <summary>
         /// Defines the <see cref="ZoomMinimum"/> property.
         /// </summary>
         public static readonly StyledProperty<double> ZoomMinimumProperty =
-            AvaloniaProperty.Register<CroppingControl, double>(nameof(ZoomMinimum), defaultValue: 1d);
+            AvaloniaProperty.Register<CroppingControl, double> (nameof (ZoomMinimum), defaultValue: 1d);
 
         /// <summary>
         /// Gets or sets RotateResizeBackgroundBrush.
         /// </summary>
         public IBrush RotateResizeBackgroundBrush
         {
-            get { return (IBrush)GetValue(RotateResizeBackgroundBrushProperty); }
-            set { SetValue(RotateResizeBackgroundBrushProperty, value); }
+            get { return (IBrush)GetValue (RotateResizeBackgroundBrushProperty); }
+            set { SetValue (RotateResizeBackgroundBrushProperty, value); }
         }
 
         /// <summary>
         /// Defines the <see cref="RotateResizeBackgroundBrush"/> property.
         /// </summary>
         public static readonly StyledProperty<IBrush> RotateResizeBackgroundBrushProperty =
-            AvaloniaProperty.Register<CroppingControl, IBrush>(nameof(RotateResizeBackgroundBrush));
+            AvaloniaProperty.Register<CroppingControl, IBrush> (nameof (RotateResizeBackgroundBrush));
 
         /// <summary>
         /// Gets or sets CancelCommand.
         /// </summary>
         public ICommand CancelCommand
         {
-            get { return (ICommand)GetValue(CancelCommandProperty); }
-            set { SetValue(CancelCommandProperty, value); }
+            get { return (ICommand)GetValue (CancelCommandProperty); }
+            set { SetValue (CancelCommandProperty, value); }
         }
 
         /// <summary>
         /// Defines the <see cref="CancelCommand"/> property.
         /// </summary>
         public static readonly StyledProperty<ICommand> CancelCommandProperty =
-            AvaloniaProperty.Register<CroppingControl, ICommand>(nameof(CancelCommand));
+            AvaloniaProperty.Register<CroppingControl, ICommand> (nameof (CancelCommand));
 
         /// <summary>
         /// Gets or sets BackgroundRectangleOpacity.
         /// </summary>
         public double BackgroundRectangleOpacity
         {
-            get { return (double)GetValue(BackgroundRectangleOpacityProperty); }
-            set { SetValue(BackgroundRectangleOpacityProperty, value); }
+            get { return (double)GetValue (BackgroundRectangleOpacityProperty); }
+            set { SetValue (BackgroundRectangleOpacityProperty, value); }
         }
 
         /// <summary>
         /// Defines the <see cref="BackgroundRectangleOpacity"/> property.
         /// </summary>
         public static readonly StyledProperty<double> BackgroundRectangleOpacityProperty =
-            AvaloniaProperty.Register<CroppingControl, double>(nameof(BackgroundRectangleOpacity), defaultValue: 0.5d);
+            AvaloniaProperty.Register<CroppingControl, double> (nameof (BackgroundRectangleOpacity),
+                defaultValue: 0.5d);
 
         /// <summary>
         /// Gets or sets ShowBackground.
         /// </summary>
         public bool ShowBackground
         {
-            get { return (bool)GetValue(ShowBackgroundProperty); }
-            set { SetValue(ShowBackgroundProperty, value); }
+            get { return (bool)GetValue (ShowBackgroundProperty); }
+            set { SetValue (ShowBackgroundProperty, value); }
         }
 
         /// <summary>
         /// Defines the ShowBackground property.
         /// </summary>
         public static readonly StyledProperty<bool> ShowBackgroundProperty =
-        AvaloniaProperty.Register<CroppingControl, bool>(nameof(ShowBackground), defaultValue: true);
+            AvaloniaProperty.Register<CroppingControl, bool> (nameof (ShowBackground), defaultValue: true);
     }
 }
