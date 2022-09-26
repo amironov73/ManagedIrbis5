de2757e54175c44e597ad0fb33a87097d115220b
Alexey Mironov <amironov73@gmail.com>
2022-09-26 08:56:02 +0800

    Code cleanup

--- Source/Tests/FormsTests/Source/Tests/ConsoleControlTest2.cs
+++ Source/Tests/FormsTests/Source/Tests/ConsoleControlTest2.cs
@@ -1,4 +1,7 @@
-﻿// ReSharper disable CheckNamespace
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
 // ReSharper disable CommentTypo
 // ReSharper disable IdentifierTypo
 // ReSharper disable UnusedMember.Global
