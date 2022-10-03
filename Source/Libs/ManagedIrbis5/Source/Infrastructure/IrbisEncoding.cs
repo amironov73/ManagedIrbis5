b3e0f2c38f75cd96678d97ceb6aa2ed43fbd68d4
Alexey Mironov <amironov73@gmail.com>
2022-10-03 15:36:25 +0800

    Add CP1251 to UTF8 direct conversion

--- Source/Libs/ManagedIrbis5/Source/Infrastructure/IrbisEncoding.cs
+++ Source/Libs/ManagedIrbis5/Source/Infrastructure/IrbisEncoding.cs
@@ -17,6 +17,7 @@
 #region Using directives
 
 using System;
+using System.IO;
 using System.Text;
 
 using AM;
@@ -69,13 +70,40 @@ public static class IrbisEncoding
                 encoderShouldEmitUTF8Identifier: false,
                 throwOnInvalidBytes: true
             );
-    } // static constructor
+    }
 
     #endregion
 
     #region Private members
 
     private static Encoding _ansi, _oem, _utf8;
+
+    /// <summary>
+    /// Таблица ручной перекодировки CP1251 &lt;-&gt; UTF8.
+    /// </summary>
+    private static readonly short[] _cp1251_unicode =
+    {
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+        19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34,
+        35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+        51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66,
+        67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82,
+        83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98,
+        99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111,
+        112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124,
+        125, 126, 127, 1026, 1027, 8218, 1107, 8222, 8230, 8224, 8225,
+        8364, 8240, 1033, 8249, 1034, 1036, 1035, 1039, 1106, 8216, 8217,
+        8220, 8221, 8226, 8211, 8212, 152, 8482, 1113, 8250, 1114, 1116,
+        1115, 1119, 160, 1038, 1118, 1032, 164, 1168, 166, 167, 1025, 169,
+        1028, 171, 172, 173, 174, 1031, 176, 177, 1030, 1110, 1169, 181,
+        182, 183, 1105, 8470, 1108, 187, 1112, 1029, 1109, 1111, 1040,
+        1041, 1042, 1043, 1044, 1045, 1046, 1047, 1048, 1049, 1050, 1051,
+        1052, 1053, 1054, 1055, 1056, 1057, 1058, 1059, 1060, 1061, 1062,
+        1063, 1064, 1065, 1066, 1067, 1068, 1069, 1070, 1071, 1072, 1073,
+        1074, 1075, 1076, 1077, 1078, 1079, 1080, 1081, 1082, 1083, 1084,
+        1085, 1086, 1087, 1088, 1089, 1090, 1091, 1092, 1093, 1094, 1095,
+        1096, 1097, 1098, 1099, 1100, 1101, 1102, 1103
+    };
 
     #endregion
 
@@ -91,6 +119,7 @@ public static class IrbisEncoding
     {
         if (string.IsNullOrEmpty (name))
         {
+            // кодировка по умолчанию
             return Utf8;
         }
 
@@ -116,7 +145,7 @@ public static class IrbisEncoding
         var result = Encoding.GetEncoding (name);
 
         return result;
-    } // namespace ByName
+    }
 
     /// <summary>
     /// Get encoding from config file.
@@ -133,7 +162,7 @@ public static class IrbisEncoding
         var result = ByName (name);
 
         return result;
-    } // method FromConfig
+    }
 
     /// <summary>
     /// Relax UTF-8 decoder, do not throw exceptions
@@ -146,7 +175,7 @@ public static class IrbisEncoding
                 encoderShouldEmitUTF8Identifier: false,
                 throwOnInvalidBytes: false
             );
-    } // method RelaxUtf8
+    }
 
     /// <summary>
     /// Strong UTF-8 decoder, throw exceptions
@@ -159,7 +188,7 @@ public static class IrbisEncoding
                 encoderShouldEmitUTF8Identifier: false,
                 throwOnInvalidBytes: true
             );
-    } // method StrongUtf8
+    }
 
     /// <summary>
     /// Override default single-byte encoding.
@@ -203,6 +232,134 @@ public static class IrbisEncoding
         }
 
         _oem = encoding;
+    }
+
+    /// <summary>
+    /// Ручная перекодировка CP1251 -&gt; UTF8.
+    /// </summary>
+    public static byte[] AnsiToUtf
+        (
+            ReadOnlySpan<byte> text
+        )
+    {
+        var result = new MemoryStream();
+        AnsiToUtf (result, text);
+
+        return result.ToArray();
+    }
+
+    /// <summary>
+    /// Ручная перекодировка CP1251 -&gt; UTF8.
+    /// </summary>
+    public static void AnsiToUtf
+        (
+            Stream output,
+            ReadOnlySpan<byte> text
+        )
+    {
+        Sure.NotNull (output);
+
+        var length = text.Length;
+
+        unchecked
+        {
+            for (var i = 0; i < length; i++)
+            {
+                var chr = _cp1251_unicode[text[i]];
+                if (chr < 128)
+                {
+                    output.WriteByte ((byte) chr);
+                }
+                else
+                {
+                    output.WriteByte ((byte) ((chr >> 6) | 0xC0));
+                    output.WriteByte ((byte) ((chr & 0x3Fu) | 0x80u));
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Ручная перекодировка UTF8 -&gt; CP1251.
+    /// </summary>
+    public static byte[] UtfToAnsi
+        (
+            ReadOnlySpan<byte> text
+        )
+    {
+        var result = new MemoryStream();
+        UtfToAnsi (result, text);
+
+        return result.ToArray();
+    }
+
+    /// <summary>
+    /// Ручная перекодировка UTF8 -&gt; CP1251.
+    /// </summary>
+    public static void UtfToAnsi
+        (
+            Stream output,
+            ReadOnlySpan<byte> text
+        )
+    {
+        Sure.NotNull (output);
+
+        var length = text.Length;
+        unchecked
+        {
+            var offset = 0;
+            while (offset < length)
+            {
+                var first = text[offset++];
+                if ((first & 0x80) == 0)
+                {
+                    output.WriteByte (first);
+                }
+                else
+                {
+                    var second = text[offset++];
+                    if ((first & 0xF0) == 0xE0)
+                    {
+                        output.WriteByte ((byte) '?');
+                        offset++;
+                        continue;
+                    }
+
+                    if ((first & 0xF8) == 0xF0)
+                    {
+                        output.WriteByte ((byte) '?');
+                        output.WriteByte ((byte) '?');
+                        offset++;
+                        offset++;
+                        continue;
+                    }
+
+                    if ((first & 0xF8) == 0xF8)
+                    {
+                        output.WriteByte ((byte) '?');
+                        output.WriteByte ((byte) '?');
+                        output.WriteByte ((byte) '?');
+                        offset++;
+                        offset++;
+                        offset++;
+                        continue;
+                    }
+
+                    var unicode = (short) (((first & 0x1F) << 6) + (second & 0x3F));
+                    var chr = (byte) '?';
+                    for (var i = 0; i < 256; i++)
+                    {
+                        if (_cp1251_unicode[i] == unicode)
+                        {
+                            chr = (byte) i;
+                            break;
+                        }
+                    }
+
+                    output.WriteByte (chr);
+                }
+            }
+        }
     }
 
     #endregion
--- Source/Tests/UnitTests/Source/ManagedIrbis/Infrastructure/IrbisEncodingTest.cs
+++ Source/Tests/UnitTests/Source/ManagedIrbis/Infrastructure/IrbisEncodingTest.cs
@@ -1,92 +1,118 @@
-﻿using System;
+﻿// ReSharper disable CheckNamespace
+// ReSharper disable IdentifierTypo
+
+using System;
 using System.Text;
 
 using ManagedIrbis.Infrastructure;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-// ReSharper disable CheckNamespace
+namespace UnitTests.ManagedIrbis;
 
-namespace UnitTests.ManagedIrbis
+[TestClass]
+public class IrbisEncodingTest
 {
-    [TestClass]
-    public class IrbisEncodingTest
+    [TestMethod]
+    public void IrbisEncoding_Static_Constructor_1()
     {
-        [TestMethod]
-        public void IrbisEncoding_Static_Constructor_1()
-        {
-            Assert.IsNotNull(IrbisEncoding.Ansi);
-            Assert.IsNotNull(IrbisEncoding.Oem);
-            Assert.IsNotNull(IrbisEncoding.Utf8);
-        }
+        Assert.IsNotNull (IrbisEncoding.Ansi);
+        Assert.IsNotNull (IrbisEncoding.Oem);
+        Assert.IsNotNull (IrbisEncoding.Utf8);
+    }
 
-        [TestMethod]
-        public void IrbisEncoding_Ansi_1()
-        {
-            Assert.AreEqual(1251, IrbisEncoding.Ansi.CodePage);
-        }
+    [TestMethod]
+    public void IrbisEncoding_Ansi_1()
+    {
+        Assert.AreEqual (1251, IrbisEncoding.Ansi.CodePage);
+    }
 
-        [TestMethod]
-        public void IrbisEncoding_Oem_1()
-        {
-            Assert.AreEqual(866, IrbisEncoding.Oem.CodePage);
-        }
+    [TestMethod]
+    public void IrbisEncoding_Oem_1()
+    {
+        Assert.AreEqual (866, IrbisEncoding.Oem.CodePage);
+    }
 
-        [TestMethod]
-        public void IrbisEncoding_Utf8_1()
-        {
-            Assert.AreEqual(65001, IrbisEncoding.Utf8.CodePage);
-        }
+    [TestMethod]
+    public void IrbisEncoding_Utf8_1()
+    {
+        Assert.AreEqual (65001, IrbisEncoding.Utf8.CodePage);
+    }
 
-        [TestMethod]
-        public void IrbisEncoding_ByName_1()
-        {
-            Assert.AreEqual(1251, IrbisEncoding.ByName("Ansi").CodePage);
-            Assert.AreEqual(866, IrbisEncoding.ByName("Oem").CodePage);
-            Assert.AreEqual(65001, IrbisEncoding.ByName("Utf").CodePage);
-            Assert.AreEqual(65001, IrbisEncoding.ByName(string.Empty).CodePage);
-            Assert.AreEqual(1252, IrbisEncoding.ByName("windows-1252").CodePage);
-        }
+    [TestMethod]
+    public void IrbisEncoding_ByName_1()
+    {
+        Assert.AreEqual (1251, IrbisEncoding.ByName ("Ansi").CodePage);
+        Assert.AreEqual (866, IrbisEncoding.ByName ("Oem").CodePage);
+        Assert.AreEqual (65001, IrbisEncoding.ByName ("Utf").CodePage);
+        Assert.AreEqual (65001, IrbisEncoding.ByName (string.Empty).CodePage);
+        Assert.AreEqual (1252, IrbisEncoding.ByName ("windows-1252").CodePage);
+    }
 
-        [TestMethod]
-        public void IrbisEncoding_RelaxUtf8_1()
-        {
-            IrbisEncoding.RelaxUtf8();
-            IrbisEncoding.StrongUtf8();
-        }
+    [TestMethod]
+    public void IrbisEncoding_RelaxUtf8_1()
+    {
+        IrbisEncoding.RelaxUtf8();
+        IrbisEncoding.StrongUtf8();
+    }
 
-        [TestMethod]
-        public void IrbisEncoding_SetAnsiEncoding_1()
-        {
-            Encoding saveEncoding = IrbisEncoding.Ansi;
-            Encoding encoding = IrbisEncoding.ByName("windows-1252");
-            IrbisEncoding.SetAnsiEncoding(encoding);
-            Assert.AreSame(encoding, IrbisEncoding.Ansi);
-            IrbisEncoding.SetAnsiEncoding(saveEncoding);
-        }
+    [TestMethod]
+    public void IrbisEncoding_SetAnsiEncoding_1()
+    {
+        var saveEncoding = IrbisEncoding.Ansi;
+        var encoding = IrbisEncoding.ByName ("windows-1252");
+        IrbisEncoding.SetAnsiEncoding (encoding);
+        Assert.AreSame (encoding, IrbisEncoding.Ansi);
+        IrbisEncoding.SetAnsiEncoding (saveEncoding);
+    }
 
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException))]
-        public void IrbisEncoding_SetAnsiEncoding_2()
-        {
-            IrbisEncoding.SetAnsiEncoding(Encoding.Unicode);
-        }
+    [TestMethod]
+    [ExpectedException (typeof (ArgumentOutOfRangeException))]
+    public void IrbisEncoding_SetAnsiEncoding_2()
+    {
+        IrbisEncoding.SetAnsiEncoding (Encoding.Unicode);
+    }
 
-        [TestMethod]
-        public void IrbisEncoding_SetOemEncoding_1()
-        {
-            Encoding saveEncoding = IrbisEncoding.Oem;
-            Encoding encoding = IrbisEncoding.ByName("windows-1252");
-            IrbisEncoding.SetOemEncoding(encoding);
-            Assert.AreSame(encoding, IrbisEncoding.Oem);
-            IrbisEncoding.SetOemEncoding(saveEncoding);
-        }
+    [TestMethod]
+    public void IrbisEncoding_SetOemEncoding_1()
+    {
+        var saveEncoding = IrbisEncoding.Oem;
+        var encoding = IrbisEncoding.ByName ("windows-1252");
+        IrbisEncoding.SetOemEncoding (encoding);
+        Assert.AreSame (encoding, IrbisEncoding.Oem);
+        IrbisEncoding.SetOemEncoding (saveEncoding);
+    }
 
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException))]
-        public void IrbisEncoding_SetOemEncoding_2()
-        {
-            IrbisEncoding.SetOemEncoding(Encoding.Unicode);
-        }
+    [TestMethod]
+    [ExpectedException (typeof (ArgumentOutOfRangeException))]
+    public void IrbisEncoding_SetOemEncoding_2()
+    {
+        IrbisEncoding.SetOemEncoding (Encoding.Unicode);
+    }
+
+    private static void _TestConversion (string text)
+    {
+        var utfEncoding = Encoding.UTF8;
+        var ansiEncoding = Encoding.GetEncoding (1251);
+
+        var utfOriginal = utfEncoding.GetBytes (text);
+        var ansiOriginal = ansiEncoding.GetBytes (text);
+
+        var utfConverted = IrbisEncoding.AnsiToUtf (ansiOriginal);
+        var ansiConverted = IrbisEncoding.UtfToAnsi (utfOriginal);
+
+        CollectionAssert.AreEqual (utfOriginal, utfConverted);
+        CollectionAssert.AreEqual (ansiOriginal, ansiConverted);
+    }
+
+
+    [TestMethod]
+    public void IrbisEncoding_AnsiToUtf_1()
+    {
+        Encoding.RegisterProvider (CodePagesEncodingProvider.Instance);
+
+        _TestConversion (string.Empty);
+        _TestConversion ("Hello");
+        _TestConversion ("Привет");
     }
 }
