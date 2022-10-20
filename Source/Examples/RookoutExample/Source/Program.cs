48832366c1872cc151e87b132a8a7e6fc3641e5d
Alexey Mironov <amironov73@gmail.com>
2022-10-20 08:19:33 +0800

    Add Rookout with example

---
+++ Source/Examples/RookoutExample/Source/Program.cs
@@ -0,0 +1,22 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable LocalizableElement
+
+/* Program.cs -- пример приложения, предназначенного для отладки Rookout
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using AM.Diagnostics;
+
+#endregion
+
+RookoutUtility.SetupRookout();
+
+var x = 1;
+var y = 2;
+var z = x + y;
+
+Console.WriteLine ($"{x} + {y} = {z}");
---
+++ Source/Libs/AM.Core5/Source/Diagnostics/RookoutUtility.cs
@@ -0,0 +1,59 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedMember.Global
+
+/* RookoutUtility.cs -- полезные методы для Rookout
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System.Collections.Generic;
+
+using Rook;
+
+#endregion
+
+#nullable enable
+
+namespace AM.Diagnostics;
+
+/// <summary>
+/// Полезные методы для Rookout.
+/// </summary>
+public static class RookoutUtility
+{
+    #region Properties
+
+    /// <summary>
+    /// Опции.
+    /// </summary>
+    public static RookOptions? Options;
+
+    #endregion
+
+    #region Public methods
+
+    /// <summary>
+    /// Настройка Rookout.
+    /// </summary>
+    public static void SetupRookout()
+    {
+        if (Options is null)
+        {
+            Options = new RookOptions
+            {
+                token = "1925fce85905b3e5c39fdf4d0092068ebd7bcc3b322db0c7344cc6c2f8dc1c28",
+                labels = new Dictionary<string, string> { { "env", "dev" } }
+            };
+            API.Start (Options);
+        }
+    }
+
+    #endregion
+}
