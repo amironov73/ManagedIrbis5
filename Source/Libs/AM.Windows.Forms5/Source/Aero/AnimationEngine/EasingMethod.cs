5c3507e46e2fb2847956b549e0c0be36446d9305
Alexey Mironov <amironov73@gmail.com>
2022-10-29 14:29:26 +0800

    Code cleanup

--- Source/Libs/AM.Windows.Forms5/Source/Aero/AnimationEngine/EasingMethod.cs
+++ Source/Libs/AM.Windows.Forms5/Source/Aero/AnimationEngine/EasingMethod.cs
@@ -1,18 +1,38 @@
-﻿using System;
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedType.Global
+
+/*
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace AeroSuite.AnimationEngine
-{
-    /// <summary>
-    /// A method that provides values for smooth transitions.
-    /// </summary>
-    /// <param name="progress">
-    ///     <para>The time progress of the animation.</para>
-    ///     <para><c>0.0</c> is the start of the animation, <c>1.0</c> is the end.</para>
-    ///     <para>Every value lower than <c>0.0</c> will return the same value as <c>0.0</c>, every value higher than <c>1.0</c> will return the same value as <c>1.0</c>.</para>
-    /// </param>
-    /// <returns>The value progress of the animation.</returns>
-    public delegate double EasingMethod(double progress);
-}
+#endregion
+
+#nullable enable
+
+namespace AeroSuite.AnimationEngine;
+
+/// <summary>
+/// A method that provides values for smooth transitions.
+/// </summary>
+/// <param name="progress">
+///     <para>The time progress of the animation.</para>
+///     <para><c>0.0</c> is the start of the animation, <c>1.0</c> is the end.</para>
+///     <para>Every value lower than <c>0.0</c> will return the same
+///     value as <c>0.0</c>, every value higher than <c>1.0</c> will return the
+///     same value as <c>1.0</c>.</para>
+/// </param>
+/// <returns>The value progress of the animation.</returns>
+public delegate double EasingMethod(double progress);
--- Source/Libs/AM.Windows.Forms5/Source/Aero/AnimationEngine/EasingMethods.Extended.cs
+++ Source/Libs/AM.Windows.Forms5/Source/Aero/AnimationEngine/EasingMethods.Extended.cs
@@ -1,22 +1,40 @@
-﻿using System;
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedType.Global
+
+/*
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace AeroSuite.AnimationEngine
-{
-    public static partial class EasingMethods
-    {
-        /// <summary>
-        /// Some more easing methods that are best for special effects.
-        /// </summary>
-        /// <remarks>
-        ///     <para>These methods are based on "Robert Penner's Easing Functions", too.</para>
-        ///     <para>They can be found at http://robertpenner.com/easing/ and they are distributed as open source under the BSD License.</para>
-        /// </remarks>
-        public static partial class Extended
-        {
+#endregion
 
-        }
+#nullable enable
+
+namespace AeroSuite.AnimationEngine;
+
+public static partial class EasingMethods
+{
+    /// <summary>
+    /// Some more easing methods that are best for special effects.
+    /// </summary>
+    /// <remarks>
+    ///     <para>These methods are based on "Robert Penner's Easing Functions", too.</para>
+    ///     <para>They can be found at http://robertpenner.com/easing/ and they are distributed as open source under the BSD License.</para>
+    /// </remarks>
+    public static partial class Extended
+    {
+
     }
 }
