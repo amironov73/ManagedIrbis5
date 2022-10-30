a6967002b0dbc20e3c5fac4a829d64838b85f2ae
Alexey Mironov <amironov73@gmail.com>
2022-10-30 12:20:02 +0800

    Code cleanup

--- Source/Libs/AM.Windows.Forms5/Source/Aero/Controls/AeroLinkLabel.cs
+++ Source/Libs/AM.Windows.Forms5/Source/Aero/Controls/AeroLinkLabel.cs
@@ -1,201 +1,244 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedType.Global
+// ReSharper disable VirtualMemberCallInConstructor
+
+/* AeroLinkLabel.cs --
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
 using System.ComponentModel;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using System.Windows.Forms.VisualStyles;
 
-namespace AeroSuite.Controls
+#endregion
+
+#nullable enable
+
+namespace AeroSuite.Controls;
+
+/// <summary>
+/// A LinkLabel whose colors fit the Windows style. It also has a fixed hand cursor.
+/// </summary>
+/// <remarks>
+/// The colors are extracted with Visual Styles (TextStyle > HyperLinkText). If running on XP or another OS, the colors are taken from the <see cref="SystemColors"/> class.
+/// The cursor is the IDC_HAND cursor from the IDC_STANDARD_CURSORS enum.
+/// </remarks>
+[DesignerCategory ("Code")]
+[DisplayName ("Aero LinkLabel")]
+[Description ("A LinkLabel whose colors fit the Windows style. It also has a fixed hand cursor.")]
+[ToolboxItem (true)]
+[ToolboxBitmap (typeof (LinkLabel))]
+public class AeroLinkLabel
+    : LinkLabel
 {
+    private const int WM_SETCURSOR = 0x20;
+    private const int IDC_HAND = 32649;
+
     /// <summary>
-    /// A LinkLabel whose colors fit the Windows style. It also has a fixed hand cursor.
+    ///
     /// </summary>
-    /// <remarks>
-    /// The colors are extracted with Visual Styles (TextStyle > HyperLinkText). If running on XP or another OS, the colors are taken from the <see cref="SystemColors"/> class.
-    /// The cursor is the IDC_HAND cursor from the IDC_STANDARD_CURSORS enum.
-    /// </remarks>
-    [DesignerCategory("Code")]
-    [DisplayName("Aero LinkLabel")]
-    [Description("A LinkLabel whose colors fit the Windows style. It also has a fixed hand cursor.")]
-    [ToolboxItem(true)]
-    [ToolboxBitmap(typeof(LinkLabel))]
-    public class AeroLinkLabel
-        : LinkLabel
+    protected Color DefaultColor;
+
+    /// <summary>
+    ///
+    /// </summary>
+    protected Color ActiveColor;
+
+    /// <summary>
+    ///
+    /// </summary>
+    protected Color PressedColor;
+
+    /// <summary>
+    ///
+    /// </summary>
+    protected Color VisitedColor;
+
+    /// <summary>
+    ///
+    /// </summary>
+    protected Color DisabledColor;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AeroLinkLabel"/> class.
+    /// </summary>
+    public AeroLinkLabel()
     {
-        private const int WM_SETCURSOR = 0x20;
-        private const int IDC_HAND = 32649;
+        AutoSize = true;
 
-        protected Color DefaultColor;
-        protected Color ActiveColor;
-        protected Color PressedColor;
-        protected Color VisitedColor;
-        protected Color DisabledColor;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="AeroLinkLabel"/> class.
-        /// </summary>
-        public AeroLinkLabel()
-            : base()
+        if (PlatformHelper.VistaOrHigher && PlatformHelper.VisualStylesEnabled)
         {
-            this.AutoSize = true;
+            LinkBehavior = LinkBehavior.HoverUnderline;
 
-            if (PlatformHelper.VistaOrHigher && PlatformHelper.VisualStylesEnabled)
-            {
-                this.LinkBehavior = LinkBehavior.HoverUnderline;
-
-                //Extract colors from visual styles
-                this.DefaultColor = new VisualStyleRenderer("TextStyle", 6, 1).GetColor(ColorProperty.TextColor);
-                this.ActiveColor = new VisualStyleRenderer("TextStyle", 6, 2).GetColor(ColorProperty.TextColor);
-                this.PressedColor = new VisualStyleRenderer("TextStyle", 6, 3).GetColor(ColorProperty.TextColor);
-                this.VisitedColor = new VisualStyleRenderer("TextStyle", 6, 0).GetColor(ColorProperty.TextColor);
-                this.DisabledColor = new VisualStyleRenderer("TextStyle", 6, 4).GetColor(ColorProperty.TextColor);
-            }
-            else
-            {
-                //Extract colors from system colors
-                this.DefaultColor = SystemColors.HotTrack;
-                this.ActiveColor = SystemColors.Highlight;
-                this.PressedColor = SystemColors.Highlight;
-                this.VisitedColor = this.VisitedLinkColor; //Don't know how to get this property so I use the default one.
-                this.DisabledColor = SystemColors.GrayText;
-            }
-
-            //Apply the extracted colors
-            this.LinkColor = this.DefaultColor;
-            this.ActiveLinkColor = this.ActiveColor;
-            this.VisitedLinkColor = this.VisitedColor;
-            this.DisabledLinkColor = this.DisabledColor;
+            //Extract colors from visual styles
+            DefaultColor = new VisualStyleRenderer ("TextStyle", 6, 1).GetColor (ColorProperty.TextColor);
+            ActiveColor = new VisualStyleRenderer ("TextStyle", 6, 2).GetColor (ColorProperty.TextColor);
+            PressedColor = new VisualStyleRenderer ("TextStyle", 6, 3).GetColor (ColorProperty.TextColor);
+            VisitedColor = new VisualStyleRenderer ("TextStyle", 6, 0).GetColor (ColorProperty.TextColor);
+            DisabledColor = new VisualStyleRenderer ("TextStyle", 6, 4).GetColor (ColorProperty.TextColor);
+        }
+        else
+        {
+            //Extract colors from system colors
+            DefaultColor = SystemColors.HotTrack;
+            ActiveColor = SystemColors.Highlight;
+            PressedColor = SystemColors.Highlight;
+            VisitedColor = VisitedLinkColor; //Don't know how to get this property so I use the default one.
+            DisabledColor = SystemColors.GrayText;
         }
 
+        //Apply the extracted colors
+        LinkColor = DefaultColor;
+        ActiveLinkColor = ActiveColor;
+        VisitedLinkColor = VisitedColor;
+        DisabledLinkColor = DisabledColor;
+    }
 
 
-        /// <summary>
-        /// Gets or sets the color used to display links in normal cases.
-        /// </summary>
-        /// <value>
-        /// The color used to display links in normal cases.
-        /// </value>
-        /// <remarks>
-        /// This property had to be overriden to prevent the designer creating code for it and so disabling the automatic style adaption.
-        /// </remarks>
-        [Category("Appearance")]
-        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
-        public virtual new Color LinkColor
+    /// <summary>
+    /// Gets or sets the color used to display links in normal cases.
+    /// </summary>
+    /// <value>
+    /// The color used to display links in normal cases.
+    /// </value>
+    /// <remarks>
+    /// This property had to be overriden to prevent the designer creating code for it and so disabling the automatic style adaption.
+    /// </remarks>
+    [Category ("Appearance")]
+    [DesignerSerializationVisibility (DesignerSerializationVisibility.Hidden)]
+    public new virtual Color LinkColor
+    {
+        get => base.LinkColor;
+        set => base.LinkColor = value;
+    }
+
+    /// <summary>
+    /// Gets or sets the color used to display active links.
+    /// </summary>
+    /// <value>
+    /// The color used to display active links.
+    /// </value>
+    /// <remarks>
+    /// This property had to be overriden to prevent the designer creating code for it and so disabling the automatic style adaption.
+    /// </remarks>
+    [Category ("Appearance")]
+    [DesignerSerializationVisibility (DesignerSerializationVisibility.Hidden)]
+    public new virtual Color ActiveLinkColor
+    {
+        get => base.ActiveLinkColor;
+        set => base.ActiveLinkColor = value;
+    }
+
+    /// <summary>
+    /// Gets or sets the color used to display disabled links.
+    /// </summary>
+    /// <value>
+    /// The color used to display disabled links.
+    /// </value>
+    /// <remarks>
+    /// This property had to be overriden to prevent the designer creating code for it and so disabling the automatic style adaption.
+    /// </remarks>
+    [Category ("Appearance")]
+    [DesignerSerializationVisibility (DesignerSerializationVisibility.Hidden)]
+    public new virtual Color DisabledLinkColor
+    {
+        get => base.DisabledLinkColor;
+        set => base.DisabledLinkColor = value;
+    }
+
+    /// <summary>
+    /// Gets or sets the color used to display the link once it has been visited.
+    /// </summary>
+    /// <value>
+    /// The color used to display the link once it has been visited..
+    /// </value>
+    /// <remarks>
+    /// This property had to be overriden to prevent the designer creating code for it and so disabling the automatic style adaption.
+    /// </remarks>
+    [Category ("Appearance")]
+    [DesignerSerializationVisibility (DesignerSerializationVisibility.Hidden)]
+    public new virtual Color VisitedLinkColor
+    {
+        get => base.VisitedLinkColor;
+        set => base.VisitedLinkColor = value;
+    }
+
+    /// <summary>
+    /// Gets or sets a value that represents how the link will be underlined.
+    /// </summary>
+    /// <value>
+    /// A value that represents how the link will be underlined.
+    /// </value>
+    /// <remarks>
+    /// This property had to be overriden to prevent the designer creating code for it and so disabling the automatic style adaption.
+    /// </remarks>
+    [Category ("Appearance")]
+    [DesignerSerializationVisibility (DesignerSerializationVisibility.Hidden)]
+    public new virtual LinkBehavior LinkBehavior
+    {
+        get => base.LinkBehavior;
+        set => base.LinkBehavior = value;
+    }
+
+
+    /// <summary>
+    /// Raises the <see cref="E:MouseDown" /> event.
+    /// </summary>
+    /// <param name="eventArgs">The <see cref="MouseEventArgs"/> instance containing the event data.</param>
+    protected override void OnMouseDown
+        (
+            MouseEventArgs eventArgs
+        )
+    {
+        ActiveLinkColor = PressedColor;
+
+        base.OnMouseDown (eventArgs);
+    }
+
+    /// <summary>
+    /// Raises the <see cref="E:MouseUp" /> event.
+    /// </summary>
+    /// <param name="eventArgs">The <see cref="MouseEventArgs"/> instance containing the event data.</param>
+    protected override void OnMouseUp
+        (
+            MouseEventArgs eventArgs
+        )
+    {
+        ActiveLinkColor = ActiveColor;
+
+        base.OnMouseUp (eventArgs);
+    }
+
+    /// <summary>
+    /// Processes the specified Windows Message.
+    /// </summary>
+    /// <param name="m">The Message to process.</param>
+    protected override void WndProc
+        (
+            ref Message m
+        )
+    {
+        //Set the cursor to the Hand Cursor specified by the current Windows Theme
+        if (m.Msg == WM_SETCURSOR && PlatformHelper.XpOrHigher)
         {
-            get { return base.LinkColor; }
-            set { base.LinkColor = value; }
+            NativeMethods.SetCursor (NativeMethods.LoadCursor (IntPtr.Zero, IDC_HAND));
+            m.Result = IntPtr.Zero;
+            return;
         }
 
-        /// <summary>
-        /// Gets or sets the color used to display active links.
-        /// </summary>
-        /// <value>
-        /// The color used to display active links.
-        /// </value>
-        /// <remarks>
-        /// This property had to be overriden to prevent the designer creating code for it and so disabling the automatic style adaption.
-        /// </remarks>
-        [Category("Appearance")]
-        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
-        public virtual new Color ActiveLinkColor
-        {
-            get { return base.ActiveLinkColor; }
-            set { base.ActiveLinkColor = value; }
-        }
-
-        /// <summary>
-        /// Gets or sets the color used to display disabled links.
-        /// </summary>
-        /// <value>
-        /// The color used to display disabled links.
-        /// </value>
-        /// <remarks>
-        /// This property had to be overriden to prevent the designer creating code for it and so disabling the automatic style adaption.
-        /// </remarks>
-        [Category("Appearance")]
-        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
-        public virtual new Color DisabledLinkColor
-        {
-            get { return base.DisabledLinkColor; }
-            set { base.DisabledLinkColor = value; }
-        }
-
-        /// <summary>
-        /// Gets or sets the color used to display the link once it has been visited.
-        /// </summary>
-        /// <value>
-        /// The color used to display the link once it has been visited..
-        /// </value>
-        /// <remarks>
-        /// This property had to be overriden to prevent the designer creating code for it and so disabling the automatic style adaption.
-        /// </remarks>
-        [Category("Appearance")]
-        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
-        public virtual new Color VisitedLinkColor
-        {
-            get { return base.VisitedLinkColor; }
-            set { base.VisitedLinkColor = value; }
-        }
-
-        /// <summary>
-        /// Gets or sets a value that represents how the link will be underlined.
-        /// </summary>
-        /// <value>
-        /// A value that represents how the link will be underlined.
-        /// </value>
-        /// <remarks>
-        /// This property had to be overriden to prevent the designer creating code for it and so disabling the automatic style adaption.
-        /// </remarks>
-        [Category("Appearance")]
-        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
-        public virtual new LinkBehavior LinkBehavior
-        {
-            get { return base.LinkBehavior; }
-            set { base.LinkBehavior = value; }
-        }
-
-
-
-        /// <summary>
-        /// Raises the <see cref="E:MouseDown" /> event.
-        /// </summary>
-        /// <param name="e">The <see cref="MouseEventArgs"/> instance containing the event data.</param>
-        protected override void OnMouseDown(MouseEventArgs e)
-        {
-            this.ActiveLinkColor = this.PressedColor;
-
-            base.OnMouseDown(e);
-        }
-
-        /// <summary>
-        /// Raises the <see cref="E:MouseUp" /> event.
-        /// </summary>
-        /// <param name="e">The <see cref="MouseEventArgs"/> instance containing the event data.</param>
-        protected override void OnMouseUp(MouseEventArgs e)
-        {
-            this.ActiveLinkColor = this.ActiveColor;
-
-            base.OnMouseUp(e);
-        }
-
-        /// <summary>
-        /// Processes the specified Windows Message.
-        /// </summary>
-        /// <param name="m">The Message to process.</param>
-        protected override void WndProc(ref Message m)
-        {
-            //Set the cursor to the Hand Cursor specified by the current Windows Theme
-            if (m.Msg == WM_SETCURSOR && PlatformHelper.XpOrHigher)
-            {
-                NativeMethods.SetCursor(NativeMethods.LoadCursor(IntPtr.Zero, IDC_HAND));
-                m.Result = IntPtr.Zero;
-                return;
-            }
-
-            //Pass the WndProc call to the base
-            base.WndProc(ref m);
-        }
+        //Pass the WndProc call to the base
+        base.WndProc (ref m);
     }
 }
--- Source/Libs/AM.Windows.Forms5/Source/Aero/Controls/AeroListView.cs
+++ Source/Libs/AM.Windows.Forms5/Source/Aero/Controls/AeroListView.cs
@@ -1,55 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedType.Global
+
+/* AeroListView.cs --
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
 using System.ComponentModel;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
-namespace AeroSuite.Controls
+#endregion
+
+#nullable enable
+
+namespace AeroSuite.Controls;
+
+/// <summary>
+/// An aero-styled ListView.
+/// </summary>
+/// <remarks>
+/// A ListView with the "Explorer"-WindowTheme applied.
+/// If the operating system is Windows XP or older, nothing will be changed.
+/// </remarks>
+[DesignerCategory ("Code")]
+[DisplayName ("Aero ListView")]
+[Description ("An aero-styled ListView.")]
+[ToolboxItem (true)]
+[ToolboxBitmap (typeof (ListView))]
+public class AeroListView
+    : ListView
 {
+    private const int LVS_EX_DOUBLEBUFFER = 0x10000;
+    private const int LVM_SETEXTENDEDLISTVIEWSTYLE = 4150;
+
     /// <summary>
-    /// An aero-styled ListView.
+    /// Initializes a new instance of the <see cref="AeroListView"/> class.
     /// </summary>
-    /// <remarks>
-    /// A ListView with the "Explorer"-WindowTheme applied.
-    /// If the operating system is Windows XP or older, nothing will be changed.
-    /// </remarks>
-    [DesignerCategory("Code")]
-    [DisplayName("Aero ListView")]
-    [Description("An aero-styled ListView.")]
-    [ToolboxItem(true)]
-    [ToolboxBitmap(typeof(ListView))]
-    public class AeroListView
-        : ListView
+    public AeroListView()
+        : base()
     {
-        private const int LVS_EX_DOUBLEBUFFER = 0x10000;
-        private const int LVM_SETEXTENDEDLISTVIEWSTYLE = 4150;
+        this.FullRowSelect = true;
+    }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="AeroListView"/> class.
-        /// </summary>
-        public AeroListView()
-            : base()
+
+    /// <summary>
+    /// Raises the <see cref="E:HandleCreated" /> event.
+    /// </summary>
+    /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+    protected override void OnHandleCreated (EventArgs e)
+    {
+        base.OnHandleCreated (e);
+
+        if (PlatformHelper.VistaOrHigher)
         {
-            this.FullRowSelect = true;
-        }
-
-
-
-        /// <summary>
-        /// Raises the <see cref="E:HandleCreated" /> event.
-        /// </summary>
-        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
-        protected override void OnHandleCreated(EventArgs e)
-        {
-            base.OnHandleCreated(e);
-
-            if (PlatformHelper.VistaOrHigher)
-            {
-                NativeMethods.SetWindowTheme(this.Handle, "explorer", null);
-                NativeMethods.SendMessage(this.Handle, LVM_SETEXTENDEDLISTVIEWSTYLE, new IntPtr(LVS_EX_DOUBLEBUFFER), new IntPtr(LVS_EX_DOUBLEBUFFER));
-            }
+            NativeMethods.SetWindowTheme (this.Handle, "explorer", null);
+            NativeMethods.SendMessage (this.Handle, LVM_SETEXTENDEDLISTVIEWSTYLE, new IntPtr (LVS_EX_DOUBLEBUFFER),
+                new IntPtr (LVS_EX_DOUBLEBUFFER));
         }
     }
 }
