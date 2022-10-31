1984c00d76bb46b182cd106f939f0d5a498db96f
Alexey Mironov <amironov73@gmail.com>
2022-10-31 08:22:28 +0800

    Code cleanup

--- Source/Libs/AM.Windows.Forms5/Source/Aero/Controls/ShieldButton.cs
+++ Source/Libs/AM.Windows.Forms5/Source/Aero/Controls/ShieldButton.cs
@@ -1,113 +1,126 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedType.Global
+
+/* ShieldButton.cs --
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
 using System.ComponentModel;
 using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
-using System.Windows.Forms.Design;
 
-namespace AeroSuite.Controls
+#endregion
+
+#nullable enable
+
+namespace AeroSuite.Controls;
+
+/// <summary>
+/// A normal button with a Shield.
+/// </summary>
+/// <remarks>
+/// The shield is extracted from the system with LoadImage if possible. Otherwise the shield will be enabled by sending the BCM_SETSHIELD Message to the control.
+/// If the operating system is not Windows Vista or higher, no shield will be displayed as there's no such thing as UAC on the target system -> the shield is obsolete.
+/// </remarks>
+[DesignerCategory ("Code")]
+[DisplayName ("Shield Button")]
+[Description ("A normal button with a Shield.")]
+[ToolboxItem (true)]
+[ToolboxBitmap (typeof (ShieldButton))]
+public class ShieldButton
+    : Button
 {
+    private const int BCM_SETSHIELD = 0x160C;
+    private static bool? isSystemAbleToLoadShield = null;
+
     /// <summary>
-    /// A normal button with a Shield.
+    /// Initializes a new instance of the <see cref="ShieldButton"/> class.
     /// </summary>
-    /// <remarks>
-    /// The shield is extracted from the system with LoadImage if possible. Otherwise the shield will be enabled by sending the BCM_SETSHIELD Message to the control.
-    /// If the operating system is not Windows Vista or higher, no shield will be displayed as there's no such thing as UAC on the target system -> the shield is obsolete.
-    /// </remarks>
-    [DesignerCategory("Code")]
-    [DisplayName("Shield Button")]
-    [Description("A normal button with a Shield.")]
-    [ToolboxItem(true)]
-    [ToolboxBitmap(typeof(ShieldButton))]
-    public class ShieldButton
-        : Button
+    public ShieldButton()
     {
-        private const int BCM_SETSHIELD = 0x160C;
-        private static bool? isSystemAbleToLoadShield = null;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ShieldButton"/> class.
-        /// </summary>
-        public ShieldButton()
-            : base()
+        Size = new Size ((int)(Width * 1.5), Height + 1);
+        if (PlatformHelper.VistaOrHigher)
         {
-            this.Size = new Size((int)(this.Width * 1.5), this.Height + 1);
-            if (PlatformHelper.VistaOrHigher)
+            //Only try to load the icon if it did not fail before
+            if (!isSystemAbleToLoadShield.HasValue || isSystemAbleToLoadShield.Value)
             {
-                //Only try to load the icon if it did not fail before
-                if (!isSystemAbleToLoadShield.HasValue || isSystemAbleToLoadShield.Value) 
+                try
                 {
-                    try
+                    var icon = IconExtractor.LoadIcon (IconExtractor.IconType.Shield, SystemInformation.SmallIconSize);
+                    if (icon != null)
                     {
-                        var icon = IconExtractor.LoadIcon(IconExtractor.IconType.Shield, SystemInformation.SmallIconSize);
-                        if (icon != null)
-                        {
-                            this.Image = icon.ToBitmap();
-                            this.TextImageRelation = TextImageRelation.ImageBeforeText;
-                            this.ImageAlign = ContentAlignment.MiddleRight;
+                        Image = icon.ToBitmap();
+                        TextImageRelation = TextImageRelation.ImageBeforeText;
+                        ImageAlign = ContentAlignment.MiddleRight;
 
-                            isSystemAbleToLoadShield = true;
-                            return;
-                        }
-                        else
-                        {
-                            isSystemAbleToLoadShield = false;
-                        }
+                        isSystemAbleToLoadShield = true;
+                        return;
                     }
-                    catch (PlatformNotSupportedException)
+                    else
                     {
-                        //This happens when the system does not support this call
-                        //Prevent future calling
                         isSystemAbleToLoadShield = false;
                     }
                 }
-                
-                //Preferred way not possible
-                base.FlatStyle = FlatStyle.System; //Need to access the base property as the overriden one would cause some overhead
-                NativeMethods.SendMessage(this.Handle, BCM_SETSHIELD, IntPtr.Zero, new IntPtr(1));
-            }
-        }
-
-        /// <summary>
-        /// Gets or sets the flat style appearance of the shield button control.
-        /// </summary>
-        /// <value>
-        /// The flat style.
-        /// </value>
-        [Category("Appearance")]
-        [Description("The flat style appearance of the shield button control.")]
-        public virtual new FlatStyle FlatStyle
-        {
-            get
-            {
-                return base.FlatStyle;
-            }
-            set
-            {
-                if (value != base.FlatStyle)
+                catch (PlatformNotSupportedException)
                 {
-                    base.FlatStyle = value;
+                    //This happens when the system does not support this call
+                    //Prevent future calling
+                    isSystemAbleToLoadShield = false;
+                }
+            }
 
-                    //Update the shield icon if needed
-                    if (PlatformHelper.VistaOrHigher)
+            //Preferred way not possible
+            base.FlatStyle =
+                FlatStyle.System; //Need to access the base property as the overriden one would cause some overhead
+            NativeMethods.SendMessage (Handle, BCM_SETSHIELD, IntPtr.Zero, new IntPtr (1));
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets the flat style appearance of the shield button control.
+    /// </summary>
+    /// <value>
+    /// The flat style.
+    /// </value>
+    [Category ("Appearance")]
+    [Description ("The flat style appearance of the shield button control.")]
+    public new virtual FlatStyle FlatStyle
+    {
+        get => base.FlatStyle;
+        set
+        {
+            if (value != base.FlatStyle)
+            {
+                base.FlatStyle = value;
+
+                //Update the shield icon if needed
+                if (PlatformHelper.VistaOrHigher)
+                {
+                    if (base.FlatStyle == FlatStyle.System)
                     {
-                        if (base.FlatStyle == FlatStyle.System)
+                        //Apply Shield the WinAPI-Way
+                        NativeMethods.SendMessage (Handle, BCM_SETSHIELD, IntPtr.Zero, new IntPtr (1));
+                    }
+                    else
+                    {
+                        //Try applying it the other way
+                        if (isSystemAbleToLoadShield.Value)
                         {
-                            //Apply Shield the WinAPI-Way
-                            NativeMethods.SendMessage(this.Handle, BCM_SETSHIELD, IntPtr.Zero, new IntPtr(1));
-                        }
-                        else
-                        {
-                            //Try applying it the other way
-                            if (isSystemAbleToLoadShield.Value)
-                            {
-                                this.Image = IconExtractor.LoadIcon(IconExtractor.IconType.Shield, SystemInformation.SmallIconSize).ToBitmap();
-                                this.TextImageRelation = TextImageRelation.ImageBeforeText;
-                                this.ImageAlign = ContentAlignment.MiddleRight;
-                            }
+                            Image = IconExtractor
+                                .LoadIcon (IconExtractor.IconType.Shield, SystemInformation.SmallIconSize).ToBitmap();
+                            TextImageRelation = TextImageRelation.ImageBeforeText;
+                            ImageAlign = ContentAlignment.MiddleRight;
                         }
                     }
                 }
--- Source/Libs/AM.Windows.Forms5/Source/Aero/Controls/VerticalAeroProgressBar.cs
+++ Source/Libs/AM.Windows.Forms5/Source/Aero/Controls/VerticalAeroProgressBar.cs
@@ -1,52 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedType.Global
+
+/* VerticalAeroProgressBar.cs --
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
 using System.ComponentModel;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
-namespace AeroSuite.Controls
+#endregion
+
+#nullable enable
+
+namespace AeroSuite.Controls;
+
+/// <summary>
+/// A vertical progress bar control with extended features.
+/// </summary>
+/// <remarks>
+/// This progress bar is a vertically displayed version of the <see cref="AeroProgressBar"/>.
+/// </remarks>
+[DesignerCategory ("Code")]
+[DisplayName ("Vertical Aero ProgressBar")]
+[Description ("A vertical progress bar control with extended features.")]
+[ToolboxItem (true)]
+[ToolboxBitmap (typeof (VerticalAeroProgressBar))]
+public class VerticalAeroProgressBar
+    : AeroProgressBar
 {
+    private const int PBS_VERTICAL = 0x4;
+
     /// <summary>
-    /// A vertical progress bar control with extended features.
+    /// Initializes a new instance of the <see cref="VerticalAeroProgressBar"/> class.
     /// </summary>
-    /// <remarks>
-    /// This progress bar is a vertically displayed version of the <see cref="AeroProgressBar"/>.
-    /// </remarks>
-    [DesignerCategory("Code")]
-    [DisplayName("Vertical Aero ProgressBar")]
-    [Description("A vertical progress bar control with extended features.")]
-    [ToolboxItem(true)]
-    [ToolboxBitmap(typeof(VerticalAeroProgressBar))]
-    public class VerticalAeroProgressBar
-        : AeroProgressBar
+    public VerticalAeroProgressBar()
     {
-        private const int PBS_VERTICAL = 0x4;
+        Size = new Size (Height, Width);
+    }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="VerticalAeroProgressBar"/> class.
-        /// </summary>
-        public VerticalAeroProgressBar()
-            : base()
+    /// <inheritdoc cref="AeroProgressBar.CreateParams"/>
+    protected override CreateParams CreateParams
+    {
+        get
         {
-            this.Size = new Size(base.Height, base.Width);
-        }
-
-        /// <summary>
-        /// Overrides AeroProgressBar.CreateParams
-        /// </summary>
-        /// <value>
-        /// The create parameters.
-        /// </value>
-        protected override CreateParams CreateParams
-        {
-            get
-            {
-                var param = base.CreateParams;
-                param.Style |= PBS_VERTICAL;
-                return param;
-            }
+            var param = base.CreateParams;
+            param.Style |= PBS_VERTICAL;
+            return param;
         }
     }
 }
