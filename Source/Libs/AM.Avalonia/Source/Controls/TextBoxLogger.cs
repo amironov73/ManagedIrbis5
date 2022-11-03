c9292a8efa9021d7793cf2a92443569082185dda
Alexey Mironov <amironov73@gmail.com>
2022-11-03 16:35:00 +0800

    Add TextBoxLogger class

---
+++ Source/Libs/AM.Avalonia/Source/Controls/TextBoxLogger.cs
@@ -0,0 +1,84 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable CoVariantArrayConversion
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable LocalizableElement
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable StringLiteralTypo
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable UnusedMember.Global
+
+/* TextBoxLogger.cs -- логгер, пишущий в текстовый бокс
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
+
+using AM.Logging;
+
+using Avalonia.Controls;
+using Avalonia.Threading;
+
+#endregion
+
+#nullable enable
+
+namespace AM.Avalonia.Controls;
+
+/// <summary>
+/// Логгер, пишущий в текстовый бокс.
+/// </summary>
+public sealed class TextBoxLogger
+    : IDisposable
+{
+    #region Construction
+
+    /// <summary>
+    /// Конструктор.
+    /// </summary>
+    public TextBoxLogger
+        (
+            TextBox textBox
+        )
+    {
+        _textBox = textBox;
+        NlogInterceptor.RegisterHandler (_Handler);
+    }
+
+    #endregion
+
+    #region Private members
+
+    private readonly TextBox _textBox;
+
+    private void _Handler (object? sender, string text)
+    {
+        Dispatcher.UIThread.Post
+            (
+                message =>
+                {
+                    _textBox.Text += message + _textBox.NewLine;
+                    _textBox.CaretIndex = int.MaxValue;
+                },
+                text
+            );
+    }
+
+    #endregion
+
+    #region IDisposable members
+
+    /// <inheritdoc cref="IDisposable.Dispose"/>
+    public void Dispose()
+    {
+        NlogInterceptor.UnregisterHandler (_Handler);
+    }
+
+    #endregion
+}
--- Source/Templates/AvaloniaStandalone/Source/Program.cs
+++ Source/Templates/AvaloniaStandalone/Source/Program.cs
@@ -7,7 +7,9 @@
 // ReSharper disable IdentifierTypo
 // ReSharper disable InconsistentNaming
 // ReSharper disable LocalizableElement
+// ReSharper disable MemberCanBePrivate.Global
 // ReSharper disable StringLiteralTypo
+// ReSharper disable UnusedAutoPropertyAccessor.Global
 
 /* Program.cs -- точка входа в приложение
  * Ars Magna project, http://arsmagna.ru
@@ -19,6 +21,13 @@ using System;
 
 using Avalonia;
 
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using NLog.Extensions.Logging;
+
 #endregion
 
 #nullable enable
@@ -27,18 +36,52 @@ namespace AvaloniaApp;
 
 internal sealed class Program
 {
+    public static IConfiguration Configuration { get; private set; } = null!;
+    public static ILogger Logger { get; private set; } = null!;
+    public static IHost ApplicationHost { get; private set; } = null!;
+    
     [STAThread]
     public static void Main
         (
             string[] args
         )
     {
+        Initialize (args);
+        
         BuildAvaloniaApp()
             .StartWithClassicDesktopLifetime (args);
     }
 
+    private static void Initialize 
+       (
+            string[] args
+       )
+    {
+        var builder = Host.CreateDefaultBuilder (args);
+        Configuration = new ConfigurationBuilder()
+            .SetBasePath (AppContext.BaseDirectory)
+            .AddJsonFile ("appsettings.json", true, true)
+            .AddEnvironmentVariables()
+            .AddCommandLine (args)
+            .Build();
+        
+        builder.ConfigureLogging (logging =>
+        {
+            logging.ClearProviders();
+            logging.AddNLog (Configuration);
+        });
+        builder.ConfigureServices (services =>
+        {
+            services.AddOptions();
+            services.AddLocalization();
+        });
+        
+        ApplicationHost = builder.Build();
+        Logger = ApplicationHost.Services.GetRequiredService<ILogger<Program>>();
+    }
+
     // Avalonia configuration, don't remove; also used by visual designer.
-    public static AppBuilder BuildAvaloniaApp()
+    private static AppBuilder BuildAvaloniaApp()
     {
         return AppBuilder.Configure<App>()
             .UsePlatformDetect()
