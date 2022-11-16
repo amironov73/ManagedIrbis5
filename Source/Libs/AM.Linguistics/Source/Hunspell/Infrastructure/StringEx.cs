02f1a607b012d0c2494de6c1e148c190c6cb1254
Alexey Mironov <amironov73@gmail.com>
2022-11-16 08:30:09 +0800

    Code cleanup

--- Source/Libs/AM.Linguistics/Source/Hunspell/Infrastructure/StringEx.cs
+++ Source/Libs/AM.Linguistics/Source/Hunspell/Infrastructure/StringEx.cs
@@ -4,8 +4,6 @@
 // ReSharper disable CheckNamespace
 // ReSharper disable CommentTypo
 // ReSharper disable IdentifierTypo
-// ReSharper disable InconsistentNaming
-// ReSharper disable MemberCanBePrivate.Global
 
 /* StringEx.cs --
  * Ars Magna project, http://arsmagna.ru
@@ -26,213 +24,238 @@ using System.Runtime.CompilerServices;
 
 #nullable enable
 
-namespace AM.Linguistics.Hunspell.Infrastructure
+namespace AM.Linguistics.Hunspell.Infrastructure;
+
+internal static class StringEx
 {
-    internal static class StringEx
+    private static readonly char[] _spaceOrTab = { ' ', '\t' };
+
+#if !NO_INLINE
+    [MethodImpl (MethodImplOptions.AggressiveInlining)]
+#endif
+    public static string[] SplitOnTabOrSpace
+        (
+            this string that
+        )
     {
-        private static readonly char[] SpaceOrTab = { ' ', '\t' };
+#if DEBUG
+        if (that == null)
+        {
+            throw new ArgumentNullException (nameof (that));
+        }
+#endif
+        return that.Split (_spaceOrTab, StringSplitOptions.RemoveEmptyEntries);
+    }
 
 #if !NO_INLINE
-        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+    [MethodImpl (MethodImplOptions.AggressiveInlining)]
 #endif
-        public static bool StartsWith (this string @this, char character)
+    public static bool IsTabOrSpace (this char c) => c is ' ' or '\t';
+
+    public static string? GetReversed
+        (
+            this string? that
+        )
+    {
+        if (that == null || that.Length <= 1)
         {
-#if DEBUG
-            if (@this == null)
-            {
-                throw new ArgumentNullException (nameof (@this));
-            }
-#endif
-            return @this.Length != 0 && @this[0] == character;
+            return that;
         }
+
+        using var owner = MemoryPool<char>.Shared.Rent (that.Length);
+        var buffer = owner.Memory.Span.Slice (0, that.Length);
+        var lastIndex = that.Length - 1;
+        for (var i = 0; i < buffer.Length; i++)
+        {
+            buffer[i] = that[lastIndex - i];
+        }
+
+        return buffer.ToString();
+    }
+
+    public static string Replace
+        (
+            this string that,
+            int index,
+            int removeCount,
+            string replacement
+        )
+    {
+        var capacity = Math.Max (that.Length, that.Length + replacement.Length - removeCount);
+        var builder = AM.Text.StringBuilderPool.Shared.Get();
+        builder.EnsureCapacity (capacity);
+        builder.Replace (index, removeCount, replacement);
+        return builder.ReturnShared();
+    }
 
 #if !NO_INLINE
-        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+    [MethodImpl (MethodImplOptions.AggressiveInlining)]
 #endif
-        public static bool EndsWith (this string @this, char character)
-        {
+    public static char GetCharOrTerminator
+        (
+            this string that,
+            int index
+        )
+    {
 #if DEBUG
-            if (@this == null)
-            {
-                throw new ArgumentNullException (nameof (@this));
-            }
-#endif
-            return @this.Length != 0 && @this[@this.Length - 1] == character;
+        if (index < 0)
+        {
+            throw new ArgumentOutOfRangeException (nameof (index));
         }
+#endif
+        return index < that.Length ? that[index] : '\0';
+    }
+
+    public static string ConcatString
+        (
+            string str0,
+            int startIndex0,
+            int count0,
+            string str1
+        )
+    {
+        return str0.AsSpan (startIndex0, count0).ConcatString (str1);
+    }
+
+    public static string ConcatString
+        (
+            string str0,
+            string str1,
+            int startIndex1,
+            int count1
+        )
+    {
+        return str0.ConcatString (str1.AsSpan (startIndex1, count1));
+    }
+
+    public static string ConcatString
+        (
+            string str0,
+            int startIndex0,
+            int count0,
+            string str1,
+            char char2,
+            string str3,
+            int startIndex3
+        )
+    {
+        var count3 = str3.Length - startIndex3;
+        var capacity = count0 + str1.Length + 1 + count3;
+        var builder = AM.Text.StringBuilderPool.Shared.Get();
+        builder.EnsureCapacity (capacity);
+        builder.Append (str0, startIndex0, count0);
+        builder.Append (str1);
+        builder.Append (char2);
+        builder.Append (str3, startIndex3, count3);
+        return builder.ReturnShared();
+    }
+
+    public static string ConcatString
+        (
+            string str0,
+            int startIndex0,
+            int count0,
+            string str1,
+            string str2,
+            int startIndex2
+        )
+    {
+        var count2 = str2.Length - startIndex2;
+        var capacity = count0 + str1.Length + count2;
+        var builder = AM.Text.StringBuilderPool.Shared.Get();
+        builder.EnsureCapacity (capacity);
+        builder.Append (str0, startIndex0, count0);
+        builder.Append (str1);
+        builder.Append (str2, startIndex2, count2);
+        return builder.ReturnShared();
+    }
+
+    public static string ConcatString
+        (
+            string str0,
+            int startIndex0,
+            int count0,
+            char char1,
+            string str2,
+            int startIndex2
+        )
+    {
+        return ConcatString (str0, startIndex0, count0, char1.ToString(), str2, startIndex2);
+    }
+
+    public static string ConcatString
+        (
+            this string that,
+            ReadOnlySpan<char> value
+        )
+    {
+#if DEBUG
+        if (that == null)
+        {
+            throw new ArgumentNullException (nameof (that));
+        }
+#endif
+        if (that.Length == 0)
+        {
+            return value.ToString();
+        }
+
+        if (value.IsEmpty)
+        {
+            return that;
+        }
+
+        var capacity = that.Length + value.Length;
+        var builder = AM.Text.StringBuilderPool.Shared.Get();
+        builder.EnsureCapacity (capacity);
+        builder.Append (that);
+        builder.Append (value);
+        return builder.ReturnShared();
+    }
 
 #if !NO_INLINE
-        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+    [MethodImpl (MethodImplOptions.AggressiveInlining)]
 #endif
-        public static string[] SplitOnTabOrSpace (this string @this)
-        {
+    public static bool IsLineBreakChar (this char c) => c is '\n' or '\r';
+
+    public static string WithoutIndex
+        (
+            this string that,
+            int index
+        )
+    {
 #if DEBUG
-            if (@this == null)
-            {
-                throw new ArgumentNullException (nameof (@this));
-            }
-#endif
-            return @this.Split (SpaceOrTab, StringSplitOptions.RemoveEmptyEntries);
+        if (that == null)
+        {
+            throw new ArgumentNullException (nameof (that));
         }
 
-#if !NO_INLINE
-        [MethodImpl (MethodImplOptions.AggressiveInlining)]
-#endif
-        public static bool IsTabOrSpace (this char c) => c == ' ' || c == '\t';
-
-        public static string GetReversed (this string @this)
+        if (index < 0)
         {
-            if (@this == null || @this.Length <= 1)
-            {
-                return @this;
-            }
-
-            using (var mo = MemoryPool<char>.Shared.Rent (@this.Length))
-            {
-                var buffer = mo.Memory.Span.Slice (0, @this.Length);
-                var lastIndex = @this.Length - 1;
-                for (var i = 0; i < buffer.Length; i++) buffer[i] = @this[lastIndex - i];
-
-                return buffer.ToString();
-            }
+            throw new ArgumentOutOfRangeException (nameof (index));
         }
 
-        public static bool Contains (this string @this, char value)
+        if (index >= that.Length)
         {
-            return @this.IndexOf (value) >= 0;
+            throw new ArgumentOutOfRangeException (nameof (index));
         }
-
-        public static string Replace (this string @this, int index, int removeCount, string replacement)
-        {
-            var capacity = Math.Max (@this.Length, @this.Length + replacement.Length - removeCount);
-            var builder = AM.Text.StringBuilderPool.Shared.Get();
-            builder.EnsureCapacity (capacity);
-            builder.Replace (index, removeCount, replacement);
-            return builder.ReturnShared();
-        }
-
-#if !NO_INLINE
-        [MethodImpl (MethodImplOptions.AggressiveInlining)]
-#endif
-        public static char GetCharOrTerminator (this string @this, int index)
-        {
-#if DEBUG
-            if (index < 0)
-            {
-                throw new ArgumentOutOfRangeException (nameof (index));
-            }
-#endif
-            return index < @this.Length ? @this[index] : '\0';
-        }
-
-        public static string ConcatString (string str0, int startIndex0, int count0, string str1)
-        {
-            return str0.AsSpan (startIndex0, count0).ConcatString (str1);
-        }
-
-        public static string ConcatString (string str0, string str1, int startIndex1, int count1)
-        {
-            return str0.ConcatString (str1.AsSpan (startIndex1, count1));
-        }
-
-        public static string ConcatString (string str0, int startIndex0, int count0, string str1, char char2,
-            string str3, int startIndex3)
-        {
-            var count3 = str3.Length - startIndex3;
-            var capacity = count0 + str1.Length + 1 + count3;
-            var builder = AM.Text.StringBuilderPool.Shared.Get();
-            builder.EnsureCapacity (capacity);
-            builder.Append (str0, startIndex0, count0);
-            builder.Append (str1);
-            builder.Append (char2);
-            builder.Append (str3, startIndex3, count3);
-            return builder.ReturnShared();
-        }
-
-        public static string ConcatString (string str0, int startIndex0, int count0, string str1, string str2,
-            int startIndex2)
-        {
-            var count2 = str2.Length - startIndex2;
-            var capacity = count0 + str1.Length + count2;
-            var builder = AM.Text.StringBuilderPool.Shared.Get();
-            builder.EnsureCapacity (capacity);
-            builder.Append (str0, startIndex0, count0);
-            builder.Append (str1);
-            builder.Append (str2, startIndex2, count2);
-            return builder.ReturnShared();
-        }
-
-        public static string ConcatString (string str0, int startIndex0, int count0, char char1, string str2,
-            int startIndex2)
-        {
-            return ConcatString (str0, startIndex0, count0, char1.ToString(), str2, startIndex2);
-        }
-
-        public static string ConcatString (this string @this, ReadOnlySpan<char> value)
-        {
-#if DEBUG
-            if (@this == null)
-            {
-                throw new ArgumentNullException (nameof (@this));
-            }
-#endif
-            if (@this.Length == 0)
-            {
-                return value.ToString();
-            }
-
-            if (value.IsEmpty)
-            {
-                return @this;
-            }
-
-            var capacity = @this.Length + value.Length;
-            var builder = AM.Text.StringBuilderPool.Shared.Get();
-            builder.EnsureCapacity (capacity);
-            builder.Append (@this);
-            builder.Append (value);
-            return builder.ReturnShared();
-        }
-
-#if !NO_INLINE
-        [MethodImpl (MethodImplOptions.AggressiveInlining)]
-#endif
-        public static bool IsLineBreakChar (this char c) => c == '\n' || c == '\r';
-
-        public static string WithoutIndex (this string @this, int index)
-        {
-#if DEBUG
-            if (@this == null)
-            {
-                throw new ArgumentNullException (nameof (@this));
-            }
-
-            if (index < 0)
-            {
-                throw new ArgumentOutOfRangeException (nameof (index));
-            }
-
-            if (index >= @this.Length)
-            {
-                throw new ArgumentOutOfRangeException (nameof (index));
-            }
 #endif
 
-            if (index == 0)
-            {
-                return @this.Substring (1);
-            }
-
-            var lastIndex = @this.Length - 1;
-            if (index == lastIndex)
-            {
-                return @this.Substring (0, lastIndex);
-            }
-
-            var builder = AM.Text.StringBuilderPool.Shared.Get();
-            builder.EnsureCapacity (lastIndex);
-            builder.Append (@this, 0, index);
-            builder.Append (@this, index + 1, lastIndex - index);
-            return builder.ReturnShared();
+        if (index == 0)
+        {
+            return that.Substring (1);
         }
+
+        var lastIndex = that.Length - 1;
+        if (index == lastIndex)
+        {
+            return that.Substring (0, lastIndex);
+        }
+
+        var builder = AM.Text.StringBuilderPool.Shared.Get();
+        builder.EnsureCapacity (lastIndex);
+        builder.Append (that, 0, index);
+        builder.Append (that, index + 1, lastIndex - index);
+        return builder.ReturnShared();
     }
 }
