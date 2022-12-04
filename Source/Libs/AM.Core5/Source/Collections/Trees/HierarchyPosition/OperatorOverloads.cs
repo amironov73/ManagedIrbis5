572d6f838723db499990d3c7e0c49a5160a0549f
Alexey Mironov <amironov73@gmail.com>
2022-12-04 11:45:58 +0800

    Code cleanup

--- Source/Libs/AM.Core5/Source/Collections/Trees/HierarchyPosition/OperatorOverloads.cs
+++ Source/Libs/AM.Core5/Source/Collections/Trees/HierarchyPosition/OperatorOverloads.cs
@@ -1,54 +1,105 @@
-﻿namespace TreeCollections
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable NonReadonlyMemberInGetHashCode
+// ReSharper disable UnusedMember.Global
+
+/* OperatorOverloads.cs --
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#nullable enable
+
+namespace TreeCollections;
+
+public partial class HierarchyPosition
 {
-    public partial class HierarchyPosition
+    /// <summary>
+    ///
+    /// </summary>
+    public static bool operator ==
+        (
+            HierarchyPosition? hp1,
+            HierarchyPosition? hp2
+        )
     {
-        public static bool operator == (HierarchyPosition hp1, HierarchyPosition hp2)
+        if (ReferenceEquals (hp1, hp2))
         {
-            if (ReferenceEquals (hp1, hp2))
-            {
-                return true;
-            }
-
-            if (((object)hp1 == null) || ((object)hp2 == null))
-            {
-                return false;
-            }
-
-            return hp1.Equals (hp2);
-        }
-
-        public static bool operator != (HierarchyPosition hp1, HierarchyPosition hp2)
-        {
-            return !(hp1 == hp2);
-        }
-
-        public static bool operator < (HierarchyPosition hp1, HierarchyPosition hp2)
-        {
-            if (!CanCompare (hp1, hp2)) return false;
-
-            return hp1.CompareTo (hp2) < 0;
-        }
-
-        public static bool operator > (HierarchyPosition hp1, HierarchyPosition hp2)
-        {
-            if (!CanCompare (hp1, hp2)) return false;
-
-            return hp1.CompareTo (hp2) > 0;
-        }
-
-        private static bool CanCompare (HierarchyPosition hp1, HierarchyPosition hp2)
-        {
-            if (ReferenceEquals (hp1, hp2))
-            {
-                return false;
-            }
-
-            if (((object)hp1 == null) || ((object)hp2 == null))
-            {
-                return false;
-            }
-
             return true;
         }
+
+        if (hp1 is null || hp2 is null)
+        {
+            return false;
+        }
+
+        return hp1.Equals (hp2);
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    public static bool operator !=
+        (
+            HierarchyPosition? hp1,
+            HierarchyPosition? hp2
+        )
+    {
+        return !(hp1 == hp2);
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    public static bool operator <
+        (
+            HierarchyPosition hp1,
+            HierarchyPosition hp2
+        )
+    {
+        if (!CanCompare (hp1, hp2))
+        {
+            return false;
+        }
+
+        return hp1.CompareTo (hp2) < 0;
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    public static bool operator >
+        (
+            HierarchyPosition hp1,
+            HierarchyPosition hp2
+        )
+    {
+        if (!CanCompare (hp1, hp2))
+        {
+            return false;
+        }
+
+        return hp1.CompareTo (hp2) > 0;
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    private static bool CanCompare
+        (
+            HierarchyPosition? hp1,
+            HierarchyPosition? hp2
+        )
+    {
+        if (ReferenceEquals (hp1, hp2))
+        {
+            return false;
+        }
+
+        return hp1 is not null && hp2 is not null;
     }
 }
--- Source/Libs/AM.Core5/Source/Collections/Trees/ItemTree/EntityTree/MutableEntityTree/OrderComparers/NodeOrderItemComparer.cs
+++ Source/Libs/AM.Core5/Source/Collections/Trees/ItemTree/EntityTree/MutableEntityTree/OrderComparers/NodeOrderItemComparer.cs
@@ -1,20 +1,58 @@
-﻿using System.Collections.Generic;
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
-namespace TreeCollections
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable NonReadonlyMemberInGetHashCode
+// ReSharper disable UnusedMember.Global
+
+/* NodeOrderItemComparer.cs --
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System.Collections.Generic;
+
+#endregion
+
+namespace TreeCollections;
+
+internal class NodeOrderItemComparer<TNode, TItem>
+    : IComparer<TNode>
+    where TNode: ItemTreeNode<TNode, TItem>
 {
-    internal class NodeOrderItemComparer<TNode, TItem> : IComparer<TNode>
-        where TNode : ItemTreeNode<TNode, TItem>
+    #region Construction
+
+    public NodeOrderItemComparer
+        (
+            IComparer<TItem> itemComparer
+        )
     {
-        private readonly IComparer<TItem> _itemComparer;
-
-        public NodeOrderItemComparer (IComparer<TItem> itemComparer)
-        {
-            _itemComparer = itemComparer;
-        }
-
-        public int Compare (TNode? x, TNode? y)
-        {
-            return _itemComparer.Compare (x!.Item, y!.Item);
-        }
+        _itemComparer = itemComparer;
     }
+
+    #endregion
+
+    #region Private members
+
+    private readonly IComparer<TItem> _itemComparer;
+
+    #endregion
+
+    #region IComparer members
+
+    /// <inheritdoc cref="IComparer{T}.Compare"/>
+    public int Compare
+        (
+            TNode? x,
+            TNode? y
+        )
+    {
+        return _itemComparer.Compare (x!.Item, y!.Item);
+    }
+
+    #endregion
 }
