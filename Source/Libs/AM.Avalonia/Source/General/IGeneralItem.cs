1d5ee67cf2cdb213872b2af368403e55b8bd1393
Alexey Mironov <amironov73@gmail.com>
2022-12-21 07:16:40 +0800

    Add IGeneralXXX interfaces

---
+++ Source/Libs/AM.Avalonia/Source/General/IGeneralItem.cs
@@ -0,0 +1,50 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable CommentTypo
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedType.Global
+
+/* IGeneralItem.cs -- интерфейс элемента пользовательского интерфейса
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using CommunityToolkit.Mvvm.Input;
+
+#endregion
+
+#nullable enable
+
+namespace AM.Avalonia.General;
+
+/// <summary>
+/// Некий элемент пользовательского интерфейса,
+/// например, кнопка или элемент меню.
+/// </summary>
+public interface IGeneralItem
+{
+    /// <summary>
+    /// Уникальный идентификатор (в пределах контейнера).
+    /// </summary>
+    string Id { get; }
+
+    /// <summary>
+    /// Текст на кнопке или элементе меню.
+    /// </summary>
+    string? Caption { get; }
+
+    /// <summary>
+    /// Разрешено в настоящий момент?
+    /// </summary>
+    bool Enabled { get; }
+
+    /// <summary>
+    /// Основное действие.
+    /// </summary>
+    IRelayCommand? Command { get; }
+}
---
+++ Source/Libs/AM.Avalonia/Source/General/IGeneralItemList.cs
@@ -0,0 +1,29 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable CommentTypo
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedType.Global
+
+/* IGeneralItemList.cs -- список элементов пользовательского интерфейса
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#nullable enable
+
+using System.Collections.Generic;
+
+namespace AM.Avalonia.General;
+
+/// <summary>
+/// Список элементов пользовательского интерфейса.
+/// Например, дочерние элементы некого родительского элемента.
+/// </summary>
+public interface IGeneralItemList
+    : IList<IGeneralItem>
+{
+    // пустое тело интерфейса
+}
