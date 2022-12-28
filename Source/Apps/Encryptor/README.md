bc1d7bd74f5a1c9ff50ec6e574a1cde5dbeebcdb
Alexey Mironov <amironov73@gmail.com>
2022-12-28 09:47:02 +0800

    Add Encryptor project

---
+++ Source/Apps/Encryptor/README.md
@@ -0,0 +1,24 @@
+﻿### Encryptor
+
+Простейший шифровальщик для паролей и прочего
+
+Командная строка:
+
+```
+Encrypt <text> <passwprd>
+```
+
+Здесь:
+
+* `text` - строка, подлежащая шифрованию.
+* `password` - произвольный пароль.
+
+Результат выводится в стандартный выходной поток.
+
+Пример:
+
+```
+$ Encryptor "host=1.2.3.4;user=librarian;password=secret;" "irbis"
+
+4kRO2z2kgSqSjnaMql9cBdSSdVOq52M1UWQoJG+MQFdGhbsLqT969SoFe+lTAdmt
+```
---
+++ Source/Apps/Encryptor/Source/Program.cs
@@ -0,0 +1,29 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable LocalizableElement
+
+/* Program.cs -- simple text encryptor
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using AM.Security;
+
+#endregion
+
+#nullable enable
+
+if (args.Length != 2)
+{
+    Console.WriteLine ("USAGE: Encryptor <text> <password>");
+    return 0;
+}
+
+var secretText = args[0];
+var password = args[1];
+var encryptedText = SecurityUtility.Encrypt (secretText, password);
+Console.WriteLine (encryptedText);
+
+return 0;
