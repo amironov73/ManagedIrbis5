83dd7cc7f3f53da57e87df26928e9978c2434cc6
Alexey Mironov <amironov73@gmail.com>
2023-01-18 07:32:45 +0800

    Working on Kotik

---
+++ Source/Libs/AM.Kotik/Source/Types/BarsikList.cs
@@ -0,0 +1,41 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+
+/* BarsikList.cs -- список объектов для Барсика
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System.Collections.Generic;
+using System.IO;
+
+#endregion
+
+#nullable enable
+
+namespace AM.Kotik;
+
+/// <summary>
+/// Список объектов для Барсика
+/// </summary>
+public sealed class BarsikList
+    : List<dynamic?>
+{
+    #region Object members
+
+    /// <inheritdoc cref="object.ToString"/>
+    public override string ToString()
+    {
+        var output = new StringWriter();
+        KotikUtility.PrintSequence (output, this);
+
+        return output.ToString();
+    }
+
+    #endregion
+}
---
+++ Source/Libs/AM.Kotik/Source/ValueWrapper.cs
@@ -0,0 +1,84 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+
+/* ValueWrapper.cs -- обертка над захваченной скриптом переменной
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
+
+#endregion
+
+#nullable enable
+
+namespace AM.Kotik;
+
+/// <summary>
+/// Обертка над захваченной скриптом переменной.
+/// </summary>
+public sealed class ValueWrapper<T>
+    : IValueWrapper
+{
+    #region Delegates
+
+    /// <summary>
+    /// Получение значения переменной.
+    /// </summary>
+    public Func<T> GetValue { get; }
+
+    /// <summary>
+    /// Установка значения переменной.
+    /// </summary>
+    public Action<T>? SetValue { get; }
+
+    #endregion
+
+    #region Construction
+
+    /// <summary>
+    /// Конструктор.
+    /// </summary>
+    public ValueWrapper
+        (
+            Func<T> getValue,
+            Action<T>? setValue = null
+        )
+    {
+        Sure.NotNull (getValue);
+
+        GetValue = getValue;
+        SetValue = setValue;
+    }
+
+    #endregion
+
+    #region IValueWrapper members
+
+    /// <inheritdoc cref="IValueWrapper.GetValue"/>
+    object? IValueWrapper.GetValue()
+    {
+        return GetValue.Invoke();
+    }
+
+    /// <inheritdoc cref="IValueWrapper.SetValue"/>
+    void IValueWrapper.SetValue
+        (
+            object? value
+        )
+    {
+        if (SetValue is null)
+        {
+            throw new InvalidOperationException ("Can't set value");
+        }
+
+        SetValue.Invoke ((T) value!);
+    }
+
+    #endregion
+}
