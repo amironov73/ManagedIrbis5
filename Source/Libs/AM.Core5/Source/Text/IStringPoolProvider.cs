37f68ab8852eb03685b67b74fc0f0d989a86a7fb
Alexey Mironov <amironov73@gmail.com>
2023-02-14 09:33:11 +0800

    Add IStringPoolProvider interface with default implementation

---
+++ Source/Libs/AM.Core5/Source/Text/IStringPoolProvider.cs
@@ -0,0 +1,42 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+
+/* IStringPoolProvider.cs -- провайдер пула строк
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using CommunityToolkit.HighPerformance.Buffers;
+
+#endregion
+
+#nullable enable
+
+namespace AM.Text;
+
+/// <summary>
+/// Провайдер пула строк <see cref="StringPool"/>.
+/// </summary>
+public interface IStringPoolProvider
+{
+    /// <summary>
+    /// Получение пула строк <see cref="StringPool"/>.
+    /// </summary>
+    StringPool GetStringPool();
+
+    /// <summary>
+    /// Возврат пула строк провайдеру. Предполагается, что строки,
+    /// полученные от пула, больше не нужны.
+    /// Провайдер может сделать с возвращенным пулом всё, что угодно,
+    /// в том числе ничего.
+    /// </summary>
+    void ReturnStringPool
+        (
+            StringPool stringPool
+        );
+}
---
+++ Source/Libs/AM.Core5/Source/Text/SharedStringPoolProvider.cs
@@ -0,0 +1,46 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+
+/* SharedStringPoolProvider.cs -- провайдер общего пула строк
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using CommunityToolkit.HighPerformance.Buffers;
+
+using JetBrains.Annotations;
+
+#endregion
+
+#nullable enable
+
+namespace AM.Text;
+
+/// <summary>
+/// Провайдер общего пула строк <see cref="StringPool"/>.
+/// </summary>
+[PublicAPI]
+public sealed class SharedStringPoolProvider
+    : IStringPoolProvider
+{
+    #region IStringPoolProvider members
+
+    /// <inheritdoc cref="IStringPoolProvider.GetStringPool"/>
+    public StringPool GetStringPool() => StringPool.Shared;
+
+    /// <inheritdoc cref="IStringPoolProvider.ReturnStringPool"/>
+    public void ReturnStringPool
+        (
+            StringPool stringPool
+        )
+    {
+        stringPool.NotUsed();
+    }
+
+    #endregion
+}
