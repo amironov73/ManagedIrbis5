4171b95ba328866a58029200af49e3a05db793e3
Alexey Mironov <amironov73@gmail.com>
2023-02-20 07:43:19 +0800

    Code cleanup

--- Source/Libs/AM.Windows.Forms5/Source/EventedTextBox.cs
+++ Source/Libs/AM.Windows.Forms5/Source/EventedTextBox.cs
@@ -4,9 +4,8 @@
 // ReSharper disable CheckNamespace
 // ReSharper disable CommentTypo
 // ReSharper disable IdentifierTypo
-// ReSharper disable UnusedMember.Global
 
-/* EventedTextBox.cs --
+/* EventedTextBox.cs -- дополнительные события для TextBox
  * Ars Magna project, http://arsmagna.ru
  */
 
@@ -19,161 +18,161 @@ using System.Windows.Forms;
 
 #nullable enable
 
-namespace AM.Windows.Forms
+namespace AM.Windows.Forms;
+
+/// <summary>
+/// <see cref="TextBox"/> с дополнительными событиями.
+/// </summary>
+[System.ComponentModel.DesignerCategory ("Code")]
+public sealed class EventedTextBox
+    : TextBox
 {
+    #region Events
+
     /// <summary>
-    ///
+    /// Отложенная версия события <see cref="Control.TextChanged"/>.
     /// </summary>
-    public sealed class EventedTextBox
-        : TextBox
+    public event EventHandler? DelayedTextChanged;
+
+    /// <summary>
+    /// Событие, возникающее при нажатии на клавишу <c>Enter</c>.
+    /// </summary>
+    public event EventHandler? EnterPressed;
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Величина задержки для <see cref="DelayedTextChanged"/>,
+    /// миллисекунды.
+    /// </summary>
+    public int Delay
     {
-        #region Events
-
-        /// <summary>
-        /// Delayed version of TextChanged event.
-        /// </summary>
-        public event EventHandler? DelayedTextChanged;
-
-        /// <summary>
-        /// Fired when Enter key pressed.
-        /// </summary>
-        public event EventHandler? EnterPressed;
-
-        #endregion
-
-        #region Properties
-
-        /// <summary>
-        /// Delay, milliseconds.
-        /// </summary>
-        public int Delay
+        get => _delay;
+        set
         {
-            get => _delay;
-            set
+            if (value < 1)
             {
-                if (value < 1)
-                {
-                    throw new ArgumentOutOfRangeException
-                        (
-                            nameof(value)
-                        );
-                }
-
-                if (_timer is not null)
-                {
-                    _timer.Enabled = false;
-                }
-                _delay = value;
-                _count = 0;
+                throw new ArgumentOutOfRangeException
+                    (
+                        nameof (value)
+                    );
             }
-        }
 
-        #endregion
-
-        #region Construction
-
-        /// <summary>
-        /// Constructor.
-        /// </summary>
-        public EventedTextBox()
-        {
-            _delay = 750;
-            _timer = new Timer
+            if (_timer is not null)
             {
-                Interval = 50
-            };
-            _timer.Tick += _timer_Tick;
+                _timer.Enabled = false;
+            }
+
+            _delay = value;
+            _count = 0;
         }
+    }
 
-        #endregion
+    #endregion
 
-        #region Private members
+    #region Construction
 
-        private int _delay;
+    /// <summary>
+    /// Конструктор по умолчанию.
+    /// </summary>
+    public EventedTextBox()
+    {
+        _delay = 750;
+        _timer = new Timer { Interval = 50 };
+        _timer.Tick += _timer_Tick;
+    }
 
-        private int _count;
+    #endregion
 
-        private Timer? _timer;
+    #region Private members
 
-        /// <inheritdoc />
-        protected override void Dispose
+    private int _delay;
+    private int _count;
+    private Timer? _timer;
+
+    /// <inheritdoc cref="TextBox.Dispose(bool)" />
+    protected override void Dispose
         (
             bool disposing
         )
-        {
-            base.Dispose(disposing);
+    {
+        base.Dispose (disposing);
 
-            if (!ReferenceEquals(_timer, null))
-            {
-                _timer.Dispose();
-                _timer = null;
-            }
+        if (disposing)
+        {
+            _timer?.Dispose();
         }
 
-        /// <inheritdoc />
-        protected override void OnKeyDown
-            (
-                KeyEventArgs e
-            )
-        {
-            base.OnKeyDown(e);
-
-            if (e.KeyData == Keys.Enter)
-            {
-                if (_timer is not null)
-                {
-                    _timer.Enabled = false;
-                }
-                _count = 0;
-
-                EnterPressed?.Invoke
-                    (
-                        this,
-                        EventArgs.Empty
-                    );
-            }
-        }
-
-        /// <inheritdoc />
-        protected override void OnTextChanged
-            (
-                EventArgs e
-            )
-        {
-            _count = 0;
-            if (_timer is not null)
-            {
-                _timer.Enabled = true;
-            }
-        }
-
-        private void _timer_Tick
-            (
-                object? sender,
-                EventArgs e
-            )
-        {
-            if (_timer is not null)
-            {
-                _count += _timer.Interval;
-            }
-
-            if (_count >= _delay)
-            {
-                if (_timer is not null)
-                {
-                    _timer.Enabled = false;
-                }
-                _count = 0;
-
-                DelayedTextChanged?.Invoke
-                    (
-                        this,
-                        EventArgs.Empty
-                    );
-            }
-        }
-
-        #endregion
+        _timer = null;
     }
+
+    /// <inheritdoc />
+    protected override void OnKeyDown
+        (
+            KeyEventArgs eventArgs
+        )
+    {
+        base.OnKeyDown (eventArgs);
+
+        if (eventArgs.KeyData == Keys.Enter)
+        {
+            if (_timer is not null)
+            {
+                _timer.Enabled = false;
+            }
+
+            _count = 0;
+
+            EnterPressed?.Invoke
+                (
+                    this,
+                    EventArgs.Empty
+                );
+        }
+    }
+
+    /// <inheritdoc cref="TextBoxBase.OnTextChanged" />
+    protected override void OnTextChanged
+        (
+            EventArgs eventArgs
+        )
+    {
+        _count = 0;
+        if (_timer is not null)
+        {
+            _timer.Enabled = true;
+        }
+    }
+
+    private void _timer_Tick
+        (
+            object? sender,
+            EventArgs eventArgs
+        )
+    {
+        if (_timer is not null)
+        {
+            _count += _timer.Interval;
+        }
+
+        if (_count >= _delay)
+        {
+            if (_timer is not null)
+            {
+                _timer.Enabled = false;
+            }
+
+            _count = 0;
+
+            DelayedTextChanged?.Invoke
+                (
+                    this,
+                    EventArgs.Empty
+                );
+        }
+    }
+
+    #endregion
 }
