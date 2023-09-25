adce995c7192a1347d7439b9d8b8924f774a77c5
Alexey Mironov <amironov73@gmail.com>
2023-09-25 15:36:41 +0800

    Working on EML

---
+++ Source/Libs/AM.Lexey/Source/Eml/Ast/ControlNode.cs
@@ -0,0 +1,30 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+
+/* ControlNode.cs -- узел, описывающий контрол
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using AM.Lexey.Ast;
+
+using JetBrains.Annotations;
+
+#endregion
+
+namespace AM.Lexey.Eml.Ast;
+
+/// <summary>
+/// Узел, описывающий контрол.
+/// </summary>
+[PublicAPI]
+public class ControlNode
+    : AstNode
+{
+    // пока пустое тело класса
+}
---
+++ Source/Libs/AM.Lexey/Source/Eml/Ast/PropertyNode.cs
@@ -0,0 +1,30 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+
+/* PropertyNode.cs -- узел, описывающий свойство
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using AM.Lexey.Ast;
+
+using JetBrains.Annotations;
+
+#endregion
+
+namespace AM.Lexey.Eml.Ast;
+
+/// <summary>
+/// Узел, описывающий свойство.
+/// </summary>
+[PublicAPI]
+public class PropertyNode
+    : AstNode
+{
+    // пока пустое тело класса
+}
