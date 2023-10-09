a1be7cad3064a6c2747a90bfbef68760f10f10fb
Alexey Mironov <amironov73@gmail.com>
2023-10-09 08:13:30 +0800

    Enhance EnglishLoremIpsum class

--- Source/Libs/AM.AOT5/Source/Lorem/EnglishLoremIpsum.cs
+++ Source/Libs/AM.AOT5/Source/Lorem/EnglishLoremIpsum.cs
@@ -16,12 +16,9 @@
 #region Using directives
 
 using System.Collections.Generic;
-using System.Text;
 using System.Text.RegularExpressions;
 
 #endregion
-
-#nullable enable
 
 namespace AM.Text.Lorem;
 
@@ -35,7 +32,7 @@ public sealed class EnglishLoremIpsum
 
     private const string Original = "Brock would have never dared to do it on his own he thought to himself. That is why Kenneth and he had become such good friends. Kenneth forced Brock out of his comfort zone and made him try new things he'd never imagine doing otherwise. Up to this point, this had been a good thing. It had expanded Brock's experiences and given him a new appreciation for life. Now that both of them were in the back of a police car, all Brock could think was that he would have never dared do it except for the influence of Kenneth.";
 
-    private readonly List<string> _arrOriginal = new ();
+    private readonly List<string> _originalWords = new ();
 
     /// <summary>
     /// Конструктор.
@@ -45,24 +42,39 @@ public sealed class EnglishLoremIpsum
         var matches = _wordSplitter.Matches (Original);
         foreach (var match in matches)
         {
-            _arrOriginal.Add (match.ToString()!);
+            _originalWords.Add (match.ToString()!);
         }
     }
 
     /// <inheritdoc cref="ILoremIpsum.GetLetters"/>
-    public string GetLetters (int length)
+    public string GetLetters
+        (
+            int length
+        )
     {
-        var output = new StringBuilder();
+        Sure.NonNegative (length);
 
         if (length == 0)
         {
             length = Original.Length;
         }
 
-        for (var i = 0; i < length; i++)
-            output.Append (Original[i]);
+        var result = StringBuilderPool.Shared.Get();
+        while (result.Length < length)
+        {
+            var remaining = (length - result.Length) % Original.Length;
+            for (var i = 0; i < remaining; i++)
+            {
+                result.Append (Original[i]);
+            }
 
-        return output.ToString();
+            if (result.Length < length)
+            {
+                result.Append (' ');
+            }
+        }
+
+        return result.ReturnShared();
     }
 
     /// <inheritdoc cref="ILoremIpsum.GetWords"/>
@@ -71,21 +83,25 @@ public sealed class EnglishLoremIpsum
             int length
         )
     {
-        var output = new StringBuilder();
+        Sure.NonNegative (length);
 
-        //var pattern = @"[\w\!\.\?\;\,\:]+/g";
-
-        var arrCounter = 0;
+        var result = StringBuilderPool.Shared.Get();
+        var counter = 0;
         for (var i = 0; i <= length; i++)
         {
-            output.Append (_arrOriginal[arrCounter]);
-            if (++arrCounter > _arrOriginal.Count)
+            if (result.Length != 0)
             {
-                arrCounter = 0;
+                result.Append (' ');
+            }
+
+            result.Append (_originalWords[counter]);
+            if (++counter > _originalWords.Count)
+            {
+                counter = 0;
             }
         }
 
-        return output.ToString();
+        return result.ReturnShared();
     }
 
     /// <inheritdoc cref="ILoremIpsum.GetParagraphs"/>
@@ -94,19 +110,21 @@ public sealed class EnglishLoremIpsum
             int count
         )
     {
-        var output = new StringBuilder();
+        Sure.NonNegative (count);
+
+        var result = StringBuilderPool.Shared.Get();
         for (var i = 0; i <= count; i++)
         {
             if (i == count)
             {
-                output.Append (Original);
+                result.Append (Original);
             }
             else
             {
-                output.Append (Original + "\n\n");
+                result.Append (Original + "\n\n");
             }
         }
 
-        return output.ToString();
+        return result.ReturnShared();
     }
 }
