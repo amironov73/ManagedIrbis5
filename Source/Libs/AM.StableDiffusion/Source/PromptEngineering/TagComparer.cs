4b1a6f12ee703349e33c8ce2410de90738d66f5a
Alexey Mironov <amironov73@gmail.com>
2024-01-16 13:50:29 +0800

    Working on TagList

---
+++ Source/Libs/AM.StableDiffusion/Source/PromptEngineering/TagComparer.cs
@@ -0,0 +1,68 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+// ReSharper disable CheckNamespace
+// ReSharper disable CommentTypo
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable MemberCanBeProtected.Global
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable UnusedMember.Global
+
+/* TagComparer.cs -- умеет сравнивать теги
+ * Ars Magna project, http://arsmagna.ru
+ */
+
+#region Using directives
+
+using System;
+
+using JetBrains.Annotations;
+
+#endregion
+
+namespace AM.StableDiffusion.PromptEngineering;
+
+/// <summary>
+/// Умеет сравнивать теги.
+/// </summary>
+[PublicAPI]
+public static class TagComparer
+{
+    #region Properties
+
+    /// <summary>
+    /// Общий экземпляр (с возможностью замены).
+    /// </summary>
+    public static StringComparer Instance { get; set; }
+        = StringComparer.InvariantCultureIgnoreCase;
+
+    #endregion
+
+    #region Public methods
+
+    /// <summary>
+    /// Сравнение двух строк.
+    /// </summary>
+    public static int Compare (string? left, string? right)
+        => Instance.Compare (left, right);
+
+    /// <summary>
+    /// Сравнение двух тегов.
+    /// </summary>
+    public static int Compare (TagInfo left, TagInfo right)
+        => Instance.Compare (left.Title, right.Title);
+
+    /// <summary>
+    /// Две строки равны?
+    /// </summary>
+    public static bool IsSame (string? left, string? right)
+        => Compare (left, right) == 0;
+
+    /// <summary>
+    /// Два тега равны?
+    /// </summary>
+    public static bool IsSame (TagInfo left, TagInfo right)
+        => Compare (left, right) == 0;
+
+    #endregion
+}
--- Source/Libs/AM.StableDiffusion/Source/PromptEngineering/TagList.cs
+++ Source/Libs/AM.StableDiffusion/Source/PromptEngineering/TagList.cs
@@ -15,6 +15,9 @@
 #region Using directives
 
 using System.Collections.Generic;
+using System.IO;
+
+using AM.Text;
 
 using JetBrains.Annotations;
 
@@ -29,5 +32,194 @@ namespace AM.StableDiffusion.PromptEngineering;
 public sealed class TagList
     : List<TagInfo>
 {
-    // TODO наполнить
+    #region Public methods
+
+    /// <summary>
+    /// Добавление (клона) тега в список.
+    /// </summary>
+    public void AppendTag
+        (
+            TagInfo tag,
+            bool toHead = false
+        )
+    {
+        Sure.NotNull (tag);
+
+        if (!string.IsNullOrWhiteSpace (tag.Title)
+            && !ContainsTag (tag))
+        {
+            if (toHead)
+            {
+                Insert (0, tag.IncompleteClone());
+            }
+            else
+            {
+                Add (tag.IncompleteClone());
+            }
+        }
+    }
+
+    /// <summary>
+    /// Построение подсказки из указанных тегов.
+    /// </summary>
+    public string BuildPrompt()
+    {
+        var first = true;
+        var builder = StringBuilderPool.Shared.Get();
+        foreach (var tag in this)
+        {
+            var title = tag.Title;
+            if (!string.IsNullOrWhiteSpace (title))
+            {
+                if (!first)
+                {
+                    builder.Append (", ");
+                }
+
+                builder.Append (title);
+                first = false;
+            }
+        }
+
+        return builder.ReturnShared();
+    }
+
+    /// <summary>
+    /// Проверка, содержится ли тег в списке.
+    /// </summary>
+    public bool ContainsTag
+        (
+            TagInfo tag
+        )
+    {
+        Sure.NotNull (tag);
+
+        foreach (var one in this)
+        {
+            if (TagComparer.IsSame (tag, one))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Проверка, содержится ли тег в списке.
+    /// </summary>
+    public bool ContainsTag
+        (
+            string? tag
+        )
+    {
+        foreach (var one in this)
+        {
+            if (TagComparer.IsSame (tag, one.Title))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Фильтрация списка "на месте".
+    /// </summary>
+    public void FilterInPlace
+        (
+            TagList unwantedTags
+        )
+    {
+        Sure.NotNull (unwantedTags);
+
+        for (var i = 0; i < Count;)
+        {
+            if (unwantedTags.ContainsTag (this[i]))
+            {
+                RemoveAt (i);
+            }
+            else
+            {
+                i++;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Фильтрация списка с созданием нового, отфильтрованного.
+    /// </summary>
+    public TagList FilterOut
+        (
+            TagList unwantedTags
+        )
+    {
+        Sure.NotNull (unwantedTags);
+
+        var result = new TagList();
+        foreach (var one in this)
+        {
+            if (!unwantedTags.ContainsTag (one))
+            {
+                result.Add (one);
+            }
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// Загрузка тегов из файла.
+    /// </summary>
+    public void LoadTags
+        (
+            string fileName
+        )
+    {
+        Sure.FileExists (fileName);
+
+        foreach (var line in File.ReadLines (fileName))
+        {
+            if (string.IsNullOrWhiteSpace (line))
+            {
+                continue;
+            }
+
+            var trimmed = line.Trim();
+            if (trimmed[0] == '#')
+            {
+                continue;
+            }
+
+            var tag = new TagInfo { Title = trimmed };
+            if (!ContainsTag (tag))
+            {
+                Add (tag);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Сохранение тегов в файле.
+    /// </summary>
+    public void SaveTags
+        (
+            string fileName
+        )
+    {
+        Sure.NotNullNorEmpty (fileName);
+
+        using var stream = File.CreateText (fileName);
+        foreach (var tag in this)
+        {
+            var title = tag.Title;
+            if (!string.IsNullOrWhiteSpace (title))
+            {
+                stream.WriteLine (title);
+            }
+        }
+    }
+
+    #endregion
 }
