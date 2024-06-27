eeaafc86ca651bcf40b7f2d6bec9d4a0c0aa2b9b
Alexey Mironov <amironov73@gmail.com>
2024-06-27 15:46:48 +0800

    Update Eremex NuGet packages

--- Source/Libs/AM.Avalonia.Eremex/Source/EremexUtility.cs
+++ Source/Libs/AM.Avalonia.Eremex/Source/EremexUtility.cs
@@ -10,11 +10,6 @@
 
 #region Using directives
 
-using System;
-
-using Avalonia;
-using Avalonia.Markup.Xaml.Styling;
-
 using JetBrains.Annotations;
 
 #endregion
@@ -27,87 +22,5 @@ namespace AM.Avalonia.Eremex;
 [PublicAPI]
 public static class EremexUtility
 {
-    #region Private members
-
-    private static void AddEremexTheme
-        (
-            this Application application,
-            string themeName
-        )
-    {
-        var uri = new Uri ($"avares://Eremex.Avalonia.Controls/Themes/{themeName}.axaml");
-        var eremex = new StyleInclude (uri)
-        {
-            Source = uri
-        };
-        application.Styles.Add (eremex);
-    }
-
-    #endregion
-
-    #region Public methods
-
-    /// <summary>
-    /// Добавление тем для контролов Eremex.
-    /// </summary>
-    public static void AddEremexTheme
-        (
-            this Application application,
-            params string[] themes
-        )
-    {
-        Sure.NotNull (application);
-
-        foreach (var theme in themes)
-        {
-            AddEremexTheme (application,theme);
-        }
-    }
-
-    /// <summary>
-    /// Добавление тема темы для контролов Eremex.
-    /// </summary>
-    public static void AddDarkEremexTheme
-        (
-            this Application application
-        )
-    {
-        Sure.NotNull (application);
-
-        AddEremexTheme (application,"Generic");
-        AddEremexTheme (application,"Dark/Theme");
-    }
-
-    /// <summary>
-    /// Добавление светлой темы для контролов Eremex.
-    /// </summary>
-    public static void AddLightEremexTheme
-        (
-            this Application application
-        )
-    {
-        Sure.NotNull (application);
-
-        AddEremexTheme (application,"Generic");
-        AddEremexTheme (application,"Light/Theme");
-    }
-
-    /// <summary>
-    /// Исправление ошибки с показом лишнего адорнера.
-    /// </summary>
-    public static TVisual FixEremexAdorner<TVisual>
-        (
-            this TVisual visual
-        )
-        where TVisual: Visual
-    {
-        Sure.NotNull (visual);
-
-        AM.EventUtility.UnsubscribeAll<TVisual> (visual, nameof (visual.AttachedToVisualTree));
-        AM.EventUtility.UnsubscribeAll<TVisual> (visual, nameof (visual.DetachedFromVisualTree));
-
-        return visual;
-    }
-
-    #endregion
+    // Пустое тело класса.
 }
